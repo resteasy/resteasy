5010db0e6a0c999bff79947b5fcec4528cb62676
patriot1burke <patriot1burke@sourceforge.net>
2008-02-22 21:22:17 +0000

    ejb testing
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@94 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ jboss-integration-testing/ejb-test/ejb/src/main/java/org/resteasy/test/ejb/SimpleResourceBean.java
@@ -0,0 +1,51 @@
+package org.resteasy.test.ejb;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.ProduceMime;
+import javax.ws.rs.ConsumeMime;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.MatrixParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ejb.Stateless;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+@Stateless
+public class SimpleResourceBean implements SimpleResource
+{
+   public String getWild()
+   {
+      return "Wild";
+   }
+
+   public String getBasic()
+   {
+      System.out.println("getBasic()");
+      return "basic";
+   }
+
+   public void putBasic(String body)
+   {
+      System.out.println(body);
+   }
+
+   public String getQueryParam(@QueryParam("param")String param)
+   {
+      return param;
+   }
+
+   public String getMatrixParam(@MatrixParam("param")String param)
+   {
+      return param;
+   }
+
+   public int getUriParam(@PathParam("param")int param)
+   {
+      return param;
+   }
+
+}
