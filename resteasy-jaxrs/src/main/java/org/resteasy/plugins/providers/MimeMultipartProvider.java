f51d8d3b7c9b04422f30fa92916a95b3334f1d7b
patriot1burke <patriot1burke@sourceforge.net>
2008-03-22 01:38:55 +0000

    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@120 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- resteasy-jaxrs/src/main/java/org/resteasy/plugins/providers/MimeMultipartProvider.java
+++ resteasy-jaxrs/src/main/java/org/resteasy/plugins/providers/MimeMultipartProvider.java
@@ -1,21 +1,21 @@
 package org.resteasy.plugins.providers;
 
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-import javax.mail.MessagingException;
-import javax.ws.rs.ConsumeMime;
-import javax.ws.rs.ProduceMime;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.ext.MessageBodyReader;
-import javax.ws.rs.ext.MessageBodyWriter;
-import javax.ws.rs.ext.Provider;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.util.ByteArrayDataSource;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.OutputStream;
+//import java.lang.annotation.Annotation;
+//import java.lang.reflect.Type;
+//import javax.mail.MessagingException;
+//import javax.ws.rs.ConsumeMime;
+//import javax.ws.rs.ProduceMime;
+//import javax.ws.rs.core.MediaType;
+//import javax.ws.rs.core.MultivaluedMap;
+//import javax.ws.rs.ext.MessageBodyReader;
+//import javax.ws.rs.ext.MessageBodyWriter;
+//import javax.ws.rs.ext.Provider;
+//import javax.mail.internet.MimeMultipart;
+//import javax.mail.util.ByteArrayDataSource;
 
 /**
  * A provider to handle multipart representations. This implementation will be 
@@ -35,98 +35,101 @@ import javax.mail.util.ByteArrayDataSource;
  *  
  * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a>
  */
-@Provider
-@ProduceMime("multipart/mixed")
-@ConsumeMime({"multipart/mixed", "multipart/form-data"})
-public class MimeMultipartProvider
-        implements MessageBodyReader<MimeMultipart>,
-        MessageBodyWriter<MimeMultipart> {
+public class MimeMultipartProvider {}
 
-    /**
-     * 
-     * @param type
-     * @param genericType
-     * @param annotations
-     * @return
-     */
-    public boolean isReadable(Class<?> type,
-                              Type genericType,
-                              Annotation[] annotations) {
-        return MimeMultipart.class.equals(type);
-    }
 
-    /**
-     * 
-     * @param type
-     * @param genericType
-     * @param annotations
-     * @return
-     */
-    public boolean isWriteable(Class<?> type,
-                               Type genericType,
-                               Annotation[] annotations) {
-        return MimeMultipart.class.equals(type);
-    }
-
-    /**
-     * 
-     * @param type
-     * @param genericType
-     * @param mediaType
-     * @param annotations
-     * @param httpHeaders
-     * @param entityStream
-     * @return
-     * @throws java.io.IOException
-     */
-    public MimeMultipart readFrom(Class<MimeMultipart> type, 
-                                  Type genericType,
-                                  MediaType mediaType, 
-                                  Annotation[] annotations,
-                                  MultivaluedMap<String, String> httpHeaders,
-                                  InputStream entityStream) throws IOException {
-        try {
-            if (mediaType != null) {
-                mediaType = new MediaType("multipart", "mixed");
-            }
-            ByteArrayDataSource ds =
-                    new ByteArrayDataSource(entityStream, mediaType.toString());
-            return new MimeMultipart(ds);
-        } catch (MessagingException ex) {
-            throw new IOException(""+ex.getMessage());
-        }
-    }
-
-    /**
-     * 
-     * @param t
-     * @return
-     */
-    public long getSize(MimeMultipart t) {
-        return -1;
-    }
-
-    /**
-     * 
-     * @param t
-     * @param genericType
-     * @param annotations
-     * @param mediaType
-     * @param httpHeaders
-     * @param entityStream
-     * @throws java.io.IOException
-     */
-    public void writeTo(MimeMultipart t, 
-                        Type genericType,
-                        Annotation[] annotations, 
-                        MediaType mediaType,
-                        MultivaluedMap<String, Object> httpHeaders,
-                        OutputStream entityStream) 
-                        throws IOException {
-        try {
-            t.writeTo(entityStream);
-        } catch (MessagingException ex) {
-            throw new IOException(""+ex.getMessage());
-        }
-    }
-}
+//@Provider
+//@ProduceMime("multipart/mixed")
+//@ConsumeMime({"multipart/mixed", "multipart/form-data"})
+//public class MimeMultipartProvider
+//        implements MessageBodyReader<MimeMultipart>,
+//        MessageBodyWriter<MimeMultipart> {
+//
+//    /**
+//     *
+//     * @param type
+//     * @param genericType
+//     * @param annotations
+//     * @return
+//     */
+//    public boolean isReadable(Class<?> type,
+//                              Type genericType,
+//                              Annotation[] annotations) {
+//        return MimeMultipart.class.equals(type);
+//    }
+//
+//    /**
+//     *
+//     * @param type
+//     * @param genericType
+//     * @param annotations
+//     * @return
+//     */
+//    public boolean isWriteable(Class<?> type,
+//                               Type genericType,
+//                               Annotation[] annotations) {
+//        return MimeMultipart.class.equals(type);
+//    }
+//
+//    /**
+//     *
+//     * @param type
+//     * @param genericType
+//     * @param mediaType
+//     * @param annotations
+//     * @param httpHeaders
+//     * @param entityStream
+//     * @return
+//     * @throws java.io.IOException
+//     */
+//    public MimeMultipart readFrom(Class<MimeMultipart> type,
+//                                  Type genericType,
+//                                  MediaType mediaType,
+//                                  Annotation[] annotations,
+//                                  MultivaluedMap<String, String> httpHeaders,
+//                                  InputStream entityStream) throws IOException {
+//        try {
+//            if (mediaType != null) {
+//                mediaType = new MediaType("multipart", "mixed");
+//            }
+//            ByteArrayDataSource ds =
+//                    new ByteArrayDataSource(entityStream, mediaType.toString());
+//            return new MimeMultipart(ds);
+//        } catch (MessagingException ex) {
+//            throw new IOException(""+ex.getMessage());
+//        }
+//    }
+//
+//    /**
+//     *
+//     * @param t
+//     * @return
+//     */
+//    public long getSize(MimeMultipart t) {
+//        return -1;
+//    }
+//
+//    /**
+//     *
+//     * @param t
+//     * @param genericType
+//     * @param annotations
+//     * @param mediaType
+//     * @param httpHeaders
+//     * @param entityStream
+//     * @throws java.io.IOException
+//     */
+//    public void writeTo(MimeMultipart t,
+//                        Type genericType,
+//                        Annotation[] annotations,
+//                        MediaType mediaType,
+//                        MultivaluedMap<String, Object> httpHeaders,
+//                        OutputStream entityStream)
+//                        throws IOException {
+//        try {
+//            t.writeTo(entityStream);
+//        } catch (MessagingException ex) {
+//            throw new IOException(""+ex.getMessage());
+//        }
+//    }
+//}
