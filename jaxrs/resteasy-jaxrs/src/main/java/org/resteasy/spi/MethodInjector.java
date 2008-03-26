f05843c7ff9733f70757934f686bb70145962e43
patriot1burke <patriot1burke@sourceforge.net>
2008-03-26 04:04:05 +0000

    comments
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@125 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/main/java/org/resteasy/spi/MethodInjector.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/resteasy/spi/MethodInjector.java
@@ -5,12 +5,31 @@ import org.resteasy.Failure;
 import javax.ws.rs.core.Response;
 
 /**
+ * Will invoke a method in the context of an HTTP request.  Does all the parameter injection for you.
+ *
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
  */
 public interface MethodInjector
 {
+   /**
+    * Invoke on a method in the context of an HTTP request.  Does all JAX-RS parameter injection.
+    *
+    * @param request
+    * @param response
+    * @param target
+    * @return
+    * @throws Failure
+    */
    Response invoke(HttpRequest request, HttpResponse response, Object target) throws Failure;
 
+   /**
+    * Create the arguments that would be used to invoke the method in the context of an HTTP request.
+    *
+    * @param request
+    * @param response
+    * @return
+    * @throws Failure
+    */
    Object[] injectArguments(HttpRequest request, HttpResponse response) throws Failure;
 }
