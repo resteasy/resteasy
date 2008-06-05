907a0e6b0986cf3c829faad01e1c7d2d66c7e11c
patriot1burke <patriot1burke@sourceforge.net>
2008-06-05 20:38:21 +0000

    ClientResponse
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@170 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ jaxrs/resteasy-jaxrs/src/main/java/org/resteasy/spi/ClientResponse.java
@@ -0,0 +1,19 @@
+package org.resteasy.spi;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+/**
+ * Response interface for the RESTEasy client framework.  Use this in your client proxy interface method return type
+ * declarations if you want access to the response entity as well as status and header information.
+ *
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public interface ClientResponse<T>
+{
+   T getEntity();
+
+   MultivaluedMap<String, String> getHeaders();
+
+   int getStatus();
+}
