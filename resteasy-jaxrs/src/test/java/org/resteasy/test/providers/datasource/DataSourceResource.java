bfcc3a5f8f2f2b52607d4abd7136845c0872dc89
rmcdonough <rmcdonough@sourceforge.net>
2008-06-24 02:18:02 +0000

    new unit test classes to cover the entity providers
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@189 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ resteasy-jaxrs/src/test/java/org/resteasy/test/providers/datasource/DataSourceResource.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.resteasy.test.providers.datasource;
+
+import java.io.IOException;
+
+import javax.activation.DataSource;
+import javax.mail.util.ByteArrayDataSource;
+import javax.ws.rs.ConsumeMime;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.ProduceMime;
+
+/**
+ * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a>
+ * Jun 23, 2008
+ *
+ */
+@Path("/jaf")
+public class DataSourceResource {
+
+    @POST
+    @ConsumeMime("image/jpeg")
+    @ProduceMime("text/plain")
+    public String postDataSource(DataSource datasource) {
+	return datasource.getContentType();
+    }
+    
+    @GET
+    @Path("/{value}")
+    public DataSource getDataSource(@PathParam("value") String value) throws IOException {
+	DataSource ds = new ByteArrayDataSource(value,"text/plain");
+	return ds;
+    }
+}
