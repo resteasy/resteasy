3aa42bf2bfb5c0c731c8562de28abdf8cbede16b
rmcdonough <rmcdonough@sourceforge.net>
2008-06-25 01:58:42 +0000

    new IIOImageProvider classes
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@194 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ resteasy-jaxrs/src/test/java/org/resteasy/test/providers/iioimage/ImageResource.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.resteasy.test.providers.iioimage;
+
+import javax.imageio.IIOImage;
+import javax.ws.rs.ConsumeMime;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.ProduceMime;
+
+/**
+ * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a>
+ * Jun 23, 2008
+ *
+ */
+@Path("/image")
+public class ImageResource {
+
+    @POST
+    @ConsumeMime("image/*")
+    @ProduceMime("image/png")
+    public IIOImage transcodeImage(IIOImage image) {
+	return image;
+    }
+    
+    
+}
