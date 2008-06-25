db0abec7d30f3b46a7977e62d83f5bdc6ad46b33
rmcdonough <rmcdonough@sourceforge.net>
2008-06-25 22:03:44 +0000

    initial commit of example applictaions
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@200 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ examples/javaee-addressbook/ejb/src/main/java/com/damnhandy/addressbook/entity/Contact.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.damnhandy.addressbook.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a>
+ * Jun 18, 2008
+ *
+ */
+public class Contact implements Serializable {
+
+}
