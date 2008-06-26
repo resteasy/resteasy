201cfe51b0195d455e1c6cf9c95a8d7a31f2e376
patriot1burke <patriot1burke@sourceforge.net>
2008-06-26 19:12:53 +0000

    Reformat code
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@202 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/test/java/org/resteasy/test/providers/multipart/SimpleMimeMultipartResource.java
+++ jaxrs/resteasy-jaxrs/src/test/java/org/resteasy/test/providers/multipart/SimpleMimeMultipartResource.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.resteasy.test.providers.multipart;
 
@@ -16,37 +16,45 @@ import java.io.IOException;
 /**
  * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a> Jun 18,
  *         2008
- * 
  */
 @Path("/mime")
-public class SimpleMimeMultipartResource {
+public class SimpleMimeMultipartResource
+{
 
-    /**
-     * @param multipart
-     * @return
-     */
-    @PUT
-    @ConsumeMime("multipart/form-data")
-    @ProduceMime("text/plain")
-    public String putData(MimeMultipart multipart) {
-        StringBuilder b = new StringBuilder("Count: ");
-        try {
-            b.append(multipart.getCount());
-            for (int i = 0; i < multipart.getCount(); i++) {
-                try {
-                    System.out.println(multipart.getBodyPart(i).getContent().toString());
-                    System.out.println(multipart.getBodyPart(i).getInputStream().available());
-                } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
+   /**
+    * @param multipart
+    * @return
+    */
+   @PUT
+   @ConsumeMime("multipart/form-data")
+   @ProduceMime("text/plain")
+   public String putData(MimeMultipart multipart)
+   {
+      StringBuilder b = new StringBuilder("Count: ");
+      try
+      {
+         b.append(multipart.getCount());
+         for (int i = 0; i < multipart.getCount(); i++)
+         {
+            try
+            {
+               System.out.println(multipart.getBodyPart(i).getContent().toString());
+               System.out.println(multipart.getBodyPart(i).getInputStream().available());
             }
-        } catch (MessagingException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return b.toString();
-    }
+            catch (IOException e)
+            {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+            }
+         }
+      }
+      catch (MessagingException e)
+      {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      return b.toString();
+   }
 
 //    @POST
 //    @ConsumeMime("multipart/form-data")
@@ -58,26 +66,27 @@ public class SimpleMimeMultipartResource {
 //        return b.toString();
 //    }
 
-    /**
-     * 
-     * @return
-     */
-    @GET
-    @ProduceMime("multipart/mixed")
-    public MimeMultipart getMimeMultipart() throws MessagingException {
-        MimeMultipart multipart = new MimeMultipart("mixed");
-        multipart.addBodyPart(createPart("Body of part 1", "text/plain",
-                "This is a description"));
-        multipart.addBodyPart(createPart("Body of part 2", "text/plain",
-                "This is another description"));
-        return multipart;
-    }
+   /**
+    * @return
+    */
+   @GET
+   @ProduceMime("multipart/mixed")
+   public MimeMultipart getMimeMultipart() throws MessagingException
+   {
+      MimeMultipart multipart = new MimeMultipart("mixed");
+      multipart.addBodyPart(createPart("Body of part 1", "text/plain",
+              "This is a description"));
+      multipart.addBodyPart(createPart("Body of part 2", "text/plain",
+              "This is another description"));
+      return multipart;
+   }
 
-    private MimeBodyPart createPart(String value, String type,
-            String description) throws MessagingException {
-        MimeBodyPart part = new MimeBodyPart();
-        part.setDescription(description);
-        part.setContent(value, type);
-        return part;
-    }
+   private MimeBodyPart createPart(String value, String type,
+                                   String description) throws MessagingException
+   {
+      MimeBodyPart part = new MimeBodyPart();
+      part.setDescription(description);
+      part.setContent(value, type);
+      return part;
+   }
 }
