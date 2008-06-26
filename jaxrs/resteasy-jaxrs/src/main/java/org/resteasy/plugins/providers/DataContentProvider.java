89d025cff4b162b343da3db91f272f7fbf7a8944
patriot1burke <patriot1burke@sourceforge.net>
2008-06-26 21:04:45 +0000

    reformat code
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@203 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/main/java/org/resteasy/plugins/providers/DataContentProvider.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/resteasy/plugins/providers/DataContentProvider.java
@@ -1,15 +1,7 @@
 /**
- * 
+ *
  */
 package org.resteasy.plugins.providers;
-
-import java.awt.datatransfer.DataFlavor;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
 
 import javax.activation.DataHandler;
 import javax.mail.util.ByteArrayDataSource;
@@ -21,84 +13,96 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
+import java.awt.datatransfer.DataFlavor;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 
 /**
  * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a> Jun 20,
  *         2008
- * 
  */
 @Provider
 @ProduceMime("*/*")
 @ConsumeMime("*/*")
 public class DataContentProvider implements MessageBodyReader<Object>,
-        MessageBodyWriter<Object> {
+        MessageBodyWriter<Object>
+{
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyReader#isReadable(java.lang.Class,
-     *      java.lang.reflect.Type, java.lang.annotation.Annotation[])
-     */
-    public boolean isReadable(Class<?> type, Type genericType,
-            Annotation[] annotations) {
-        // TODO Auto-generated method stub
-        return true;
-    }
+   /*
+   * (non-Javadoc)
+   *
+   * @see javax.ws.rs.ext.MessageBodyReader#isReadable(java.lang.Class,
+   *      java.lang.reflect.Type, java.lang.annotation.Annotation[])
+   */
+   public boolean isReadable(Class<?> type, Type genericType,
+                             Annotation[] annotations)
+   {
+      // TODO Auto-generated method stub
+      return true;
+   }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyReader#readFrom(java.lang.Class,
-     *      java.lang.reflect.Type, java.lang.annotation.Annotation[],
-     *      javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap,
-     *      java.io.InputStream)
-     */
-    public Object readFrom(Class type, Type genericType,
-            Annotation[] annotations, MediaType mediaType,
-            MultivaluedMap httpHeaders, InputStream entityStream)
-            throws IOException, WebApplicationException {
-        ByteArrayDataSource ds = new ByteArrayDataSource(
-                new BufferedInputStream(entityStream), mediaType.toString());
-        DataHandler dh = new DataHandler(ds);
-        DataFlavor df[] = dh.getTransferDataFlavors();
-        //DataFlavor flava = findMatchingFlavor(type, df);
-        return null;
-    }
+   /*
+   * (non-Javadoc)
+   *
+   * @see javax.ws.rs.ext.MessageBodyReader#readFrom(java.lang.Class,
+   *      java.lang.reflect.Type, java.lang.annotation.Annotation[],
+   *      javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap,
+   *      java.io.InputStream)
+   */
+   public Object readFrom(Class type, Type genericType,
+                          Annotation[] annotations, MediaType mediaType,
+                          MultivaluedMap httpHeaders, InputStream entityStream)
+           throws IOException, WebApplicationException
+   {
+      ByteArrayDataSource ds = new ByteArrayDataSource(
+              new BufferedInputStream(entityStream), mediaType.toString());
+      DataHandler dh = new DataHandler(ds);
+      DataFlavor df[] = dh.getTransferDataFlavors();
+      //DataFlavor flava = findMatchingFlavor(type, df);
+      return null;
+   }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object)
-     */
-    public long getSize(Object t) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+   /*
+   * (non-Javadoc)
+   *
+   * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object)
+   */
+   public long getSize(Object t)
+   {
+      // TODO Auto-generated method stub
+      return 0;
+   }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class,
-     *      java.lang.reflect.Type, java.lang.annotation.Annotation[])
-     */
-    public boolean isWriteable(Class type, Type genericType,
-            Annotation[] annotations) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+   /*
+   * (non-Javadoc)
+   *
+   * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class,
+   *      java.lang.reflect.Type, java.lang.annotation.Annotation[])
+   */
+   public boolean isWriteable(Class type, Type genericType,
+                              Annotation[] annotations)
+   {
+      // TODO Auto-generated method stub
+      return false;
+   }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object,
-     *      java.lang.Class, java.lang.reflect.Type,
-     *      java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType,
-     *      javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
-     */
-    public void writeTo(Object t, Class type, Type genericType,
-            Annotation[] annotations, MediaType mediaType,
-            MultivaluedMap httpHeaders, OutputStream entityStream)
-            throws IOException, WebApplicationException {
-        // TODO Auto-generated method stub
-    }
+   /*
+   * (non-Javadoc)
+   *
+   * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object,
+   *      java.lang.Class, java.lang.reflect.Type,
+   *      java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType,
+   *      javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
+   */
+   public void writeTo(Object t, Class type, Type genericType,
+                       Annotation[] annotations, MediaType mediaType,
+                       MultivaluedMap httpHeaders, OutputStream entityStream)
+           throws IOException, WebApplicationException
+   {
+      // TODO Auto-generated method stub
+   }
 }
--- jaxrs/resteasy-jaxrs/src/main/java/org/resteasy/plugins/providers/MimeMultipartProvider.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/resteasy/plugins/providers/MimeMultipartProvider.java
@@ -1,11 +1,5 @@
 package org.resteasy.plugins.providers;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-
 import javax.activation.DataSource;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMultipart;
@@ -15,107 +9,119 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
 
 /**
  * A provider to handle multipart representations. This implementation will be
  * invoked when a method parameter takes a {@link MimeMultipart} as a method
  * parameter or a return value and the
- * 
+ *
+ * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a>
  * @ConsumeMime value is either multipart/mixed or multipart/form-data.
- * 
+ * <p/>
  * <code>
  * @POST
- * @ConsumeMime("multipart/form-data")
- * public void postData(MimeMultipart multipart) {
+ * @ConsumeMime("multipart/form-data") public void postData(MimeMultipart multipart) {
  * ...
  * </code>
- * 
+ * <p/>
  * When the {@link MimeMultipart} is passed to the method body, it is up to the
  * developer to extract the various parts.
- * 
- * @author <a href="mailto:ryan@damnhandy.com">Ryan J. McDonough</a>
  */
 
 @Provider
 @ProduceMime("multipart/mixed")
-@ConsumeMime( { "multipart/mixed", "multipart/form-data" })
-public class MimeMultipartProvider extends AbstractEntityProvider<MimeMultipart>{
+@ConsumeMime({"multipart/mixed", "multipart/form-data"})
+public class MimeMultipartProvider extends AbstractEntityProvider<MimeMultipart>
+{
 
-    /**
-     * 
-     * @param type
-     * @param genericType
-     * @param annotations
-     * @return
-     */
-    public boolean isReadable(Class<?> type, Type genericType,
-	    Annotation[] annotations) {
-	return MimeMultipart.class.equals(type);
-    }
+   /**
+    * @param type
+    * @param genericType
+    * @param annotations
+    * @return
+    */
+   public boolean isReadable(Class<?> type, Type genericType,
+                             Annotation[] annotations)
+   {
+      return MimeMultipart.class.equals(type);
+   }
 
-    /**
-     * 
-     * @param type
-     * @param genericType
-     * @param annotations
-     * @return
-     */
-    public boolean isWriteable(Class<?> type, Type genericType,
-	    Annotation[] annotations) {
-	return MimeMultipart.class.equals(type);
-    }
+   /**
+    * @param type
+    * @param genericType
+    * @param annotations
+    * @return
+    */
+   public boolean isWriteable(Class<?> type, Type genericType,
+                              Annotation[] annotations)
+   {
+      return MimeMultipart.class.equals(type);
+   }
 
-    /**
-     * 
-     * @param t
-     * @return
-     */
-    public long getSize(MimeMultipart mimeMultipart) {
-	return -1;
-    }
+   /**
+    * @param t
+    * @return
+    */
+   public long getSize(MimeMultipart mimeMultipart)
+   {
+      return -1;
+   }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyReader#readFrom(java.lang.Class,
-     *      java.lang.reflect.Type, java.lang.annotation.Annotation[],
-     *      javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap,
-     *      java.io.InputStream)
-     */
-    public MimeMultipart readFrom(Class<MimeMultipart> type, Type genericType,
-	    Annotation[] annotations, MediaType mediaType,
-	    MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
-	    throws IOException, WebApplicationException {
-	try {
-	    DataSource ds = readDataSource(entityStream, mediaType);
-	    return new MimeMultipart(ds);
-	} catch (MessagingException e) {
-	    throw new WebApplicationException(e);
-	}
-    }
+   /*
+   * (non-Javadoc)
+   *
+   * @see javax.ws.rs.ext.MessageBodyReader#readFrom(java.lang.Class,
+   *      java.lang.reflect.Type, java.lang.annotation.Annotation[],
+   *      javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap,
+   *      java.io.InputStream)
+   */
+   public MimeMultipart readFrom(Class<MimeMultipart> type, Type genericType,
+                                 Annotation[] annotations, MediaType mediaType,
+                                 MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
+           throws IOException, WebApplicationException
+   {
+      try
+      {
+         DataSource ds = readDataSource(entityStream, mediaType);
+         return new MimeMultipart(ds);
+      }
+      catch (MessagingException e)
+      {
+         throw new WebApplicationException(e);
+      }
+   }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object,
-     *      java.lang.Class, java.lang.reflect.Type,
-     *      java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType,
-     *      javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
-     */
-    public void writeTo(MimeMultipart mimeMultipart, Class<?> type,
-	    Type genericType, Annotation[] annotations, MediaType mediaType,
-	    MultivaluedMap<String, Object> httpHeaders,
-	    OutputStream entityStream) throws IOException,
-	    WebApplicationException {
-	try {
-	    // replace the Content-Type header to include the boundry
-	    // information
-	    httpHeaders.putSingle("Content-Type", MediaType
-		    .valueOf(mimeMultipart.getContentType()));
-	    mimeMultipart.writeTo(entityStream);
-	} catch (MessagingException e) {
-	    throw new WebApplicationException(e);
-	}
+   /*
+   * (non-Javadoc)
+   *
+   * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object,
+   *      java.lang.Class, java.lang.reflect.Type,
+   *      java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType,
+   *      javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
+   */
+   public void writeTo(MimeMultipart mimeMultipart, Class<?> type,
+                       Type genericType, Annotation[] annotations, MediaType mediaType,
+                       MultivaluedMap<String, Object> httpHeaders,
+                       OutputStream entityStream) throws IOException,
+           WebApplicationException
+   {
+      try
+      {
+         // replace the Content-Type header to include the boundry
+         // information
+         httpHeaders.putSingle("Content-Type", MediaType
+                 .valueOf(mimeMultipart.getContentType()));
+         mimeMultipart.writeTo(entityStream);
+      }
+      catch (MessagingException e)
+      {
+         throw new WebApplicationException(e);
+      }
 
-    }
+   }
 }
