c353c8111aad66f1d1bf74f63f5fcc5ae26bdbad
patriot1burke <patriot1burke@sourceforge.net>
2008-09-02 21:26:19 +0000

    update spring example
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@319 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ examples/spring-hibernate-contacts/core/src/main/java/org/jboss/resteasy/examples/contacts/core/ContactAttrs.java
@@ -0,0 +1,12 @@
+package org.jboss.resteasy.examples.contacts.core;
+
+
+/**
+ * @author <a href="mailto:obrand@yahoo.com">Olivier Brand</a>
+ * Jun 28, 2008
+ * 
+ */
+public enum ContactAttrs
+{
+    name, telephone, email, id;
+}
