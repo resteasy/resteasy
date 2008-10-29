3993a5bcf9dfe9f943160a99501fce3eda38a568
sduskis <sduskis@sourceforge.net>
2008-10-29 01:30:22 +0000

    added TJWS Spring MVC integration for easier testing
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@373 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ resteasy-springwebmvc/src/main/java/org/jboss/resteasy/springmvc/tjws/TJWSSpringMVCDispatcher.java
@@ -0,0 +1,41 @@
+package org.jboss.resteasy.springmvc.tjws;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.resteasy.plugins.server.embedded.SecurityDomain;
+import org.jboss.resteasy.plugins.server.tjws.TJWSRequestPreProcessor;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class TJWSSpringMVCDispatcher extends DispatcherServlet {
+    private TJWSRequestPreProcessor requestPreProcessor;
+
+    public TJWSSpringMVCDispatcher()
+    {
+        this.requestPreProcessor = new TJWSRequestPreProcessor(); 
+    }
+
+    public TJWSSpringMVCDispatcher(SecurityDomain domain)
+    {
+       this.requestPreProcessor = new TJWSRequestPreProcessor(domain);
+    }
+
+    public void setContextPath(String contextPath)
+    {
+       requestPreProcessor.setContextPath(contextPath);
+    }
+
+    public void setSecurityDomain(SecurityDomain domain)
+    {
+        requestPreProcessor.setSecurityDomain(domain);
+    }
+
+    @Override
+    protected void doService(HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+       HttpServletRequest processedRequest = requestPreProcessor.preProcessRequest(request, response);
+       if( processedRequest != null )
+          super.doService(processedRequest, response);
+    }
+
+}
