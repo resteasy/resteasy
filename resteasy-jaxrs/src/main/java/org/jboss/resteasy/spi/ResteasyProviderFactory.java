fe70f298c1b06f25d2bd9668b13694e664ba6e44
brackxm <brackxm@sourceforge.net>
2008-11-21 10:09:00 +0000

    RESTEASY-160 fix for simple super case
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@425 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/spi/ResteasyProviderFactory.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/spi/ResteasyProviderFactory.java
@@ -419,19 +419,23 @@ public class ResteasyProviderFactory extends RuntimeDelegate implements Provider
       providers.put(provider.getClass(), provider);
       PropertyInjectorImpl injector = new PropertyInjectorImpl(provider.getClass(), this);
       injector.inject(provider);
-      Type[] intfs = provider.getClass().getGenericInterfaces();
-      for (Type type : intfs)
-      {
-         if (type instanceof ParameterizedType)
+      Class clazz = provider.getClass();
+      do {
+         Type[] intfs = clazz.getGenericInterfaces();
+         for (Type type : intfs)
          {
-            ParameterizedType pt = (ParameterizedType) type;
-            if (pt.getRawType().equals(StringConverter.class))
+            if (type instanceof ParameterizedType)
             {
-               Class<?> aClass = Types.getRawType(pt.getActualTypeArguments()[0]);
-               stringConverters.put(aClass, provider);
+               ParameterizedType pt = (ParameterizedType) type;
+               if (pt.getRawType().equals(StringConverter.class))
+               {
+                  Class<?> aClass = Types.getRawType(pt.getActualTypeArguments()[0]);
+                  stringConverters.put(aClass, provider);
+               }
             }
          }
-      }
+         clazz = clazz.getSuperclass();
+      } while (clazz != null);
    }
 
    public List<ContextResolver> getContextResolvers(Class<?> clazz, MediaType type)
