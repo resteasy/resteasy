77ecae8e36d10ba4c39dd1e46a6ffcc4266bac26
sduskis <sduskis@sourceforge.net>
2008-12-17 02:45:58 +0000

    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@486 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- resteasy-spring/src/test/java/org/jboss/resteasy/springmvc/test/spring/AsynchSpringTest.java
+++ resteasy-spring/src/test/java/org/jboss/resteasy/springmvc/test/spring/AsynchSpringTest.java
@@ -18,7 +18,6 @@ import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.jboss.resteasy.core.AsynchronousDispatcher;
 import org.jboss.resteasy.springmvc.tjws.TJWSEmbeddedSpringMVCServerBean;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
