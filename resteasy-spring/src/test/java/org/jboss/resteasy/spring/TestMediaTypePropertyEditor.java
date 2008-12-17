85edf8060ff712096a7cfc9f0c441b39805c954e
justinedelson <justinedelson@sourceforge.net>
2008-12-17 04:08:30 +0000

    Adding media type property editor and an extension-based type mapping test
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@487 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ resteasy-spring/src/test/java/org/jboss/resteasy/spring/TestMediaTypePropertyEditor.java
@@ -0,0 +1,84 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.resteasy.spring;
+
+import static org.junit.Assert.*;
+
+import java.beans.PropertyEditor;
+
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.plugins.spring.MediaTypePropertyEditor;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestMediaTypePropertyEditor
+{
+
+   private PropertyEditor propertyEditor;
+
+   @BeforeClass
+   public static void setup()
+   {
+      ResteasyProviderFactory.initializeInstance();
+   }
+
+   @Before
+   public void setupEditor()
+   {
+      propertyEditor = new MediaTypePropertyEditor();
+   }
+
+   @Test
+   public void testSetAsText()
+   {
+      propertyEditor.setAsText("application/xml");
+      MediaType type = (MediaType) propertyEditor.getValue();
+      assertEquals("application", type.getType());
+      assertEquals("xml", type.getSubtype());
+   }
+
+   @Test
+   public void testSetAsTextWithCharset()
+   {
+      propertyEditor.setAsText("application/xml;charset=UTF-8");
+      MediaType type = (MediaType) propertyEditor.getValue();
+      assertEquals("application", type.getType());
+      assertEquals("xml", type.getSubtype());
+      assertEquals("UTF-8", type.getParameters().get("charset"));
+   }
+
+   @Test
+   public void testSetAsTextCustom()
+   {
+      propertyEditor.setAsText("application/custom");
+      MediaType type = (MediaType) propertyEditor.getValue();
+      assertEquals("application", type.getType());
+      assertEquals("custom", type.getSubtype());
+   }
+
+   @Test
+   public void testGetAsText()
+   {
+      MediaType type = MediaType.valueOf("application/xml");
+      propertyEditor.setValue(type);
+      String text = propertyEditor.getAsText();
+      assertEquals("application/xml", text);
+   }
+
+   @Test
+   public void testGetAsTextCustom()
+   {
+      MediaType type = MediaType.valueOf("application/custom");
+      propertyEditor.setValue(type);
+      String text = propertyEditor.getAsText();
+      assertEquals("application/custom", text);
+   }
+
+}
---
+++ resteasy-spring/src/test/java/org/jboss/resteasy/springmvc/test/spring/TypeMappingTest.java
@@ -0,0 +1,151 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ * 
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.resteasy.springmvc.test.spring;
+
+import static org.jboss.resteasy.test.TestPortProvider.*;
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.jboss.resteasy.core.SynchronousDispatcher;
+import org.jboss.resteasy.springmvc.tjws.TJWSEmbeddedSpringMVCServer;
+import org.jboss.resteasy.test.TestPortProvider;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+
+/*@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations =
+{"classpath:spring-typemapping-test-server.xml"})*/
+public class TypeMappingTest
+{
+
+   private HttpClient hc = new HttpClient();
+
+   private static TJWSEmbeddedSpringMVCServer server;
+
+   @Test
+   public void acceptJSONAndXMLRequestNoProducesJSONExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "json", "application/json, application/xml", "application/json");
+   }
+
+   @Test
+   public void acceptJSONAndXMLRequestNoProducesXMLExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "xml", "application/json, application/xml", "application/xml");
+   }
+
+   @Test
+   public void acceptJSONOnlyRequestNoProducesJSONExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "json", "application/json", "application/json");
+   }
+
+   @Test
+   public void acceptJSONOnlyRequestNoProducesNoExtension() throws Exception
+   {
+      requestAndAssert("noproduces", null, "application/json", "application/json");
+   }
+
+   @Test
+   public void acceptJSONOnlyRequestNoProducesXMLExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "xml", "application/json", "application/xml");
+   }
+
+   @Test
+   public void acceptNullRequestNoProducesJSONExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "json", null, "application/json");
+   }
+
+   @Test
+   public void acceptNullRequestNoProducesXMLExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "xml", null, "application/xml");
+   }
+
+   @Test
+   public void acceptXMLAndJSONRequestNoProducesJSONExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "json", "application/xml, application/json", "application/json");
+   }
+
+   @Test
+   public void acceptXMLAndJSONRequestNoProducesXMLExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "xml", "application/xml, application/json", "application/xml");
+   }
+
+   @Test
+   public void acceptXMLOnlyRequestNoProducesJSONExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "json", "application/xml", "application/json");
+   }
+
+   @Test
+   public void acceptXMLOnlyRequestNoProducesNoExtension() throws Exception
+   {
+      requestAndAssert("noproduces", null, "application/xml", "application/xml");
+   }
+
+   @Test
+   public void acceptXMLOnlyRequestNoProducesXMLExtension() throws Exception
+   {
+      requestAndAssert("noproduces", "xml", "application/xml", "application/xml");
+   }
+
+   @BeforeClass
+   public static void startServer()
+   {
+      server = new TJWSEmbeddedSpringMVCServer("classpath:spring-typemapping-test-server.xml", TestPortProvider
+            .getPort());
+      server.start();
+   }
+
+   @AfterClass
+   public static void stopServer()
+   {
+      server.stop();
+   }
+
+   @Test
+   public void validateTypeMappingsExist()
+   {
+      ApplicationContext ctx = server.getApplicationContext();
+      SynchronousDispatcher dispatcher = (SynchronousDispatcher) ctx.getBean("resteasy.dispatcher");
+      Map<String, MediaType> mappings = dispatcher.getMediaTypeMappings();
+      assertEquals(2, mappings.size());
+      assertEquals("application/xml", mappings.get("xml").toString());
+      assertEquals("application/json", mappings.get("json").toString());
+   }
+
+   private void requestAndAssert(String path, String extension, String accept, String expectedContentType)
+         throws Exception
+   {
+      String url = generateURL("/test/" + path);
+      if (extension != null)
+      {
+         url = url + "." + extension;
+      }
+      GetMethod gm = new GetMethod(url);
+      if (accept != null)
+      {
+         gm.setRequestHeader("Accept", accept);
+      }
+      int status = hc.executeMethod(gm);
+      assertEquals("Request for " + url + " returned a non-200 status", 200, status);
+      assertEquals("Request for " + url + " returned an unexpected content type", expectedContentType, gm
+            .getResponseHeader("Content-type").getValue());
+   }
+}
