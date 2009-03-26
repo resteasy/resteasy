7b33d6a0c58c4099add4216537643b15b3a1cf18
patriot1burke <patriot1burke@sourceforge.net>
2009-03-26 22:57:51 +0000

    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@609 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/server/tjws/TJWSServletDispatcher.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/server/tjws/TJWSServletDispatcher.java
@@ -3,6 +3,8 @@ package org.jboss.resteasy.plugins.server.tjws;
 import org.jboss.resteasy.plugins.server.embedded.SecurityDomain;
 import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -41,5 +43,17 @@ public class TJWSServletDispatcher extends HttpServletDispatcher
    public void setSecurityDomain(SecurityDomain domain)
    {
       requestPreProcessor.setSecurityDomain(domain);
+   }
+
+   @Override
+   public ServletConfig getServletConfig()
+   {
+      return null;  // it will never be initialized
+   }
+
+   @Override
+   public ServletContext getServletContext()
+   {
+      return null;  // it will never be initialized
    }
 }
