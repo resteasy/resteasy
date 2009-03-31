fe686241ccbbc9d7e4e247d541b0c6d9a9858d04
sduskis <sduskis@sourceforge.net>
2009-03-31 11:27:56 +0000

    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@621 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/examples/api-clients/src/main/java/org/jboss/resteasy/examples/flickr/FlickrResponse.java
+++ jaxrs/examples/api-clients/src/main/java/org/jboss/resteasy/examples/flickr/FlickrResponse.java
@@ -4,12 +4,13 @@ import java.util.List;
 import java.util.concurrent.FutureTask;
 
 import javax.swing.ImageIcon;
+import javax.ws.rs.core.UriBuilder;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-@XmlRootElement(name="rsp")
+@XmlRootElement(name = "rsp")
 public class FlickrResponse {
 	@XmlElementWrapper(name = "photos")
 	public List<Photo> photo;
@@ -17,7 +18,12 @@ public class FlickrResponse {
 
 class Photo {
 	@XmlAttribute
-	public String server, id, secret, title;
+	public String server, id, secret, title, owner;
 	@XmlTransient
 	public FutureTask<ImageIcon> image;
+
+	public String getPublicURL() {
+		return UriBuilder.fromUri("http://www.flickr.com/photos/").path(
+				"/{owner}/{id}").build(owner, id).toString();
+	}
 }
