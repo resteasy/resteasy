31a0842c780382e298dfcab862f5376bc70b9266
sduskis <sduskis@sourceforge.net>
2009-03-31 01:55:58 +0000

    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@615 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/examples/api-clients/src/main/java/org/jboss/resteasy/examples/flickr/FlickrClient.java
+++ jaxrs/examples/api-clients/src/main/java/org/jboss/resteasy/examples/flickr/FlickrClient.java
@@ -2,149 +2,126 @@ package org.jboss.resteasy.examples.flickr;
 
 import java.awt.BorderLayout;
 import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.util.Date;
-import java.util.List;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.ws.rs.core.UriBuilder;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
-import org.jboss.resteasy.client.ClientRequest;
-import org.jboss.resteasy.client.cache.BrowserCache;
-import org.jboss.resteasy.client.cache.CacheFactory;
 import org.jboss.resteasy.client.cache.LightweightBrowserCache;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 public class FlickrClient {
-	static final String photoSearchUrl = "http://www.flickr.com/services/rest?method=flickr.photos.search&per_page=10&sort=interestingness-desc";
-	static final String photoServer = "http://static.flickr.com";
-	static final String photoPath = "/{server}/{id}_{secret}_m.jpg";
 
-	static JFrame frame = null;
-	static JPanel dataPanel = null;
-	static String apiKey = null;
-	static BrowserCache cache = new LightweightBrowserCache();
-
-	@XmlRootElement()
-	public static class Rsp {
-		@XmlElementWrapper(name = "photos")
-		public List<Photo> photo;
-		@XmlTransient
-		public String searchTerm;
-	}
-
-	public static class Photo {
-		@XmlAttribute
-		public String server, id, secret, title;
-
-		public URI getURI() throws Exception {
-			return UriBuilder.fromUri(photoServer).path(photoPath).build(
-					server, id, secret);
-		}
-	}
+	FlickrSearchService flickrSearchService;
 
 	public static void main(String args[]) throws Exception {
-		RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
-		apiKey = args[0];
-		createInitialFrame();
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		ResteasyProviderFactory instance = ResteasyProviderFactory
+				.getInstance();
+		RegisterBuiltin.register(instance);
+		instance.registerProvider(ImageIconMessageBodyReader.class);
+
+		FlickrSearchService flickrSearchService = new FlickrSearchService(
+				args[0], new LightweightBrowserCache());
+
+		FlickrClient client = new FlickrClient(flickrSearchService);
+		client.frame.pack();
+		Dimension preferredSize = client.frame.getPreferredSize();
+		client.frame.setSize(new Dimension(preferredSize.width + 500,
+				preferredSize.height));
+		client.frame.setVisible(true);
 	}
 
-	private static void createInitialFrame() {
+	JFrame frame = null;
+	JPanel dataPanel = null;
+	JTextField textField = null;
+
+	public FlickrClient(FlickrSearchService flickrSearchService) {
+		this.flickrSearchService = flickrSearchService;
 		frame = new JFrame();
 		frame.setTitle("Flickr Search");
 		frame.setLayout(new BorderLayout());
 		frame.add(createQueryPanel(), BorderLayout.NORTH);
 		frame.add(dataPanel = new JPanel(), BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		frame.pack();
-		frame.setSize(frame.getSize().width + 400, frame.getSize().height);
-		frame.setVisible(true);
 	}
 
-	private static Rsp getPhotos(final String searchTerm) throws Exception {
-		ClientRequest request = new ClientRequest(photoSearchUrl)
-				.queryParameter("api key", apiKey).queryParameter("tags",
-						searchTerm);
-		CacheFactory.makeCacheable(request, cache);
-		Rsp photos = request.get(Rsp.class).getEntity();
-		photos.searchTerm = searchTerm;
-		return photos;
-	}
-
-	private static JPanel createQueryPanel() {
+	private JPanel createQueryPanel() {
 		JPanel queryPanel = new JPanel(new BorderLayout());
 		queryPanel.add(new JLabel("Query:"), BorderLayout.WEST);
-		final JTextField textField = new JTextField();
+
+		textField = new JTextField();
+		queryPanel.add(textField, BorderLayout.CENTER);
+
+		JButton searchButton = new JButton("Search");
+		queryPanel.add(searchButton, BorderLayout.EAST);
+
 		textField.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-					updatePhotos(textField.getText());
+					updatePhotos();
 				}
 			}
 		});
-		queryPanel.add(textField, BorderLayout.CENTER);
-		JButton searchButton = new JButton("Search");
 		searchButton.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				updatePhotos(textField.getText());
+				updatePhotos();
 			}
 		});
-		queryPanel.add(searchButton, BorderLayout.EAST);
+
 		return queryPanel;
 	}
 
-	private static void updatePhotos(String searchTerm) {
-		try {
-			frame.setCursor(new Cursor(Cursor.WAIT_CURSOR));
-			System.out.println(new Date() + " search for " + searchTerm);
-			Rsp photos = getPhotos(searchTerm);
-			System.out.println(new Date() + " got " + photos.photo.size() + " results for " + searchTerm);
-			displayPhotos(photos);
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		} finally {
-			System.out.println(new Date() + " finished search for " + searchTerm);
-			frame.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-		}
+	private void updatePhotos() {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				String searchTerm = textField.getText();
+				try {
+					frame.setCursor(new Cursor(Cursor.WAIT_CURSOR));
+					displayPhotos(flickrSearchService.searchPhotos("tags",
+							searchTerm));
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				} finally {
+					System.out.println(new Date() + " finished search for "
+							+ searchTerm);
+					frame.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+					frame.pack();
+				}
+			}
+		});
 	}
 
-	private static void displayPhotos(Rsp photos) throws MalformedURLException,
-			Exception {
+	private void displayPhotos(FlickrResponse photos)
+			throws MalformedURLException, Exception {
 		frame.setTitle("Flickr Search for " + photos.searchTerm);
-		if(dataPanel != null)
-		frame.remove(dataPanel);
-		dataPanel=new JPanel();
+		if (dataPanel != null)
+			frame.remove(dataPanel);
+		dataPanel = new JPanel();
 		dataPanel.setLayout(new GridLayout(2, photos.photo.size() / 2));
-		
+
 		for (Photo photo : photos.photo) {
 			JPanel panel = new JPanel(new BorderLayout());
-			System.out.println(new Date() + " reading photo " + photo.getURI());
-			
 			panel.add(new JLabel(photo.title), BorderLayout.NORTH);
-			panel.add(new JLabel(new ImageIcon(photo.getURI().toURL())),
+			panel.add(new JLabel(flickrSearchService.getImageIcon(photo)),
 					BorderLayout.CENTER);
 			dataPanel.add(panel);
 		}
 		frame.add(dataPanel, BorderLayout.CENTER);
-		frame.pack();
 	}
 }
