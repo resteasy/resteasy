1ba2553f2c9b769db4bcae48b25e0cb07ca071c5
sduskis <sduskis@sourceforge.net>
2009-04-07 02:12:00 +0000

    applyDefaultInterceptors
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@634 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/client/ProxyFactory.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/client/ProxyFactory.java
@@ -1,7 +1,9 @@
 package org.jboss.resteasy.client;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.jboss.resteasy.client.core.ApacheHttpClientExecutor;
 import org.jboss.resteasy.client.core.ClientInvoker;
+import org.jboss.resteasy.client.core.ClientInvokerInterceptorFactory;
 import org.jboss.resteasy.client.core.ClientProxy;
 import org.jboss.resteasy.client.core.ResteasyClientProxy;
 import org.jboss.resteasy.spi.ProviderFactoryDelegate;
@@ -29,18 +31,28 @@ public class ProxyFactory
 
    public static <T> T create(Class<T> clazz, String base, HttpClient client)
    {
+   	return create(clazz, createUri(base), client, ResteasyProviderFactory.getInstance());
+   }
+
+	public static URI createUri(String base)
+	{
       try
       {
-         return create(clazz, new URI(base), client, ResteasyProviderFactory.getInstance());
+         return new URI(base);
       }
       catch (URISyntaxException e)
       {
          throw new RuntimeException(e);
       }
-   }
+	}
 
-   @SuppressWarnings("unchecked")
    public static <T> T create(Class<T> clazz, URI baseUri, HttpClient httpClient, ResteasyProviderFactory providerFactory)
+   {
+   	return create(clazz, baseUri, new ApacheHttpClientExecutor(httpClient), providerFactory);
+   }
+   
+   @SuppressWarnings("unchecked")
+   public static <T> T create(Class<T> clazz, URI baseUri, ClientExecutor executor, ResteasyProviderFactory providerFactory)
    {
       HashMap<Method, ClientInvoker> methodMap = new HashMap<Method, ClientInvoker>();
 
@@ -56,7 +68,8 @@ public class ProxyFactory
          if (httpMethods.size() != 1)
             throw new RuntimeException("You must use at least one, but no more than one http method annotation");
 
-         invoker = new ClientInvoker(baseUri, clazz, method, providerFactory, httpClient);
+         invoker = new ClientInvoker(baseUri, clazz, method, providerFactory, executor);
+         ClientInvokerInterceptorFactory.applyDefaultInterceptors(invoker, providerFactory, clazz, method);
          invoker.setHttpMethod(httpMethods.iterator().next());
          methodMap.put(method, invoker);
       }
