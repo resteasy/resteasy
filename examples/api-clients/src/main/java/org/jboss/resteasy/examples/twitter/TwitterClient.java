688d1190c46906880584c85af6fa65cd41774579
sduskis <sduskis@sourceforge.net>
2009-04-08 21:32:00 +0000

    updated RESTEasy twitter client
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@645 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- examples/api-clients/src/main/java/org/jboss/resteasy/examples/twitter/TwitterClient.java
+++ examples/api-clients/src/main/java/org/jboss/resteasy/examples/twitter/TwitterClient.java
@@ -1,94 +1,96 @@
 package org.jboss.resteasy.examples.twitter;
 
+import java.util.Date;
 import java.util.List;
 
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.adapters.XmlAdapter;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
-import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ProxyFactory;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
-public class TwitterClient {
-	private final static DateTimeFormatter TWITTER_DATE_FORMATTER = DateTimeFormat
-			.forPattern("EEE MMM dd HH:mm:ss Z yyyy");
+public class TwitterClient
+{
+   static final String friendTimeline = "http://twitter.com/statuses/friends_timeline.xml";
 
-	public static class DateTimeAdapter extends XmlAdapter<String, DateTime> {
+   public static void main(String[] args) throws Exception
+   {
+      RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
+      TwitterResource twitter = ProxyFactory.create(TwitterResource.class,
+            "http://twitter.com", createClient(args[0], args[1]));
+      System.out.println("===> first run");
+      printStatuses(twitter.getFriendsTimelines());
+      
+      twitter
+            .updateStatus("I programmatically tweeted with the RESTEasy Client at "
+                  + new Date());
+      
+      System.out.println("===> second run");
+      printStatuses(twitter.getFriendsTimelines());
+   }
 
-		@Override
-		public String marshal(DateTime date) throws Exception {
-			return TWITTER_DATE_FORMATTER.print(date);
-		}
+   public static interface TwitterResource
+   {
+      @Path("/statuses/friends_timeline.xml")
+      @GET
+      Statuses getFriendsTimelines();
 
-		@Override
-		public DateTime unmarshal(String string) throws Exception {
-			try {
-				return TWITTER_DATE_FORMATTER.parseDateTime(string);
-			} catch (IllegalArgumentException e) {
-				System.err.println(String.format(
-						"Could not parse date string '%s'", string));
-				return null;
-			}
-		}
-	}
+      @Path("/statuses/update.xml")
+      @POST
+      Status updateStatus(@FormParam("status") String status);
+   }
 
-	@XmlRootElement
-	public static class Statuses {
-		public List<Status> status;
-	}
+   private static void printStatuses(Statuses statuses)
+   {
+      for (Status status : statuses.status)
+         System.out.println(status);
+   }
 
-	@XmlRootElement
-	public static class Status {
-		public String text;
-		public User user;
+   private static HttpClient createClient(String userId, String password)
+   {
+      Credentials credentials = new UsernamePasswordCredentials(userId,
+            password);
+      HttpClient httpClient = new HttpClient();
+      httpClient.getState().setCredentials(AuthScope.ANY, credentials);
+      httpClient.getParams().setAuthenticationPreemptive(true);
+      return httpClient;
+   }
 
-		@XmlElement(name = "created_at")
-		@XmlJavaTypeAdapter(value = DateTimeAdapter.class)
-		public DateTime created;
+   @XmlRootElement
+   public static class Statuses
+   {
+      public List<Status> status;
+   }
 
-		public String toString() {
-			return String.format("== %s: %s (%s)", user.name, text, created
-					.toDate().toString());
+   @XmlRootElement
+   public static class Status
+   {
+      public String text;
+      public User user;
 
-		}
-	}
+      @XmlElement(name = "created_at")
+      @XmlJavaTypeAdapter(value = DateAdapter.class)
+      public Date created;
 
-	public static class User {
-		public String name;
-	}
+      public String toString()
+      {
+         return String.format("== %s: %s (%s)", user.name, text, created);
+      }
+   }
 
-	public static void main(String[] args) throws Exception {
-		final String friendsTimeline = "http://twitter.com/statuses/friends_timeline.xml";
-		RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
-		Credentials credentials = new UsernamePasswordCredentials(args[0],
-				args[1]);
-		HttpClient httpClient = new HttpClient();
-		httpClient.getState().setCredentials(AuthScope.ANY, credentials);
-		httpClient.getParams().setAuthenticationPreemptive(true);
-		displayStatuses(new ClientRequest(friendsTimeline, httpClient).get(
-				Statuses.class).getEntity());
-		
-		// uncomment to perform a 'write' operation 
-//		String status = "I programmatically tweeted with the RESTEasy Client at "
-//				+ new Date();
-//		System.out.println(new ClientRequest(
-//				"http://twitter.com/statuses/update.xml", httpClient)
-//				.formParameter("status", status).post(Status.class).getEntity());
-		 
-	}
+   public static class User
+   {
+      public String name;
+   }
 
-	private static void displayStatuses(Statuses statuses) {
-		for (Status status : statuses.status) {
-			System.out.println(status);
-		}
-	}
 }
