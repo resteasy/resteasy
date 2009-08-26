557f9ce4ef7e8ae5a2a8abd22cff29ce8250281d
leandroferro <leandroferro@sourceforge.net>
2009-08-26 03:13:57 +0000

    Added javadoc
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@793 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/AcceptParameterHttpPreprocessor.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/AcceptParameterHttpPreprocessor.java
@@ -19,11 +19,36 @@ import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.HttpRequestPreprocessor;
 import org.jboss.resteasy.util.MediaTypeHelper;
 
+/**
+ * 
+ * <p>Enables content negotiation through a query parameter, instead of the Accept Header.</p>
+ * <p>To enable this feature, use the context-param in web.xml:</p> 
+ * <p>
+ * <code>
+ * &lt;context-param&gt;<br />
+ * &nbsp;&nbsp;&nbsp;&nbsp;&lt;param-name&gt;<strong>resteasy.media.type.param.mapping</strong>&lt;/param-name&gt;<br />
+ * &nbsp;&nbsp;&nbsp;&nbsp;&lt;param-value&gt;<i>someName</i>&lt;/param-value&gt;<br />
+ * &lt;/context-param&gt;<br />
+ * </code>
+ * <p>So, in a request like 
+ * <code>http://service.foo.com/resouce?someName=application/xml</code>
+ * the application/xml media type will received the highest priority in the content negotiation.
+ * <p>In the cases where the request contains both the parameter and the Accept header, the parameter will be more relevant.
+ * <p>It is possible to left the <code>param-value</code> empty, what will cause the processor to look for an <strong>accept</strong> parameter.
+ * @author <a href="leandro.ferro@gmail.com">Leandro Ferro Luzia</a>
+ * @version $Revision: 1.2 $
+ */
 public class AcceptParameterHttpPreprocessor implements HttpRequestPreprocessor
 {
 
    private final String paramMapping;
 
+   /**
+    * 
+    * Create a new AcceptParameterHttpPreprocessor.
+    * 
+    * @param paramMapping The name of query parameter that will be used to do the content negotiation
+    */
    public AcceptParameterHttpPreprocessor(String paramMapping)
    {
       if (paramMapping == null || paramMapping.matches("\\s+"))
