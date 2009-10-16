ec51c7443a2a839eb6a44b7da8e2766421f5d69e
patriot1burke <patriot1burke@sourceforge.net>
2009-10-16 21:12:22 +0000

    chat app and updates to star messaging to support it
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/rest-star/messaging@840 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ core/src/main/java/org/jboss/resteasy/star/messaging/TopicServletContextListener.java
@@ -0,0 +1,55 @@
+package org.jboss.resteasy.star.messaging;
+
+import org.jboss.resteasy.spi.Registry;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public class TopicServletContextListener implements ServletContextListener
+{
+   protected SimpleDeployment deployment = new SimpleDeployment();
+
+   public void contextInitialized(ServletContextEvent event)
+   {
+      final Registry registry = (Registry) event.getServletContext().getAttribute(Registry.class.getName());
+      if (registry == null)
+         throw new RuntimeException("RESTeasy Registry is null, do ou have the ResteasyBootstrap listener configured?");
+
+      deployment.setRegistry(registry);
+      String topics = event.getServletContext().getInitParameter("resteasy.star.messaging.topics");
+      if (topics == null)
+      {
+         throw new RuntimeException("There were no topics declared within resteasy.star.messaging.topics");
+
+      }
+      String[] split = topics.split(",");
+      for (String topic : split)
+      {
+         deployment.getTopics().add(topic.trim());
+      }
+      try
+      {
+         deployment.start();
+      }
+      catch (Exception e)
+      {
+         throw new RuntimeException(e);
+      }
+   }
+
+   public void contextDestroyed(ServletContextEvent servletContextEvent)
+   {
+      try
+      {
+         deployment.stop();
+      }
+      catch (Exception e)
+      {
+         throw new RuntimeException(e);
+      }
+   }
+}
