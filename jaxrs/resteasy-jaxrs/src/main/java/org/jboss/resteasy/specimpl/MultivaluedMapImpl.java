4957011f9b091375bf1445601ee0f1cc96a237ea
sduskis <sduskis@sourceforge.net>
2009-11-26 04:07:03 +0000

    Addition for InMemoryClientExecutor... will help with performance as well MultivaluedMapImpl.addMultiple
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@887 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/specimpl/MultivaluedMapImpl.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/specimpl/MultivaluedMapImpl.java
@@ -2,6 +2,7 @@ package org.jboss.resteasy.specimpl;
 
 import javax.ws.rs.core.MultivaluedMap;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
@@ -9,30 +10,41 @@ import java.util.List;
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
  */
+@SuppressWarnings("serial")
 public class MultivaluedMapImpl<K, V> extends HashMap<K, List<V>> implements MultivaluedMap<K, V>
 {
 
    public void putSingle(K key, V value)
    {
-      List<V> list = new ArrayList<V>(1);
+      List<V> list = new ArrayList<V>();
       list.add(value);
       put(key, list);
    }
 
-   public void add(K key, V value)
+   public final void add(K key, V value)
    {
-      List<V> list = get(key);
-      if (list == null)
-      {
-         list = new ArrayList<V>(1);
-         put(key, list);
-      }
-      list.add(value);
+      getList(key).add(value);
+   }
+
+
+   public final void addMultiple(K key, Collection<V> values)
+   {
+      getList(key).addAll(values);
    }
 
    public V getFirst(K key)
    {
       List<V> list = get(key);
-      return (list == null) ? null : list.get(0);
+      return list == null ? null : list.get(0);
    }
+
+   public final List<V> getList(K key)
+   {
+      List<V> list = get(key);
+      if (list == null)
+         put(key, list = new ArrayList<V>());
+      return list;
+   }
+
+   
 }
