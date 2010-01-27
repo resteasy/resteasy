3a395ff3e8cb82591e22ad7944621482f771cbfb
sduskis <sduskis@sourceforge.net>
2010-01-27 11:23:15 +0000

    RESTEASY-353 - applied main & test patches
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@906 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/core/injection/ProviderInjectionTest.java
@@ -0,0 +1,72 @@
+package org.jboss.resteasy.test.core.injection;
+
+import static org.jboss.resteasy.test.TestPortProvider.generateBaseUrl;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.test.BaseResourceTest;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This test verifies that Providers instance can be injected into a Provider
+ * using constructor or field injection.
+ * 
+ * @author Jozef Hartinger
+ * 
+ */
+public class ProviderInjectionTest extends BaseResourceTest
+{
+
+   @Before
+   public void setUp() throws Exception
+   {
+      getProviderFactory().registerProvider(SimpleMessageBodyWriter.class);
+      addPerRequestResource(SimpleResourceImpl.class);
+
+      // do a request (force provider instantiation if providers were created
+      // lazily)
+      SimpleResource proxy = ProxyFactory.create(SimpleResource.class, generateBaseUrl());
+      assertEquals(proxy.foo(), "bar");
+   }
+
+   @Test
+   public void testConstructorInjection()
+   {
+      for (SimpleMessageBodyWriter writer : SimpleMessageBodyWriter.getInstances())
+      {
+         assertTrue(writer.getConstructorProviders() != null);
+      }
+   }
+
+   @Test
+   public void testFieldInjection()
+   {
+      for (SimpleMessageBodyWriter writer : SimpleMessageBodyWriter.getInstances())
+      {
+         assertTrue(writer.getFieldProviders() != null);
+      }
+   }
+
+   @Path("/test")
+   public static interface SimpleResource
+   {
+      @GET
+      @Produces("text/plain")
+      String foo();
+   }
+
+   public static class SimpleResourceImpl implements SimpleResource
+   {
+      public String foo()
+      {
+         return "foo";
+      }
+   }
+}
