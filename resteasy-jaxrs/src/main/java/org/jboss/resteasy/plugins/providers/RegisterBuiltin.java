0d1f21d4b61d6c35ed43d8b043ec19496f1200ff
sduskis <sduskis@sourceforge.net>
2010-02-07 12:24:22 +0000

    Small fix for RegisterBuiltin for ClassNotFound situations
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@923 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/providers/RegisterBuiltin.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/providers/RegisterBuiltin.java
@@ -1,16 +1,17 @@
 package org.jboss.resteasy.plugins.providers;
 
-import org.jboss.resteasy.core.LoggerCategories;
-import org.jboss.resteasy.spi.ResteasyProviderFactory;
-import org.slf4j.Logger;
-
-import javax.ws.rs.ext.Providers;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.LinkedHashSet;
+
+import javax.ws.rs.ext.Providers;
+
+import org.jboss.resteasy.core.LoggerCategories;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.slf4j.Logger;
 
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
@@ -62,16 +63,19 @@ public class RegisterBuiltin
       }
       for (String line : set)
       {
-         Class clazz = null;
          try
          {
-            clazz = Thread.currentThread().getContextClassLoader().loadClass(line);
+            Class clazz = Thread.currentThread().getContextClassLoader().loadClass(line);
+            factory.registerProvider(clazz, true);
+         }
+         catch (NoClassDefFoundError e)
+         {
+            logger.warn("NoClassDefFoundError: Unable to load builtin provider: " + line);
          }
          catch (ClassNotFoundException e)
          {
             logger.warn("ClassNotFoundException: Unable to load builtin provider: " + line);
          }
-         factory.registerProvider(clazz, true);
       }
    }
 
