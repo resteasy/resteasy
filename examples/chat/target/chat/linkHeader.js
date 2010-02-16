ec97a7c029b8a06810b3663c650f3c4c4ce07abe
patriot1burke <patriot1burke@sourceforge.net>
2010-02-16 22:46:31 +0000

    workflow prototype
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/rest-star/messaging@946 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ examples/chat/target/chat/linkHeader.js
@@ -0,0 +1,44 @@
+var linkHeaderExpression=/<[^>]*>\s*(\s*;\s*[^\(\)<>@,;:"\/\[\]\?={} \t]+=(([^\(\)<>@,;:"\/\[\]\?={} \t]+)|("[^"]*")))*(,|$)/g;
+var linkHeaderParamExpression=/[^\(\)<>@,;:"\/\[\]\?={} \t]+=(([^\(\)<>@,;:"\/\[\]\?={} \t]+)|("[^"]*"))/g;
+
+function unquote(value)
+{
+    if (value.charAt(0) == '"' && value.charAt(value.length - 1) == '"') return value.substring(1, value.length - 1);
+    return value;
+}
+
+function parseLinkHeader(value)
+{
+    var matches = value.match(linkHeaderExpression);
+    var rels = new Object();
+    var titles = new Object();
+    for (i = 0; i < matches.length; i++)
+    {
+        var split = matches[i].split('>');
+        var href = split[0].substring(1);
+        var ps = split[1];
+        var link = new Object();
+        link.href = href;
+        var s = ps.match(linkHeaderParamExpression);
+        for (j = 0; j < s.length; j++)
+        {
+            var p = s[j];
+            var paramsplit = p.split('=');
+            var name = paramsplit[0];
+            link[name] = unquote(paramsplit[1]);
+        }
+
+        if (link.rel != undefined)
+        {
+            rels[link.rel] = link;
+        }
+        if (link.title != undefined)
+        {
+            titles[link.title] = link;
+        }
+    }
+    var linkheader = new Object();
+    linkheader.rels = rels;
+    linkheader.titles = titles;
+    return linkheader;
+}
