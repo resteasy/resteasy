b7f1e6c9ce7154710983402c28179abd48b0d7fc
sduskis <sduskis@sourceforge.net>
2010-02-25 11:32:15 +0000

    Initial commit of resteasy-html.  It's not as robust functionality-wise as HTMLEasy, but it's a good start.  For now, this requires a specialized servlet that defers opening response.getOutputStream unless there's a need for it by a writer.
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@958 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ jaxrs/providers/resteasy-html/src/main/java/org/jboss/resteasy/plugins/providers/html/Renderable.java
@@ -0,0 +1,25 @@
+package org.jboss.resteasy.plugins.providers.html;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.WebApplicationException;
+
+/**
+ * If you return one of these from a resource method, RESTEasy-HTML will render it.  This
+ * interface allows for exotic view rendering types.
+ * 
+ * @author Jeff Schnitzer <jeff@infohazard.org>
+ */
+public interface Renderable
+{
+   /**
+    * Called to do the actual work of rendering a view.  Note that while ServletException
+    * can be thrown, WebApplicationException is preferred.
+    */
+   public void render(HttpServletRequest request, HttpServletResponse response)
+       throws IOException, ServletException, WebApplicationException;
+
+}
