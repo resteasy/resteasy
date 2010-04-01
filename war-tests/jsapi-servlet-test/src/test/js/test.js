2320082a0c3ebcf38904d2c1d6b2982a3dff709b
unfromage <unfromage@sourceforge.net>
2010-04-01 14:48:40 +0000

    Finally can commit the JSAPI integration tests :)
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@993 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ war-tests/jsapi-servlet-test/src/test/js/test.js
@@ -0,0 +1,95 @@
+var baseURI = 'http://localhost:9095';
+
+Envjs(baseURI+'/test.html', {
+	scriptTypes : {
+	"text/javascript"   : true
+	},
+	logLevel: Envjs.DEBUG
+});
+
+REST.loglevel = 1;
+
+function testFunctions() {
+	assertNotNull("get function", MyResource.get);
+}
+
+function testGet() {
+	var data = MyResource.get();
+	assertEquals("ok", data);
+}
+
+function testGetFoo() {
+	var data = MyResource.getFoo();
+	assertEquals("foo", data);
+}
+
+function testGetParam() {
+	var value = "bla";
+	var data = MyResource.getParam({param: value});
+	assertEquals(value, data);
+}
+
+function testGetFooParam() {
+	var data = MyResource.getFooParam({param: "paramv", other: "otherv",
+		q : "qv", c : "cv", h: "hv", m: "mv"});
+	// cookie is null since env.js doesn't support cookies yet :(
+	assertEquals("param=paramv;other=otherv;q=qv;c=null;h=hv;m=mv;", data);
+}
+
+function testPutFooParam() {
+	var data = MyResource.putFooParam({param: "paramv", other: "otherv",
+		q : "qv", c : "cv", h: "hv", m: "mv", $entity: "entityv"});
+	// cookie is null since env.js doesn't support cookies yet :(
+	assertEquals("param=paramv;other=otherv;q=qv;c=null;h=hv;m=mv;entity=entityv;", data);
+}
+
+function testGetXML() {
+	var data = MyResource.getXML();
+	print(data);
+	assertTrue(data instanceof Document);
+	var root = data.documentElement;
+	assertEquals("test", root.nodeName);
+	assertEquals(1, root.childNodes.length);
+	assertEquals("var", root.childNodes[0].nodeName);
+	assertEquals(1, root.childNodes[0].childNodes.length);
+	assertEquals("foo", root.childNodes[0].childNodes[0].nodeValue);
+}
+
+function testGetJSON() {
+	var data = MyResource.getJSON();
+	assertEquals(data['var'], "foo");
+}
+
+function testPutJSON() {
+	var toSend = {'var' : "ok"};
+	var data = MyResource.putJSON({$entity: toSend});
+	assertEquals("ok", data);
+}
+
+function testPutXML() {
+	var toSend = document.implementation.createDocument(null, "test", null);
+	var v = toSend.createElement("var");
+	toSend.documentElement.appendChild(v);
+	v.appendChild(toSend.createTextNode("ok"));
+	var data = MyResource.putXML({$entity: toSend});
+	assertEquals("ok", data);
+}
+
+function testGetMultiRepresentationXML(){
+	var data = MyResource.getMultiRepresentation({$accepts: "application/xml"});
+	assertTrue(data instanceof Document);
+	var root = data.documentElement;
+	assertEquals(23, root.childNodes.length);
+}
+
+function testGetMultiRepresentationJSON(){
+	var data = MyResource.getMultiRepresentation({$accepts: "application/json"});
+	assertTrue(data instanceof Array);
+	assertEquals(23, data.length);
+}
+
+function testLookup(){
+	var value = "foo";
+	var data = MyResource.lookup({id: value});
+	assertEquals(baseURI+"/rest/mine/"+value, data);
+}
