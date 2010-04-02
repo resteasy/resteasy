5c91306fbb4fdbbaa5daa3a7aeecbb00d1fac9cc
unfromage <unfromage@sourceforge.net>
2010-04-02 08:40:27 +0000

    RESTEASY-311: Client proxy sends @Context parameters instead of ignoring them
    Ignore @Context parameters, which can be passed as null by the client.
    
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@994 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/client/ContextTest.java
@@ -0,0 +1,58 @@
+package org.jboss.resteasy.test.client;
+
+import static org.jboss.resteasy.test.TestPortProvider.generateBaseUrl;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+
+import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.jboss.resteasy.test.BaseResourceTest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ */
+public class ContextTest extends BaseResourceTest
+{
+   public static class Resource implements ResourceInterface
+   {
+
+      public String echo(UriInfo info)
+      {
+    	  Assert.assertNotNull(info);
+         return "content";
+      }
+   }
+
+   @Path(value = "/test")
+   public interface ResourceInterface
+   {
+
+      @GET
+      @Produces("text/plain")
+      public String echo(@Context UriInfo info);
+   }
+
+   @Before
+   public void setUp() throws Exception
+   {
+      addPerRequestResource(Resource.class);
+   }
+
+
+   @Test
+   public void testEcho()
+   {
+      RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
+      ResourceInterface client = ProxyFactory.create(ResourceInterface.class, generateBaseUrl());
+      Assert.assertEquals("content", client.echo(null));
+   }
+
+
+}
