c8d71ead73619195875bab70b936ceea4104396c
patriot1burke <patriot1burke@sourceforge.net>
2010-08-05 19:33:31 +0000

    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1171 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- rest-star/messaging/session-interface/examples/jms-to-rest/src/main/java/RestReceive.java
+++ rest-star/messaging/session-interface/examples/jms-to-rest/src/main/java/RestReceive.java
@@ -14,25 +14,27 @@ public class RestReceive
       ClientRequest request = new ClientRequest("http://localhost:9095/queues/jms.queue.orders");
       ClientResponse res = request.head();
       Link pullConsumers = res.getHeaderAsLink("msg-pull-consumers");
-      res = pullConsumers.request().post();
-      Link consumeNext = res.getHeaderAsLink("msg-consume-next");
+      res = pullConsumers.request().formParameter("autoAck", "false").post();
+      Link ackNext = res.getHeaderAsLink("msg-acknowledge-next");
       while (true)
       {
          System.out.println("Waiting...");
-         res = consumeNext.request()
+         res = ackNext.request()
                  .header("Accept-Wait", "10")
                  .header("Accept", "application/xml")
                  .post();
          if (res.getStatus() == 503)
          {
             System.out.println("Timeout...");
-            consumeNext = res.getHeaderAsLink("msg-consume-next");
+            ackNext = res.getHeaderAsLink("msg-acknowledge-next");
          }
          else if (res.getStatus() == 200)
          {
             Order order = (Order) res.getEntity(Order.class);
             System.out.println(order);
-            consumeNext = res.getHeaderAsLink("msg-consume-next");
+            Link ack = res.getHeaderAsLink("msg-acknowledgement");
+            res = ack.request().formParameter("acknowledge", "true").post();
+            ackNext = res.getHeaderAsLink("msg-acknowledge-next");
          }
          else
          {
--- rest-star/messaging/session-interface/examples/push/src/main/java/ReceiveShipping.java
+++ rest-star/messaging/session-interface/examples/push/src/main/java/ReceiveShipping.java
@@ -4,11 +4,9 @@ import org.hornetq.rest.Jms;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
-import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
-import javax.jms.ObjectMessage;
 import javax.jms.Session;
 
 /**
@@ -19,7 +17,6 @@ public class ReceiveShipping
 {
    public static void main(String[] args) throws Exception
    {
-      System.out.println("Receive Setup...");
       ConnectionFactory factory = JmsHelper.createConnectionFactory("hornetq-client.xml");
       Destination destination = (HornetQDestination) HornetQDestination.fromAddress("jms.queue.shipping");
 
@@ -28,29 +25,13 @@ public class ReceiveShipping
       {
          Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
          MessageConsumer consumer = session.createConsumer(destination);
-         consumer.setMessageListener(new MessageListener() {
+         consumer.setMessageListener(new MessageListener()
+         {
             @Override
             public void onMessage(Message message)
             {
-               System.out.println("Received Message: " );
-               Order order = null;
-               if (Jms.isHttpMessage(message))
-               {
-//                  String o = Jms.getEntity(message, String.class);
-//                  System.out.println(o);
-                  order = Jms.getEntity(message, Order.class);
-               }
-               else
-               {
-                  try
-                  {
-                     order = (Order)((ObjectMessage)message).getObject();
-                  }
-                  catch (JMSException e)
-                  {
-                     throw new RuntimeException(e);
-                  }
-               }
+               System.out.println("Received Message: ");
+               Order order = Jms.getEntity(message, Order.class);
                System.out.println(order);
             }
          }
