96aea01d30b2d0a7f197bc6b8b38f87791e88875
liweinan <liweinan@sourceforge.net>
2010-09-17 04:32:28 +0000

    RESTEASY-448, JBPAPP-2993
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@1232 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- examples/spring-hibernate-contacts/services/src/main/java/org/jboss/resteasy/examples/contacts/services/ContactServiceImpl.java
+++ examples/spring-hibernate-contacts/services/src/main/java/org/jboss/resteasy/examples/contacts/services/ContactServiceImpl.java
@@ -11,50 +11,43 @@ import org.jboss.resteasy.examples.contacts.core.Contact;
 import org.jboss.resteasy.examples.contacts.core.Contacts;
 import org.jboss.resteasy.examples.contacts.persistence.ContactDao;
 
-
 /**
- * @author <a href="mailto:obrand@yahoo.com">Olivier Brand</a>
- *         Jun 28, 2008
+ * @author <a href="mailto:obrand@yahoo.com">Olivier Brand</a> Jun 28, 2008
  */
 @Path("contactservice")
 @Transactional
-public class ContactServiceImpl implements ContactService
-{
-   // DAO class used for interacting with the database
-   private ContactDao contactDao;
+public class ContactServiceImpl implements ContactService {
+	// DAO class used for interacting with the database
+	private ContactDao contactDao;
 
-   public ContactServiceImpl()
-   {
-      System.out.println("In Constructor ContactServiceImpl");
-   }
+	public ContactServiceImpl() {
+		System.out.println("In Constructor ContactServiceImpl");
+	}
 
-   @GET
-   @Path("/contacts")
-   @Produces("application/xml")
-   public Contacts getAllContacts()
-   {
-      Contacts contacts = new Contacts();
-      contacts.setContacts(contactDao.findAllContacts());
-      return contacts;
-   }
+	@GET
+	@Path("/contacts")
+	@Produces("application/xml")
+	public Contacts getAllContacts() {
+		Contacts contacts = new Contacts();
+		contacts.setContacts(contactDao.findAllContacts());
+		return contacts;
+	}
 
-   @Path("/contacts/{id}")
-   @Produces("application/xml")
-   public Contact getContactById(@PathParam("id") Long id)
-   {
-      Contact contact = contactDao.findContactById(id);
+	@GET
+	@Path("/contacts/{id}")
+	@Produces("application/xml")
+	public Contact getContactById(@PathParam("id") Long id) {
+		Contact contact = contactDao.findContactById(id);
 
-      return contact;
-   }
+		return contact;
+	}
 
-   public ContactDao getContactDao()
-   {
-      return contactDao;
-   }
+	public ContactDao getContactDao() {
+		return contactDao;
+	}
 
-   public void setContactDao(ContactDao contactDao)
-   {
-      this.contactDao = contactDao;
-   }
+	public void setContactDao(ContactDao contactDao) {
+		this.contactDao = contactDao;
+	}
 
 }
