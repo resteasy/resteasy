200a05dc1b80a20261159985e6b642dc67c0b043
liweinan <liweinan@sourceforge.net>
2010-10-14 07:09:41 +0000

    RESTEASY-442,JBPAPP-4995 - TwitterClient needs to be updated to use OAuth
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1252 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/examples/api-clients/README.txt
+++ jaxrs/examples/api-clients/README.txt
@@ -12,9 +12,35 @@ In root directoy
 
 mvn clean compile
 
-Flickr Client:
+****Flickr Client:
 1. apply for an API key from Flickr - http://www.flickr.com/services/api/keys/apply 
 2. mvn exec:java -Dexec.mainClass="org.jboss.resteasy.examples.flickr.FlickrClient" -Dexec.args="<apiKey>"
 
-Twitter Client
-mvn exec:java -Dexec.mainClass="org.jboss.resteasy.examples.twitter.TwitterClient" -Dexec.args="<userId> <password>"
+****Twitter Client
+From August 31, 2010, Basic Auth has been deprecated by Twitter, and all applications must now use OAuth.
+So the Twitter Client now uses OAuth to do the authentication. In this example you can see how to inject 
+OAuth Authentication Header via RESTEasy Proxy Client.
+
+Be sure to run the example *in following step!*:
+1. request token from twitter:
+mvn exec:java -Dexec.mainClass="org.jboss.resteasy.examples.twitter.TwitterClient" -Dexec.args=request
+
+2. authorize token by twitter:
+mvn exec:java -Dexec.mainClass="org.jboss.resteasy.examples.twitter.TwitterClient" -Dexec.args=authorize
+In this step you will be provided a link for authentication. Paste it to web browser and grant access to this example.  
+
+3. request access token for our example:
+mvn exec:java -Dexec.mainClass="org.jboss.resteasy.examples.twitter.TwitterClient" -Dexec.args=access
+Now we can play with twitter: 
+
+4. tweet:
+mvn exec:java -Dexec.mainClass="org.jboss.resteasy.examples.twitter.TwitterClient" -Dexec.args=update
+
+5. query the status:
+mvn exec:java -Dexec.mainClass="org.jboss.resteasy.examples.twitter.TwitterClient" -Dexec.args=query
+
+If you meet a '401 Authorization Problem' in the last two steps, it's most probably your token has expired. 
+Redo the first three steps may solve the problem. Have fun!  
+
+
+ 
--- jaxrs/examples/api-clients/src/main/java/org/jboss/resteasy/examples/twitter/TwitterClient.java
+++ jaxrs/examples/api-clients/src/main/java/org/jboss/resteasy/examples/twitter/TwitterClient.java
@@ -1,20 +1,27 @@
 package org.jboss.resteasy.examples.twitter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.Credentials;
-import org.apache.http.auth.UsernamePasswordCredentials;
+import net.oauth.OAuthMessage;
+
+import org.apache.http.HttpHost;
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.jboss.resteasy.client.ClientExecutor;
 import org.jboss.resteasy.client.ProxyFactory;
@@ -22,83 +29,128 @@ import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
 import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
-public class TwitterClient
-{
-   static final String friendTimeline = "http://twitter.com/statuses/friends_timeline.xml";
+public class TwitterClient {
 
-   public static void main(String[] args) throws Exception
-   {
-      RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
-      final ClientExecutor clientExecutor = new ApacheHttpClient4Executor(createClient(args[0], args[1]));
-      TwitterResource twitter = ProxyFactory.create(TwitterResource.class,
-            "http://twitter.com", clientExecutor);
-      
-      System.out.println("===> first run");
-      twitter
-      .updateStatus("1st: I programmatically tweeted with the RESTEasy Client at "
-            + new Date());
-      printStatuses(twitter.getFriendsTimelines());
-      
-      System.out.println("===> second run");
-      twitter
-      .updateStatus("2nd: I programmatically tweeted with the RESTEasy Client at "
-            + new Date());
-      printStatuses(twitter.getFriendsTimelines());
-   }
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			System.err.println("TwitterClient [help | <operation>]");
+			return;
+		} else if ("help".equals(args[0])) {
+			System.err.println("\n*Please see the readme.txt*\n");
+			return;
+		}
 
-   public static interface TwitterResource
-   {
-      @Path("/statuses/friends_timeline.xml")
-      @GET
-      Statuses getFriendsTimelines();
+		new TwitterClient().execute(args[0]);
+	}
 
-      @Path("/statuses/update.xml")
-      @POST
-      Status updateStatus(@FormParam("status") String status);
-   }
+	private Properties props;
+	private File propFile;
+	private TwitterClientOAuthHelper oauthHelper;
 
-   private static void printStatuses(Statuses statuses)
-   {
-      for (Status status : statuses.status)
-         System.out.println(status);
-   }
+	private TwitterResource twitter;
 
-	private static HttpClient createClient(String userId, String password) {
+	public TwitterClient() {
+		try {
+			props = new Properties();
+			propFile = new File(TwitterClient.class.getResource(
+					"/twitter.properties").getFile());
+			props.load(new FileInputStream(propFile));
+			oauthHelper = new TwitterClientOAuthHelper(props, propFile);
 
-		Credentials credentials = new UsernamePasswordCredentials(userId,
-				password);
+			RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
+			final ClientExecutor clientExecutor = new ApacheHttpClient4Executor(
+					createClient());
+			twitter = ProxyFactory.create(TwitterResource.class,
+					"http://api.twitter.com/1", clientExecutor);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void execute(String operation) throws Exception {
+		if ("request".equals(operation)) {
+			oauthHelper.request();
+			System.out.println(propFile.getCanonicalPath() + " updated");
+		} else if ("authorize".equals(operation)) {
+			String url = oauthHelper.authorize();
+			System.out.println("Paste this in a browser:");
+			System.out.println(url);
+		} else if ("access".equals(operation)) {
+			oauthHelper.access();
+			System.out.println(propFile.getCanonicalPath() + " updated");
+		} else if ("query".equals(operation)) {
+			printStatuses(twitter
+					.getFriendsTimelines(oauthHelper
+							.prepareOAuthHeaderForAccesingResources(
+									props,
+									OAuthMessage.GET,
+									"http://api.twitter.com/1/statuses/friends_timeline.xml",
+									null)));
+		} else if ("update".equals(operation)) {
+			String status = "I programmatically tweeted with the RESTEasy Client at "
+					+ new Date();
+			twitter.updateStatus(oauthHelper
+					.prepareOAuthHeaderForAccesingResources(props,
+							OAuthMessage.POST,
+							"http://api.twitter.com/1/statuses/update.xml",
+							status), status);
+		}
+
+	}
+
+	public static interface TwitterResource {
+		@Path("/statuses/friends_timeline.xml")
+		@GET
+		Statuses getFriendsTimelines(@HeaderParam("Authorization") String header);
+
+		@Path("/statuses/update.xml")
+		@POST
+		Status updateStatus(@HeaderParam("Authorization") String header,
+				@FormParam("status") String status);
+	}
+
+	private static void printStatuses(Statuses statuses) {
+		for (Status status : statuses.status)
+			System.out.println(status);
+	}
+
+	private static HttpClient createClient() {
 		HttpClient httpClient = new DefaultHttpClient();
-		((DefaultHttpClient) httpClient).getCredentialsProvider()
-				.setCredentials(AuthScope.ANY, credentials);
-
+				
+		// Use Proxy to access if you are in one of the countries that cannot connects to twitter directly.   
+//		HttpHost proxy = new HttpHost("your_proxy_url", your_proxy_port);
+//		httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
+//				proxy);
+				
 		return httpClient;
 	}
 
-   @XmlRootElement
-   public static class Statuses
-   {
-      public List<Status> status;
-   }
+	@XmlRootElement
+	public static class Statuses {
+		public List<Status> status;
+	}
 
-   @XmlRootElement
-   public static class Status
-   {
-      public String text;
-      public User user;
+	@XmlRootElement
+	public static class Status {
+		public String text;
+		public User user;
 
-      @XmlElement(name = "created_at")
-      @XmlJavaTypeAdapter(value = DateAdapter.class)
-      public Date created;
+		@XmlElement(name = "created_at")
+		@XmlJavaTypeAdapter(value = DateAdapter.class)
+		public Date created;
 
-      public String toString()
-      {
-         return String.format("== %s: %s (%s)", user.name, text, created);
-      }
-   }
+		public String toString() {
+			return String.format("== %s: %s (%s)", user.name, text, created);
+		}
+	}
 
-   public static class User
-   {
-      public String name;
-   }
+	public static class User {
+		public String name;
+	}
 
 }
