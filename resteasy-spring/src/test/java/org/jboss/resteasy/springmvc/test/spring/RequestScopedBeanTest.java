559afe33d8577d0a6b03b55c92930694e4267eb2
sduskis <sduskis@sourceforge.net>
2011-01-14 14:54:33 +0000

    format fixing for RequesetScoedBeanTest and an additional "assertTrue"
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@1293 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- resteasy-spring/src/test/java/org/jboss/resteasy/springmvc/test/spring/RequestScopedBeanTest.java
+++ resteasy-spring/src/test/java/org/jboss/resteasy/springmvc/test/spring/RequestScopedBeanTest.java
@@ -11,6 +11,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
 
+import junit.framework.Assert;
+
 import org.jboss.resteasy.core.InjectorFactoryImpl;
 import org.jboss.resteasy.core.ValueInjector;
 import org.jboss.resteasy.spi.HttpRequest;
@@ -27,74 +29,97 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-public class RequestScopedBeanTest {
-	@Provider
-	public static class QualifierInjectorFactoryImpl extends InjectorFactoryImpl implements BeanFactoryAware {
-		BeanFactory beanFactory;
+public class RequestScopedBeanTest
+{
+   @Provider
+   public static class QualifierInjectorFactoryImpl extends InjectorFactoryImpl implements
+         BeanFactoryAware
+   {
+      BeanFactory beanFactory;
 
-		public QualifierInjectorFactoryImpl(ResteasyProviderFactory factory) {
-			super(factory);
-		}
+      public QualifierInjectorFactoryImpl(ResteasyProviderFactory factory)
+      {
+         super(factory);
+      }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		public ValueInjector createParameterExtractor(Class injectTargetClass, AccessibleObject injectTarget, Class type, Type genericType, Annotation[] annotations) {
-			final Qualifier qualifier = FindAnnotation.findAnnotation(annotations, Qualifier.class);
-			if (qualifier == null) {
-				return super.createParameterExtractor(injectTargetClass, injectTarget, type, genericType, annotations);
-			} else {
-				return new ValueInjector() {
-					public Object inject(HttpRequest request, HttpResponse response) {
-						return beanFactory.getBean(qualifier.value());
-					}
+      @SuppressWarnings("unchecked")
+      @Override
+      public ValueInjector createParameterExtractor(Class injectTargetClass,
+            AccessibleObject injectTarget, Class type, Type genericType, Annotation[] annotations)
+      {
+         final Qualifier qualifier = FindAnnotation.findAnnotation(annotations, Qualifier.class);
+         if (qualifier == null)
+         {
+            return super.createParameterExtractor(injectTargetClass, injectTarget, type,
+                  genericType, annotations);
+         }
+         else
+         {
+            return new ValueInjector()
+            {
+               public Object inject(HttpRequest request, HttpResponse response)
+               {
+                  return beanFactory.getBean(qualifier.value());
+               }
 
-					public Object inject() {
-						// do nothing.
-						return null;
-					}
-				};
-			}
-		}
+               public Object inject()
+               {
+                  // do nothing.
+                  return null;
+               }
+            };
+         }
+      }
 
-		@Override
-		public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-			this.beanFactory = beanFactory;
-		}
-	}
-	
-	public static class TestBean{
-		String configured;
-		
-		public void setConfigured(String configured) {
-			this.configured = configured;
-		}
-	}
+      @Override
+      public void setBeanFactory(BeanFactory beanFactory) throws BeansException
+      {
+         this.beanFactory = beanFactory;
+      }
+   }
 
-	@Path("/")
-	public static class TestBeanResource{
-		@GET
-		public String test(@Qualifier("testBean") TestBean bean){
-			return bean.configured;
-		}
-	}
-	
-	private TJWSEmbeddedSpringMVCServer server;
+   public static class TestBean
+   {
+      String configured;
 
-	@Before
-	public void startServer() {
-		server = new TJWSEmbeddedSpringMVCServer("classpath:spring-request-scope-test-server.xml", TestPortProvider.getPort());
-		server.start();
-	}
+      public void setConfigured(String configured)
+      {
+         this.configured = configured;
+      }
+   }
 
-	@After
-	public void stopServer() {
-		server.stop();
-	}
-	
-	@Test
-	public void testBean() throws Exception{
-		String result = createClientRequest("/").accept(MediaType.TEXT_PLAIN_TYPE).get(String.class).getEntity();
-		System.out.println(result);
-	}
-	
+   @Path("/")
+   public static class TestBeanResource
+   {
+      @GET
+      public String test(@Qualifier("testBean") TestBean bean)
+      {
+         return bean.configured;
+      }
+   }
+
+   private TJWSEmbeddedSpringMVCServer server;
+
+   @Before
+   public void startServer()
+   {
+      server = new TJWSEmbeddedSpringMVCServer("classpath:spring-request-scope-test-server.xml",
+            TestPortProvider.getPort());
+      server.start();
+   }
+
+   @After
+   public void stopServer()
+   {
+      server.stop();
+   }
+
+   @Test
+   public void testBean() throws Exception
+   {
+      String result = createClientRequest("/").accept(MediaType.TEXT_PLAIN_TYPE).get(String.class)
+            .getEntity();
+     Assert.assertEquals("configuredValue", result);
+   }
+
 }
