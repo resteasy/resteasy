0f700d24191400a53686f062d36b77119bc7522d
patriot1burke <patriot1burke@sourceforge.net>
2011-04-04 21:07:52 +0000

    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1328 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/providers/jaxb/src/test/java/org/jboss/resteasy/test/providers/jaxb/regression/TestBadContentType.java
+++ jaxrs/providers/jaxb/src/test/java/org/jboss/resteasy/test/providers/jaxb/regression/TestBadContentType.java
@@ -2,14 +2,23 @@ package org.jboss.resteasy.test.providers.jaxb.regression;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
 import org.jboss.resteasy.plugins.server.tjws.TJWSEmbeddedJaxrsServer;
+import org.jboss.resteasy.test.BaseResourceTest;
 import org.junit.After;
+
+import static org.jboss.resteasy.test.TestPortProvider.generateURL;
 import static org.junit.Assert.*;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -18,7 +27,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @author edelsonj
  */
 
-public class TestBadContentType
+public class TestBadContentType extends BaseResourceTest
 {
 
    @Path("/test")
@@ -31,6 +40,12 @@ public class TestBadContentType
          TestBean bean = new TestBean();
          bean.setName("myname");
          return bean;
+      }
+
+      @POST
+      public void post(TestBean bean)
+      {
+
       }
 
    }
@@ -53,29 +68,36 @@ public class TestBadContentType
    }
 
 
-   private TJWSEmbeddedJaxrsServer tjws;
-
    @Before
-   public void startServer()
+   public void setUp() throws Exception
    {
-      tjws = new TJWSEmbeddedJaxrsServer();
-      tjws.setPort(9090);
-      tjws.start();
-      tjws.getDeployment().getRegistry().addPerRequestResource(TestResource.class);
-
+      addPerRequestResource(TestResource.class);
    }
 
-   @After
-   public void stopServer()
+   /**
+    * RESTEASY-519
+    *
+    * @throws Exception
+    */
+   @Test
+   public void testBadRequest() throws Exception
    {
-      tjws.stop();
+      ClientRequest request = new ClientRequest(generateURL("/test"));
+      request.body("application/xml", "<junk");
+      ClientResponse res = request.post();
+      Assert.assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), res.getStatus());
    }
 
+   /**
+    *  * Test case for RESTEASY-169
+    *
+    * @throws Exception
+    */
    @Test
    public void testHtmlError() throws Exception
    {
       HttpClient hc = new HttpClient();
-      GetMethod gm = new GetMethod("http://localhost:9090/test");
+      GetMethod gm = new GetMethod(generateURL("/test"));
       gm.addRequestHeader("accept", "text/html");
       int status = hc.executeMethod(gm);
       assertEquals(500, status);
