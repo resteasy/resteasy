1e622df8c057bc6585ddad068616817772fd5bdf
liweinan <liweinan@sourceforge.net>
2011-06-11 03:34:53 +0000

    https://issues.jboss.org/browse/JBPAPP-6647
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1373 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/examples/spring-hibernate-contacts/services/src/main/java/org/jboss/resteasy/examples/contacts/services/ContactService.java
+++ jaxrs/examples/spring-hibernate-contacts/services/src/main/java/org/jboss/resteasy/examples/contacts/services/ContactService.java
@@ -8,9 +8,10 @@ import org.jboss.resteasy.examples.contacts.core.Contacts;
  * @author <a href="mailto:obrand@yahoo.com">Olivier Brand</a>
  *         Jun 28, 2008
  */
-public interface ContactService
-{
-   public Contact getContactById(Long id);
+public interface ContactService {
+	public Contact getContactById(Long id);
 
-   public Contacts getAllContacts();
+	public Contacts getAllContacts();
+
+	public Contacts getContactsOfContact(Long id);
 }
