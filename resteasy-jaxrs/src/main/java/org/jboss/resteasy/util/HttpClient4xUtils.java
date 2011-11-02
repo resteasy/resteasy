01de0207b304c4baaeda9bb258b096be2336def7
ron_sigal <ron_sigal@sourceforge.net>
2011-11-02 19:08:21 +0000

    RESTEASY-516: New utility class useful to unit tests that use HttpClient (4.x).
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@1474 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/util/HttpClient4xUtils.java
@@ -0,0 +1,44 @@
+package org.jboss.resteasy.util;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.jboss.resteasy.logging.Logger;
+
+/**
+ * HttpClient4xUtils provides utility methods useful for changes
+ * necessitated by switching from HttpClient 3.x to HttpClient 4.x.
+ * 
+ * @author <a href="mailto:ron.sigal@jboss.com">Ron Sigal</a>
+ * @version $Revision: 1 $
+ */
+public class HttpClient4xUtils
+{
+   private final static Logger log = Logger.getLogger(HttpClient4xUtils.class);
+
+   static public void consumeEntity(HttpResponse response)
+   {
+      try
+      {
+         EntityUtils.consume(response.getEntity());
+      } catch (IOException e)
+      {
+         log.info("unable to close entity stream", e);
+      }
+   }
+   
+   static public String updateQuery(String uriString, String query)
+   {
+      try
+      {
+         URI uri = new URI(uriString);
+         return new URI(uri.getScheme(), uri.getAuthority(), uri.getPath(), query, uri.getFragment()).toString();
+      } catch (URISyntaxException e)
+      {
+         throw new RuntimeException(e);
+      }
+   }
+}
