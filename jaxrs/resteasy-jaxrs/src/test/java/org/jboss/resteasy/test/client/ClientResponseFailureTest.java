f18aca43eaa23275938fa5780988ea00a5251c73
ron_sigal <ron_sigal@sourceforge.net>
2011-11-02 19:32:43 +0000

    RESTEASY-516: Now uses RESTEasy client framework.
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1498 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/client/ClientResponseFailureTest.java
+++ jaxrs/resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/client/ClientResponseFailureTest.java
@@ -1,22 +1,9 @@
 package org.jboss.resteasy.test.client;
 
-//import org.apache.commons.httpclient.HttpClient;
-
 import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
 import org.apache.commons.httpclient.params.HttpClientParams;
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.params.ConnManagerParams;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
 import org.jboss.resteasy.client.ClientResponseFailure;
 import org.jboss.resteasy.client.ProxyFactory;
-import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
 import org.jboss.resteasy.client.core.executors.ApacheHttpClientExecutor;
 import org.jboss.resteasy.spi.NoLogWebApplicationException;
 import org.jboss.resteasy.test.BaseResourceTest;
@@ -75,8 +62,7 @@ public class ClientResponseFailureTest extends BaseResourceTest
    @Test
    public void testStreamStillOpen() throws Exception
    {
-      final ApacheHttpClient4Executor executor = createClient();
-      final MyResource proxy = ProxyFactory.create(MyResource.class, "http://localhost:8081", executor);
+      final MyResource proxy = ProxyFactory.create(MyResource.class, "http://localhost:8081");
       boolean failed = true;
       try
       {
@@ -91,28 +77,6 @@ public class ClientResponseFailureTest extends BaseResourceTest
       }
 
       Assert.assertTrue(failed);
-   }
-
-
-   private ApacheHttpClient4Executor createClient()
-   {
-      HttpParams params = new BasicHttpParams();
-      ConnManagerParams.setMaxTotalConnections(params, 3);
-      ConnManagerParams.setTimeout(params, 1000);
-
-      // Create and initialize scheme registry
-      SchemeRegistry schemeRegistry = new SchemeRegistry();
-      schemeRegistry.register(
-              new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-
-      // Create an HttpClient with the ThreadSafeClientConnManager.
-      // This connection manager must be used if more than one thread will
-      // be using the HttpClient.
-      ClientConnectionManager cm = new ThreadSafeClientConnManager(params, schemeRegistry);
-      HttpClient httpClient = new DefaultHttpClient(cm, params);
-
-      final ApacheHttpClient4Executor executor = new ApacheHttpClient4Executor(httpClient);
-      return executor;
    }
 
    @Test
