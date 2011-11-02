c4ad2ff20d234d7fffea0fe549f3d90bb3b2596b
ron_sigal <ron_sigal@sourceforge.net>
2011-11-02 20:25:34 +0000

    RESTEASY-622: Added "Test" to end of name.
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1514 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/form/FormBodyTestResourceTest.java
+++ jaxrs/resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/form/FormBodyTestResourceTest.java
@@ -13,7 +13,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-public class FormBodyTestResource extends BaseResourceTest
+public class FormBodyTestResourceTest extends BaseResourceTest
 {
 
    public static class MyForm
