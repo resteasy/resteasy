7536db5fdaa5e46145a48543512809c2f1ac50d0
ron_sigal <ron_sigal@sourceforge.net>
2011-11-02 19:31:17 +0000

    RESTEASY-516: Now uses RESTEasy client framework.
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1496 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/providers/yaml/src/test/java/org/jboss/resteasy/test/providers/yaml/TestYamlProvider.java
+++ jaxrs/providers/yaml/src/test/java/org/jboss/resteasy/test/providers/yaml/TestYamlProvider.java
@@ -3,10 +3,8 @@ package org.jboss.resteasy.test.providers.yaml;
 import static org.jboss.resteasy.test.TestPortProvider.*;
 import junit.framework.Assert;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
 import org.jboss.resteasy.test.BaseResourceTest;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,70 +14,68 @@ public class TestYamlProvider extends BaseResourceTest {
 
     private static final String TEST_URI = generateURL("/yaml");
 
-    HttpClient client;
-
     @Before
     public void setUp() {
 
         addPerRequestResource(YamlResource.class);
-
-        client = new HttpClient();
 
     }
 
     @Test
     public void testGet() throws Exception {
 
-        GetMethod get = new GetMethod(TEST_URI);
+       ClientRequest request = new ClientRequest(TEST_URI);
+       
+       ClientResponse<String> response = request.get(String.class);
+       
+       Assert.assertEquals(200, response.getStatus());
+       
+       Assert.assertEquals("text/x-yaml", response.getHeaders().getFirst("Content-Type")); 
+       
+       String s = response.getEntity();       
+       
+       MyObject o1 = YamlResource.createMyObject();
 
-        MyObject o1 = YamlResource.createMyObject();
-
-        String s1 = new Yaml().dump(o1);
-
-        client.executeMethod(get);
-
-        Assert.assertEquals(200, get.getStatusCode());
-
-        Assert.assertEquals("text/x-yaml", get.getResponseHeader("Content-Type").getValue());
-
-        String s = get.getResponseBodyAsString();
-
-        Assert.assertEquals(s1, s);
+       String s1 = new Yaml().dump(o1);
+       
+       Assert.assertEquals(s1, s);
 
     }
 
     @Test
     public void testPost() throws Exception {
 
-        PostMethod post = new PostMethod(TEST_URI);
+       ClientRequest request = new ClientRequest(TEST_URI);
+       
+       MyObject o1 = YamlResource.createMyObject();
 
-        MyObject o1 = YamlResource.createMyObject();
-
-        String s1 = new Yaml().dump(o1);
-
-        post.setRequestEntity(new StringRequestEntity(s1, "text/x-yaml", "utf-8"));
-
-        client.executeMethod(post);
-
-        Assert.assertEquals(200, post.getStatusCode());
-
-        Assert.assertEquals("text/x-yaml", post.getResponseHeader("Content-Type").getValue());
-
-        Assert.assertEquals(s1, post.getResponseBodyAsString());
+       String s1 = new Yaml().dump(o1); new String();
+       
+       request.body("text/x-yaml", s1);
+       
+       ClientResponse<String> response = request.post(String.class);
+       
+       Assert.assertEquals(200, response.getStatus());
+       
+       Assert.assertEquals("text/x-yaml", response.getHeaders().getFirst("Content-Type"));       
+       
+       Assert.assertEquals(s1, response.getEntity());       
 
     }
 
     @Test
     public void testBadPost() throws Exception {
 
-        PostMethod post = new PostMethod(TEST_URI);
-
-        post.setRequestEntity(new StringRequestEntity("---! bad", "text/x-yaml", "utf-8"));
-
-        client.executeMethod(post);
-
-        Assert.assertEquals(500, post.getStatusCode());
-
+       ClientRequest request = new ClientRequest(TEST_URI);
+       
+       request.body("text/x-yaml", "---! bad");
+       
+       ClientResponse<?> response = request.post();
+       
+       Assert.assertEquals(500, response.getStatus());
+       
+       response.releaseConnection();
+       
     }
 
 }
