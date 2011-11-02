c810479297082227c670fff1fab3b001bb715958
ron_sigal <ron_sigal@sourceforge.net>
2011-11-02 20:32:51 +0000

    RESTEASY-516: Now uses HttpClient (4.x) and ApacheHttpClient4Executor.
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1525 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-links/src/test/java/org/jboss/resteasy/links/test/TestLinks.java
+++ jaxrs/resteasy-links/src/test/java/org/jboss/resteasy/links/test/TestLinks.java
@@ -4,10 +4,12 @@ import static org.jboss.resteasy.test.TestPortProvider.generateBaseUrl;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.httpclient.HttpClient;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.jboss.resteasy.client.ProxyFactory;
-import org.jboss.resteasy.client.core.executors.ApacheHttpClientExecutor;
+import org.jboss.resteasy.client.core.executors.ApacheHttpClient4Executor;
 import org.jboss.resteasy.core.Dispatcher;
 import org.jboss.resteasy.links.RESTServiceDiscovery;
 import org.jboss.resteasy.links.RESTServiceDiscovery.AtomLink;
@@ -59,8 +61,8 @@ public class TestLinks
 	public void before(){
 		POJOResourceFactory noDefaults = new POJOResourceFactory(resourceType);
 		dispatcher.getRegistry().addResourceFactory(noDefaults);
-		httpClient = new HttpClient();
-		ApacheHttpClientExecutor executor = new ApacheHttpClientExecutor(httpClient);
+		httpClient = new DefaultHttpClient();
+		ApacheHttpClient4Executor executor = new ApacheHttpClient4Executor(httpClient);
 		url = generateBaseUrl();
 		client = ProxyFactory.create(BookStoreService.class, url,
 					executor);
@@ -70,7 +72,7 @@ public class TestLinks
 	public void after(){
 		// TJWS does not support chunk encodings well so I need to kill kept
 		// alive connections
-		httpClient.getHttpConnectionManager().closeIdleConnections(0);
+		httpClient.getConnectionManager().closeIdleConnections(0, TimeUnit.MILLISECONDS);
 		dispatcher.getRegistry().removeRegistrations(resourceType);
 	}
 	
