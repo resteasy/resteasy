e7f20544cfe10c5f9bfc46b739ca4c852d8ec075
ron_sigal <ron_sigal@sourceforge.net>
2011-11-02 19:49:23 +0000

    RESTEASY-516: Now uses RESTEasy client framework.
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk@1507 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- jaxrs/resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/finegrain/resource/CacheControlAnnotationTest.java
+++ jaxrs/resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/finegrain/resource/CacheControlAnnotationTest.java
@@ -1,9 +1,9 @@
 package org.jboss.resteasy.test.finegrain.resource;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.GetMethod;
 import org.jboss.resteasy.annotations.cache.Cache;
 import org.jboss.resteasy.annotations.cache.NoCache;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
 import org.jboss.resteasy.test.BaseResourceTest;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,10 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.CacheControl;
-import java.io.IOException;
 
 import static org.jboss.resteasy.test.TestPortProvider.*;
-
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
@@ -53,48 +51,42 @@ public class CacheControlAnnotationTest extends BaseResourceTest
    @Test
    public void testResource() throws Exception
    {
-
-      HttpClient client = new HttpClient();
-      GetMethod method = createGetMethod("/maxage");
+      ClientRequest request = new ClientRequest(generateURL("/maxage"));
       try
       {
-         int status = client.executeMethod(method);
-         Assert.assertEquals(status, HttpServletResponse.SC_OK);
-         System.out.println("Cache-Control: " + method.getResponseHeader("cache-control").getValue());
-         CacheControl cc = CacheControl.valueOf(method.getResponseHeader("cache-control").getValue());
+         ClientResponse<?> response = request.get();
+         Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatus());
+         System.out.println("Cache-Control: " + response.getHeaders().getFirst("cache-control"));
+         CacheControl cc = CacheControl.valueOf(response.getHeaders().getFirst("cache-control"));
          Assert.assertFalse(cc.isPrivate());
          Assert.assertEquals(3600, cc.getMaxAge());
+         response.releaseConnection();
       }
-      catch (IOException e)
+      catch (Exception e)
       {
          throw new RuntimeException(e);
       }
-      method.releaseConnection();
-
    }
 
    @Test
    public void testResource2() throws Exception
    {
-
-      HttpClient client = new HttpClient();
-      GetMethod method = createGetMethod("/nocache");
+      ClientRequest request = new ClientRequest(generateURL("/nocache"));
       try
       {
-         int status = client.executeMethod(method);
-         Assert.assertEquals(status, HttpServletResponse.SC_OK);
-         String value = method.getResponseHeader("cache-control").getValue();
+         ClientResponse<?> response = request.get();
+         Assert.assertEquals(HttpServletResponse.SC_OK, response.getStatus());
+         String value = response.getHeaders().getFirst("cache-control");
          Assert.assertEquals("no-cache", value);
          System.out.println("Cache-Control: " + value);
          CacheControl cc = CacheControl.valueOf(value);
          Assert.assertTrue(cc.isNoCache());
+         response.releaseConnection();
       }
-      catch (IOException e)
+      catch (Exception e)
       {
          throw new RuntimeException(e);
       }
-      method.releaseConnection();
-
    }
 
 }
