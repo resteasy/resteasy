3c1d6b24d41888baff4681a1585df59905142e3c
ron_sigal <ron_sigal@sourceforge.net>
2011-11-03 03:14:49 +0000

    RESTEASY-602: Removed unused import.
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@1541 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

--- resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/tjws/ChunkedEmptyEntityTest.java
+++ resteasy-jaxrs/src/test/java/org/jboss/resteasy/test/tjws/ChunkedEmptyEntityTest.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.net.URI;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HEAD;
