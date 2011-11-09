2bc68fa4834c523b1d97d015ad602cea516cab7b
patriot1burke <patriot1burke@sourceforge.net>
2011-11-09 14:22:49 +0000

    590
    git-svn-id: file:///Users/billburke/jboss/resteasy/resteasy-git/svn-server-sync/resteasy/trunk/jaxrs@1565 2b1ed4c4-5db3-0410-90e4-80a7a6204c25

---
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/util/WeightedLanguage.java
@@ -0,0 +1,117 @@
+package org.jboss.resteasy.util;
+
+import org.jboss.resteasy.spi.LoggableFailure;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public class WeightedLanguage implements Comparable<WeightedLanguage>
+{
+   private float weight = 1.0f;
+   private String language;
+   private Locale locale;
+   private Map<String, String> params;
+
+
+   private WeightedLanguage(String lang, Map<String, String> parameters)
+   {
+      this.language = lang;
+      this.params = parameters;
+      this.locale = LocaleHelper.extractLocale(lang);
+      if (params != null)
+      {
+         String q = params.get("q");
+         if (q != null)
+         {
+            weight = getQWithParamInfo(this, q);
+         }
+      }
+   }
+
+   public float getWeight()
+   {
+      return weight;
+   }
+
+   public Locale getLocale()
+   {
+      return locale;
+   }
+
+   public int compareTo(WeightedLanguage o)
+   {
+      WeightedLanguage type2 = this;
+      WeightedLanguage type1 = o;
+
+      if (type1.weight < type2.weight) return -1;
+      if (type1.weight > type2.weight) return 1;
+
+      return 0;
+   }
+
+   public String toString()
+   {
+      String rtn = language;
+      if (params == null || params.size() == 0) return rtn;
+      for (String name : params.keySet())
+      {
+         String val = params.get(name);
+         rtn += ";" + name + "=\"" + val + "\"";
+      }
+      return rtn;
+   }
+
+
+   public static WeightedLanguage parse(String lang)
+   {
+      String params = null;
+      int idx = lang.indexOf(";");
+      if (idx > -1)
+      {
+         params = lang.substring(idx + 1).trim();
+         lang = lang.substring(0, idx);
+      }
+      HashMap<String, String> typeParams = new HashMap<String, String>();
+      if (params != null && !params.equals(""))
+      {
+         int start = 0;
+         while (start < params.length())
+         {
+            start = HeaderParameterParser.setParam(typeParams, params, start);
+         }
+      }
+      return new WeightedLanguage(lang, typeParams);
+   }
+
+
+   private static float getQWithParamInfo(WeightedLanguage lang, String val)
+   {
+      try
+      {
+         if (val != null)
+         {
+            float rtn = Float.valueOf(val);
+            if (rtn > 1.0F)
+               throw new LoggableFailure("Accept-Language q value cannot be greater than 1.0 " + lang.toString(), HttpResponseCodes.SC_BAD_REQUEST);
+            return rtn;
+         }
+      }
+      catch (NumberFormatException e)
+      {
+         throw new LoggableFailure("MediaType q parameter must be a float: " + lang, HttpResponseCodes.SC_BAD_REQUEST);
+      }
+      return 1.0f;
+   }
+
+   @Override
+   public boolean equals(Object obj)
+   {
+      return super.equals(obj);
+   }
+
+}
