fb294de6a086221fda0320c7d6fb11f5384b30aa
Bill Burke <bburke@redhat.com>
2012-01-04 17:39:07 -0500

    638

--- jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/MediaTypeMap.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/MediaTypeMap.java
@@ -216,11 +216,15 @@ public class MediaTypeMap<T>
       // Although, these reference should get cleared up with any add() invocation
       private WeakReference<Class> clazz;
       private MediaType mediaType;
+      private final int hash;
 
       private CachedMediaTypeAndClass(Class clazz, MediaType mediaType)
       {
          this.clazz = new WeakReference(clazz);
          this.mediaType = mediaType;
+         int result = getClazz().hashCode();
+         result = 31 * result + (mediaType.getType() != null ? mediaType.getType().hashCode() : 0) +  (mediaType.getSubtype() != null ? mediaType.getSubtype().hashCode() : 0);
+         hash = result;
       }
 
       private Class getClazz()
@@ -240,7 +244,18 @@ public class MediaTypeMap<T>
          if (getClazz() == null || that.getClazz() == null) return false;
 
          if (!getClazz().equals(that.getClazz())) return false;
-         if (!mediaType.equals(that.mediaType)) return false;
+
+         if (mediaType.getType() != null)
+         {
+            if (!mediaType.getType().equals(that.mediaType.getType())) return false;
+         }
+         else if ((mediaType.getType() != that.mediaType.getType())) return false;
+
+         if (mediaType.getSubtype() != null)
+         {
+            if (!mediaType.getSubtype().equals(that.mediaType.getSubtype())) return false;
+         }
+         else if ((mediaType.getSubtype() != that.mediaType.getSubtype())) return false;
 
          return true;
       }
@@ -248,9 +263,7 @@ public class MediaTypeMap<T>
       @Override
       public int hashCode()
       {
-         int result = getClazz().hashCode();
-         result = 31 * result + mediaType.hashCode();
-         return result;
+         return hash;
       }
    }
 
@@ -318,7 +331,11 @@ public class MediaTypeMap<T>
       return convert(matches);
    }
 
-   private static final boolean useCache = true;
+   /**
+    * By default, MediaTypeMap will cache possible MediaType/Class matches.
+    *
+    */
+   public static final boolean useCache = true;
 
    public List<T> getPossible(MediaType accept, Class type)
    {
