329b8eaad7c0dcd6a6c0df1f92863885d0629204
Ron Sigal <rsigal@redhat.com>
2012-04-14 21:46:51 -0400

    RESTEASY-694: Adding validation per JAX-RS spec (left out SerializableProvider).

---
+++ jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/providers/SerializableProvider.java
@@ -0,0 +1,89 @@
+package org.jboss.resteasy.plugins.providers;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+
+/**
+ * @author <a href="ron.sigal@jboss.com">Ron Sigal</a>
+ * @version $Revision: 1.1 $
+ *
+ * Created Mar 23, 2012
+ */
+@Provider
+@Produces("application/x-java-serialized-object")
+@Consumes("application/x-java-serialized-object")
+public class SerializableProvider implements MessageBodyReader<Serializable>, MessageBodyWriter<Serializable>
+{
+   public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
+   {
+      return Serializable.class.isAssignableFrom(type) && "application/x-java-serialized-object".equals(mediaType.toString());
+   }
+
+   public long getSize(Serializable t, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
+   {
+      try
+      {
+         ByteArrayOutputStream baos = new ByteArrayOutputStream();
+         ObjectOutputStream oos = new ObjectOutputStream(baos);
+         oos.writeObject(t);
+         oos.close();
+         return baos.toByteArray().length;
+      }
+      catch (IOException e)
+      {
+         return -1;
+      }
+   }
+
+   public void writeTo(Serializable t, Class<?> type, Type genericType,
+         Annotation[] annotations, MediaType mediaType,
+         MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
+         throws IOException, WebApplicationException
+   {
+      BufferedOutputStream bos = new BufferedOutputStream(entityStream);
+      ObjectOutputStream oos = new ObjectOutputStream(bos);
+      oos.writeObject(t);
+      oos.close();
+   }
+
+   public boolean isReadable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
+   {
+      return Serializable.class.isAssignableFrom(type) && "application/x-java-serialized-object".equals(mediaType.toString());
+   }
+
+   public Serializable readFrom(Class<Serializable> type, Type genericType,
+         Annotation[] annotations, MediaType mediaType,
+         MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
+         throws IOException, WebApplicationException
+   {
+      BufferedInputStream bis = new BufferedInputStream(entityStream);
+      ObjectInputStream ois = new ObjectInputStream(bis);
+      try
+      {
+         return Serializable.class.cast(ois.readObject());
+      }
+      catch (ClassNotFoundException e)
+      {
+         throw new WebApplicationException(e);
+      }
+   }
+}
