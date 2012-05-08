a35f26b248bdf24e97682874caa7f022f39cd364
norman <norman@apache.org>
2012-05-08 10:50:35 +0200

    Move Encoding of NettyHttpResponse to extra class

--- server-adapters/resteasy-netty/src/main/java/org/jboss/resteasy/plugins/server/netty/RequestHandler.java
+++ server-adapters/resteasy-netty/src/main/java/org/jboss/resteasy/plugins/server/netty/RequestHandler.java
@@ -1,36 +1,21 @@
 package org.jboss.resteasy.plugins.server.netty;
 
-import org.jboss.netty.buffer.ChannelBuffer;
-import org.jboss.netty.buffer.ChannelBuffers;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.is100ContinueExpected;
+import static org.jboss.netty.handler.codec.http.HttpHeaders.isKeepAlive;
+import static org.jboss.netty.handler.codec.http.HttpResponseStatus.CONTINUE;
+import static org.jboss.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
 import org.jboss.netty.channel.ChannelFuture;
 import org.jboss.netty.channel.ChannelFutureListener;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
-import org.jboss.netty.handler.codec.http.Cookie;
-import org.jboss.netty.handler.codec.http.CookieDecoder;
-import org.jboss.netty.handler.codec.http.CookieEncoder;
 import org.jboss.netty.handler.codec.http.DefaultHttpResponse;
 import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpResponse;
-import org.jboss.netty.handler.codec.http.HttpResponseStatus;
-import org.jboss.netty.handler.codec.http.QueryStringDecoder;
-import org.jboss.netty.util.CharsetUtil;
 import org.jboss.resteasy.logging.Logger;
 import org.jboss.resteasy.spi.Failure;
-
-import javax.ws.rs.ext.RuntimeDelegate;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import static org.jboss.netty.handler.codec.http.HttpHeaders.Names.*;
-import static org.jboss.netty.handler.codec.http.HttpHeaders.*;
-import static org.jboss.netty.handler.codec.http.HttpResponseStatus.*;
-import static org.jboss.netty.handler.codec.http.HttpVersion.*;
 
 /**
  * @author <a href="http://www.jboss.org/netty/">The Netty Project</a>
@@ -74,69 +59,13 @@ public class RequestHandler extends SimpleChannelUpstreamHandler
          response.setStatus(500);
          logger.error("Unexpected", ex);
       }
-
-      writeResponse(request, response, e);
-   }
-
-   private void writeResponse(HttpRequest request, NettyHttpResponse nettyResponse, MessageEvent e)
-   {
-      // Decide whether to close the connection or not.
-      boolean keepAlive = isKeepAlive(request);
-
-      // Build the response object.
-      HttpResponseStatus status = null;
-      if (nettyResponse.getMessage() != null)
-      {
-         status = new HttpResponseStatus(nettyResponse.getStatus(), nettyResponse.getMessage());
-      }
-      else
-      {
-          status = HttpResponseStatus.valueOf(nettyResponse.getStatus());
-      }
-      HttpResponse response = new DefaultHttpResponse(HTTP_1_1, status);
-
-      for (Map.Entry<String, List<Object>> entry : nettyResponse.getOutputHeaders().entrySet())
-      {
-         String key = entry.getKey();
-         for (Object value : entry.getValue())
-         {
-            RuntimeDelegate.HeaderDelegate delegate = dispatcher.providerFactory.createHeaderDelegate(value.getClass());
-            if (delegate != null)
-            {
-               //System.out.println("addResponseHeader: " + key + " " + delegate.toString(value));
-               response.addHeader(key, delegate.toString(value));
-            }
-            else
-            {
-               //System.out.println("addResponseHeader: " + key + " " + value.toString());
-               response.setHeader(key, value.toString());
-            }
-         }
-      }
-
-      try
-      {
-         nettyResponse.getOutputStream().flush();
-      }
-      catch (IOException e1)
-      {
-         throw new RuntimeException(e1);
-      }
-      response.setContent(nettyResponse.getBuffer());
-      response.setHeader(CONTENT_LENGTH, response.getContent().readableBytes());
-
-      // not sure why this is done
-      /*
-      if (keepAlive)
-      {
-         // Add 'Content-Length' header only for a keep-alive connection.
-         response.setHeader(CONTENT_LENGTH, response.getContent().readableBytes());
-      }
-      */
+      
 
       // Write the response.
       ChannelFuture future = e.getChannel().write(response);
 
+      // Decide whether to close the connection or not.
+      boolean keepAlive = isKeepAlive(request);
       // Close the non-keep-alive connection after the write operation is done.
       if (!keepAlive)
       {
