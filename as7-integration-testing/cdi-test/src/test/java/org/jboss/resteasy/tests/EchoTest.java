44ee47aa7d698d6c2b178113e08acd3976af4992
Bill Burke <bburke@redhat.com>
2012-05-23 16:07:15 -0400

    docs

--- as7-integration-testing/cdi-test/src/test/java/org/jboss/resteasy/tests/EchoTest.java
+++ as7-integration-testing/cdi-test/src/test/java/org/jboss/resteasy/tests/EchoTest.java
@@ -19,5 +19,36 @@ public class EchoTest
       System.out.println("Format: " + format);
       Assert.assertEquals("foo 1.1", format);
    }
+
+   @Test
+   public void testHeaders() throws Exception
+   {
+      {
+      ClientRequest request = new ClientRequest("http://localhost:8080/cdi-test/jaxrs/caching/conditional");
+      String format = request.getTarget(String.class);
+      System.out.println("Format: " + format);
+      }
+      {
+      ClientRequest request = new ClientRequest("http://localhost:8080/cdi-test/jaxrs/caching/conditional");
+      request.accept("application/xml")
+              .header("If-Modified-Since", "Sun, 20 May 2012 17:08:26 GMT")
+              .header("foo", "bar");
+      String format = request.getTarget(String.class);
+      System.out.println("Format: " + format);
+      }
+      {
+      ClientRequest request = new ClientRequest("http://localhost:8080/cdi-test/jaxrs/caching/conditional");
+      String format = request.getTarget(String.class);
+      System.out.println("Format: " + format);
+      }
+      {
+      ClientRequest request = new ClientRequest("http://localhost:8080/cdi-test/jaxrs/caching/conditional");
+      request.accept("application/xml")
+              .header("If-Modified-Since", "Sun, 20 May 2012 17:08:26 GMT")
+              .header("foo", "bar");
+      String format = request.getTarget(String.class);
+      System.out.println("Format: " + format);
+      }
+   }
 }
 
