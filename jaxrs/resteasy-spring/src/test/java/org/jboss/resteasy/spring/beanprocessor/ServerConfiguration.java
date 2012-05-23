4ae2bfbfc5de7f702bb17c05b0d6c41a7c44fc23
Bill Burke <bburke@redhat.com>
2012-05-23 16:38:43 -0400

    resteasy-679

--- jaxrs/resteasy-spring/src/test/java/org/jboss/resteasy/spring/beanprocessor/ServerConfiguration.java
+++ jaxrs/resteasy-spring/src/test/java/org/jboss/resteasy/spring/beanprocessor/ServerConfiguration.java
@@ -1,6 +1,7 @@
 package org.jboss.resteasy.spring.beanprocessor;
 
 import org.jboss.resteasy.plugins.spring.EmbeddedContainerBean;
+
 import org.jboss.resteasy.plugins.spring.SpringBeanProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
