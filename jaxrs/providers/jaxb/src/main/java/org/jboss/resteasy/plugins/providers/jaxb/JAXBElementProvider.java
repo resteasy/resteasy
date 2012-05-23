c8791ffe3939af75778e13c10c7773720c503b15
Bill Burke <bburke@redhat.com>
2012-05-23 17:35:04 -0400

    resteasy-711

--- jaxrs/providers/jaxb/src/main/java/org/jboss/resteasy/plugins/providers/jaxb/JAXBElementProvider.java
+++ jaxrs/providers/jaxb/src/main/java/org/jboss/resteasy/plugins/providers/jaxb/JAXBElementProvider.java
@@ -79,6 +79,7 @@ public class JAXBElementProvider extends AbstractJAXBProvider<JAXBElement<?>>
       try
       {
          Unmarshaller unmarshaller = jaxb.createUnmarshaller();
+         unmarshaller = decorateUnmarshaller(type, annotations, mediaType, unmarshaller);
          if (suppressExpandEntityExpansion())
          {
             unmarshaller = new ExternalEntityUnmarshaller(unmarshaller);
