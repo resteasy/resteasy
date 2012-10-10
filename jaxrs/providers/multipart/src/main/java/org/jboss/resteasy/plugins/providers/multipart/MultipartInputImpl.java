9fc0f5cbaf6c6cf5b4e8deaab5928650659477fb
Bill Burke <bburke@redhat.com>
2012-10-09 21:21:56 -0400

    multipart - 1

--- jaxrs/providers/multipart/src/main/java/org/jboss/resteasy/plugins/providers/multipart/MultipartInputImpl.java
+++ jaxrs/providers/multipart/src/main/java/org/jboss/resteasy/plugins/providers/multipart/MultipartInputImpl.java
@@ -1,14 +1,26 @@
 package org.jboss.resteasy.plugins.providers.multipart;
 
+import org.apache.james.mime4j.MimeException;
+import org.apache.james.mime4j.MimeIOException;
+import org.apache.james.mime4j.codec.Base64InputStream;
+import org.apache.james.mime4j.codec.QuotedPrintableInputStream;
+import org.apache.james.mime4j.descriptor.BodyDescriptor;
 import org.apache.james.mime4j.field.ContentTypeField;
 import org.apache.james.mime4j.message.BinaryBody;
 import org.apache.james.mime4j.message.Body;
+import org.apache.james.mime4j.message.BodyFactory;
 import org.apache.james.mime4j.message.BodyPart;
+import org.apache.james.mime4j.message.Entity;
 import org.apache.james.mime4j.message.Message;
+import org.apache.james.mime4j.message.MessageBuilder;
 import org.apache.james.mime4j.message.Multipart;
 import org.apache.james.mime4j.message.TextBody;
 import org.apache.james.mime4j.parser.Field;
+import org.apache.james.mime4j.parser.MimeStreamParser;
+import org.apache.james.mime4j.storage.DefaultStorageProvider;
+import org.apache.james.mime4j.storage.StorageProvider;
 import org.apache.james.mime4j.util.CharsetUtil;
+import org.apache.james.mime4j.util.MimeUtil;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.jboss.resteasy.util.CaseInsensitiveMap;
@@ -28,12 +40,15 @@ import java.io.SequenceInputStream;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
@@ -48,6 +63,98 @@ public class MultipartInputImpl implements MultipartInput
    protected static final Annotation[] empty = {};
    protected MediaType defaultPartContentType = MultipartConstants.TEXT_PLAIN_WITH_CHARSET_US_ASCII_TYPE;
    protected String defaultPartCharset = "us-ascii";
+
+   private static class BinaryOnlyMessageBuilder extends MessageBuilder
+   {
+      private Method expectMethod;
+      private java.lang.reflect.Field bodyFactoryField;
+      private java.lang.reflect.Field stackField;
+
+      private void init()
+      {
+         try
+         {
+            expectMethod = MessageBuilder.class.getDeclaredMethod("expect", Class.class);
+            expectMethod.setAccessible(true);
+            bodyFactoryField = MessageBuilder.class.getDeclaredField("bodyFactory");
+            bodyFactoryField.setAccessible(true);
+            stackField = MessageBuilder.class.getDeclaredField("stack");
+            stackField.setAccessible(true);
+         }
+         catch (Exception e)
+         {
+            throw new RuntimeException(e);
+         }
+      }
+
+      private BinaryOnlyMessageBuilder(Entity entity)
+      {
+         super(entity);
+         init();
+      }
+
+      private BinaryOnlyMessageBuilder(Entity entity, StorageProvider storageProvider)
+      {
+         super(entity, storageProvider);
+         init();
+      }
+
+      @Override
+      public void body(BodyDescriptor bd, InputStream is) throws MimeException, IOException
+      {
+         try
+         {
+            expectMethod.invoke(this, Entity.class);
+         }
+         catch (Exception e)
+         {
+            throw new RuntimeException(e);
+         }
+
+         final String enc = bd.getTransferEncoding();
+
+         final Body body;
+
+         final InputStream decodedStream;
+         if (MimeUtil.ENC_BASE64.equals(enc)) {
+            decodedStream = new Base64InputStream(is);
+         } else if (MimeUtil.ENC_QUOTED_PRINTABLE.equals(enc)) {
+            decodedStream = new QuotedPrintableInputStream(is);
+         } else {
+            decodedStream = is;
+         }
+
+         try
+         {
+            BodyFactory factory = (BodyFactory)bodyFactoryField.get(this);
+            body = factory.binaryBody(decodedStream);
+
+            Stack<Object> st = (Stack<Object>)stackField.get(this);
+            Entity entity = ((Entity) st.peek());
+            entity.setBody(body);
+         }
+         catch (Exception e)
+         {
+            throw new RuntimeException(e);
+         }
+
+      }
+   }
+
+   private static class BinaryMessage extends Message
+   {
+      private BinaryMessage(InputStream is) throws IOException, MimeIOException
+      {
+         try {
+            MimeStreamParser parser = new MimeStreamParser(null);
+            parser.setContentHandler(new BinaryOnlyMessageBuilder(this, DefaultStorageProvider.getInstance()));
+            parser.parse(is);
+         } catch (MimeException e) {
+            throw new MimeIOException(e);
+         }
+
+      }
+   }
 
    public MultipartInputImpl(MediaType contentType, Providers workers)
    {
@@ -89,7 +196,7 @@ public class MultipartInputImpl implements MultipartInput
 
    public void parse(InputStream is) throws IOException
    {
-      mimeMessage = new Message(addHeaderToHeadlessStream(is));
+      mimeMessage = new BinaryMessage(addHeaderToHeadlessStream(is));
       extractParts();
    }
 
@@ -219,85 +326,6 @@ public class MultipartInputImpl implements MultipartInput
       public boolean isContentTypeFromMessage()
       {
          return contentTypeFromMessage;
-      }
-   }
-
-   private static class ReaderBackedInputStream extends InputStream
-   {
-      private final InputStreamReader reader;
-      private byte[] bytes = new byte[0];
-      private int bpos = 0;
-      private boolean eof;
-
-      private ReaderBackedInputStream(InputStreamReader reader) throws IOException
-      {
-         this.reader = reader;
-         String charset = reader.getEncoding();
-         System.out.println("encoding: " + charset);
-      }
-
-      @Override
-      public int read() throws IOException
-      {
-         if (eof)
-         {
-            return -1;
-         }
-         
-         int c;
-         if (bpos >= bytes.length)
-         {
-            bpos = 0;
-            int value = reader.read();
-            if (value == -1)
-            {
-               eof = true;
-               return -1;
-            }
-            if (value > 0x00FFFFFF) // 4 bytes
-            {
-               bytes =new byte[] {
-                       (byte)value,
-                       (byte)(value >>> 8),
-                       (byte)(value >>> 16),
-                       (byte)(value >>> 24),
-               };
-               return bytes[bpos++];
-
-            }
-            else if (value > 0x0000FFFF) // 3 bytes
-            {
-               bytes =new byte[] {
-                       (byte)value,
-                       (byte)(value >>> 8),
-                       (byte)(value >>> 16),
-               };
-               return bytes[bpos++];
-            }
-            else if (value > 0x000000FF) // 2 bytes
-            {
-               bytes =new byte[] {
-                       (byte)value,
-                       (byte)(value >>> 8),
-               };
-               return bytes[bpos++];
-            }
-            else
-            {
-               return value;
-            }
-         }
-         else
-         {
-            return bytes[bpos++];
-         }
-      }
-
-      @Override
-      public void close() throws IOException
-      {
-         reader.close();
-         super.close();
       }
    }
 
