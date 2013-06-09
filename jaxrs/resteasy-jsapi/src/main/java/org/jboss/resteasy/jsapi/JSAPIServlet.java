522321bc09a3642f3ec7816d0e36f1666c2e3fe2
Bill Burke <bburke@redhat.com>
2013-06-09 09:48:56 -0400

    JSAPI fixes

--- jaxrs/resteasy-jsapi/src/main/java/org/jboss/resteasy/jsapi/JSAPIServlet.java
+++ jaxrs/resteasy-jsapi/src/main/java/org/jboss/resteasy/jsapi/JSAPIServlet.java
@@ -11,9 +11,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.jboss.resteasy.core.ResourceMethodRegistry;
 import org.jboss.resteasy.logging.Logger;
+import org.jboss.resteasy.plugins.server.servlet.ResteasyContextParameters;
 import org.jboss.resteasy.spi.Registry;
+import org.jboss.resteasy.spi.ResteasyDeployment;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 
 
@@ -23,59 +29,69 @@ import org.jboss.resteasy.spi.ResteasyProviderFactory;
 public class JSAPIServlet extends HttpServlet
 {
 
-	private static final long serialVersionUID = -1985015444704126795L;
+   private static final long serialVersionUID = -1985015444704126795L;
 
-	private final static Logger logger = Logger.getLogger(JSAPIServlet.class);
-	private ServiceRegistry service;
+   private final static Logger logger = Logger.getLogger(JSAPIServlet.class);
+   private Map<String, ServiceRegistry> services;
 
-	private JSAPIWriter apiWriter;
+   private JSAPIWriter apiWriter = new JSAPIWriter();
 
-	@Override
-	public void init(ServletConfig config) throws ServletException
-	{
-		super.init(config);
-		if (logger.isDebugEnabled())
-			logger.info("Loading JSAPI Servlet");
+   @Override
+   public void init(ServletConfig config) throws ServletException
+   {
+      super.init(config);
+      if (logger.isDebugEnabled())
+         logger.info("Loading JSAPI Servlet");
 
-		scanResources();
-		
-		if (logger.isDebugEnabled())
-			logger.debug("JSAPIServlet loaded");
+      scanResources();
 
-		// make it possible to get to us for rescanning
-		ServletContext servletContext = config .getServletContext();
-		servletContext.setAttribute(getClass().getName(), this);
-	}
+      if (logger.isDebugEnabled())
+         logger.debug("JSAPIServlet loaded");
 
-	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp)
-	throws ServletException, IOException
-	{
-		String pathInfo = req.getPathInfo();
-		String uri = req.getRequestURL().toString();
-		uri = uri.substring(0, uri.length() - req.getServletPath().length());
-		if (logger.isDebugEnabled())
-		{
-			logger.debug("Serving " + pathInfo);
-			logger.debug("Query " + req.getQueryString());
-		}
+      // make it possible to get to us for rescanning
+      ServletContext servletContext = config.getServletContext();
+      servletContext.setAttribute(getClass().getName(), this);
+   }
 
-        this.apiWriter.writeJavaScript(uri, req, resp, service);
-	}
+   @Override
+   protected void service(HttpServletRequest req, HttpServletResponse resp)
+           throws ServletException, IOException
+   {
+      String pathInfo = req.getPathInfo();
+      String uri = req.getRequestURL().toString();
+      uri = uri.substring(0, uri.length() - req.getServletPath().length());
+      if (logger.isDebugEnabled())
+      {
+         logger.debug("Serving " + pathInfo);
+         logger.debug("Query " + req.getQueryString());
+      }
+      if (this.services == null) scanResources();
+      if (this.services == null)
+      {
+         resp.sendError(503, "There are no Resteasy deployments initialized yet to scan from.  Either set the load-on-startup on each Resteasy servlet, or, if in an EE environment like JBoss or Wildfly, you'll have to do an invocation on each of your REST services to get the servlet loaded.");
+      }
+      this.apiWriter.writeJavaScript(uri, req, resp, services);
+   }
 
-	public void scanResources(){
+   public void scanResources()
+   {
 
-		ServletConfig config = getServletConfig();
-		ServletContext servletContext = config .getServletContext();
-		ResourceMethodRegistry registry = (ResourceMethodRegistry) servletContext
-		.getAttribute(Registry.class.getName());
-		ResteasyProviderFactory providerFactory = 
-			(ResteasyProviderFactory) servletContext.getAttribute(ResteasyProviderFactory.class.getName());
+      ServletConfig config = getServletConfig();
+      ServletContext servletContext = config.getServletContext();
+      Map<String, ResteasyDeployment> deployments = (Map<String, ResteasyDeployment>) servletContext.getAttribute(ResteasyContextParameters.RESTEASY_DEPLOYMENTS);
 
-		String restPath = servletContext
-		.getInitParameter("resteasy.servlet.mapping.prefix");
-
-		service = new ServiceRegistry(null, registry, providerFactory, null);
-		apiWriter = new JSAPIWriter(restPath);
-	}
+      if (deployments == null) return;
+      synchronized (this)
+      {
+         services = new HashMap<String, ServiceRegistry>();
+         for (Map.Entry<String, ResteasyDeployment> entry : deployments.entrySet())
+         {
+            ResourceMethodRegistry registry = (ResourceMethodRegistry) entry.getValue().getRegistry();
+            ResteasyProviderFactory providerFactory =
+                    (ResteasyProviderFactory) entry.getValue().getProviderFactory();
+            ServiceRegistry service = new ServiceRegistry(null, registry, providerFactory, null);
+            services.put(entry.getKey(), service);
+         }
+      }
+   }
 }
--- jaxrs/resteasy-jsapi/src/main/java/org/jboss/resteasy/jsapi/JSAPIWriter.java
+++ jaxrs/resteasy-jsapi/src/main/java/org/jboss/resteasy/jsapi/JSAPIWriter.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.zip.GZIPOutputStream;
@@ -19,43 +20,41 @@ import java.util.zip.GZIPOutputStream;
 public class JSAPIWriter
 {
 
-	private static final long serialVersionUID = -1985015444704126795L;
+   private static final long serialVersionUID = -1985015444704126795L;
 
-	private final static Logger logger = Logger.getLogger(JSAPIWriter.class);
-	private String restPath;
+   private final static Logger logger = Logger.getLogger(JSAPIWriter.class);
 
-	public JSAPIWriter(String restPath)
-	{
-		this.restPath = restPath;
-	}
+   public void writeJavaScript(String base, HttpServletRequest req, HttpServletResponse resp,
+                               Map<String, ServiceRegistry> serviceRegistries) throws IOException
+   {
+      logger.debug("// start RESTEasy client API");
 
-    public void writeJavaScript(String uri, HttpServletRequest req, HttpServletResponse resp,
-                                ServiceRegistry serviceRegistry) throws IOException {
-        if (restPath != null)
-            uri = uri + restPath;
-        logger.info("rest path: " + uri);
+      // RESTEASY-776
+      // before writing generated javascript, we generate Etag and compare it with client request.
+      // If nothing changed, we send back 304 Not Modified for client browser to use cached js.
+      String ifNoneMatch = req.getHeader("If-None-Match");
+      String etag = generateEtag(serviceRegistries);
+      resp.setHeader("Etag", etag);
 
-        logger.info("// start RESTEasy client API");
+      if (ifNoneMatch != null && ifNoneMatch.equals(etag))
+      {
+         resp.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+         return;
+      }
 
-        // RESTEASY-776
-        // before writing generated javascript, we generate Etag and compare it with client request.
-        // If nothing changed, we send back 304 Not Modified for client browser to use cached js.
-        String ifNoneMatch = req.getHeader("If-None-Match");
-        String etag = generateEtag(serviceRegistry);
-        resp.setHeader("Etag", etag);
+      for (Map.Entry<String, ServiceRegistry> entry : serviceRegistries.entrySet())
+      {
+         String uri = base;
+         if (entry.getKey() != null) uri += entry.getKey();
 
-        if (ifNoneMatch != null && ifNoneMatch.equals(etag)) {
-            resp.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
-            return;
-        }
+         StringWriter stringWriter = new StringWriter();
+         PrintWriter writer = new PrintWriter(new BufferedWriter(stringWriter));
+         writeJavaScript(uri, writer, entry.getValue());
+         writer.flush();
+         writer.close();
 
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(new BufferedWriter(stringWriter));
-        writeJavaScript(uri, writer, serviceRegistry);
-        writer.flush();
-        writer.close();
-
-        if (clientIsGzipSupported(req)) {
+         if (clientIsGzipSupported(req))
+         {
             ByteArrayOutputStream compressedContent = new ByteArrayOutputStream();
             GZIPOutputStream gzipstream = new GZIPOutputStream(compressedContent);
             gzipstream.write(stringWriter.toString().getBytes());
@@ -73,226 +72,244 @@ public class JSAPIWriter
             output.flush();
             output.close();
 
-        } else {
+         }
+         else
+         {
             ServletOutputStream output = resp.getOutputStream();
             byte[] bytes = stringWriter.toString().getBytes();
             resp.setContentLength(bytes.length);
             output.write(bytes);
             output.flush();
             output.close();
-        }
+         }
+      }
 
-    }
+   }
 
-    private boolean clientIsGzipSupported(HttpServletRequest req) {
-        String encoding = req.getHeader("Accept-Encoding");
-        return encoding != null && encoding.contains("gzip");
-    }
+   private boolean clientIsGzipSupported(HttpServletRequest req)
+   {
+      String encoding = req.getHeader("Accept-Encoding");
+      return encoding != null && encoding.contains("gzip");
+   }
 
-    public void writeJavaScript(String uri, PrintWriter writer,
-                                ServiceRegistry serviceRegistry) throws IOException {
-        copyResource("/resteasy-client.js", writer);
-        logger.info("// start JAX-RS API");
-        logger.info("REST.apiURL = '" + uri + "';");
-        writer.println("REST.apiURL = '" + uri + "';");
-        Set<String> declaredPrefixes = new HashSet<String>();
-        printService(writer, serviceRegistry, declaredPrefixes);
+   public void writeJavaScript(String uri, PrintWriter writer,
+                               ServiceRegistry serviceRegistry) throws IOException
+   {
+      copyResource("/resteasy-client.js", writer);
+      logger.debug("// start JAX-RS API");
+      logger.debug("REST.apiURL = '" + uri + "';");
+      writer.println("REST.apiURL = '" + uri + "';");
+      Set<String> declaredPrefixes = new HashSet<String>();
+      printService(writer, serviceRegistry, declaredPrefixes);
 
-    }
-
-
-    private String generateEtag(ServiceRegistry serviceRegistry) {
-        StringBuilder etagBuilder = new StringBuilder();
-        generateEtag(serviceRegistry, etagBuilder);
-        return String.valueOf(Math.abs(etagBuilder.toString().hashCode()));
-    }
-
-    private void generateEtag(ServiceRegistry serviceRegistry, StringBuilder etagBuilder) {
-        for (MethodMetaData methodMetaData : serviceRegistry.getMethodMetaData()) {
-                etagBuilder.append(methodMetaData.hashCode());
-
-            for(ServiceRegistry subService : serviceRegistry.getLocators()) {
-                generateEtag(subService, etagBuilder);
-            }
-
-        }
-    }
-
-    private void printService(PrintWriter writer,
-			ServiceRegistry serviceRegistry, Set<String> declaredPrefixes) {
+   }
 
 
-		for (MethodMetaData methodMetaData : serviceRegistry.getMethodMetaData())
-		{
-			logger.info("Path: " + methodMetaData.getUri());
-			logger.info(" Invoker: " + methodMetaData.getResource());
-			String declaringPrefix = methodMetaData.getFunctionPrefix();
-			declarePrefix(writer, declaringPrefix, declaredPrefixes);
-			
-			for (String httpMethod : methodMetaData.getHttpMethods())
-			{
-				print(writer, httpMethod, methodMetaData);
-			}
-		}
-		for(ServiceRegistry subService : serviceRegistry.getLocators())
-			printService(writer, subService, declaredPrefixes);
-	}
+   private String generateEtag(Map<String, ServiceRegistry> serviceRegistries)
+   {
+      StringBuilder etagBuilder = new StringBuilder();
+      for (Map.Entry<String, ServiceRegistry> entry : serviceRegistries.entrySet())
+      {
+         if (entry.getKey() != null) etagBuilder.append(entry.getKey()).append(':');
+         generateEtag(entry.getValue(), etagBuilder);
+      }
+      return String.valueOf(Math.abs(etagBuilder.toString().hashCode()));
+   }
 
-	private void declarePrefix(PrintWriter writer, String declaringPrefix, Set<String> declaredPrefixes) {
-		if (declaredPrefixes.add(declaringPrefix))
-		{
-			int lastDot = declaringPrefix.lastIndexOf(".");
-			if(lastDot == -1)
-				writer.println("var " + declaringPrefix + " = {};");
-			else{
-				declarePrefix(writer, declaringPrefix.substring(0, lastDot), declaredPrefixes);
-				writer.println(declaringPrefix + " = {};");
-			}
-		}
+   private void generateEtag(ServiceRegistry serviceRegistry, StringBuilder etagBuilder)
+   {
+      for (MethodMetaData methodMetaData : serviceRegistry.getMethodMetaData())
+      {
+         etagBuilder.append(methodMetaData.hashCode());
 
-	}
+         for (ServiceRegistry subService : serviceRegistry.getLocators())
+         {
+            generateEtag(subService, etagBuilder);
+         }
 
-	private void copyResource(String name, PrintWriter writer)
-	throws IOException
-	{
-		Reader reader = new InputStreamReader(getClass()
-				.getResourceAsStream(name));
-		char[] array = new char[1024];
-		int read;
-		while ((read = reader.read(array)) >= 0)
-		{
-			writer.write(array, 0, read);
-		}
-		reader.close();
-	}
+      }
+   }
 
-	private void print(PrintWriter writer, String httpMethod,
-			MethodMetaData methodMetaData)
-	{
-		String uri = methodMetaData.getUri();
-		writer.println("// " + httpMethod + " " + uri);
-		writer
-		.println(methodMetaData.getFunctionName() + " = function(_params){");
-		writer.println(" var params = _params ? _params : {};");
-		writer.println(" var request = new REST.Request();");
-		writer.println(" request.setMethod('" + httpMethod + "');");
-		writer
-		.println(" var uri = params.$apiURL ? params.$apiURL : REST.apiURL;");
-		if (uri.contains("{"))
-		{
-			printURIParams(uri, writer);
-		} else
-		{
-			writer.println(" uri += '" + uri + "';");
-		}
-		printOtherParams(methodMetaData, writer);
-		writer.println(" request.setURI(uri);");
-		writer.println(" if(params.$username && params.$password)");
-		writer
-		.println("  request.setCredentials(params.$username, params.$password);");
-		writer.println(" if(params.$accepts)");
-		writer.println("  request.setAccepts(params.$accepts);");
-		if (methodMetaData.getWants() != null)
-		{
-			writer.println(" else");
-			writer.println("  request.setAccepts('" + methodMetaData.getWants()
-					+ "');");
-		}
-
-        writer.println("if (REST.antiBrowserCache == true) {");
-        writer.println("  request.addQueryParameter('resteasy_jsapi_anti_cache', (new Date().getTime()));");
-        writer.println("    var cached_obj = REST._get_cache_signature(REST._generate_cache_signature(uri));");
-
-        writer.println("    if (cached_obj != null) { request.addHeader('If-Modified-Since', cached_obj[1]['Last-Modified']); request.addHeader('If-None-Match', cached_obj[1]['Etag']);}");
-
-        writer.println("}");
+   private void printService(PrintWriter writer,
+                             ServiceRegistry serviceRegistry, Set<String> declaredPrefixes)
+   {
 
 
-        writer.println(" if(params.$contentType)");
-		writer.println("  request.setContentType(params.$contentType);");
-		writer.println(" else");
-		writer.println("  request.setContentType('"
-				+ methodMetaData.getConsumesMIMEType() + "');");
-		writer.println(" if(params.$callback){");
-		writer.println("  request.execute(params.$callback);");
-		writer.println(" }else{");
-		writer.println("  var returnValue;");
-		writer.println("  request.setAsync(false);");
-		writer
-		.println("  var callback = function(httpCode, xmlHttpRequest, value){ returnValue = value;};");
-		writer.println("  request.execute(callback);");
-		writer.println("  return returnValue;");
-		writer.println(" }");
-		writer.println("};");
-	}
+      for (MethodMetaData methodMetaData : serviceRegistry.getMethodMetaData())
+      {
+         logger.debug("Path: " + methodMetaData.getUri());
+         logger.debug(" Invoker: " + methodMetaData.getResource());
+         String declaringPrefix = methodMetaData.getFunctionPrefix();
+         declarePrefix(writer, declaringPrefix, declaredPrefixes);
 
-	private void printOtherParams(MethodMetaData methodMetaData,
-			PrintWriter writer)
-	{
-		List<MethodParamMetaData> params = methodMetaData.getParameters();
-		for (MethodParamMetaData methodParamMetaData : params)
-		{
-			printParameter(methodParamMetaData, writer);
-		}
-	}
+         for (String httpMethod : methodMetaData.getHttpMethods())
+         {
+            print(writer, httpMethod, methodMetaData);
+         }
+      }
+      for (ServiceRegistry subService : serviceRegistry.getLocators())
+         printService(writer, subService, declaredPrefixes);
+   }
 
-	private void printParameter(MethodParamMetaData metaData,
-			PrintWriter writer)
-	{
-		switch(metaData.getParamType()){
-		case QUERY_PARAMETER:
-			print(metaData, writer, "QueryParameter");
-			break;
-		case HEADER_PARAMETER:
-			print(metaData, writer, "Header");
-			// FIXME: warn about forbidden headers:
-			// http://www.w3.org/TR/XMLHttpRequest/#the-setrequestheader-method
-			break;
-		case COOKIE_PARAMETER:
-			print(metaData, writer, "Cookie");
-			break;
-		case MATRIX_PARAMETER:
-			print(metaData, writer, "MatrixParameter");
-			break;
-		case FORM_PARAMETER:
-			print(metaData, writer, "FormParameter");
-			break;
-        case FORM:
+   private void declarePrefix(PrintWriter writer, String declaringPrefix, Set<String> declaredPrefixes)
+   {
+      if (declaredPrefixes.add(declaringPrefix))
+      {
+         int lastDot = declaringPrefix.lastIndexOf(".");
+         if (lastDot == -1)
+            writer.println("var " + declaringPrefix + " = {};");
+         else
+         {
+            declarePrefix(writer, declaringPrefix.substring(0, lastDot), declaredPrefixes);
+            writer.println(declaringPrefix + " = {};");
+         }
+      }
+
+   }
+
+   private void copyResource(String name, PrintWriter writer)
+           throws IOException
+   {
+      Reader reader = new InputStreamReader(getClass()
+              .getResourceAsStream(name));
+      char[] array = new char[1024];
+      int read;
+      while ((read = reader.read(array)) >= 0)
+      {
+         writer.write(array, 0, read);
+      }
+      reader.close();
+   }
+
+   private void print(PrintWriter writer, String httpMethod,
+                      MethodMetaData methodMetaData)
+   {
+      String uri = methodMetaData.getUri();
+      writer.println("// " + httpMethod + " " + uri);
+      writer
+              .println(methodMetaData.getFunctionName() + " = function(_params){");
+      writer.println(" var params = _params ? _params : {};");
+      writer.println(" var request = new REST.Request();");
+      writer.println(" request.setMethod('" + httpMethod + "');");
+      writer
+              .println(" var uri = params.$apiURL ? params.$apiURL : REST.apiURL;");
+      if (uri.contains("{"))
+      {
+         printURIParams(uri, writer);
+      }
+      else
+      {
+         writer.println(" uri += '" + uri + "';");
+      }
+      printOtherParams(methodMetaData, writer);
+      writer.println(" request.setURI(uri);");
+      writer.println(" if(params.$username && params.$password)");
+      writer
+              .println("  request.setCredentials(params.$username, params.$password);");
+      writer.println(" if(params.$accepts)");
+      writer.println("  request.setAccepts(params.$accepts);");
+      if (methodMetaData.getWants() != null)
+      {
+         writer.println(" else");
+         writer.println("  request.setAccepts('" + methodMetaData.getWants()
+                 + "');");
+      }
+
+      writer.println("if (REST.antiBrowserCache == true) {");
+      writer.println("  request.addQueryParameter('resteasy_jsapi_anti_cache', (new Date().getTime()));");
+      writer.println("    var cached_obj = REST._get_cache_signature(REST._generate_cache_signature(uri));");
+
+      writer.println("    if (cached_obj != null) { request.addHeader('If-Modified-Since', cached_obj[1]['Last-Modified']); request.addHeader('If-None-Match', cached_obj[1]['Etag']);}");
+
+      writer.println("}");
+
+
+      writer.println(" if(params.$contentType)");
+      writer.println("  request.setContentType(params.$contentType);");
+      writer.println(" else");
+      writer.println("  request.setContentType('"
+              + methodMetaData.getConsumesMIMEType() + "');");
+      writer.println(" if(params.$callback){");
+      writer.println("  request.execute(params.$callback);");
+      writer.println(" }else{");
+      writer.println("  var returnValue;");
+      writer.println("  request.setAsync(false);");
+      writer
+              .println("  var callback = function(httpCode, xmlHttpRequest, value){ returnValue = value;};");
+      writer.println("  request.execute(callback);");
+      writer.println("  return returnValue;");
+      writer.println(" }");
+      writer.println("};");
+   }
+
+   private void printOtherParams(MethodMetaData methodMetaData,
+                                 PrintWriter writer)
+   {
+      List<MethodParamMetaData> params = methodMetaData.getParameters();
+      for (MethodParamMetaData methodParamMetaData : params)
+      {
+         printParameter(methodParamMetaData, writer);
+      }
+   }
+
+   private void printParameter(MethodParamMetaData metaData,
+                               PrintWriter writer)
+   {
+      switch (metaData.getParamType())
+      {
+         case QUERY_PARAMETER:
+            print(metaData, writer, "QueryParameter");
+            break;
+         case HEADER_PARAMETER:
+            print(metaData, writer, "Header");
+            // FIXME: warn about forbidden headers:
+            // http://www.w3.org/TR/XMLHttpRequest/#the-setrequestheader-method
+            break;
+         case COOKIE_PARAMETER:
+            print(metaData, writer, "Cookie");
+            break;
+         case MATRIX_PARAMETER:
+            print(metaData, writer, "MatrixParameter");
+            break;
+         case FORM_PARAMETER:
+            print(metaData, writer, "FormParameter");
+            break;
+         case FORM:
             print(metaData, writer, "Form");
             break;
-		case ENTITY_PARAMETER:
-			// the entity
-			writer.println(" if(params.$entity)");
-			writer.println("  request.setEntity(params.$entity);");
-			break;
-		}
-	}
+         case ENTITY_PARAMETER:
+            // the entity
+            writer.println(" if(params.$entity)");
+            writer.println("  request.setEntity(params.$entity);");
+            break;
+      }
+   }
 
-	private void print(MethodParamMetaData metaData, PrintWriter writer,
-			String type)
-	{
-		String paramName = metaData.getParamName();
-		writer.println(String.format(" if(Object.prototype.hasOwnProperty.call(params, '%s'))\n  request.add%s('%s', params.%s);", paramName, type, paramName, paramName));
-	}
+   private void print(MethodParamMetaData metaData, PrintWriter writer,
+                      String type)
+   {
+      String paramName = metaData.getParamName();
+      writer.println(String.format(" if(Object.prototype.hasOwnProperty.call(params, '%s'))\n  request.add%s('%s', params.%s);", paramName, type, paramName, paramName));
+   }
 
 
-	private void printURIParams(String uri, PrintWriter writer)
-	{
-		String replacedCurlyURI = PathHelper.replaceEnclosedCurlyBraces(uri);
-		Matcher matcher = PathHelper.URI_PARAM_PATTERN.matcher(replacedCurlyURI);
-		int i = 0;
-		while (matcher.find())
-		{
-			if (matcher.start() > i)
-			{
-				writer.println(" uri += '"
-						+ replacedCurlyURI.substring(i, matcher.start()) + "';");
-			}
-			String name = matcher.group(1);
-			writer.println(" uri += REST.Encoding.encodePathSegment(params." + name + ");");
-			i = matcher.end();
-		}
-		if (i < replacedCurlyURI.length())
-			writer.println(" uri += '" + replacedCurlyURI.substring(i) + "';");
-	}
+   private void printURIParams(String uri, PrintWriter writer)
+   {
+      String replacedCurlyURI = PathHelper.replaceEnclosedCurlyBraces(uri);
+      Matcher matcher = PathHelper.URI_PARAM_PATTERN.matcher(replacedCurlyURI);
+      int i = 0;
+      while (matcher.find())
+      {
+         if (matcher.start() > i)
+         {
+            writer.println(" uri += '"
+                    + replacedCurlyURI.substring(i, matcher.start()) + "';");
+         }
+         String name = matcher.group(1);
+         writer.println(" uri += REST.Encoding.encodePathSegment(params." + name + ");");
+         i = matcher.end();
+      }
+      if (i < replacedCurlyURI.length())
+         writer.println(" uri += '" + replacedCurlyURI.substring(i) + "';");
+   }
 }
