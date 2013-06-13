487711ec6f9e0a950a517357448166143088cd52
Ron Sigal <rsigal@redhat.com>
2013-06-13 04:17:18 -0400

    RESTEASY-694, RESTEASY-879: 1) Added test for LazyValidatoryFactory; 2) pared down dependencies in validator module.xml files.

---
+++ jaxrs/arquillian/ValidationTest-WF8/src/test/java/org/jboss/resteasy/test/validation/TestLazyValidator.java
@@ -0,0 +1,49 @@
+package org.jboss.resteasy.test.validation;
+
+import junit.framework.Assert;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.resteasy.client.ClientRequest;
+import org.jboss.resteasy.client.ClientResponse;
+import org.jboss.resteasy.validation.JaxRsActivator;
+import org.jboss.resteasy.validation.TestResourceLazyValidator;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * 
+ * @author <a href="ron.sigal@jboss.com">Ron Sigal</a>
+ * @version $Revision: 1.1 $
+ *
+ * Created Junk 8, 2013
+ */
+@RunWith(Arquillian.class)
+public class TestLazyValidator
+{
+   @Deployment
+   public static Archive<?> createTestArchive()
+   {
+      WebArchive war = ShrinkWrap.create(WebArchive.class, "Validation-test.war")
+            .addClasses(JaxRsActivator.class)
+            .addClasses(TestResourceLazyValidator.class)
+            ;
+      System.out.println(war.toString(true));
+      return war;
+   }
+   
+   @Test
+   public void testLazyValidator() throws Exception
+   {
+      // Valid native constraint
+      ClientRequest request = new ClientRequest("http://localhost:8080/Validation-test/rest/lazy");
+      ClientResponse<?> response = request.get();     
+      Assert.assertEquals(200, response.getStatus());
+      Assert.assertTrue(response.getEntity(boolean.class));
+   }
+}
