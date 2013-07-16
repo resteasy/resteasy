f4944e117ea60c900ec45035ca67962d8a1d6a9c
Bill Burke <bburke@redhat.com>
2013-07-16 08:23:34 -0400

    remove lgpl

--- resteasy-spring/src/main/java/javax/ws/rs/core/MediaTypeEditor.java
+++ resteasy-spring/src/main/java/javax/ws/rs/core/MediaTypeEditor.java
@@ -1,9 +1,3 @@
-/*
- * JBoss, the OpenSource J2EE webOS
- * 
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
- */
 package javax.ws.rs.core;
 
 import java.beans.PropertyEditorSupport;
--- resteasy-spring/src/test/java/org/jboss/resteasy/spring/TestMediaTypePropertyEditor.java
+++ resteasy-spring/src/test/java/org/jboss/resteasy/spring/TestMediaTypePropertyEditor.java
@@ -1,9 +1,3 @@
-/*
- * JBoss, the OpenSource J2EE webOS
- * 
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
- */
 package org.jboss.resteasy.spring;
 
 import org.jboss.resteasy.core.SynchronousDispatcher;
