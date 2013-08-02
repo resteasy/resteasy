8eb9aeabd4fa7eacc4b249c387c060d25719fd04
rsigal <rsigal@redhat.com>
2013-08-02 16:14:11 -0400

    Back up to commit e6e24

--- arquillian/RESTEASY-802-AS71/src/main/java/org/jboss/resteasy/resteasy802/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This test verifies that org.jboss.as.jaxrs.deployment.JaxrsComponentDeployer in an older version
- * of AS, 7.1.0.Final, is compatible with org.jboss.resteasy.util.GetRestful in resteasy-jaxrs.
- * 
- */
-package org.jboss.resteasy.resteasy802;
