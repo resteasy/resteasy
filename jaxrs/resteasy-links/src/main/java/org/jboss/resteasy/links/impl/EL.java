1f95cefc716d88533551c282977aa9b0ba2a8f10
Tomaz Cerar <tomaz.cerar@gmail.com>
2014-03-06 00:47:22 +0100

    update depenedancies and cleanup

--- jaxrs/resteasy-links/src/main/java/org/jboss/resteasy/links/impl/EL.java
+++ jaxrs/resteasy-links/src/main/java/org/jboss/resteasy/links/impl/EL.java
@@ -1,8 +1,5 @@
 package org.jboss.resteasy.links.impl;
 
-import org.jboss.el.ExpressionFactoryImpl;
-import org.jboss.el.lang.FunctionMapperImpl;
-import org.jboss.el.lang.VariableMapperImpl;
 
 import javax.el.ArrayELResolver;
 import javax.el.BeanELResolver;
@@ -10,51 +7,32 @@ import javax.el.CompositeELResolver;
 import javax.el.ELContext;
 import javax.el.ELResolver;
 import javax.el.ExpressionFactory;
-import javax.el.FunctionMapper;
 import javax.el.ListELResolver;
 import javax.el.MapELResolver;
 import javax.el.ResourceBundleELResolver;
-import javax.el.VariableMapper;
+import javax.el.StandardELContext;
 
 public class EL {
-	public static final ExpressionFactory EXPRESSION_FACTORY = new ExpressionFactoryImpl();
+    public static final ExpressionFactory EXPRESSION_FACTORY = ExpressionFactory.newInstance();
 
-	private static ELResolver createELResolver(Object base) {
-		CompositeELResolver resolver = new CompositeELResolver();
-		if(base != null)
-			resolver.add(new BaseELResolver(base));
-		resolver.add(new MapELResolver());
-		resolver.add(new ListELResolver());
-		resolver.add(new ArrayELResolver());
-		resolver.add(new ResourceBundleELResolver());
-		resolver.add(new BeanELResolver());
-		return resolver;
-	}
+    private static ELResolver createELResolver(Object base) {
+        CompositeELResolver resolver = new CompositeELResolver();
+        if (base != null) { resolver.add(new BaseELResolver(base)); }
+        resolver.add(new MapELResolver());
+        resolver.add(new ListELResolver());
+        resolver.add(new ArrayELResolver());
+        resolver.add(new ResourceBundleELResolver());
+        resolver.add(new BeanELResolver());
+        return resolver;
+    }
 
-	public static ELContext createELContext(Object base) {
-		return createELContext(createELResolver(base), new FunctionMapperImpl());
-	}
+    public static ELContext createELContext(final Object base) {
+        return new StandardELContext(EXPRESSION_FACTORY) {
+            @Override
+            public ELResolver getELResolver() {
+                return createELResolver(base);
+            }
 
-	public static ELContext createELContext(final ELResolver resolver,
-			final FunctionMapper functionMapper) {
-		return new ELContext() {
-			final VariableMapperImpl variableMapper = new VariableMapperImpl();
-
-			@Override
-			public ELResolver getELResolver() {
-				return resolver;
-			}
-
-			@Override
-			public FunctionMapper getFunctionMapper() {
-				return functionMapper;
-			}
-
-			@Override
-			public VariableMapper getVariableMapper() {
-				return variableMapper;
-			}
-
-		};
-	}
+        };
+    }
 }
