9e8444763d252118ee3cb5a323a6244b93b8e901
Przemyslaw Bielicki <pbielicki@gmail.com>
2014-05-23 10:20:27 +0200

    Added FastInfoset providers supporting java.util.Collection and java.util.Map types

---
+++ jaxrs/providers/fastinfoset/src/main/java/org/jboss/resteasy/plugins/providers/jaxb/fastinfoset/FastInfosetCollectionProvider.java
@@ -0,0 +1,21 @@
+package org.jboss.resteasy.plugins.providers.jaxb.fastinfoset;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.plugins.providers.jaxb.CollectionProvider;
+
+/**
+ * @author <a href="mailto:pbielicki@gmail.com">Przemyslaw Bielicki</a>
+ */
+@Provider
+@Consumes("application/*+fastinfoset")
+@Produces("application/*+fastinfoset")
+public class FastInfosetCollectionProvider extends CollectionProvider {
+
+  @Override
+  protected boolean suppressExpandEntityExpansion() {
+    return false;
+  }
+}
