7a092d1727283d601a3a5d8d9c5947316362d15b
Bill Burke <bburke@redhat.com>
2014-09-15 16:54:33 -0400

    minor fix

--- jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/spi/ResteasyProviderFactory.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/spi/ResteasyProviderFactory.java
@@ -1806,7 +1806,7 @@ public class ResteasyProviderFactory extends RuntimeDelegate implements Provider
          try
          {
             addContextResolver((ContextResolver) provider);
-            int priority = getPriority(priorityOverride, contracts, ExceptionMapper.class, provider.getClass());
+            int priority = getPriority(priorityOverride, contracts, ContextResolver.class, provider.getClass());
             newContracts.put(ContextResolver.class, priority);
          }
          catch (Exception e)
