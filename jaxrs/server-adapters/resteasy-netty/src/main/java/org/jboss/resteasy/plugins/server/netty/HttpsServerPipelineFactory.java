bace4a4d0ba64084d5ea1bac5efcead31ca5ac0f
Jochen Schalanda <jochen@schalanda.name>
2014-10-01 17:54:44 +0200

    Add customization hooks to resteasy-netty NettyJaxrsServer
    This changeset adds customization hooks for ChannelHandler and channel options
    to NettyJaxrsServer. It enables users to customize Netty's ServerBootstrap in
    a limited way which can be used to add logging, transparent compression handling,
    and other things.

--- jaxrs/server-adapters/resteasy-netty/src/main/java/org/jboss/resteasy/plugins/server/netty/HttpsServerPipelineFactory.java
+++ jaxrs/server-adapters/resteasy-netty/src/main/java/org/jboss/resteasy/plugins/server/netty/HttpsServerPipelineFactory.java
@@ -1,11 +1,13 @@
 package org.jboss.resteasy.plugins.server.netty;
 
+import org.jboss.netty.channel.ChannelHandler;
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.handler.ssl.SslHandler;
 import org.jboss.resteasy.plugins.server.netty.RestEasyHttpRequestDecoder.Protocol;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
+import java.util.List;
 
 /**
  * {@link HttpServerPipelineFactory} subclass which enable the use of HTTPS
@@ -18,9 +20,9 @@ public class HttpsServerPipelineFactory extends HttpServerPipelineFactory
 
     private final SSLContext context;
 
-    public HttpsServerPipelineFactory(RequestDispatcher dispatcher, String root, int executorThreadCount, int maxRequestSize, boolean isKeepAlive, SSLContext context) 
+    public HttpsServerPipelineFactory(RequestDispatcher dispatcher, String root, int executorThreadCount, int maxRequestSize, boolean isKeepAlive, List<ChannelHandler> additionalChannelHandlers, SSLContext context)
     {
-        super(dispatcher, root, executorThreadCount, maxRequestSize, isKeepAlive);
+        super(dispatcher, root, executorThreadCount, maxRequestSize, isKeepAlive, additionalChannelHandlers);
         this.context = context;
     }
 
