7ecd406e6a0f18ddb7b81546b440def4b942bfb0
rsigal <rsigal@redhat.com>
2014-10-11 19:39:36 -0400

    RESTEASY-719: Removed cyclic dependency on resteasy-jaxrs-test from jaxrs-api.

--- jaxrs-api/src/test/java/org/jboss/resteasy/test/i18n/TestMessagesAbstract.java
+++ jaxrs-api/src/test/java/org/jboss/resteasy/test/i18n/TestMessagesAbstract.java
@@ -7,7 +7,6 @@ import java.util.Locale;
 import junit.framework.Assert;
 
 import org.jboss.resteasy.jaxrs_api.i18n.Messages;
-import org.jboss.resteasy.test.resteasy_jaxrs.i18n.TestMessagesParent;
 import org.junit.Test;
 
 /**
