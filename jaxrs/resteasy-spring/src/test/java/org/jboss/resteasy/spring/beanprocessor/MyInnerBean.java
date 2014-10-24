26ff6557cb477a1649c6a518a236ee4065243925
Stijn <stijn.sv@gmail.com>
2014-10-24 09:15:36 +0200

    fixes #585 a problem with the way certain factories are used in spring. More specificaly the "Activiti engine" library, but other libraries might encounter the same problems.

---
+++ jaxrs/resteasy-spring/src/test/java/org/jboss/resteasy/spring/beanprocessor/MyInnerBean.java
@@ -0,0 +1,10 @@
+package org.jboss.resteasy.spring.beanprocessor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sgv
+ * Date: 24/10/14
+ * Time: 8:44
+ */
+public interface MyInnerBean {
+}
---
+++ jaxrs/resteasy-spring/src/test/java/org/jboss/resteasy/spring/beanprocessor/MyInnerBeanImpl.java
@@ -0,0 +1,10 @@
+package org.jboss.resteasy.spring.beanprocessor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sgv
+ * Date: 24/10/14
+ * Time: 8:45
+ */
+public class MyInnerBeanImpl implements MyInnerBean {
+}
