04dbbf742c82e104900bc06e2f22385db77a6838
rsigal <rsigal@redhat.com>
2015-05-18 23:30:52 -0400

    Revert "Revert "RESTEASY-950: Added more information about deprecated classes and""
    This reverts commit 3ca1fe6d2c90677a41b1f8a1195fcf2c71231962.

---
+++ jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/client/package-info.java
@@ -0,0 +1,23 @@
+/**
+ * JAX-RS specification 1.1 ({@link <a href="https://jcp.org/en/jsr/detail?id=311">https://jcp.org/en/jsr/detail?id=311</a>})
+ * had no client framework, so Resteasy release 2.x provided one. It has two ways to access a server resource:
+ * 
+ * <ol>
+ * <li>
+ * {@link org.jboss.resteasy.client.ClientRequest}
+ * </li>
+ * <li>
+ * a proxy that implements an interface implemented by the target resource
+ * </ol>
+ * 
+ * JAX-RS specification 2.0 
+ * ({@link <a href="https://www.jcp.org/aboutJava/communityprocess/final/jsr339/index.html">https://www.jcp.org/aboutJava/communityprocess/final/jsr339/index.html</a>})
+ * introduced an official client framework, so the older Resteasy client framework is now deprecated. However, the new JAX-RS
+ * framework does not include a proxy feature, so Resteasy extends the official client framework with a new proxy feature.
+ * A proxy can be created using {@link org.jboss.resteasy.client.jaxrs.ResteasyWebTarget}, which extends the api class
+ * javax.ws.rs.client.WebTarget
+ * <p>
+ * For more information, see the Resteasy User Guide
+ * {@link <a href="http://docs.jboss.org/resteasy/docs/">http://docs.jboss.org/resteasy/docs/</a>}.
+ */
+package org.jboss.resteasy.client;
--- jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/interception/ClientExecutionContextImpl.java
+++ jaxrs/resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/interception/ClientExecutionContextImpl.java
@@ -9,9 +9,14 @@ import org.jboss.resteasy.spi.interception.ClientExecutionInterceptor;
 import java.util.List;
 
 /**
- * @deprecated
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
+ * 
+ * @deprecated The Resteasy interceptor facility introduced in release 2.x
+ * is replaced by the JAX-RS 2.0 compliant interceptor facility in release 3.0.x.
+ * 
+ * @see jaxrs-api
+ * 
  */
 @Deprecated
 public class ClientExecutionContextImpl implements ClientExecutionContext
