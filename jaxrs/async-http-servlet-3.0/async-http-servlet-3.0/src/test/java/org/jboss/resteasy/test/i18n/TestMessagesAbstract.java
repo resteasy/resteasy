e11d8893b2da992dbf996fb8fbaa4fb4afb96aee
rsigal <rsigal@redhat.com>
2015-08-26 16:55:01 -0400

    RESTEASY-1224: Internationalized async-http-servlet-3.0.

---
+++ jaxrs/async-http-servlet-3.0/async-http-servlet-3.0/src/test/java/org/jboss/resteasy/test/i18n/TestMessagesAbstract.java
@@ -0,0 +1,45 @@
+package org.jboss.resteasy.test.i18n;
+
+import java.util.Locale;
+
+import junit.framework.Assert;
+
+import org.jboss.resteasy.plugins.server.servlet.i18n.Messages;
+import org.jboss.resteasy.test.resteasy_jaxrs.i18n.TestMessagesParent;
+import org.junit.Test;
+
+/**
+ * 
+ * @author <a href="ron.sigal@jboss.com">Ron Sigal</a>
+ * @version $Revision: 1.1 $
+ *
+ * Copyright Aug 25, 2015
+ */
+abstract public class TestMessagesAbstract extends TestMessagesParent
+{  
+   protected static final String BASE = String.format("00%4s", Messages.BASE).substring(0, 3);
+   
+   @Test
+   public void testLocale() throws Exception
+   {  
+      Locale locale = getLocale();
+      String filename = "org/jboss/resteasy/plugins/server/servlet/i18n/Messages.i18n_" + locale.toString() + ".properties";
+      if (!before(locale, filename))
+      {
+         System.out.println(getClass() + ": " + filename + " not found.");
+         return;
+      }
+      
+      Assert.assertEquals(getExpected(BASE + "000", "alreadyCanceled"), Messages.MESSAGES.alreadyCanceled());
+      Assert.assertEquals(getExpected(BASE + "020", "cancellingWith503"), Messages.MESSAGES.cancellingWith503());
+      Assert.assertEquals(getExpected(BASE + "045", "schedulingTimeout"), Messages.MESSAGES.schedulingTimeout());
+   }
+   
+   @Override
+   protected int getExpectedNumberOfMethods()
+   {
+      return Messages.class.getDeclaredMethods().length;  
+   }
+   
+   abstract protected Locale getLocale();
+}
