639f29e4177cff5c6113ff693bf2d8b350514f87
Dmitrii Tikhomirov <dtikhomi@redhat.com>
2015-11-16 15:53:51 +0100

    [JBEAP-1904] DataSourceProvider produces FileNotFoundException upon second call of getInputStream() [RESTEASY-1182] DataSourceProvider produces FileNotFoundException upon second call of getInputStream()

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/SynchronousDispatcher.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/SynchronousDispatcher.java
@@ -1,5 +1,7 @@
 package org.jboss.resteasy.core;
 
+import org.jboss.resteasy.plugins.server.servlet.Cleanable;
+import org.jboss.resteasy.plugins.server.servlet.Cleanables;
 import org.jboss.resteasy.resteasy_jaxrs.i18n.LogMessages;
 import org.jboss.resteasy.resteasy_jaxrs.i18n.Messages;
 import org.jboss.resteasy.specimpl.RequestImpl;
@@ -34,6 +36,7 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -478,6 +481,7 @@ public class SynchronousDispatcher implements Dispatcher
       contextDataMap.put(Request.class, new RequestImpl(request));
 
       contextDataMap.putAll(defaultContextObjects);
+      contextDataMap.put(Cleanables.class, new Cleanables());
    }
 
    public Response internalInvocation(HttpRequest request, HttpResponse response, Object entity)
@@ -513,9 +517,24 @@ public class SynchronousDispatcher implements Dispatcher
 
    public void clearContextData()
    {
-      ResteasyProviderFactory.clearContextData();
-      // just in case there were internalDispatches that need to be cleaned up
-      MessageBodyParameterInjector.clearBodies();
+         Cleanables cleanables = ResteasyProviderFactory.getContextData(Cleanables.class);
+         if (cleanables != null)
+         {
+             for (Iterator<Cleanable> it = cleanables.getCleanables().iterator(); it.hasNext(); )
+             {
+                 try
+                 {
+                     it.next().clean();
+                 }
+                 catch(Exception e)
+                 {
+                     // Empty
+                 }
+            }
+        }  
+        ResteasyProviderFactory.clearContextData();
+        // just in case there were internalDispatches that need to be cleaned up
+        MessageBodyParameterInjector.clearBodies();
    }
 
    public void invoke(HttpRequest request, HttpResponse response, ResourceInvoker invoker)
---
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/server/servlet/Cleanable.java
@@ -0,0 +1,6 @@
+package org.jboss.resteasy.plugins.server.servlet;
+
+public interface Cleanable
+{
+	public void clean() throws Exception;
+}
