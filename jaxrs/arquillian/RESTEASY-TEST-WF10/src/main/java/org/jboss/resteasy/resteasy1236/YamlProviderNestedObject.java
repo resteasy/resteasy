a90792ec22d163ede897eeffc5e6c7c60ed343f0
Ron Sigal <rsigal@redhat.com>
2016-04-20 13:27:44 -0400

    RESTEASY-1236
    Upgrade snakeyaml to 1.17, and added RESTEASY-TEST-WF10 with yaml test.

---
+++ jaxrs/arquillian/RESTEASY-TEST-WF10/src/main/java/org/jboss/resteasy/resteasy1236/YamlProviderNestedObject.java
@@ -0,0 +1,20 @@
+package org.jboss.resteasy.resteasy1236;
+
+public class YamlProviderNestedObject {
+
+    private String moreText;
+
+    public String getMoreText() {
+        return moreText;
+    }
+
+    public void setMoreText(String moreText) {
+        this.moreText = moreText;
+    }
+
+    @Override
+    public String toString() {
+        return "YamlProviderNestedObject[" + moreText + "]";
+    }
+
+}
