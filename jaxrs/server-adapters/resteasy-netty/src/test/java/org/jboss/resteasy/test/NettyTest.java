5d875437e1f2dde4c9ac1595cf65bf2a96ae9bf3
Ivo Studensky <istudens@redhat.com>
2016-06-17 09:32:13 +0200

    RESTEASY-1365 HEAD should return the same content length as GET

--- jaxrs/server-adapters/resteasy-netty/src/test/java/org/jboss/resteasy/test/NettyTest.java
+++ jaxrs/server-adapters/resteasy-netty/src/test/java/org/jboss/resteasy/test/NettyTest.java
@@ -4,15 +4,18 @@ import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.jboss.resteasy.client.jaxrs.internal.ClientInvocation;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 
 import static org.jboss.resteasy.test.TestPortProvider.generateURL;
@@ -68,6 +71,18 @@ public class NettyTest
       ResteasyWebTarget target = client.target(generateURL("/test"));
       String val = target.request().get(String.class);
       Assert.assertEquals("hello world", val);
+   }
+
+   @Test
+   public void testHeadContentLength() throws Exception
+   {
+      ResteasyClient client = new ResteasyClientBuilder().build();
+      ResteasyWebTarget target = client.target(generateURL("/test"));
+      Response getResponse = target.request().buildGet().invoke();
+      String val = ClientInvocation.extractResult(new GenericType<String>(String.class), getResponse, null);
+      Assert.assertEquals("hello world", val);
+      Response headResponse = target.request().build(HttpMethod.HEAD).invoke();
+      Assert.assertEquals("HEAD method should return the same Content-Length as the GET method", getResponse.getLength(), headResponse.getLength());
    }
 
    @Test
