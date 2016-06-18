a5ad7a255e69c7f97842e7ae4a323ba0e3e68a33
Chris Rankin <rankincj@googlemail.com>
2016-06-18 21:57:24 +0100

    [RESTEASY-1405] Add Arquillian tests.

---
+++ arquillian/RESTEASY-1405-WF8/src/main/java/org/jboss/resteasy/resteasy1405/ByFieldForm.java
@@ -0,0 +1,30 @@
+package org.jboss.resteasy.resteasy1405;
+
+import javax.ws.rs.FormParam;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+
+public class ByFieldForm {
+
+    @FormParam("name")
+    private String name;
+
+    @FormParam("data")
+    private InputPart data;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public InputPart getData() {
+        return data;
+    }
+
+    public void setData(InputPart data) {
+        this.data = data;
+    }
+
+}
---
+++ arquillian/RESTEASY-1405-WF8/src/main/java/org/jboss/resteasy/resteasy1405/BySetterForm.java
@@ -0,0 +1,29 @@
+package org.jboss.resteasy.resteasy1405;
+
+import javax.ws.rs.FormParam;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+
+public class BySetterForm {
+
+    private String name;
+    private InputPart data;
+
+    public String getName() {
+        return name;
+    }
+
+    @FormParam("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public InputPart getData() {
+        return data;
+    }
+
+    @FormParam("data")
+    public void setData(InputPart data) {
+        this.data = data;
+    }
+
+}
