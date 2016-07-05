d42021d3abd67e09d64e88cb69dd5f6900f37edc
Sebastian Laskawiec <slaskawi@redhat.com>
2016-07-05 08:13:37 +0200

    RESTEASY-1429 Pick random port when port=0

---
+++ jaxrs/server-adapters/resteasy-netty/src/test/java/org/jboss/resteasy/test/PortAssigningTest.java
@@ -0,0 +1,32 @@
+package org.jboss.resteasy.test;
+
+import org.jboss.resteasy.plugins.server.netty.NettyJaxrsServer;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests if supplying port <code>0</code> works correctly. When using port <code>0</code> the container should take the
+ * first available port and return it in {@link NettyJaxrsServer#getPort()}.
+ *
+ * @author Sebastian Łaskawiec
+ * @since 4.0
+ * @see https://issues.jboss.org/browse/RESTEASY-1429
+ */
+public class PortAssigningTest
+{
+    @Test
+    public void testZeroPort() throws Exception {
+        //given
+        NettyJaxrsServer server = new NettyJaxrsServer();
+        server.setPort(0);
+
+        //when
+        server.start();
+        int ip = server.getPort();
+        server.stop();
+
+        //then
+        Assert.assertTrue(ip != 0);
+    }
+
+}
