1c3cb6d86015f4f830ba0186f3e7857ca3533efd
Alessio Soldano <asoldano@redhat.com>
2016-09-05 13:02:32 +0200

    [RESTEASY-1497] * Cleanup i18n test resources from main tree in prepare-package phase to avoid polluting artifacts * Run i18n tests in a different surefire surefire execution (to avoid requiring fork mode for the other tests if i18n profile is on)

---
+++ server-adapters/resteasy-vertx/src/test/java/org/jboss/resteasy/test/i18n/I18nTestMessages_xx.java
@@ -0,0 +1,19 @@
+package org.jboss.resteasy.test.i18n;
+
+import java.util.Locale;
+
+/**
+ * 
+ * @author <a href="ron.sigal@jboss.com">Ron Sigal</a>
+ * @version $Revision: 1.1 $
+ *
+ * Copyright Sep 1, 2015
+ */
+public class I18nTestMessages_xx extends TestMessagesAbstract
+{  
+   @Override
+   protected Locale getLocale()
+   {
+      return new Locale("xx");
+   }
+}
