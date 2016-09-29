44253757936b206f96fa2f4b58de2de72f8a54a5
kanovotn <kanovotn@redhat.com>
2016-09-29 16:50:25 +0200

    [RESTEASY-1507] Add tests for http response codes with spring mvc dispatcher

---
+++ testsuite/integration-tests-spring/inmodule/src/test/java/org/jboss/resteasy/test/spring/inmodule/resource/SpringMvcHttpResponseCodesPerson.java
@@ -0,0 +1,17 @@
+package org.jboss.resteasy.test.spring.inmodule.resource;
+
+public class SpringMvcHttpResponseCodesPerson {
+    private String name;
+
+    public SpringMvcHttpResponseCodesPerson(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
