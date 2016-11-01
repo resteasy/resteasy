732455551e2591685ee24cc894340b86b822b442
Alessio Soldano <asoldano@redhat.com>
2016-11-01 23:53:45 +0100

    * Moving javax.ws.rs.client.ClientBuilder back to resteasy-client * Adding resteasy-jaxrs-api jboss module and moving to new jboss-jaxrs-api (JBEE-169) * Adding 2 jaxrs impl integration tests

---
+++ testsuite/legacy-integration-tests/src/test/java/org/jboss/resteasy/test/core/ResteasyJAXRSImplTest.java
@@ -0,0 +1,64 @@
+package org.jboss.resteasy.test.core;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.ext.RuntimeDelegate;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.spi.old.ResteasyProviderFactory;
+import org.jboss.resteasy.test.core.basic.resource.AcceptLanguagesResource;
+import org.jboss.resteasy.utils.TestUtil;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ResteasyJAXRSImplTest
+{
+
+   @Deployment
+   public static Archive<?> deploy()
+   {
+      WebArchive war = TestUtil.prepareArchive(ResteasyJAXRSImplTest.class.getSimpleName());
+      return TestUtil.finishContainerPrepare(war, null, AcceptLanguagesResource.class);
+   }
+
+   @Test
+   @RunAsClient
+   public void testClientBuilder() throws Exception
+   {
+      ClientBuilder client = ClientBuilder.newBuilder();
+      Assert.assertEquals(ResteasyClientBuilder.class.getName(), client.getClass().getName());
+   }
+
+   @Test
+   public void testInContainerClientBuilder() throws Exception
+   {
+      ClientBuilder client = ClientBuilder.newBuilder();
+      Assert.assertEquals(ResteasyClientBuilder.class.getName(), client.getClass().getName());
+   }
+
+   @Test
+   @RunAsClient
+   public void testRuntimeDelegate() throws Exception
+   {
+      RuntimeDelegate.setInstance(null);
+      RuntimeDelegate rd = RuntimeDelegate.getInstance();
+      Assert.assertEquals(ResteasyProviderFactory.class.getName(), rd.getClass().getName());
+      RuntimeDelegate.setInstance(null);
+   }
+
+   @Test
+   public void testInContainerRuntimeDegate() throws Exception
+   {
+      RuntimeDelegate.setInstance(null);
+      RuntimeDelegate rd = RuntimeDelegate.getInstance();
+      Assert.assertEquals(ResteasyProviderFactory.class.getName(), rd.getClass().getName());
+      RuntimeDelegate.setInstance(null);
+   }
+
+}
