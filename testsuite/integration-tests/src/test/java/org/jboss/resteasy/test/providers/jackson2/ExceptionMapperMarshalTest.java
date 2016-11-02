a8ed789418c9abd615ad93f94f25474332e96bbb
rsearls <rsearls@localhost.localdomain>
2016-11-02 10:35:23 -0400

    [RESTEASY-1532] The JsonGenerator object should not be closed right away when it throws a IOException.  Other code needs to run first
    remove comments

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jackson2/ExceptionMapperMarshalTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jackson2/ExceptionMapperMarshalTest.java
@@ -1,33 +1,34 @@
 package org.jboss.resteasy.test.providers.jackson2;
 
+import java.lang.reflect.Type;
+import java.util.List;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.test.providers.jackson2.resource.ExceptionMapperIOExceptionMapper;
 import org.jboss.resteasy.test.providers.jackson2.resource.ExceptionMapperMarshalErrorMessage;
 import org.jboss.resteasy.test.providers.jackson2.resource.ExceptionMapperMarshalMyCustomException;
-import org.jboss.resteasy.test.providers.jackson2.resource.ExceptionMapperMarshalName;
 import org.jboss.resteasy.test.providers.jackson2.resource.ExceptionMapperMarshalMyCustomExceptionMapper;
+import org.jboss.resteasy.test.providers.jackson2.resource.ExceptionMapperMarshalName;
 import org.jboss.resteasy.test.providers.jackson2.resource.ExceptionMapperMarshalResource;
+import org.jboss.resteasy.test.providers.jackson2.resource.MyEntity;
 import org.jboss.resteasy.util.HttpResponseCodes;
 import org.jboss.resteasy.util.Types;
 import org.jboss.resteasy.utils.PortProviderUtil;
 import org.jboss.resteasy.utils.TestUtil;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Before;
 import org.junit.After;
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import javax.ws.rs.core.GenericType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-import java.lang.reflect.Type;
-import java.util.List;
 
 /**
  * @tpSubChapter Jackson2 provider
@@ -47,7 +48,9 @@ public class ExceptionMapperMarshalTest {
         WebArchive war = TestUtil.prepareArchive(ProxyWithGenericReturnTypeJacksonTest.class.getSimpleName());
         war.addClass(Jackson2Test.class);
         return TestUtil.finishContainerPrepare(war, null, ExceptionMapperMarshalErrorMessage.class, ExceptionMapperMarshalMyCustomException.class,
-                ExceptionMapperMarshalMyCustomExceptionMapper.class, ExceptionMapperMarshalName.class, ExceptionMapperMarshalResource.class);
+            MyEntity.class,
+            ExceptionMapperIOExceptionMapper.class,
+            ExceptionMapperMarshalMyCustomExceptionMapper.class, ExceptionMapperMarshalName.class, ExceptionMapperMarshalResource.class);
     }
 
     @Before
@@ -79,5 +82,15 @@ public class ExceptionMapperMarshalTest {
         List<ExceptionMapperMarshalErrorMessage> errors = response.readEntity(new GenericType<List<ExceptionMapperMarshalErrorMessage>>() {
         });
         Assert.assertEquals("The response has unexpected content", "error", errors.get(0).getError());
+    }
+    @Test
+    public void testMyCustomUsed() {
+        Response response = client.target(generateURL("/resource/customME")).request().get();
+        String text = response.readEntity(String.class);
+        System.out.println("status: " + response.getStatus());
+        System.out.println("entity: " + text);
+
+        Assert.assertEquals(response.getStatus(), HttpResponseCodes.SC_OK);
+        Assert.assertTrue("Response does not contain UN_KNOWN_ERR", text.contains("UN_KNOWN_ERR"));
     }
 }
