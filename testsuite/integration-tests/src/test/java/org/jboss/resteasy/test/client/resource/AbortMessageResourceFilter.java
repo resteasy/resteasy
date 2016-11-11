d52f9ddac7d9bbfaee1597b5ed0e802376dbdea5
Alessio Soldano <asoldano@redhat.com>
2016-11-11 11:48:17 +0100

    [RESTEASY-1450] removing comment

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/resource/AbortMessageResourceFilter.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/resource/AbortMessageResourceFilter.java
@@ -27,8 +27,5 @@ public class AbortMessageResourceFilter implements ClientRequestFilter {
     public Response showProblem() {
         Client c = ClientBuilder.newClient().register(this);
         return c.target("http://doesnotmattersinceitisaborted").request().get();
-        // the problem is that the response body is lost somehow. I would have expected the text "This is an aborted response"
-        // but the body is empty and the Content-Length header says 0. The Aborted: true header comes back as expected and so does the content type
-        // i have run the same test against jersey and here i get the body
     }
 }
