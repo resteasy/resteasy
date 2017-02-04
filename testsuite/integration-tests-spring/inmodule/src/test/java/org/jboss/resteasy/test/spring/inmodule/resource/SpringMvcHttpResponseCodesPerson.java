3d657a49f1d4d73255c3667cf701296e9da24fa1
Alessio Soldano <asoldano@redhat.com>
2017-02-04 01:31:59 +0100

    [RESTEASY-1507] Adding specific catch blocks in ResteasyHandlerMapping and fixing SpringMvcHttpResponseCodesTest to properly cover all scenarios

--- testsuite/integration-tests-spring/inmodule/src/test/java/org/jboss/resteasy/test/spring/inmodule/resource/SpringMvcHttpResponseCodesPerson.java
+++ testsuite/integration-tests-spring/inmodule/src/test/java/org/jboss/resteasy/test/spring/inmodule/resource/SpringMvcHttpResponseCodesPerson.java
@@ -3,6 +3,10 @@ package org.jboss.resteasy.test.spring.inmodule.resource;
 public class SpringMvcHttpResponseCodesPerson {
     private String name;
 
+    public SpringMvcHttpResponseCodesPerson() {
+        
+    }
+
     public SpringMvcHttpResponseCodesPerson(String name) {
         this.name = name;
     }
