ad60c429393f067b988a1f32a594a36017b0089a
Ron Sigal <rsigal@redhat.com>
2017-02-06 12:49:33 -0500

    [RESTEASY-1225]
    Correcting ResteasyProviderFactory's handling of the context map stack.

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/spi/ResteasyProviderFactory.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/spi/ResteasyProviderFactory.java
@@ -524,9 +524,14 @@ public class ResteasyProviderFactory extends RuntimeDelegate implements Provider
 
    public static void pushContextDataMap(Map<Class<?>, Object> map)
    {
-      contextualData.setLast(map);
+      contextualData.push(map);
    }
 
+   public static void popContextDataMap()
+   {
+      contextualData.pop();
+   }
+   
    public static Map<Class<?>, Object> getContextDataMap()
    {
       return getContextDataMap(true);
