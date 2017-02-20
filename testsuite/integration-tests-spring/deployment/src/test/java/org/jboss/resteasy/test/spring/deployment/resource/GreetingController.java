2eef172e0a51954ebb1ac9989cdfed34761cd2fe
rsearls <rsearls@localhost.localdomain>
2017-02-20 16:23:43 -0500

    [RESTEASY-1611] added testcase for springframeworkMVC

---
+++ testsuite/integration-tests-spring/deployment/src/test/java/org/jboss/resteasy/test/spring/deployment/resource/GreetingController.java
@@ -0,0 +1,18 @@
+package org.jboss.resteasy.test.spring.deployment.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: rsearls
+ * Date: 2/20/17
+ */
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return "World";
+    }
+}
+
---
+++ testsuite/integration-tests-spring/deployment/src/test/java/org/jboss/resteasy/test/spring/deployment/resource/NumbersResource.java
@@ -0,0 +1,35 @@
+package org.jboss.resteasy.test.spring.deployment.resource;
+
+import java.util.stream.Stream;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * User: rsearls
+ * Date: 2/20/17
+ */
+@Controller
+@Path("numbers")
+public class NumbersResource {
+
+    @GET
+    @Produces("application/json")
+    public JsonArray numbers() {
+        JsonArrayBuilder array = Json.createArrayBuilder();
+        Stream<String> numberStream = Stream.generate(System::currentTimeMillis)
+            .map(String::valueOf)
+            .limit(10);
+        numberStream.forEach(array::add);
+        return array.build();
+
+    }
+
+}
+
