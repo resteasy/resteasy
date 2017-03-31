14c8ef2199e55b14fe94c20f673210ecc730648c
Ron Sigal <rsigal@redhat.com>
2017-03-30 21:19:02 -0400

    [RESTEASY-1422]
    Handle response headers with View entity in resteasy-html.

--- providers/resteasy-html/src/main/java/org/jboss/resteasy/plugins/providers/html/HtmlRenderableWriter.java
+++ providers/resteasy-html/src/main/java/org/jboss/resteasy/plugins/providers/html/HtmlRenderableWriter.java
@@ -1,8 +1,15 @@
 package org.jboss.resteasy.plugins.providers.html;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.WriteListener;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -11,6 +18,8 @@ import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
@@ -30,7 +39,7 @@ public class HtmlRenderableWriter implements MessageBodyWriter<Renderable>
 	 * @see javax.ws.rs.ext.MessageBodyWriter#getSize(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
 	 */
 	//@Override
-	public long getSize(Renderable obj, Class type, Type genericType, Annotation[] annotations, MediaType mediaType)
+	public long getSize(Renderable obj, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
 	{
 		// No chance of figuring this out ahead of time
 		return -1;
@@ -40,7 +49,7 @@ public class HtmlRenderableWriter implements MessageBodyWriter<Renderable>
 	 * @see javax.ws.rs.ext.MessageBodyWriter#isWriteable(java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType)
 	 */
 	//@Override
-	public boolean isWriteable(Class type, Type genericType, Annotation[] annotations, MediaType mediaType)
+	public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType)
 	{
 		return Renderable.class.isAssignableFrom(type);
 	}
@@ -49,12 +58,108 @@ public class HtmlRenderableWriter implements MessageBodyWriter<Renderable>
 	 * @see javax.ws.rs.ext.MessageBodyWriter#writeTo(java.lang.Object, java.lang.Class, java.lang.reflect.Type, java.lang.annotation.Annotation[], javax.ws.rs.core.MediaType, javax.ws.rs.core.MultivaluedMap, java.io.OutputStream)
 	 */
 	//@Override
-	public void writeTo(Renderable viewingPleasure, Class type, Type genericType, Annotation[] annotations, MediaType mediaType,
-			MultivaluedMap httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException
+	public void writeTo(Renderable viewingPleasure, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+			MultivaluedMap<String, Object> httpHeaders, final OutputStream entityStream) throws IOException, WebApplicationException
 	{
 		try
 		{
-			viewingPleasure.render(getContextData(HttpServletRequest.class), getContextData(HttpServletResponse.class));
+			viewingPleasure.render(
+					// disable async processing as that would mess with interceptors,
+					// and entityStream is committed after writeTo and interceptors returns.
+					new HttpServletRequestWrapper(getContextData(HttpServletRequest.class)) {
+
+						@Override
+						public boolean isAsyncSupported() {
+							return false;
+						}
+
+						@Override
+						public boolean isAsyncStarted() {
+							return false;
+						}
+
+						@Override
+						public AsyncContext getAsyncContext() {
+							throw new IllegalStateException();
+						}
+
+						@Override
+						public AsyncContext startAsync() throws IllegalStateException {
+							throw new IllegalStateException();
+						}
+
+						@Override
+						public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+							throw new IllegalStateException();
+						}
+					},
+
+					// RESTEASY-1422: wrap entityStream to make sure headers added through JAX-RS are committed when the ServletOutputStream is written to.
+					// Also disable async processing on the ServletOutputStream, for consistency with the request.
+					new HttpServletResponseWrapper(getContextData(HttpServletResponse.class)) {
+
+						private ServletOutputStream outputStream;
+						private PrintWriter writer;
+
+						@Override
+						public ServletOutputStream getOutputStream() {
+
+							if (writer != null) {
+								throw new IllegalStateException();
+							}
+							if (outputStream == null) {
+								outputStream = new ServletOutputStream() {
+
+									@Override
+									public boolean isReady() {
+										return true;
+									}
+
+									@Override
+									public void setWriteListener(WriteListener writeListener) {
+										throw new IllegalStateException();
+									}
+
+									@Override
+									public void write(int b) throws IOException {
+										entityStream.write(b);
+									}
+
+									@Override
+									public void write(byte[] b) throws IOException {
+										entityStream.write(b);
+									}
+
+									@Override
+									public void write(byte[] b, int off, int len) throws IOException {
+										entityStream.write(b, off, len);
+									}
+
+									@Override
+									public void flush() throws IOException {
+										entityStream.flush();
+									}
+
+									@Override
+									public void close() throws IOException {
+										entityStream.close();
+									}
+								};
+							}
+							return outputStream;
+						}
+
+						@Override
+						public PrintWriter getWriter() throws IOException {
+							if (outputStream != null) {
+								throw new IllegalStateException();
+							}
+							if (writer == null) {
+								writer = new PrintWriter(new OutputStreamWriter(entityStream, getCharacterEncoding()));
+							}
+							return writer;
+						}
+					});
 		}
 		catch (ServletException ex)
 		{
---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/html/HeadersInViewResponseTest.java
@@ -0,0 +1,93 @@
+package org.jboss.resteasy.test.providers.html;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.NewCookie;
+import javax.ws.rs.core.Response;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.test.providers.html.resource.HeadersInViewResponseResource;
+import org.jboss.resteasy.utils.PortProviderUtil;
+import org.jboss.resteasy.utils.TestUtil;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @tpSubChapter HTML provider
+ * @tpChapter Integration tests
+ * @tpSince RESTEasy 3.1.3.Final
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class HeadersInViewResponseTest {
+
+    static ResteasyClient client;
+
+    @Deployment()
+    public static Archive<?> deploy() {
+        WebArchive war = TestUtil.prepareArchive(HeadersInViewResponseTest.class.getSimpleName());
+        war.addAsLibrary(getResteasyHtmlJar());
+        return TestUtil.finishContainerPrepare(war, null, HeadersInViewResponseResource.class);
+    }
+
+    @Before
+    public void init() {
+        client = new ResteasyClientBuilder().build();
+    }
+
+    @After
+    public void after() throws Exception {
+        client.close();
+    }
+
+    private String generateURL(String path) {
+        return PortProviderUtil.generateURL(path, HeadersInViewResponseTest.class.getSimpleName());
+    }
+
+    /**
+     * @tpTestDetails Tests HTTP headers set in Response with View entity.
+     * @tpInfo RESTEASY-1422
+     * @tpSince RESTEasy 3.1.3.Final
+     */
+    @Test
+    public void testView() throws Exception {
+       
+       Invocation.Builder request = client.target(generateURL("/test/get")).request();
+       Response response = request.get();
+       Map<String, NewCookie> map = response.getCookies();
+       Assert.assertEquals("123", response.getHeaderString("abc"));
+       Assert.assertEquals("value1", map.get("name1").getValue());
+       Assert.assertEquals("789", response.getHeaderString("xyz"));
+       Assert.assertEquals("value2", map.get("name2").getValue());
+    }
+    
+    private static File getResteasyHtmlJar()
+    {
+       Path path = Paths.get("..", "..", "providers", "resteasy-html", "target");
+       String s = path.toAbsolutePath().toString();
+       System.out.println("s: " + s);
+       File dir = new File(s);
+       for (File file : dir.listFiles())
+       {
+          String name = file.getName();
+          if (name.startsWith("resteasy-html") && name.endsWith(".jar") && !name.contains("sources"))
+          {
+             return file;
+          }
+       }
+       return null;
+    }
+}
