f7933055ae97543c4bd57907a60092f98a9fdbee
kanovotn <kanovotn@redhat.com>
2017-04-11 09:25:38 +0200

    [RESTEASY-1639] Test - exclude jaxrs subsystem from the deployment

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jackson2/resource/CustomJackson2ProviderApplication.java
@@ -0,0 +1,9 @@
+package org.jboss.resteasy.test.providers.jackson2.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class CustomJackson2ProviderApplication extends Application {
+
+}
