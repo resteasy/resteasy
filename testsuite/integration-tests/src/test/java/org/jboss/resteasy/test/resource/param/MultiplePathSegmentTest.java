d82d38c9a37873528f5d256c0d9e4c9bdf85244c
Ron Sigal <rsigal@redhat.com>
2017-05-17 21:19:34 -0400

    [RESTEASY-1653]
    Correct treatment of parameters consisting of multiple PathSegments.

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/resource/param/MultiplePathSegmentTest.java
@@ -0,0 +1,137 @@
+package org.jboss.resteasy.test.resource.param;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.core.Response;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.resteasy.test.resource.param.resource.MultiplePathSegmentResource;
+import org.jboss.resteasy.utils.PortProviderUtil;
+import org.jboss.resteasy.utils.TestUtil;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @tpSubChapter Parameters
+ * @tpChapter Integration tests
+ * @tpTestCaseDetails Test for @PathParam capturing multiple PathSegments (RESTEASY-1653)
+ * @tpSince RESTEasy 3.0.23.Final
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class MultiplePathSegmentTest {
+
+   private static Client client;
+
+   @Deployment
+   public static Archive<?> deploy() {
+      WebArchive war = TestUtil.prepareArchive(MultiplePathSegmentTest.class.getSimpleName());
+      return TestUtil.finishContainerPrepare(war, null, MultiplePathSegmentResource.class);
+   }
+
+   private String generateURL(String path) {
+      return PortProviderUtil.generateURL(path, MultiplePathSegmentTest.class.getSimpleName());
+   }
+
+   @BeforeClass
+   public static void setup() {
+       client = ClientBuilder.newClient();
+   }
+
+   @AfterClass
+   public static void cleanup() {
+       client.close();
+   }
+   
+   /**
+    * @tpTestDetails Array of PathSegments captured by wildcard
+    * @tpSince RESTEasy 3.0.23.Final
+    */
+   @Test
+   public void testWildcardArray() throws Exception
+   {
+      Invocation.Builder request = client.target(generateURL("/a/b/c/array/3")).request();
+      Response response = request.get();
+      System.out.println("status: " + response.getStatus());
+      Assert.assertEquals(200, response.getStatus());
+      response.close();
+   }
+
+   /**
+    * @tpTestDetails List of PathSegments captured by wildcard
+    * @tpSince RESTEasy 3.0.23.Final
+    */
+   @Test
+   public void testWildcardList() throws Exception
+   {
+      Invocation.Builder request = client.target(generateURL("/a/b/c/list/3")).request();
+      Response response = request.get();
+      System.out.println("status: " + response.getStatus());
+      Assert.assertEquals(200, response.getStatus());
+      response.close();
+   }
+
+   /**
+    * @tpTestDetails ArrayList of PathSegments captured by wildcard
+    * @tpSince RESTEasy 3.0.23.Final
+    */
+   @Test
+   public void testWildcardArrayList() throws Exception
+   {
+      Invocation.Builder request = client.target(generateURL("/a/b/c/arraylist/3")).request();
+      Response response = request.get();
+      System.out.println("status: " + response.getStatus());
+      Assert.assertEquals(200, response.getStatus());
+      response.close();
+   }
+
+   /**
+    * @tpTestDetails Array of PathSegments captured by two separate segments with the same name
+    * @tpSince RESTEasy 3.0.23.Final
+    */
+   @Test
+   public void testTwoSegmentsArray() throws Exception
+   {
+      Invocation.Builder request = client.target(generateURL("/a/b/array")).request();
+      Response response = request.get();
+      System.out.println("status: " + response.getStatus());
+      Assert.assertEquals(200, response.getStatus());
+      response.close();
+   }
+
+   /**
+    * @tpTestDetails List of PathSegments captured by two separate segments with the same name
+    * @tpSince RESTEasy 3.0.23.Final
+    */
+   @Test
+   public void testTwoSegmentsList() throws Exception
+   {
+      Invocation.Builder request = client.target(generateURL("/a/b/list")).request();
+      Response response = request.get();
+      System.out.println("status: " + response.getStatus());
+      Assert.assertEquals(200, response.getStatus());
+      response.close();
+   }
+
+   /**
+    * @tpTestDetails ArrayList of PathSegments captured by two separate segments with the same name
+    * @tpSince RESTEasy 3.0.23.Final
+    */
+   @Test
+   public void testTwoSegmentsArrayList() throws Exception
+   {
+      Invocation.Builder request = client.target(generateURL("/a/b/arraylist")).request();
+      Response response = request.get();
+      System.out.println("status: " + response.getStatus());
+      Assert.assertEquals(200, response.getStatus());
+      response.close();
+   }
+}
---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/resource/param/resource/MultiplePathSegmentResource.java
@@ -0,0 +1,53 @@
+package org.jboss.resteasy.test.resource.param.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.Response;
+
+@Path("")
+public class MultiplePathSegmentResource {
+
+   @GET
+   @Path("{segments:.*}/array/{count}")
+   public Response getWildcardArray(@PathParam("segments") PathSegment[] segments, @PathParam("count") int count) {
+      return Response.status(segments.length == count ? 200 : 400).build();
+   }
+
+   @GET
+   @Path("{segments:.*}/list/{count}")
+   public Response getWildcardList(@PathParam("segments") List<PathSegment> segments, @PathParam("count") int count) {
+      return Response.status(segments.size() == count ? 200 : 400).build();
+   }
+
+   @GET
+   @Path("{segments:.*}/arraylist/{count}")
+   public Response getWildcardArrayList(@PathParam("segments") ArrayList<PathSegment> segments, @PathParam("count") int count) {
+      return Response.status(segments.size() == count ? 200 : 400).build();
+   }
+
+   @GET
+   @Path("{segment}/{segment}/array")
+   public Response getTwoSegmentsArray(@PathParam("segment") PathSegment[] segments) {
+      System.out.println("array segments: " + segments.length);
+      return Response.status(segments.length == 2 ? 200 : 400).build();
+   }
+
+   @GET
+   @Path("{segment}/{segment}/list")
+   public Response getTwoSegmentsList(@PathParam("segment") List<PathSegment> segments) {
+      System.out.println("array segments: " + segments.size());
+      return Response.status(segments.size() == 2 ? 200 : 400).build();
+   }
+
+   @GET
+   @Path("{segment}/{segment}/arraylist")
+   public Response getTwoSegmentsArrayList(@PathParam("segment") ArrayList<PathSegment> segments) {
+      System.out.println("array segments: " + segments.size());
+      return Response.status(segments.size() == 2 ? 200 : 400).build();
+   }
+}
