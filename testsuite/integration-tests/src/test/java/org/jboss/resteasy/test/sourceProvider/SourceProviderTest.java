15dd0ce909c129f67c0a47be1cf17dbe8999f61d
Ingo Weiss <ingo@redhat.com>
2017-05-25 09:05:38 +0100

    [RESTEASY-1649] SourceProvider fails to write StreamSource created with StringReader
    Issue: https://issues.jboss.org/browse/RESTEASY-1649

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/sourceProvider/SourceProviderTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/sourceProvider/SourceProviderTest.java
@@ -3,18 +3,22 @@ package org.jboss.resteasy.test.sourceProvider;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.test.sourceProvider.resource.Book;
 import org.jboss.resteasy.test.sourceProvider.resource.BookResource;
 import org.jboss.resteasy.test.sourceProvider.resource.SourceProviderApp;
 import org.jboss.resteasy.util.HttpResponseCodes;
+import org.jboss.resteasy.utils.PortProviderUtil;
+import org.jboss.resteasy.utils.TestUtil;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 import javax.xml.transform.stream.StreamSource;
@@ -28,51 +32,43 @@ import java.nio.charset.StandardCharsets;
 public class SourceProviderTest {
 
     private static Client client;
-    private static final String PATH = "http://localhost:8080/sourceProvider/test";
-
-    String book = "<book><title>Monkey kingdom</title></book>";
+    private String book = "<book><title>Monkey kingdom</title></book>";
 
     @Deployment
     public static Archive<?> createTestArchive() {
-        WebArchive war = ShrinkWrap.create(WebArchive.class, "sourceProvider.war")
-            .addClasses(SourceProviderApp.class)
-            .addClasses(Book.class)
-            .addClasses(BookResource.class);
-        System.out.println(war.toString(true));
-        return war;
+        WebArchive war = TestUtil.prepareArchive(SourceProviderTest.class.getSimpleName());
+        return TestUtil.finishContainerPrepare(war, null, SourceProviderApp.class,
+                BookResource.class, Book.class);
     }
 
     @Before
-    public void init() {
-        client = new ResteasyClientBuilder().build();
+    public void before() throws Exception {
+        client = ClientBuilder.newClient();
     }
 
     @After
-    public void after() throws Exception {
+    public void close() throws Exception {
         client.close();
     }
 
-    // This shows the bug
-    @Test
-    public void testSourceWithStringReader() throws Exception {
-        try {
-            Response response = client.target(PATH).request()
-                .post(Entity.entity(new StreamSource(new StringReader(book)), "application/*+xml"));
-            Assert.assertEquals(HttpResponseCodes.SC_OK, response.getStatus());
-            String entity = response.readEntity(String.class);
-            Assert.assertTrue(entity.contentEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><book><title>Monkey kingdom</title></book>"));
-        } catch (Exception e) {
-            e.printStackTrace();    //todo for debugging only
-            throw e;
-        }
+    private String generateURL(String path) {
+        return PortProviderUtil.generateURL(path, SourceProviderTest.class.getSimpleName());
     }
 
-    // This case works correctly
+    @Test
+    public void testSourceWithStringReader() throws Exception {
+        Response response = client.target(generateURL("/test")).request()
+                .post(Entity.entity(new StreamSource(new StringReader(book)), "application/*+xml"));
+        Assert.assertEquals(HttpResponseCodes.SC_OK, response.getStatus());
+        String entity = response.readEntity(String.class);
+        Assert.assertTrue(entity.contentEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><book><title>Monkey kingdom</title></book>"));
+    }
+
     @Test
     public void testSourceWithInputStream() throws Exception {
         InputStream stream = new ByteArrayInputStream(book.getBytes(StandardCharsets.UTF_8));
-        Response response = client.target(PATH).request()
-            .post(Entity.entity(new StreamSource(stream), "application/*+xml"));
+        Response response = client.target(generateURL("/test")).request()
+                .post(Entity.entity(new StreamSource(stream), "application/*+xml"));
         Assert.assertEquals(HttpResponseCodes.SC_OK, response.getStatus());
         String entity = response.readEntity(String.class);
         Assert.assertTrue(entity.contentEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><book><title>Monkey kingdom</title></book>"));
