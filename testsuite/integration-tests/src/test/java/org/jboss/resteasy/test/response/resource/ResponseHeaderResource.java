4249b1a9e651deeb0c0fadd09fdac58a36fb274d
rsearls <rsearls@redhat.com>
2017-06-06 11:29:02 -0400

    [RESTEASY-1659] changed the algorithm for HEADER putAll and added test for change (#1179)

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/resource/ResponseHeaderResource.java
@@ -0,0 +1,15 @@
+package org.jboss.resteasy.test.response.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+@Path("/test")
+public class ResponseHeaderResource {
+   @GET
+   @Produces("text/plain")
+   public String get() {
+      throw new ResponseHeaderExceptionMapperRuntimeException();
+   }
+}
