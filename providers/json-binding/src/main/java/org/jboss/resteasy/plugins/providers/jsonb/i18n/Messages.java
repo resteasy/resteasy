9f13e45055f2a574733befed82b28d8a3abaaa7e
rsearls <rsearls@localhost.localdomain>
2017-06-18 22:22:52 -0400

    [RESTEASY-1665] JSON-Binding provider and client-side support

---
+++ providers/json-binding/src/main/java/org/jboss/resteasy/plugins/providers/jsonb/i18n/Messages.java
@@ -0,0 +1,20 @@
+package org.jboss.resteasy.plugins.providers.jsonb.i18n;
+
+import org.jboss.logging.annotations.Message;
+import org.jboss.logging.annotations.Message.Format;
+import org.jboss.logging.annotations.MessageBundle;
+
+/**
+ */
+@MessageBundle(projectCode = "RESTEASY")
+public interface Messages
+{
+   Messages MESSAGES = org.jboss.logging.Messages.getBundle(Messages.class);
+   int BASE = 8200;
+
+   @Message(id = BASE + 00, value = "JSON Binding deserialization error {0}", format=Format.MESSAGE_FORMAT)
+   String jsonBDeserializationError(String element);
+
+   @Message(id = BASE + 05, value = "JSON Binding serialization error {0}", format=Format.MESSAGE_FORMAT)
+   String jsonBSerializationError(String element);
+}
