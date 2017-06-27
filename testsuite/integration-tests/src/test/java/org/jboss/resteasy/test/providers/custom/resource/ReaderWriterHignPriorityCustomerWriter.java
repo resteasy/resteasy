fd6f975d1d9774f1c1f1a0ecbf00ad3299b99937
Jim Ma <ema@redhat.com>
2017-06-27 13:59:38 +0800

    [RESTEASY-1672]:Select highest priority provider if there are more suitable MessageBodyReaders/MessageBodyWriters

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/custom/resource/ReaderWriterHignPriorityCustomerWriter.java
@@ -0,0 +1,31 @@
+package org.jboss.resteasy.test.providers.custom.resource;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Produces("application/xml")
+@Priority(Priorities.USER - 100)
+public class ReaderWriterHignPriorityCustomerWriter extends ReaderWriterCustomerWriter implements MessageBodyWriter<ReaderWriterCustomer>
+{
+
+   @Override
+   public void writeTo(ReaderWriterCustomer customer, Class<?> type, Type genericType, Annotation[] annotations,
+         MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream)
+         throws IOException, WebApplicationException
+   {
+      String out = "<customer><name>high priority</name></customer>";
+      entityStream.write(out.getBytes());
+   }
+}
