88cf7040471823235f9ef2ccefd69b4e65cd7fec
kanovotn <kanovotn@redhat.com>
2017-06-28 13:57:53 +0200

    [RESTEASY-1227] Add user testcase

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/custom/resource/MediaTypeFromMessageBodyWriterResource2.java
@@ -0,0 +1,15 @@
+package org.jboss.resteasy.test.providers.custom.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("")
+public class MediaTypeFromMessageBodyWriterResource2 {
+
+    @GET
+    public Response getJson() {
+        return Response.ok().entity(new CustomProviderPreferenceUser("dummy", "dummy@dummy.com")).build();
+    }
+}
--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/resource/basic/MediaTypeFromMessageBodyWriterTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/resource/basic/MediaTypeFromMessageBodyWriterTest.java
@@ -7,13 +7,18 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.test.providers.custom.resource.CustomProviderPreferenceUser;
+import org.jboss.resteasy.test.providers.custom.resource.MediaTypeFromMessageBodyWriterResource2;
+import org.jboss.resteasy.test.providers.custom.resource.MediaTypeFromMessageBodyWriterTextJson;
 import org.jboss.resteasy.test.resource.basic.resource.MediaTypeFromMessageBodyWriterListAsText;
 import org.jboss.resteasy.test.resource.basic.resource.MediaTypeFromMessageBodyWriterListAsXML;
 import org.jboss.resteasy.test.resource.basic.resource.MediaTypeFromMessageBodyWriterResource;
+import org.jboss.resteasy.util.HttpResponseCodes;
 import org.jboss.resteasy.utils.PortProviderUtil;
 import org.jboss.resteasy.utils.TestUtil;
 import org.jboss.shrinkwrap.api.Archive;
@@ -67,13 +72,22 @@ public class MediaTypeFromMessageBodyWriterTest {
        accepts.add("text/plain");
     }
 
-    @Deployment
+    @Deployment(name = "multiple")
     public static Archive<?> deploy() {
         WebArchive war = TestUtil.prepareArchive(MediaTypeFromMessageBodyWriterTest.class.getSimpleName());
         return TestUtil.finishContainerPrepare(war, null,
               MediaTypeFromMessageBodyWriterListAsText.class,
               MediaTypeFromMessageBodyWriterListAsXML.class,
               MediaTypeFromMessageBodyWriterResource.class);
+    }
+
+    @Deployment(name = "single")
+    public static Archive<?> deploy2() {
+        WebArchive war = TestUtil.prepareArchive(MediaTypeFromMessageBodyWriterTest.class.getSimpleName() + "_single");
+        return TestUtil.finishContainerPrepare(war, null,
+                CustomProviderPreferenceUser.class,
+                MediaTypeFromMessageBodyWriterTextJson.class,
+                MediaTypeFromMessageBodyWriterResource2.class);
     }
 
     @Before
@@ -90,11 +104,16 @@ public class MediaTypeFromMessageBodyWriterTest {
         return PortProviderUtil.generateURL(path, MediaTypeFromMessageBodyWriterTest.class.getSimpleName());
     }
 
+    private String generateURLUserCase(String path) {
+        return PortProviderUtil.generateURL(path, MediaTypeFromMessageBodyWriterTest.class.getSimpleName()+ "_single");
+    }
+
     /**
      * @tpTestDetails Test that MessageBodyWriters can be consulted for media type
      * @tpSince RESTEasy 3.1.3.Final
      */
     @Test
+    @OperateOnDeployment("multiple")
     public void test() throws Exception {
        WebTarget base = client.target(generateURL(""));
        Response response = null;
@@ -113,5 +132,19 @@ public class MediaTypeFromMessageBodyWriterTest {
           }
        }
        client.close();
+    }
+
+    /**
+     * @tpTestDetails Test use case described in RESTEASY-1227. If multiple Accept headers are send by the client the chosen
+     * response Content-type will be the one which we are able to write (MessageBodyWriter exists for it), not the one
+     * we are not able to write.
+     * @tpSince RESTEasy 3.1.3.Final
+     */
+    @Test
+    @OperateOnDeployment("single")
+    public void test2() throws Exception {
+        Response response = client.target(generateURLUserCase("")).request().accept("text/html", "image/jpg", "text/json", "*").get();
+        Assert.assertEquals(HttpResponseCodes.SC_OK, response.getStatus());
+        Assert.assertEquals("text/json;charset=UTF-8", response.getHeaderString("Content-type"));
     }
 }
