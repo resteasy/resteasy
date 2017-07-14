a2e9740f473c6fe37ef94c89fda56450b00a2487
Alessio Soldano <asoldano@redhat.com>
2017-07-14 16:26:53 +0200

    [RESTEASY-1686] Adding a bunch of integration tests, including scenarios described on JIRA (thanks Nicolas Nesmon)

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/mbw/resource/MessageBodyWriterObjectMessage.java
@@ -0,0 +1,9 @@
+package org.jboss.resteasy.test.providers.mbw.resource;
+
+public class MessageBodyWriterObjectMessage {
+   
+   private String s;
+   public MessageBodyWriterObjectMessage(String s) { this.s = s; }
+   public String getS() { return s; }
+   public void setS(String s) { this.s = s; }
+}
