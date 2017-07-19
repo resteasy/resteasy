d6f201dce93846c9351c9f2335548ade111882b8
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2017-07-19 17:07:57 +0100

    [JBEAP-12010] [RESTEASY-1200][RESTEASY-975][RESTEASY-1463] Automatically discover Apache HTTP 43+ availability

--- jaxrs/resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/ResteasyClientBuilder.java
+++ jaxrs/resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/ResteasyClientBuilder.java
@@ -70,6 +70,16 @@ public class ResteasyClientBuilder extends ClientBuilder
    }
 
    private static final boolean useOldHTTPClient = Boolean.getBoolean("org.jboss.resteasy.client.useOldHTTPClient");
+   private static final boolean newHTTPClientAvailable;
+   static {
+      boolean res = true;
+      try {
+         Class.forName(HttpClientBuilder43.class.getName());
+      } catch (Throwable t) {
+         res = false;
+      }
+      newHTTPClientAvailable = res;
+   }
    protected KeyStore truststore;
    protected KeyStore clientKeyStore;
    protected String clientPrivateKeyPassword;
@@ -372,7 +382,7 @@ public class ResteasyClientBuilder extends ClientBuilder
    @Override
    public ResteasyClient build()
    {
-      if (useOldHTTPClient) {
+      if (useOldHTTPClient || !newHTTPClientAvailable) {
          return buildOld();
       }
       ClientConfiguration config = new ClientConfiguration(getProviderFactory());
