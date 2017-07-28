065320b336ed13536fd4e1dd4c619c2a619d1a49
rsearls <rsearls@localhost.localdomain>
2017-07-28 08:59:00 -0400

    [RESTEASY-1671] checked for null inputStream

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/proxy/resource/ProxyNullInputStreamResource.java
@@ -0,0 +1,16 @@
+package org.jboss.resteasy.test.client.proxy.resource;
+
+import javax.ws.rs.HEAD;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+/**
+ * Created by rsearls on 8/23/17.
+ */
+@Path("test")
+public interface ProxyNullInputStreamResource {
+   @HEAD
+   @Path("/user/{db}")
+   public void getUserHead(@PathParam("db") String db);
+}
+
