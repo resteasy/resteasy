7f531c445055f2213a8a6e99f1b924b61d42e6f7
Stephane Epardaud <stef@epardaud.fr>
2017-09-04 13:29:30 +0200

    Added support for AsyncStreamProvider

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/ThreadLocalResteasyProviderFactory.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/core/ThreadLocalResteasyProviderFactory.java
@@ -34,6 +34,7 @@ import org.jboss.resteasy.core.interception.jaxrs.JaxrsInterceptorRegistry;
 import org.jboss.resteasy.core.interception.jaxrs.ReaderInterceptorRegistry;
 import org.jboss.resteasy.core.interception.jaxrs.WriterInterceptorRegistry;
 import org.jboss.resteasy.spi.AsyncResponseProvider;
+import org.jboss.resteasy.spi.AsyncStreamProvider;
 import org.jboss.resteasy.spi.ConstructorInjector;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.spi.HttpResponse;
@@ -581,6 +582,18 @@ public class ThreadLocalResteasyProviderFactory extends ResteasyProviderFactory 
    public Map<Class<?>, AsyncResponseProvider> getAsyncResponseProviders()
    {
       return getDelegate().getAsyncResponseProviders();
+   }
+
+   @Override
+   public <T> AsyncStreamProvider<T> getAsyncStreamProvider(Class<T> type)
+   {
+      return getDelegate().getAsyncStreamProvider(type);
+   }
+   
+   @Override
+   public Map<Class<?>, AsyncStreamProvider> getAsyncStreamProviders()
+   {
+      return getDelegate().getAsyncStreamProviders();
    }
 
    @Override
