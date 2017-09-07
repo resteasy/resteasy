1bbf2daaf4d44eff8d9950c5a228bbd9b8a2c0a5
Jim Ma <ema@redhat.com>
2017-09-07 13:31:40 +0800

    [RESTEAY-1680]:Reads event with different eol; clean up charset constant

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/providers/sse/SseConstants.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/providers/sse/SseConstants.java
@@ -1,6 +1,5 @@
 package org.jboss.resteasy.plugins.providers.sse;
 
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 import javax.ws.rs.core.GenericType;
@@ -8,14 +7,23 @@ import javax.ws.rs.core.GenericType;
 public class SseConstants
 {
    public static final String LAST_EVENT_ID_HEADER = "Last-Event-ID";
+
    public static final GenericType<String> STRING_AS_GENERIC_TYPE = new GenericType<>(String.class);
-   public static final Charset UTF8 = Charset.forName("UTF-8");   
+
    public static final byte[] COMMENT_LEAD = ": ".getBytes(StandardCharsets.UTF_8);
-   public static final byte[] NAME_LEAD = "event: ".getBytes(UTF8);
+
+   public static final byte[] NAME_LEAD = "event: ".getBytes(StandardCharsets.UTF_8);
+
    public static final byte[] ID_LEAD = "id: ".getBytes(StandardCharsets.UTF_8);
+
    public static final byte[] RETRY_LEAD = "retry: ".getBytes(StandardCharsets.UTF_8);
+
    public static final byte[] DATA_LEAD = "data: ".getBytes(StandardCharsets.UTF_8);
+
    public static final byte[] EOL = "\n".getBytes(StandardCharsets.UTF_8);
+
+   //event delimiter can be '\r\r', '\n\n' or '\r\n\r\n'
+   public static final byte[] EVENT_DELIMITER = "\r\n\r\n".getBytes(StandardCharsets.UTF_8);
 
    public enum EVENT {
       START, COMMENT, FIELD,
