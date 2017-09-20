fc5c0d4b2c8a2c73ce718548e4f531dccb91ee28
Frederik Boster <frederik.boster@kpit.com>
2017-09-20 17:02:01 +0200

    [RESTEASY-1706] Add integration test for jax-rs annotation inheritance with generics.
    * Add the Arquillian integration test AnnotationInheritanceGenericsTest to resteasy-integration-tests.
    JIRA Issue: https://issues.jboss.org/projects/RESTEASY/issues/RESTEASY-1706

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/core/basic/resource/AnnotationInheritanceGenericsEntity.java
@@ -0,0 +1,20 @@
+package org.jboss.resteasy.test.core.basic.resource;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "entity")
+public class AnnotationInheritanceGenericsEntity {
+
+    private Long id;
+
+    @XmlElement
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/core/basic/resource/AnnotationInheritanceGenericsInterface.java
@@ -0,0 +1,26 @@
+package org.jboss.resteasy.test.core.basic.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.Collection;
+
+public interface AnnotationInheritanceGenericsInterface<ENTITY_TYPE, ID_TYPE> {
+
+    @GET
+    Collection<ENTITY_TYPE> get();
+
+    @GET
+    @Path("{id}")
+    ENTITY_TYPE get(@PathParam("id") final ID_TYPE id);
+
+    @POST
+    ENTITY_TYPE post(ENTITY_TYPE entity);
+
+    @PUT
+    @Path("{id}")
+    ENTITY_TYPE put(@PathParam("id") final ID_TYPE id, final ENTITY_TYPE entity);
+
+}
