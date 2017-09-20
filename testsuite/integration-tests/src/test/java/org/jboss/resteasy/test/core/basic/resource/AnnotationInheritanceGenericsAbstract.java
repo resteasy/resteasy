0a05163865ae58bfa331c4caa123c3efdfaf797a
Frederik Boster <frederik.boster@kpit.com>
2017-09-20 17:02:01 +0200

    [RESTEASY-1706] Add integration test for jax-rs annotation inheritance with generics.
    * Add the Arquillian integration test AnnotationInheritanceGenericsTest to resteasy-integration-tests.
    JIRA Issue: https://issues.jboss.org/projects/RESTEASY/issues/RESTEASY-1706

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/core/basic/resource/AnnotationInheritanceGenericsAbstract.java
@@ -0,0 +1,34 @@
+package org.jboss.resteasy.test.core.basic.resource;
+
+import javax.ws.rs.NotAllowedException;
+import javax.ws.rs.PUT;
+import java.util.Collection;
+
+public class AnnotationInheritanceGenericsAbstract<ENTITY_TYPE, ID_TYPE> implements AnnotationInheritanceGenericsInterface<ENTITY_TYPE, ID_TYPE> {
+
+    @Override
+    public Collection<ENTITY_TYPE> get() {
+        throw new NotAllowedException((Throwable) null);
+    }
+
+    @Override
+    public ENTITY_TYPE get(final ID_TYPE id) {
+        throw new NotAllowedException((Throwable) null);
+    }
+
+    @Override
+    public ENTITY_TYPE post(final ENTITY_TYPE entity) {
+        throw new NotAllowedException((Throwable) null);
+    }
+
+    @Override
+    public ENTITY_TYPE put(final ID_TYPE id, final ENTITY_TYPE entity) {
+        throw new NotAllowedException((Throwable) null);
+    }
+
+    @PUT
+    public ENTITY_TYPE put(final ENTITY_TYPE entity) {
+        throw new NotAllowedException((Throwable) null);
+    }
+
+}
