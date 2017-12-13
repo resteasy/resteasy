070f2c48d12c7d8b729f6188a9add158865a0c1b
Alessio Soldano <asoldano@redhat.com>
2017-12-13 16:14:14 +0100

    Further improve AsyncResponseCallback

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/resource/AsyncResponseCallback.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/resource/AsyncResponseCallback.java
@@ -1,27 +1,36 @@
 package org.jboss.resteasy.test.response.resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import javax.ws.rs.container.CompletionCallback;
 
 public class AsyncResponseCallback implements CompletionCallback {
 
-   private static volatile boolean called;
-   private static volatile Throwable error;
+   private static CountDownLatch latch;
+   private static Throwable error;
 
    public AsyncResponseCallback()
    {
-      called = false;
+      latch = new CountDownLatch(1);
       error = null;
    }
    
    @Override
    public void onComplete(Throwable throwable)
    {
-      called = true;
+      latch.countDown();
       error = throwable;
    }
    
    public static void assertCalled(boolean withError) 
    {
+      boolean called = false;
+      try {
+         called = latch.await(2, TimeUnit.SECONDS);
+      } catch (Exception e) {
+         //ignore
+      }
       if(!called)
          throw new AssertionError("Not called");
       if(withError && error == null
