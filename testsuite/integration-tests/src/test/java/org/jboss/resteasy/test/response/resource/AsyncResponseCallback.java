18ad10016112922197a706c8c68593ae6791ec9a
Alessio Soldano <asoldano@redhat.com>
2017-12-13 15:05:34 +0100

    wait 2 seconds before giving up on receiving the callback invocation (the callback is actually invoked *after* invocation is actually completed)

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/resource/AsyncResponseCallback.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/resource/AsyncResponseCallback.java
@@ -1,34 +1,46 @@
 package org.jboss.resteasy.test.response.resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import javax.ws.rs.container.CompletionCallback;
 
 public class AsyncResponseCallback implements CompletionCallback {
 
-   private static volatile boolean called;
-   private static volatile Throwable error;
-   private final String text;
-   private static String st;
+//   private static volatile boolean called;
+   private static CountDownLatch latch;
+   private static Throwable error;
+//   private final String text;
+//   private static String st;
 
    public AsyncResponseCallback(String text)
    {
-      System.out.println("ARC: " + text);
-      this.text = text;
-      st = text;
-      called = false;
+//      System.out.println("ARC: " + text);
+//      this.text = text;
+//      st = text;
+      latch = new CountDownLatch(1);
+//      called = false;
       error = null;
    }
    
    @Override
    public void onComplete(Throwable throwable)
    {
-      System.out.println("ARC.oncomplete: " + text);
-      called = true;
+//      System.out.println("ARC.oncomplete: " + text);
+      latch.countDown();
+//      called = true;
       error = throwable;
    }
    
    public static void assertCalled(boolean withError) 
    {
-      System.out.println("ARC.assertCalled (s): " + st);
+//      System.out.println("ARC.assertCalled (s): " + st);
+      boolean called = false;
+      try {
+         called = latch.await(2, TimeUnit.SECONDS);
+      } catch (Exception e) {
+         //ignore
+      }
       if(!called)
          throw new AssertionError("Not called");
       if(withError && error == null
