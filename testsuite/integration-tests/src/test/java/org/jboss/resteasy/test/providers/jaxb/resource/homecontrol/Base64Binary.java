202ef6c3211c6bb7b383a78f636d6af5216dc2b7
rsearls <rsearls@localhost.localdomain>
2017-12-14 20:30:56 -0500

    [RESTEASY-1754] add getter for JAXBContext and add testcase

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jaxb/resource/homecontrol/Base64Binary.java
@@ -0,0 +1,85 @@
+package org.jboss.resteasy.test.providers.jaxb.resource.homecontrol;
+
+import org.jboss.resteasy.test.providers.jaxb.resource.homecontrol.BinaryType;
+
+import javax.xml.bind.annotation.*;
+
+
+/**
+ * <p>Java-Klasse für base64Binary complex type.
+ *
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ *
+ * <pre>
+ * &lt;complexType name="base64Binary"&gt;
+ *   &lt;simpleContent&gt;
+ *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema&gt;base64Binary"&gt;
+ *       &lt;attribute ref="{http://www.w3.org/2005/05/xmlmime}contentType"/&gt;
+ *     &lt;/extension&gt;
+ *   &lt;/simpleContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ *
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "base64Binary", propOrder = {
+        "value"
+})
+@XmlSeeAlso({
+        BinaryType.class
+})
+public class Base64Binary {
+
+   @XmlValue
+   protected byte[] value;
+   @XmlAttribute(name = "contentType", namespace = "http://www.w3.org/2005/05/xmlmime")
+   protected String contentType;
+
+   /**
+    * Ruft den Wert der value-Eigenschaft ab.
+    *
+    * @return
+    *     possible object is
+    *     byte[]
+    */
+   public byte[] getValue() {
+      return value;
+   }
+
+   /**
+    * Legt den Wert der value-Eigenschaft fest.
+    *
+    * @param value
+    *     allowed object is
+    *     byte[]
+    */
+   public void setValue(byte[] value) {
+      this.value = value;
+   }
+
+   /**
+    * Ruft den Wert der contentType-Eigenschaft ab.
+    *
+    * @return
+    *     possible object is
+    *     {@link String }
+    *
+    */
+   public String getContentType() {
+      return contentType;
+   }
+
+   /**
+    * Legt den Wert der contentType-Eigenschaft fest.
+    *
+    * @param value
+    *     allowed object is
+    *     {@link String }
+    *
+    */
+   public void setContentType(String value) {
+      this.contentType = value;
+   }
+
+}
---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jaxb/resource/homecontrol/ObjectFactory.java
@@ -0,0 +1,108 @@
+package org.jboss.resteasy.test.providers.jaxb.resource.homecontrol;
+
+import org.jboss.resteasy.test.providers.jaxb.resource.homecontrol.UserType;
+import org.jboss.resteasy.test.providers.jaxb.resource.homecontrol.RoleType;
+import org.jboss.resteasy.test.providers.jaxb.resource.homecontrol.ErrorType;
+import org.jboss.resteasy.test.providers.jaxb.resource.homecontrol.ErrorMessageType;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each
+ * Java content interface and Java element interface
+ * generated in the de.creaity.homecontrol.service.rest.jaxb.v1 package.
+ * <p>An ObjectFactory allows you to programatically
+ * construct new instances of the Java representation
+ * for XML content. The Java representation of XML
+ * content can consist of schema derived interfaces
+ * and classes representing the binding of schema
+ * type definitions, element declarations and model
+ * groups.  Factory methods for each of these are
+ * provided in this class.
+ *
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+   private final static QName _User_QNAME = new QName("http://creaity.de/homecontrol/rest/types/v1", "user");
+   private final static QName _Error_QNAME = new QName("http://creaity.de/homecontrol/rest/types/v1", "error");
+   private final static QName _Id_QNAME = new QName("http://creaity.de/homecontrol/rest/types/v1", "id");
+
+   /**
+    * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: de.creaity.homecontrol.service.rest.jaxb.v1
+    *
+    */
+   public ObjectFactory() {
+   }
+
+   /**
+    * Create an instance of {@link UserType }
+    *
+    */
+   public UserType createUserType() {
+      return new UserType();
+   }
+
+   /**
+    * Create an instance of {@link ErrorMessageType }
+    *
+    */
+   public ErrorMessageType createErrorMessageType() {
+      return new ErrorMessageType();
+   }
+
+   /**
+    * Create an instance of {@link IDType }
+    */
+   public IDType createIDType() {
+      return new IDType();
+   }
+
+   /**
+    * Create an instance of {@link BinaryType }
+    *
+    */
+   public BinaryType createBinaryType() {
+      return new BinaryType();
+   }
+
+   /**
+    * Create an instance of {@link UserType.Credentials }
+    *
+    */
+   public UserType.Credentials createUserTypeCredentials() {
+      return new UserType.Credentials();
+   }
+
+   /**
+    * Create an instance of {@link JAXBElement }{@code <}{@link UserType }{@code >}}
+    *
+    */
+   @XmlElementDecl(namespace = "http://creaity.de/homecontrol/rest/types/v1", name = "user")
+   public JAXBElement<UserType> createUser(UserType value) {
+      return new JAXBElement<UserType>(_User_QNAME, UserType.class, null, value);
+   }
+
+   /**
+    * Create an instance of {@link JAXBElement }{@code <}{@link ErrorMessageType }{@code >}}
+    *
+    */
+   @XmlElementDecl(namespace = "http://creaity.de/homecontrol/rest/types/v1", name = "error")
+   public JAXBElement<ErrorMessageType> createError(ErrorMessageType value) {
+      return new JAXBElement<ErrorMessageType>(_Error_QNAME, ErrorMessageType.class, null, value);
+   }
+
+   /**
+    * Create an instance of {@link JAXBElement }{@code <}{@link IDType }{@code >}}
+    *
+    */
+   @XmlElementDecl(namespace = "http://creaity.de/homecontrol/rest/types/v1", name = "id")
+   public JAXBElement<IDType> createId(IDType value) {
+      return new JAXBElement<IDType>(_Id_QNAME, IDType.class, null, value);
+   }
+
+}
