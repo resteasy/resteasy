76120e89a49e20c506da368f9041d313757a00f1
NicoNes <nicolas.nesmon@gmail.com>
2018-01-27 11:34:02 +0100

    Once the SseBroadcaster is closed, subsequent calls have no effect and are ignored. Once the SseBroadcaster is closed, invoking any other method on the broadcaster instance would result in an IllegalStateException being thrown
    Signed-off-by: NicoNes <nicolas.nesmon@gmail.com>

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/providers/sse/SseBroadcasterImpl.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/providers/sse/SseBroadcasterImpl.java
@@ -5,6 +5,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -12,22 +13,29 @@ import javax.ws.rs.sse.OutboundSseEvent;
 import javax.ws.rs.sse.SseBroadcaster;
 import javax.ws.rs.sse.SseEventSink;
 
+import org.jboss.resteasy.resteasy_jaxrs.i18n.Messages;
+
 public class SseBroadcasterImpl implements SseBroadcaster
 {
-   private ConcurrentLinkedQueue<SseEventSink> outputQueue = new ConcurrentLinkedQueue<SseEventSink>();
+   private ConcurrentLinkedQueue<SseEventSink> outputQueue = new ConcurrentLinkedQueue<>();
 
    private final List<BiConsumer<SseEventSink, Throwable>> onErrorConsumers = new CopyOnWriteArrayList<>();
 
    private final List<Consumer<SseEventSink>> closeConsumers = new CopyOnWriteArrayList<>();
+   
+   private final AtomicBoolean closed = new AtomicBoolean();
 
    public SseBroadcasterImpl()
    {
-
    }
 
    @Override
    public void close()
    {
+	   if (!closed.compareAndSet(false, true))
+	   {
+		   return;
+	   }
       //Javadoc says close the broadcaster and all subscribed {@link SseEventSink} instances.
       //is it necessay to close the subsribed SseEventSink ?
       outputQueue.forEach(evenSink -> {
@@ -38,29 +46,40 @@ public class SseBroadcasterImpl implements SseBroadcaster
       });
       outputQueue.clear();
    }
+   
+   private void checkClosed()
+   {
+	   if (closed.get())
+	   {
+		   throw new IllegalStateException(Messages.MESSAGES.sseBroadcasterIsClosed());
+	   }
+   }
 
    @Override
    public void onError(BiConsumer<SseEventSink, Throwable> onError)
    {
+	  checkClosed();
       onErrorConsumers.add(onError);
    }
 
    @Override
    public void onClose(Consumer<SseEventSink> onClose)
    {
+	  checkClosed();
       closeConsumers.add(onClose);
    }
 
    @Override
    public void register(SseEventSink sseEventSink)
    {
+	  checkClosed();
       outputQueue.add(sseEventSink);
-
    }
 
    @Override
    public CompletionStage<?> broadcast(OutboundSseEvent event)
    {
+	  checkClosed();
       //return event immediately and doesn't block anything
       return CompletableFuture.runAsync(() -> {
          outputQueue.forEach(eventSink -> {
