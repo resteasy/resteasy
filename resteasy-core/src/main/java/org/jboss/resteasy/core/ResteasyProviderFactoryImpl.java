275c39932a09d08e45224f6d6a3cbed6c16b8cc9
NicoNes <nicolas.nesmon@gmail.com>
2018-02-11 12:48:37 +0100

    Rollback methods protection level to protected and non static
    Signed-off-by: NicoNes <nicolas.nesmon@gmail.com>

--- resteasy-core/src/main/java/org/jboss/resteasy/core/ResteasyProviderFactoryImpl.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/core/ResteasyProviderFactoryImpl.java
@@ -1288,12 +1288,12 @@ public class ResteasyProviderFactoryImpl extends ResteasyProviderFactory impleme
       return isA(target.getClass(), type, contracts);
    }
 
-   public static boolean isA(Class target, Class type, Map<Class<?>, Integer> contracts)
+   protected boolean isA(Class target, Class type, Map<Class<?>, Integer> contracts)
    {
       return isA(target, type, contracts == null ? null : contracts.keySet());
    }
    
-   public static boolean isA(Object target, Class type, Map<Class<?>, Integer> contracts)
+   protected boolean isA(Object target, Class type, Map<Class<?>, Integer> contracts)
    {
       return isA(target.getClass(), type, contracts);
    }
