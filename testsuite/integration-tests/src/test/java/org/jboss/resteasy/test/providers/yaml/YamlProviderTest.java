42b9e94c9cc6719c3ea046bd820edf34b3aed4e3
Alessio Soldano <asoldano@redhat.com>
2018-02-20 15:20:46 +0100

    Removing useless imports

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/yaml/YamlProviderTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/yaml/YamlProviderTest.java
@@ -2,7 +2,6 @@ package org.jboss.resteasy.test.providers.yaml;
 
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.resteasy.category.ExpectedFailing;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.junit.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -19,7 +18,6 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.yaml.snakeyaml.Yaml;
 
