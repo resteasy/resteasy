23cfbcdfd37e8e7b5a15e7f0254d95b69e6277f4
Petr Jurak <pjurak@redhat.com>
2018-03-27 09:02:34 +0200

    [RESTEASY-798] RestEasy takes into account (interface) methods without annotations

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/core/basic/resource/PartialAnnotationResource.java
@@ -0,0 +1,26 @@
+package org.jboss.resteasy.test.core.basic.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * A PartialAnnotationResource.
+ * 
+ * @author pjurak
+ */
+@Path("/test")
+public interface PartialAnnotationResource
+{
+   @GET
+   @Produces("text/plain")
+   String bar();
+
+   /** This is not REST method.
+    * 
+    * @return 
+    */
+   // @GET
+   // @Produces("text/plain")
+   String foo();
+}
