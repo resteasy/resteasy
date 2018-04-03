3a7338c6223a3ff3e317f25288f288954199141d
Jim Ma <ema@redhat.com>
2018-04-03 11:53:27 +0800

    [RESTEASY-1714]:Enable JSON-B binding provider by default

--- providers/json-binding/src/main/java/org/jboss/resteasy/plugins/providers/jsonb/JsonBindingProvider.java
+++ providers/json-binding/src/main/java/org/jboss/resteasy/plugins/providers/jsonb/JsonBindingProvider.java
@@ -1,9 +1,14 @@
 package org.jboss.resteasy.plugins.providers.jsonb;
 
-import org.jboss.resteasy.plugins.providers.jsonb.i18n.Messages;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.Map;
 
-import javax.json.bind.Jsonb;
 import javax.annotation.Priority;
+import javax.json.bind.Jsonb;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.ProcessingException;
@@ -13,17 +18,16 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
 import javax.ws.rs.ext.Provider;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Type;
-
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSeeAlso;
 import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
-import org.jboss.resteasy.util.FindAnnotation;
 import org.jboss.resteasy.plugins.providers.jsonb.i18n.LogMessages;
+import org.jboss.resteasy.plugins.providers.jsonb.i18n.Messages;
+import org.jboss.resteasy.util.FindAnnotation;
+import org.jboss.resteasy.util.Types;
 
 /**
  * Created by rsearls on 6/26/17.
@@ -38,9 +42,12 @@ public class JsonBindingProvider extends AbstractJsonBindingProvider
    @Override
    public boolean isReadable(Class<?> type, Type genericType,
                              Annotation[] annotations, MediaType mediaType) {
+      if (isGenericJaxb(type, genericType))
+      {
+         return false;
+      }
       return (isSupportedMediaType(mediaType))
-         && ((!type.isAnnotationPresent(XmlRootElement.class) && !type.isAnnotationPresent(XmlType.class))
-         || (FindAnnotation.findJsonBindingAnnotations(annotations).length != 0));
+            && ((!isJaxbClass(type)) || (FindAnnotation.findJsonBindingAnnotations(annotations).length != 0));
    }
 
    @Override
@@ -66,9 +73,12 @@ public class JsonBindingProvider extends AbstractJsonBindingProvider
    @Override
    public boolean isWriteable(Class<?> type, Type genericType,
                               Annotation[] annotations, MediaType mediaType) {
+      if (isGenericJaxb(type, genericType))
+      {
+         return false;
+      }
       return (isSupportedMediaType(mediaType))
-         && ((!type.isAnnotationPresent(XmlRootElement.class) && !type.isAnnotationPresent(XmlType.class))
-              || (FindAnnotation.findJsonBindingAnnotations(annotations).length != 0));
+            && ((!isJaxbClass(type)) || (FindAnnotation.findJsonBindingAnnotations(annotations).length != 0));
    }
 
    @Override
@@ -92,5 +102,39 @@ public class JsonBindingProvider extends AbstractJsonBindingProvider
       {
          throw new ProcessingException(Messages.MESSAGES.jsonBSerializationError(e.toString()));
       }
+   }
+   
+   private boolean isGenericJaxb(Class<?> type, Type genericType)
+   {
+      if (Map.class.isAssignableFrom(type) && genericType != null)
+      {
+         Class<?> valueType = Types.getMapValueType(genericType);
+         if (valueType != null && isJaxbClass(valueType))
+         {
+            return true;
+         }
+      }
+
+      if ((Collection.class.isAssignableFrom(type) || type.isArray()) && genericType != null)
+      {
+         Class<?> baseType = Types.getCollectionBaseType(type, genericType);
+         if (baseType != null && isJaxbClass(baseType))
+         {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   private boolean isJaxbClass(Class<?> classType)
+   {
+      if (classType.isAnnotationPresent(XmlRootElement.class) || classType.isAnnotationPresent(XmlType.class)
+            || classType.isAnnotationPresent(XmlJavaTypeAdapter.class)
+            || classType.isAnnotationPresent(XmlSeeAlso.class) || JAXBElement.class.equals(classType))
+      {
+         return true;
+      }
+      return false;
+
    }
 }
