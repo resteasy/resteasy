df18db31f72e4a727fc376ff0a16b0821b7951b2
Alessio Soldano <soldano.servizi@email.it>
2018-04-24 00:35:20 +0200

    [RESTEASY-1862] JettyClientEngine (#1495)
    * Jetty 9.4.9
    
    * Initial commit of JettyClientEngine
    
    * unit-tests: enable slf4j-simple logging
    
    * Add a request timeout parameter
    
    * remove shadowing variable, correct request body failure handler
    
    * JettyClientEngine WIP: disable streaming request body for now
    
    * WIP on defer tests
    
    * Prevent aborting requests that are already completing; force buffering mode for jax-rs client instead of throwing
    
    * Brief documentation
    
    * Updates to doc and removal of commented code

---
+++ resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/engines/JettyClientResponse.java
@@ -0,0 +1,33 @@
+package org.jboss.resteasy.client.jaxrs.engines;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.jboss.resteasy.client.jaxrs.internal.ClientConfiguration;
+import org.jboss.resteasy.client.jaxrs.internal.ClientResponse;
+
+class JettyClientResponse extends ClientResponse {
+    private final Runnable cancel;
+    private InputStream stream;
+
+    JettyClientResponse(ClientConfiguration configuration, InputStream stream, Runnable cancel) {
+        super(configuration);
+        this.cancel = cancel;
+        this.stream = stream;
+    }
+
+    @Override
+    protected InputStream getInputStream() {
+        return stream;
+    }
+
+    @Override
+    protected void setInputStream(InputStream is) {
+        stream = is;
+    }
+
+    @Override
+    public void releaseConnection() throws IOException {
+        cancel.run();
+    }
+}
---
+++ resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/engines/JettyResponseStream.java
@@ -0,0 +1,81 @@
+package org.jboss.resteasy.client.jaxrs.engines;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.function.ToIntFunction;
+
+import org.eclipse.jetty.util.Callback;
+
+class JettyResponseStream extends InputStream {
+    private final Deque<Chunk> chunks = new LinkedList<>();
+    private Chunk readTop;
+    private volatile boolean closed;
+
+    void offer(ByteBuffer content, Callback callback) {
+        if (closed) {
+            final IllegalStateException x = new IllegalStateException("closed");
+            callback.failed(x);
+            throw x;
+        }
+        chunks.add(new Chunk(content, callback));
+    }
+
+    @Override
+    public int read() throws IOException {
+        return read0(chunk -> chunk.buf.get());
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        return read0(chunk -> {
+            final int r = Math.min(chunk.buf.remaining(), len);
+            chunk.buf.get(b, off, r);
+            return r;
+        });
+    }
+
+    private int read0(ToIntFunction<Chunk> reader) throws IOException {
+        if (closed) {
+            throw new IllegalStateException("closed");
+        }
+        if (readTop == null) {
+            readTop = chunks.pollFirst();
+        }
+        if (readTop == null || closed) {
+            return -1;
+        }
+
+        final int result = reader.applyAsInt(readTop);
+        if (!readTop.buf.hasRemaining()) {
+            readTop.callback.succeeded();
+            readTop = null;
+        }
+        return result;
+    }
+
+    @Override
+    public void close() throws IOException {
+        closed = true;
+        if (readTop != null) {
+            readTop.callback.succeeded();
+            readTop = null;
+        }
+        chunks.removeIf(c -> {
+            c.callback.succeeded();
+            return true;
+        });
+    }
+
+    static class Chunk {
+        final ByteBuffer buf;
+        final Callback callback;
+
+        Chunk(ByteBuffer buf, Callback callback) {
+            this.buf = buf;
+            this.callback = callback;
+        }
+    }
+}
