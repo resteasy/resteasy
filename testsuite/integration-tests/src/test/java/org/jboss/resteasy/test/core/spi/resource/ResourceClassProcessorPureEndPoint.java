dde89355fb558e1b328fed2a91dade574b4386b1
Marek Kopecky <mkopecky@redhat.com>
2018-05-04 13:22:57 +0200

    [RESTEASY-1805] Tests for resource metadata SPI

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/core/spi/resource/ResourceClassProcessorPureEndPoint.java
@@ -0,0 +1,15 @@
+package org.jboss.resteasy.test.core.spi.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/pure")
+public class ResourceClassProcessorPureEndPoint {
+    @GET
+    @Path("pure")
+    @Produces("text/plain")
+    public String getLocating() {
+        return "<a></a>";
+    }
+}
