0d8d71c9b087b1d70e9ccec7c838bb5f929e439b
Marek Kopecky <mkopecky@redhat.com>
2018-05-22 15:39:59 +0200

    [RESTEASY-1910] Fix ValidationComplexTest custom bean validation constraint

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/validation/resource/ValidationComplexOtherGroupConstraint.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/validation/resource/ValidationComplexOtherGroupConstraint.java
@@ -10,7 +10,7 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Documented
-@Constraint(validatedBy = ValidationComplexClassValidator.class)
+@Constraint(validatedBy = ValidationComplexOtherGroupValidator.class)
 @Target({TYPE})
 @Retention(RUNTIME)
 public @interface ValidationComplexOtherGroupConstraint {
