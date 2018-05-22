bdd1ba3363906fc39c1e16442ad7cac19ab9248d
Weinan Li <l.weinan@gmail.com>
2018-05-22 21:34:36 +0800

    .

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/tracing/OnDemandTracingTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/tracing/OnDemandTracingTest.java
@@ -3,10 +3,7 @@ package org.jboss.resteasy.test.tracing;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.plugins.server.servlet.ResteasyContextParameters;
-import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
-import org.jboss.resteasy.test.resource.basic.ConstructedInjectionTest;
 import org.jboss.resteasy.tracing.RESTEasyTracingLogger;
 import org.jboss.resteasy.util.HttpResponseCodes;
 import org.jboss.resteasy.utils.PortProviderUtil;
@@ -28,11 +25,11 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Arquillian.class)
 @RunAsClient
-public class BasicTracingTest {
+public class OnDemandTracingTest {
     static WebArchive war;
     static Client client;
 
@@ -48,45 +45,58 @@ public class BasicTracingTest {
     }
 
     private String generateURL(String path) {
-        return PortProviderUtil.generateURL(path, BasicTracingTest.class.getSimpleName());
+        return PortProviderUtil.generateURL(path, OnDemandTracingTest.class.getSimpleName());
     }
 
     @Deployment
     public static Archive<?> createDeployment() {
-        war = TestUtil.prepareArchive(BasicTracingTest.class.getSimpleName());
-        war.addAsResource(new File(BasicTracingTest.class.getClassLoader().getResource("org/jboss/resteasy/test/tracing/logging.properties").getFile()), "logging.properties");
-        Map<String, String> params = new HashMap<>();
-        params.put(ResteasyContextParameters.RESTEASY_TRACING_TYPE, ResteasyContextParameters.RESTEASY_TRACING_TYPE_ALL);
-        params.put(ResteasyContextParameters.RESTEASY_TRACING_THRESHOLD, ResteasyContextParameters.RESTEASY_TRACING_LEVEL_VERBOSE);
+        war = TestUtil.prepareArchive(OnDemandTracingTest.class.getSimpleName());
+        war.addAsResource(new File(BasicTracingTest.class.getClassLoader()
+                .getResource("org/jboss/resteasy/test/tracing/logging.properties").getFile()), "logging.properties");
+//        war.as(ZipExporter.class).exportTo(new File("/tmp/" + war.getName()), true);
 
+        Map<String, String> params = new HashMap<>();
+        params.put(ResteasyContextParameters.RESTEASY_TRACING_TYPE, ResteasyContextParameters.RESTEASY_TRACING_TYPE_ON_DEMAND);
         return TestUtil.finishContainerPrepare(war, params, TracingApp.class,
                 TracingConfigResource.class, HttpMethodOverride.class);
     }
 
     @Test
-    public void testBasic() throws InterruptedException {
-//        war.as(ZipExporter.class).exportTo(new File("/tmp/" + war.getName()), true);
-//        Thread.currentThread().join();
+    public void testOnDemand() {
         String url = generateURL("/logger");
-//        System.out.println("::: " + url);
-        Thread.currentThread().join();
         WebTarget base = client.target(url);
         try {
+
             Response response = base.request().get();
+            testTracingEnabled(response, false);
             Assert.assertEquals(HttpResponseCodes.SC_OK, response.getStatus());
-            boolean hasTracing = false;
-            for (Map.Entry entry : response.getStringHeaders().entrySet()) {
-                System.out.println("<K, V> ->" + entry);
-                if (entry.getKey().toString().startsWith(RESTEasyTracingLogger.HEADER_TRACING_PREFIX)) {
-                    hasTracing = true;
-                    break;
-                }
-            }
-            assertTrue(hasTracing);
+
             response.close();
+
+//            Thread.currentThread().join();
+
+            // enable ON_DEMAND mode
+            Response response2 = base.request().header(RESTEasyTracingLogger.HEADER_ACCEPT, "").
+                    header(RESTEasyTracingLogger.HEADER_THRESHOLD, ResteasyContextParameters.RESTEASY_TRACING_LEVEL_VERBOSE).
+                    get();
+            testTracingEnabled(response2, true);
+            response2.close();
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-}
 
+    private void testTracingEnabled(Response response, boolean flag) {
+        boolean hasTracing = false;
+        for (Map.Entry entry : response.getStringHeaders().entrySet()) {
+            System.out.println("<K, V> ->" + entry);
+            if (entry.getKey().toString().startsWith(RESTEasyTracingLogger.HEADER_TRACING_PREFIX)) {
+                hasTracing = true;
+                break;
+            }
+        }
+        assertEquals(flag, hasTracing);
+    }
+
+}
