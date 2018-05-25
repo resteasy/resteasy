f8963b87545a336b2ee8f21b38c83bff783b1a5e
Petr Jurak <pjurak@redhat.com>
2018-05-25 11:36:37 +0200

    [RESTEASY-1872] Fixed javadoc for providers-html

--- providers/resteasy-html/src/main/java/org/jboss/resteasy/plugins/providers/html/Renderable.java
+++ providers/resteasy-html/src/main/java/org/jboss/resteasy/plugins/providers/html/Renderable.java
@@ -10,13 +10,18 @@ import java.io.IOException;
  * If you return one of these from a resource method, RESTEasy-HTML will render it.  This
  * interface allows for exotic view rendering types.
  * 
- * @author Jeff Schnitzer <jeff@infohazard.org>
+ * @author <a href="mailto:jeff@infohazard.org">Jeff Schnitzer</a>
  */
 public interface Renderable
 {
    /**
     * Called to do the actual work of rendering a view.  Note that while ServletException
     * can be thrown, WebApplicationException is preferred.
+    * @param request http request
+    * @param response http response
+    * @throws IOException if I/O error occurred
+    * @throws ServletException if servlet error occurred
+    * @throws WebApplicationException if application error occurred
     */
    public void render(HttpServletRequest request, HttpServletResponse response)
        throws IOException, ServletException, WebApplicationException;
