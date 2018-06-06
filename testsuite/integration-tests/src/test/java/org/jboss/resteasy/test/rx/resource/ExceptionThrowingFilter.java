a1534bac43d481abd46f755ee9d227d9aa8eece3
Stephane Epardaud <stef@epardaud.fr>
2018-06-06 11:43:48 +0200

    Test filters that throw exceptions in async responses

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/rx/resource/ExceptionThrowingFilter.java
@@ -0,0 +1,25 @@
+package org.jboss.resteasy.test.rx.resource;
+
+import java.io.IOException;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@FilterException
+@Provider
+public class ExceptionThrowingFilter implements ContainerResponseFilter
+{
+
+   @Override
+   public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+         throws IOException
+   {
+      throw new WebApplicationException(Response.ok("exception", MediaType.TEXT_PLAIN).build());
+   }
+
+}
