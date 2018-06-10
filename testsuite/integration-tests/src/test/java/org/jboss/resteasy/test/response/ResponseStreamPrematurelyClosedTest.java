589cb79447ea00d19d43d105aace390d3d38f098
Alessio Soldano <soldano.servizi@email.it>
2018-06-10 02:29:36 +0200

    Avoid  intermittent "java.lang.OutOfMemoryError: Java heap space" on Travis when running ResponseStreamPrematurelyClosedTest
    * Print more logs in case of error
    * Avoid java.lang.OutOfMemoryError: Java heap space on Travis

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/ResponseStreamPrematurelyClosedTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/ResponseStreamPrematurelyClosedTest.java
@@ -105,7 +105,7 @@ public class ResponseStreamPrematurelyClosedTest {
                     coutDown.await(10, TimeUnit.SECONDS);
 
                     IOUtils.copy(ins, baos);
-                    Assert.assertEquals("Received string: " + baos.toShortString(), 100000000, baos.size());
+                    Assert.assertEquals("Received string: " + baos.toShortString(), 10000000, baos.size());
                 } finally {
                     //remove the listener
                     for (GarbageCollectorMXBean gcbean : gcbeans) {
@@ -132,10 +132,10 @@ public class ResponseStreamPrematurelyClosedTest {
         
         public String toShortString() {
             int s = size();
-            if (s <= 100) {
+            if (s <= 14000) {
                 return toString();
             } else {
-                return getSubstring(0, 50) + "..." + getSubstring(s - 50, 50);
+                return getSubstring(0, 1000) + "..." + getSubstring(s - 13000, 13000);
             }
         }
     }
