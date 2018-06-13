0a7b86c2900cbe8537712b854cef867249e2ecc8
Alessio Soldano <asoldano@redhat.com>
2018-06-13 11:36:34 +0200

    Further potential concurrency issue fixes

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jaxb/resource/XmlJavaTypeAdapterAlienAdapter.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jaxb/resource/XmlJavaTypeAdapterAlienAdapter.java
@@ -2,23 +2,25 @@ package org.jboss.resteasy.test.providers.jaxb.resource;
 
 import org.jboss.logging.Logger;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class XmlJavaTypeAdapterAlienAdapter extends XmlAdapter<XmlJavaTypeAdapterHuman, XmlJavaTypeAdapterAlien> {
-    public static int marshalCounter;
-    public static int unmarshalCounter;
+    public static AtomicInteger marshalCounter = new AtomicInteger(0);
+    public static AtomicInteger unmarshalCounter = new AtomicInteger(0);
     private static Logger logger = Logger.getLogger(XmlJavaTypeAdapterResource.class.getName());
 
     public static void reset() {
-        marshalCounter = 0;
-        unmarshalCounter = 0;
+        marshalCounter.set(0);
+        unmarshalCounter.set(0);
         logger.info("reset()");
     }
 
     @Override
     public XmlJavaTypeAdapterHuman marshal(XmlJavaTypeAdapterAlien alien) throws Exception {
         logger.info("Entering AlienAdapter.marshal()");
-        marshalCounter++;
+        marshalCounter.incrementAndGet();
         XmlJavaTypeAdapterHuman human = new XmlJavaTypeAdapterHuman();
         human.setName(reverse(alien.getName()));
         return human;
@@ -27,7 +29,7 @@ public class XmlJavaTypeAdapterAlienAdapter extends XmlAdapter<XmlJavaTypeAdapte
     @Override
     public XmlJavaTypeAdapterAlien unmarshal(XmlJavaTypeAdapterHuman human) throws Exception {
         logger.info("Entering AlienAdapter.unmarshal()");
-        unmarshalCounter++;
+        unmarshalCounter.incrementAndGet();
         XmlJavaTypeAdapterAlien alien = new XmlJavaTypeAdapterAlien();
         alien.setName(reverse(human.getName()));
         return alien;
