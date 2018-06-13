939b93704a2508d537c9cca6f716e1d051c031df
Alessio Soldano <asoldano@redhat.com>
2018-06-13 11:36:34 +0200

    Further potential concurrency issue fixes

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/resource/ClientCacheService.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/resource/ClientCacheService.java
@@ -19,7 +19,7 @@ public class ClientCacheService {
     @Produces("text/plain")
     @Cache(maxAge = 2)
     public String get() {
-        ClientCacheTest.count++;
+        ClientCacheTest.count.incrementAndGet();
         return "hello world" + ClientCacheTest.count;
     }
 
@@ -27,40 +27,40 @@ public class ClientCacheService {
     @GET
     @Produces("text/plain")
     public Response getEtagged(@Context Request request) {
-        ClientCacheTest.count++;
+        ClientCacheTest.count.incrementAndGet();
         Response.ResponseBuilder builder = request.evaluatePreconditions(new EntityTag("42"));
         CacheControl cc = new CacheControl();
         cc.setMaxAge(2);
         if (builder != null) {
             return builder.cacheControl(cc).build();
         }
-        return Response.ok("hello" + ClientCacheTest.count).cacheControl(cc).tag("42").build();
+        return Response.ok("hello" + ClientCacheTest.count.get()).cacheControl(cc).tag("42").build();
     }
 
     @Path("/etag/never/good")
     @GET
     @Produces("text/plain")
     public Response getEtaggedNeverGood(@Context Request request) {
-        ClientCacheTest.count++;
+        ClientCacheTest.count.incrementAndGet();
         Response.ResponseBuilder builder = request.evaluatePreconditions(new EntityTag("42"));
         if (builder != null) {
             return Response.serverError().build();
         }
         CacheControl cc = new CacheControl();
         cc.setMaxAge(2);
-        return Response.ok("hello" + ClientCacheTest.count).cacheControl(cc).tag("32").build();
+        return Response.ok("hello" + ClientCacheTest.count.get()).cacheControl(cc).tag("32").build();
     }
 
     @Path("/etag/always/validate")
     @GET
     @Produces("text/plain")
     public Response getValidateEtagged(@Context Request request) {
-        ClientCacheTest.count++;
+        ClientCacheTest.count.incrementAndGet();
         Response.ResponseBuilder builder = request.evaluatePreconditions(new EntityTag("42"));
         if (builder != null) {
             return builder.build();
         }
-        return Response.ok("hello" + ClientCacheTest.count).tag("42").build();
+        return Response.ok("hello" + ClientCacheTest.count.get()).tag("42").build();
     }
 
     @Path("/cacheit/{id}")
@@ -68,8 +68,8 @@ public class ClientCacheService {
     @Produces("text/plain")
     @Cache(maxAge = 3000)
     public String getCacheit(@PathParam("id") String id) {
-        ClientCacheTest.count++;
-        return "cachecache" + ClientCacheTest.count;
+        ClientCacheTest.count.incrementAndGet();
+        return "cachecache" + ClientCacheTest.count.get();
     }
 
 }
