99da7bcd5d467189443250c22a549cc95d674e88
Jan Bliznak <jbliznak@redhat.com>
2018-07-24 17:02:08 +0200

    [RESTEASY-1884] Use custom manually started container for GZIP tests instead of surefire execution

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/interceptor/gzip/NotAllowGzipOnServerAbstractTestBase.java
@@ -0,0 +1,32 @@
+package org.jboss.resteasy.test.interceptor.gzip;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.Archive;
+
+/**
+ * Abstract base class for tests with gzip disabled on server side.
+ *
+ * This abstract class provides deployments.
+ *
+ * This abstract class is extended by:
+ *      NotAllowGzipOnServerAllowGzipOnClientTest
+ *      NotAllowGzipOnServerNotAllowGzipOnClientTest
+ */
+public class NotAllowGzipOnServerAbstractTestBase extends GzipAbstractTestBase {
+
+   /**
+    * Deployment with javax.ws.rs.ext.Providers file, that contains gzip interceptor definition
+    */
+   @Deployment(name = WAR_WITH_PROVIDERS_FILE, testable = false)
+   public static Archive<?> createWebDeploymentWithGzipProvidersFile() {
+      return createWebArchive(WAR_WITH_PROVIDERS_FILE, true);
+   }
+
+   /**
+    * Deployment without any javax.ws.rs.ext.Providers file
+    */
+   @Deployment(name = WAR_WITHOUT_PROVIDERS_FILE, testable = false)
+   public static Archive<?> createWebDeploymentWithoutGzipProvidersFile() {
+      return createWebArchive(WAR_WITHOUT_PROVIDERS_FILE, false);
+   }
+}
