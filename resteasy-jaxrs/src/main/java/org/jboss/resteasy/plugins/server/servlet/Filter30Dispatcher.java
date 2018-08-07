11bce5639181fb6d4b8a0d9ee87290473672a093
Ron Sigal <rsigal@redhat.com>
2018-08-07 12:43:41 -0400

    [RESTEASY-1748]
    Added "value" parameter to @WebFilter and @WebServlet annotations, which
    is required by Servlet spec.

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/server/servlet/Filter30Dispatcher.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/server/servlet/Filter30Dispatcher.java
@@ -17,7 +17,7 @@ import java.util.concurrent.ScheduledExecutorService;
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
  * @version $Revision: 1 $
  */
-@WebFilter(asyncSupported = true)
+@WebFilter(asyncSupported = true, value="")
 public class Filter30Dispatcher extends FilterDispatcher
 {
    ScheduledExecutorService asyncCancelScheduler = Executors.newScheduledThreadPool(0); // this is to get around TCK tests that call setTimeout in a separate thread which is illegal.
