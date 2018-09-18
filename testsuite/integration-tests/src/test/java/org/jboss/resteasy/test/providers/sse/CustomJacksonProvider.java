107442f21c793818a2318c3021adf932db16aa2b
jimma <ema@redhat.com>
2018-09-18 10:29:41 +0800

    [RESTEASY-1916]:Fix InboundSseEventImpl doesn't getReader from ClientConfiguration (#1694)

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/sse/CustomJacksonProvider.java
@@ -0,0 +1,27 @@
+package org.jboss.resteasy.test.providers.sse;
+
+import javax.annotation.Priority;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Priority(Priorities.USER-200)
+public class CustomJacksonProvider extends ResteasyJackson2Provider {
+
+   public CustomJacksonProvider()
+   {
+      super();
+      ObjectMapper mapper = new ObjectMapper();
+      mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+      this.setMapper(mapper);
+   }
+ }
