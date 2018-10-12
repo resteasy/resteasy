276dc5d34197382142ecaf8d9e9128c03021d103
Alessio Soldano <asoldano@redhat.com>
2018-10-12 15:12:12 +0200

    [RESTEASY-2037] ..and close clients

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/asynch/AsyncUnhandledExceptionTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/asynch/AsyncUnhandledExceptionTest.java
@@ -47,8 +47,12 @@ public class AsyncUnhandledExceptionTest {
    public void testPost() {
 
       Client client = ClientBuilder.newClient();
-      Response response = client.target(generateURL("/listener")).request().post(Entity.entity("aaa", "text/plain"));
-      Assert.assertEquals(500, response.getStatus());
+      try {
+         Response response = client.target(generateURL("/listener")).request().post(Entity.entity("aaa", "text/plain"));
+         Assert.assertEquals(500, response.getStatus());
+      } finally {
+         client.close();
+      }
    }
    
    /**
@@ -58,7 +62,11 @@ public class AsyncUnhandledExceptionTest {
    @Test
    public void testGet() {
       Client client = ClientBuilder.newClient();
-      Response response = client.target(generateURL("/thread")).request().get();
-      Assert.assertEquals(500, response.getStatus());
+      try {
+         Response response = client.target(generateURL("/thread")).request().get();
+         Assert.assertEquals(500, response.getStatus());
+      } finally {
+         client.close();
+      }
    }
 }
