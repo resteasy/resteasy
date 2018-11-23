5e1d41b790ad1f5b55cb8237a6a9d7a7e2a38f37
Weinan Li <l.weinan@gmail.com>
2018-11-23 15:33:23 +0800

    remove left jettison tests

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jackson2/resource/Jackson2XmlResource.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jackson2/resource/Jackson2XmlResource.java
@@ -1,7 +1,6 @@
 package org.jboss.resteasy.test.providers.jackson2.resource;
 
 import org.jboss.resteasy.annotations.providers.NoJackson;
-//import org.jboss.resteasy.annotations.providers.jaxb.json.BadgerFish;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
