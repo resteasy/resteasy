86be017ac07479d5989b6909380104027e18ebdf
weli <l.weinan@gmail.com>
2019-03-05 11:56:58 +0800

    add resteasy-undertow-spring

---
+++ server-adapters/resteasy-undertow-spring/src/main/java/org/jboss/resteasy/plugins/server/undertow/spring/UndertowJaxrsSpringServer.java
@@ -0,0 +1,9 @@
+package org.jboss.resteasy.plugins.server.undertow.spring;
+
+
+import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
+
+public class UndertowJaxrsSpringServer extends UndertowJaxrsServer
+{
+
+}
