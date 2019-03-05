f0cf4b12d9703d2fabd650e9feb923e69e6b83ba
weli <l.weinan@gmail.com>
2019-03-05 22:13:24 +0800

    move test to resteasy-undertow-spring

--- server-adapters/resteasy-undertow-spring/src/test/java/org/jboss/resteasy/test/undertow/BasicTest.java
+++ server-adapters/resteasy-undertow-spring/src/test/java/org/jboss/resteasy/test/undertow/BasicTest.java
@@ -1,4 +1,4 @@
-package org.jboss.resteasy.springmvc.test.undertow;
+package org.jboss.resteasy.test.undertow;
 
 import io.undertow.Handlers;
 import io.undertow.Undertow;
@@ -7,22 +7,20 @@ import io.undertow.servlet.Servlets;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-import javax.servlet.ServletException;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class BasicTest {
     private static UndertowJaxrsServer server;
 
-    @Test
-    public void fooTest() {
-        UndertowJaxrsServer server = new UndertowJaxrsServer().start();
-        server.start();
-        server.stop();
-    }
-
-    @Test
-    public void basic() throws ServletException {
+    @Before
+    public void before() throws Exception {
+        server = new UndertowJaxrsServer().start();
         DeploymentInfo deploymentInfo = Servlets.deployment()
                 .setClassLoader(BasicTest.class.getClassLoader())
                 .setContextPath("/spring")
@@ -34,11 +32,35 @@ public class BasicTest {
         manager.deploy();
         PathHandler path = Handlers.path(Handlers.redirect("/myapp"))
                 .addPrefixPath("/spring", manager.start());
-
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
                 .setHandler(path)
                 .build();
         server.start();
     }
+
+    @After
+    public void after() {
+        server.stop();
+    }
+
+    @Test
+    public void findConfigurationTest() throws Exception {
+
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("springmvc-resteasy.xml");
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = is.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+        buffer.flush();
+        byte[] byteArray = buffer.toByteArray();
+
+        String text = new String(byteArray, StandardCharsets.UTF_8);
+        System.out.println(text);
+    }
+
 }
