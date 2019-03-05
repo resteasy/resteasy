80c99dc4f95d0d5a6ee7e2915f7eb7858b2542b5
Alessio Soldano <asoldano@redhat.com>
2019-03-05 23:52:35 +0100

    [RESTEASY-1715] Adding a warning in case of users forgetting to close apache http client 4.3 engine + providing another engine without finalizer for users who are brave enough to use it ;-)

--- resteasy-client-api/src/main/java/org/jboss/resteasy/client/jaxrs/i18n/LogMessages.java
+++ resteasy-client-api/src/main/java/org/jboss/resteasy/client/jaxrs/i18n/LogMessages.java
@@ -23,4 +23,8 @@ public interface LogMessages extends BasicLogger
    @LogMessage(level = Level.DEBUG)
    @Message(id = Messages.BASE + 171, value = "Ignoring exception thrown within InvocationCallback")
    void exceptionIgnored(@Cause Throwable ex);
+
+   @LogMessage(level = Level.WARN)
+   @Message(id = Messages.BASE + 187, value = "Closing a %s instance for you. Please close clients yourself.")
+   void closingForYou(Class<?> clazz);
 }
