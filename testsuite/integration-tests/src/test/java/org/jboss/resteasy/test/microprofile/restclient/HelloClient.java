6df89c0410e6d0c38105e7c419ae99aac3a20b18
Alessio Soldano <asoldano@redhat.com>
2019-03-06 17:36:39 +0100

    [RESTEASY-2139] Adding testcase

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/microprofile/restclient/HelloClient.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/microprofile/restclient/HelloClient.java
@@ -1,7 +1,12 @@
 package org.jboss.resteasy.test.microprofile.restclient;
 
+import java.util.concurrent.CompletionStage;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import io.reactivex.Single;
 
 @Path("/")
 public interface HelloClient {
@@ -10,4 +15,11 @@ public interface HelloClient {
     @Path("/hello")
     String hello();
 
+    @GET
+    @Path("some/{id}")
+    Single<String> single(@PathParam("id") String id);
+
+    @GET
+    @Path("some/{id}")
+    CompletionStage<String> cs(@PathParam("id") String id);
 }
