80440df6576bf282bd61e6d80eb09b34ee5a9c42
Alessio Soldano <asoldano@redhat.com>
2019-03-10 00:34:21 +0100

    [RESTEASY-2157] Fixing issue by using a composite proxy classloader

--- resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/ProxyBuilder.java
+++ resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/ProxyBuilder.java
@@ -12,12 +12,19 @@ import javax.ws.rs.Path;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.net.URL;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
+import java.security.SecureClassLoader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class ProxyBuilder<T>
@@ -82,7 +89,14 @@ public class ProxyBuilder<T>
       // infrastructure had some problems without this.
       clientProxy.setClazz(iface);
 
-      return (T) Proxy.newProxyInstance(config.getLoader(), intfs, clientProxy);
+      ClassLoader cl = config.getLoader();
+      try {
+         cl.loadClass(iface.getName());
+      } catch (Throwable t) {
+         cl = new DelegateClassLoader(iface.getClassLoader(), cl);
+      }
+
+      return (T) Proxy.newProxyInstance(cl, intfs, clientProxy);
    }
 
    private static <T> ClientInvoker createClientInvoker(Class<T> clazz, Method method, ResteasyWebTarget base, ProxyConfig config)
@@ -153,5 +167,107 @@ public class ProxyBuilder<T>
    }
 
 
+   public static class DelegateClassLoader extends SecureClassLoader
+   {
+      private final ClassLoader delegate;
+
+      private final ClassLoader parent;
+
+      public DelegateClassLoader(final ClassLoader delegate, final ClassLoader parent)
+      {
+         super(parent);
+         this.delegate = delegate;
+         this.parent = parent;
+      }
+
+      /** {@inheritDoc} */
+      @Override
+      public Class<?> loadClass(final String className) throws ClassNotFoundException
+      {
+         if (parent != null)
+         {
+            try
+            {
+               return parent.loadClass(className);
+            }
+            catch (ClassNotFoundException cnfe)
+            {
+               //NOOP, use delegate
+            }
+         }
+         return delegate.loadClass(className);
+      }
+
+      /** {@inheritDoc} */
+      @Override
+      public URL getResource(final String name)
+      {
+         URL url = null;
+         if (parent != null)
+         {
+            url = parent.getResource(name);
+         }
+         return (url == null) ? delegate.getResource(name) : url;
+      }
+
+      /** {@inheritDoc} */
+      @Override
+      public Enumeration<URL> getResources(final String name) throws IOException
+      {
+         final ArrayList<Enumeration<URL>> foundResources = new ArrayList<Enumeration<URL>>();
+
+         foundResources.add(delegate.getResources(name));
+         if (parent != null)
+         {
+            foundResources.add(parent.getResources(name));
+         }
+
+         return new Enumeration<URL>()
+         {
+            private int position = foundResources.size() - 1;
+
+            public boolean hasMoreElements()
+            {
+               while (position >= 0)
+               {
+                  if (foundResources.get(position).hasMoreElements())
+                  {
+                     return true;
+                  }
+                  position--;
+               }
+               return false;
+            }
+
+            public URL nextElement()
+            {
+               while (position >= 0)
+               {
+                  try
+                  {
+                     return (foundResources.get(position)).nextElement();
+                  }
+                  catch (NoSuchElementException e)
+                  {
+                  }
+                  position--;
+               }
+               throw new NoSuchElementException();
+            }
+         };
+      }
+
+      /** {@inheritDoc} */
+      @Override
+      public InputStream getResourceAsStream(final String name)
+      {
+         InputStream is = null;
+         if (parent != null)
+         {
+            is = parent.getResourceAsStream(name);
+         }
+         return (is == null) ? delegate.getResourceAsStream(name) : is;
+      }
+   }
 
 }
