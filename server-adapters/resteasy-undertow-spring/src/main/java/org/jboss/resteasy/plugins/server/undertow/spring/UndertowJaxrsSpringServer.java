fdc4e9192e36918a12e4fdbd4e46aeb9988fe4e9
weli <l.weinan@gmail.com>
2019-03-09 14:18:16 +0800

    add UndertowJaxrsServer

--- server-adapters/resteasy-undertow-spring/src/main/java/org/jboss/resteasy/plugins/server/undertow/spring/UndertowJaxrsSpringServer.java
+++ server-adapters/resteasy-undertow-spring/src/main/java/org/jboss/resteasy/plugins/server/undertow/spring/UndertowJaxrsSpringServer.java
@@ -1,9 +1,31 @@
 package org.jboss.resteasy.plugins.server.undertow.spring;
 
 
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.ServletInfo;
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
+import org.springframework.web.servlet.DispatcherServlet;
 
-public class UndertowJaxrsSpringServer extends UndertowJaxrsServer
-{
+import static io.undertow.servlet.Servlets.servlet;
 
+public class UndertowJaxrsSpringServer extends UndertowJaxrsServer {
+    public DeploymentInfo undertowDeployment(String mapping, String contextConfigLocation) {
+        if (mapping == null) mapping = "/";
+        if (!mapping.startsWith("/")) mapping = "/" + mapping;
+        if (!mapping.endsWith("/")) mapping += "/";
+        mapping = mapping + "*";
+        String prefix = null;
+
+        if (!mapping.equals("/*")) prefix = mapping.substring(0, mapping.length() - 2);
+
+        ServletInfo servlet =
+                servlet("ResteasyServlet", DispatcherServlet.class)
+                        .setLoadOnStartup(1)
+                        .addInitParam("contextConfigLocation", contextConfigLocation)
+                        .addMapping(mapping);
+
+        return new DeploymentInfo()
+                .addServlet(servlet);
+
+    }
 }
