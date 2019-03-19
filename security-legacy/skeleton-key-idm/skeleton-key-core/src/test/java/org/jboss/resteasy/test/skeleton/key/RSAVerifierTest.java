7fb1bfa14e0daae77876274d29ed328c36c8fcc4
R Searls <rsearls@redhat.com>
2019-03-19 12:56:34 -0400

    [RESTEASY-2195] 3.7 branch upgrade from sun security to Bouncycastle certificate generation

--- security-legacy/skeleton-key-idm/skeleton-key-core/src/test/java/org/jboss/resteasy/test/skeleton/key/RSAVerifierTest.java
+++ security-legacy/skeleton-key-idm/skeleton-key-core/src/test/java/org/jboss/resteasy/test/skeleton/key/RSAVerifierTest.java
@@ -1,32 +1,41 @@
 package org.jboss.resteasy.test.skeleton.key;
 
-import org.jboss.logging.Logger;
-import org.junit.Assert;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v1CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.crypto.util.PrivateKeyFactory;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMWriter;
-import org.bouncycastle.x509.X509V1CertificateGenerator;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.bc.BcRSAContentSignerBuilder;
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.jose.jws.JWSBuilder;
 import org.jboss.resteasy.jwt.JsonSerialization;
 import org.jboss.resteasy.skeleton.key.RSATokenVerifier;
 import org.jboss.resteasy.skeleton.key.ResourceMetadata;
 import org.jboss.resteasy.skeleton.key.VerificationException;
 import org.jboss.resteasy.skeleton.key.representations.SkeletonKeyToken;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import javax.security.auth.x500.X500Principal;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.math.BigInteger;
-import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Security;
-import java.security.SignatureException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Date;
 
@@ -50,25 +59,41 @@ public class RSAVerifierTest
       if (Security.getProvider("BC") == null) Security.addProvider(new BouncyCastleProvider());
    }
 
-   public static X509Certificate generateTestCertificate(String subject, String issuer, KeyPair pair) throws InvalidKeyException,
-         NoSuchProviderException, SignatureException
-   {
+   public static X509Certificate generateTestCertificate(String subject, String issuer,
+                  KeyPair pair) throws IOException, OperatorCreationException,
+                  CertificateException {
 
-      X509V1CertificateGenerator certGen = new X509V1CertificateGenerator();
+         X500Name subjectDN = new X500Name(subject);
+         X500Name    issuerDN = new X500Name(issuer);
 
-      certGen.setSerialNumber(BigInteger.valueOf(System.currentTimeMillis()));
-      certGen.setIssuerDN(new X500Principal(issuer));
-      certGen.setNotBefore(new Date(System.currentTimeMillis() - 10000));
-      certGen.setNotAfter(new Date(System.currentTimeMillis() + 10000));
-      certGen.setSubjectDN(new X500Principal(subject));
-      certGen.setPublicKey(pair.getPublic());
-      certGen.setSignatureAlgorithm("SHA256WithRSAEncryption");
+         Date validityStartDate = new Date(System.currentTimeMillis() - 10000);
+         Date validityEndDate = new Date(System.currentTimeMillis() + 10000);
+         SubjectPublicKeyInfo subPubKeyInfo = SubjectPublicKeyInfo
+            .getInstance(pair.getPublic().getEncoded());
 
-      return certGen.generateX509Certificate(pair.getPrivate(), "BC");
+         X509v1CertificateBuilder certGen = new X509v1CertificateBuilder(
+            issuerDN, BigInteger.valueOf(System.currentTimeMillis()),
+            validityStartDate, validityEndDate, subjectDN, subPubKeyInfo);
+
+         X509CertificateHolder holder = certGen.build(createSigner(pair.getPrivate()));
+
+         return new JcaX509CertificateConverter().getCertificate(holder);
+   }
+
+   private static ContentSigner createSigner(PrivateKey privateKey) throws IOException,
+      OperatorCreationException {
+         AlgorithmIdentifier sigAlgId = new DefaultSignatureAlgorithmIdentifierFinder()
+            .find("SHA256WithRSAEncryption");
+         AlgorithmIdentifier digAlgId = new DefaultDigestAlgorithmIdentifierFinder()
+            .find(sigAlgId);
+
+         return new BcRSAContentSignerBuilder(sigAlgId, digAlgId)
+            .build(PrivateKeyFactory.createKey(privateKey.getEncoded()));
    }
 
    @BeforeClass
-   public static void setupCerts() throws NoSuchAlgorithmException, InvalidKeyException, NoSuchProviderException, SignatureException
+   public static void setupCerts() throws NoSuchAlgorithmException,
+      IOException, OperatorCreationException, CertificateException
    {
       badPair = KeyPairGenerator.getInstance("RSA").generateKeyPair();
       idpPair = KeyPairGenerator.getInstance("RSA").generateKeyPair();
