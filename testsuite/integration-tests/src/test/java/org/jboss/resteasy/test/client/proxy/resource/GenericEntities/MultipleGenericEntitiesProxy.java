dbe700c8e0d8811cda339567fe0d5ddc2627dfaf
Marek Marusic <marek.marusic@gmail.com>
2019-03-22 12:34:15 +0100

    [RESTEASY-1432] - Interface with generic type does not work with client proxy

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/proxy/resource/GenericEntities/MultipleGenericEntitiesProxy.java
@@ -0,0 +1,5 @@
+package org.jboss.resteasy.test.client.proxy.resource.GenericEntities;
+
+public interface MultipleGenericEntitiesProxy extends MultipleGenericEntities<String, EntityExtendingBaseEntity> {
+
+}
