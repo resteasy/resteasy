aedabdb753b50c3a893af86a4fffa53774f2f386
Jan Bliznak <jbliznak@redhat.com>
2019-03-27 19:58:24 +0100

    [RESTEASY-2205] Fix JSON mapping to object at JSON tracing test

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/tracing/JsonFormattedTracingInfoTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/tracing/JsonFormattedTracingInfoTest.java
@@ -1,11 +1,11 @@
 package org.jboss.resteasy.test.tracing;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.logging.Logger;
-import org.jboss.resteasy.tracing.api.RESTEasyTracing;
-import org.jboss.resteasy.tracing.api.RESTEasyTracingMessage;
 import org.jboss.resteasy.spi.HttpResponseCodes;
+import org.jboss.resteasy.tracing.api.RESTEasyTracing;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -23,10 +23,7 @@ public class JsonFormattedTracingInfoTest extends BasicTracingTest {
 
    @Test
    @OperateOnDeployment(WAR_BASIC_TRACING_FILE)
-//    @Category({ExpectedFailingOnWildFly11.class,
-//            ExpectedFailingOnWildFly12.class,})
-   public void testJsonTracing() throws Exception {
-//        Thread.currentThread().join();
+   public void testJsonTracing() {
 
       String url = generateURL("/logger", WAR_BASIC_TRACING_FILE);
       WebTarget base = client.target(url);
@@ -37,16 +34,17 @@ public class JsonFormattedTracingInfoTest extends BasicTracingTest {
          Assert.assertEquals(HttpResponseCodes.SC_OK, response.getStatus());
          boolean hasTracing = false;
          for (Map.Entry entry : response.getStringHeaders().entrySet()) {
-//                System.out.println("<K, V> ->" + entry);
             if (entry.getKey().toString().startsWith(RESTEasyTracing.HEADER_TRACING_PREFIX)) {
                hasTracing = true;
                String jsonText = entry.getValue().toString();
                ObjectMapper objectMapper = new ObjectMapper();
 
-               List<RESTEasyTracingMessage> messageList = objectMapper.readValue(jsonText, List.class);
+               TypeReference<List<List<Map<String, String>>>> jsonTraceMsgsListTypeRef =
+                     new TypeReference<List<List<Map<String, String>>>>() {};
+               List<List<Map>> messageList = objectMapper.readValue(jsonText, jsonTraceMsgsListTypeRef);
                assertNotNull(messageList);
                assertNotNull(messageList.get(0));
-               List<Map> list = (List<Map>) messageList.get(0);
+               List<Map> list = messageList.get(0);
 
                String[] keys = {"requestId", "duration", "text", "event", "timestamp"};
                for (Map map : list) {
