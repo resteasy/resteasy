5a9384ba400951d5e065054935bf081dc003e230
Steven Schlansker <stevenschlansker@gmail.com>
2019-04-16 03:22:34 -0700

    [RESTEASY-2218] Remove unnecessary and dangerous ArrayList cast (#1992)
    ```
    java.lang.ClassCastException: class java.util.LinkedList cannot be cast to class java.util.ArrayList (java.util.LinkedList and java.util.ArrayList are in module java.base of loader 'bootstrap')
    at org.jboss.resteasy.plugins.interceptors.MessageSanitizerContainerResponseFilter.filter(MessageSanitizerContainerResponseFilter.java:46)
    at org.jboss.resteasy.core.interception.jaxrs.ContainerResponseContextImpl.filter(ContainerResponseContextImpl.java:362)
    at org.jboss.resteasy.core.ServerResponseWriter.executeFilters(ServerResponseWriter.java:219)
    ```

--- resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/interceptors/encoding/MessageSanitizerContainerResponseFilter.java
+++ resteasy-jaxrs/src/main/java/org/jboss/resteasy/plugins/interceptors/encoding/MessageSanitizerContainerResponseFilter.java
@@ -1,7 +1,7 @@
 package org.jboss.resteasy.plugins.interceptors.encoding;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 
 import javax.annotation.Priority;
@@ -43,7 +43,7 @@ public class MessageSanitizerContainerResponseFilter implements ContainerRespons
       if (HttpResponseCodes.SC_BAD_REQUEST == responseContext.getStatus()) {
          Object entity = responseContext.getEntity();
          if (entity != null && entity instanceof String) {
-            ArrayList contentTypes = (ArrayList)responseContext.getHeaders().get("Content-Type");
+            List<Object> contentTypes = responseContext.getHeaders().get("Content-Type");
             if (contentTypes != null  && containsHtmlText(contentTypes)) {
                String escapedMsg = escapeXml((String) entity);
                responseContext.setEntity(escapedMsg);
@@ -88,8 +88,8 @@ public class MessageSanitizerContainerResponseFilter implements ContainerRespons
       return sb.toString();
    }
 
-   private boolean containsHtmlText(ArrayList<Object> list) {
-      for (Object o : list) {
+   private boolean containsHtmlText(List<Object> list) {
+      for (Object o :list) {
          if (o instanceof MediaType && MediaType.TEXT_HTML_TYPE.isCompatible((MediaType) o)) {
             return true;
          }
