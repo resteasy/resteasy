2c6739f3966b719b58fb3661872f93a4915aeb21
Alessio Soldano <asoldano@redhat.com>
2019-05-06 12:30:41 +0200

    [RESTEASY-2228] Allow loading and running MP Config sources contributed by RESTEEasy even when the Servlet API is not available.

--- resteasy-core/src/main/java/org/jboss/resteasy/microprofile/config/ServletConfigSource.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/microprofile/config/ServletConfigSource.java
@@ -13,10 +13,26 @@ import org.jboss.resteasy.core.ResteasyContext;
 
 public class ServletConfigSource implements ConfigSource {
 
+   private static final boolean SERVLET_AVAILABLE;
+   static {
+      Class<?> clazz = null;
+      try {
+         clazz = Class.forName(ServletConfig.class.getName());
+      }
+      catch (Throwable e)
+      {
+         //RESTEASY-2228: allow loading and running this ConfigSource even when Servlet API is not available
+      }
+      SERVLET_AVAILABLE = clazz != null;
+   }
+
    private volatile String name;
 
    @Override
    public Map<String, String> getProperties() {
+      if (!SERVLET_AVAILABLE) {
+         return Collections.<String, String>emptyMap();
+      }
       ServletConfig config = ResteasyContext.getContextData(ServletConfig.class);
       if (config == null) {
          return Collections.<String, String>emptyMap();
@@ -35,6 +51,9 @@ public class ServletConfigSource implements ConfigSource {
 
    @Override
    public String getValue(String propertyName) {
+      if (!SERVLET_AVAILABLE) {
+         return null;
+      }
       ServletConfig config = ResteasyContext.getContextData(ServletConfig.class);
       if (config == null) {
          return null;
@@ -47,6 +66,9 @@ public class ServletConfigSource implements ConfigSource {
       if (name == null) {
          synchronized(this) {
             if (name == null) {
+               if (!SERVLET_AVAILABLE) {
+                  name = toString();
+               }
                ServletContext servletContext = ResteasyContext.getContextData(ServletContext.class);
                ServletConfig servletConfig = ResteasyContext.getContextData(ServletConfig.class);
                StringBuilder sb = new StringBuilder();
