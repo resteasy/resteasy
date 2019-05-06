e1819f21642c97e2d23f6cfb67574646e20446c4
Alessio Soldano <asoldano@redhat.com>
2019-05-06 12:30:41 +0200

    [RESTEASY-2228] Allow loading and running MP Config sources contributed by RESTEEasy even when the Servlet API is not available.

--- resteasy-core/src/main/java/org/jboss/resteasy/microprofile/config/ServletContextConfigSource.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/microprofile/config/ServletContextConfigSource.java
@@ -12,10 +12,26 @@ import org.jboss.resteasy.core.ResteasyContext;
 
 public class ServletContextConfigSource implements ConfigSource {
 
+   private static final boolean SERVLET_AVAILABLE;
+   static {
+      Class<?> clazz = null;
+      try {
+         clazz = Class.forName(ServletContext.class.getName());
+      }
+      catch (Throwable e)
+      {
+         //RESTEASY-2228: allow loading and running this ConfigSource even when Servlet API is not available
+      }
+      SERVLET_AVAILABLE = clazz != null;
+   }
+
    private volatile String name;
 
    @Override
    public Map<String, String> getProperties() {
+      if (!SERVLET_AVAILABLE) {
+         return Collections.<String, String>emptyMap();
+      }
       ServletContext context = ResteasyContext.getContextData(ServletContext.class);
       if (context == null) {
          return Collections.<String, String>emptyMap();
@@ -34,6 +50,9 @@ public class ServletContextConfigSource implements ConfigSource {
 
    @Override
    public String getValue(String propertyName) {
+      if (!SERVLET_AVAILABLE) {
+         return null;
+      }
       ServletContext context = ResteasyContext.getContextData(ServletContext.class);
       if (context == null) {
          return null;
@@ -46,6 +65,9 @@ public class ServletContextConfigSource implements ConfigSource {
       if (name == null) {
          synchronized(this) {
             if (name == null) {
+               if (!SERVLET_AVAILABLE) {
+                  name = toString();
+               }
                ServletContext servletContext = ResteasyContext.getContextData(ServletContext.class);
                StringBuilder sb = new StringBuilder();
                name = sb.append(servletContext != null ? servletContext.getServletContextName() : null)
