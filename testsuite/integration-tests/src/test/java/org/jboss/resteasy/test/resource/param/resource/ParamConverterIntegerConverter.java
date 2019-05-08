b29a9b161b379b373189ad528df7285a9956392a
Alessio Soldano <asoldano@redhat.com>
2019-05-09 00:09:59 +0200

    [RESTEASY-2222] Adding test

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/resource/param/resource/ParamConverterIntegerConverter.java
@@ -0,0 +1,14 @@
+package org.jboss.resteasy.test.resource.param.resource;
+
+import javax.ws.rs.ext.ParamConverter;
+
+public class ParamConverterIntegerConverter implements ParamConverter<Integer> {
+   public Integer fromString(String str) {
+      return Integer.valueOf(str + str);
+   }
+
+   public String toString(Integer value) {
+      String s = value.toString();
+      return s.substring(s.length() / 2);
+   }
+}
