d632dbe1366a196239866a2e0d3910f6e71319af
Alessio Soldano <soldano.servizi@email.it>
2019-05-12 04:42:02 +0200

    [RESTEASY-2222] ParamConverter and primitive types (#2026)
    * [RESTEASY-2222] Move check on primitive type in StringParameterInjector
    
    * [RESTEASY-2222] Adding test

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/resource/param/resource/ParamConverterIntegerConverterProvider.java
@@ -0,0 +1,17 @@
+package org.jboss.resteasy.test.resource.param.resource;
+
+import javax.ws.rs.ext.ParamConverter;
+import javax.ws.rs.ext.ParamConverterProvider;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+public class ParamConverterIntegerConverterProvider implements ParamConverterProvider {
+   @SuppressWarnings(value = "unchecked")
+   @Override
+   public <T> ParamConverter<T> getConverter(Class<T> rawType, Type genericType, Annotation[] annotations) {
+      if (!int.class.equals(rawType)) {
+         return null;
+      }
+      return (ParamConverter<T>) new ParamConverterIntegerConverter();
+   }
+}
