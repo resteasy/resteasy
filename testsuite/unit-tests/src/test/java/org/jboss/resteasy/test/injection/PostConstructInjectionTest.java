dfc3f965d610c3af3eae69ce6831a4658564e1e7
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2019-05-22 09:46:50 +0100

    [RESTEASY-2249] Fixing private PostConstruct method detection

--- testsuite/unit-tests/src/test/java/org/jboss/resteasy/test/injection/PostConstructInjectionTest.java
+++ testsuite/unit-tests/src/test/java/org/jboss/resteasy/test/injection/PostConstructInjectionTest.java
@@ -48,6 +48,17 @@ public class PostConstructInjectionTest {
       }
    }
 
+   public static class TC5 {
+      @PostConstruct
+      private void m() {
+         return;
+      }
+   }
+
+   public static class TC6 extends TC5 {
+      // empty on purpose - should inherit @PostConstruct
+   }
+
    /**
     * @tpTestDetails Test valid @PostContruct method
     * @tpSince RESTEasy 3.7.0
@@ -91,5 +102,23 @@ public class PostConstructInjectionTest {
    @Test
    public void testStaticMethod() throws Exception {
       Assert.assertFalse(Types.hasPostConstruct(TC4.class));
+   }
+
+   /**
+    * @tpTestDetails Test valid @PostConstruct private method
+    * @tpSince RESTEasy 3.7.0
+    */
+   @Test
+   public void testPrivateMethod() throws Exception {
+      Assert.assertTrue(Types.hasPostConstruct(TC5.class));
+   }
+
+   /**
+    * @tpTestDetails Test valid @PostConstruct inherited method
+    * @tpSince RESTEasy 3.7.0
+    */
+   @Test
+   public void testInheritedMethod() throws Exception {
+      Assert.assertTrue(Types.hasPostConstruct(TC6.class));
    }
 }
