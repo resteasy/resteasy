90c0919f7deea14e8ead55a22289eb365f23b682
Anil Gursel <agursel@paypal.com>
2019-06-07 17:46:51 -0500

    [RESTEASY-2271] AsyncClientHttpEngine for Reactor Netty Http Client

---
+++ resteasy-client-reactor-netty/src/test/java/org/jboss/resteasy/client/jaxrs/engines/Person.java
@@ -0,0 +1,30 @@
+package org.jboss.resteasy.client.jaxrs.engines;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(){}
+
+    public Person(final String name, final int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
