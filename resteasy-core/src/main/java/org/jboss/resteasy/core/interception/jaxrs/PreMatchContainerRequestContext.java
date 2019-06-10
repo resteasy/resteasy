3615672eac918c04ea13e95ad04d063ca93565c3
NicoNes <nicolas.nesmon@gmail.com>
2019-06-10 11:47:07 -0400

    [RESTEASY-2268] ResteasyHttpHeaders does not follow JAX-RS spec concerning modifiability (#2063)
    * ResteasyHttpHeaders.getRequestHeaders() and ResteasyHttpHeaders.getRequestHeader() must return not modifiable collection
    
    Signed-off-by: NicoNes <nicolas.nesmon@gmail.com>
    
    * Fix formatting
    
    Signed-off-by: NicoNes <nicolas.nesmon@gmail.com>
    
    * ContainerRequestContext.getHeaders() must return a mutable map
    
    Signed-off-by: NicoNes <nicolas.nesmon@gmail.com>
    
    * HttpRequest.getMutableHeaders() should be used anywhere we have to edit request headers
    
    Signed-off-by: NicoNes <nicolas.nesmon@gmail.com>

--- resteasy-core/src/main/java/org/jboss/resteasy/core/interception/jaxrs/PreMatchContainerRequestContext.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/core/interception/jaxrs/PreMatchContainerRequestContext.java
@@ -24,6 +24,7 @@ import javax.ws.rs.core.UriInfo;
 import org.jboss.resteasy.core.ResteasyContext;
 import org.jboss.resteasy.core.SynchronousDispatcher;
 import org.jboss.resteasy.specimpl.BuiltResponse;
+import org.jboss.resteasy.specimpl.ResteasyHttpHeaders;
 import org.jboss.resteasy.spi.ApplicationException;
 import org.jboss.resteasy.spi.HttpRequest;
 import org.jboss.resteasy.tracing.RESTEasyTracingLogger;
@@ -134,7 +135,7 @@ public class PreMatchContainerRequestContext implements SuspendableContainerRequ
    @Override
    public MultivaluedMap<String, String> getHeaders()
    {
-      return httpRequest.getHttpHeaders().getRequestHeaders();
+      return ((ResteasyHttpHeaders) httpRequest.getHttpHeaders()).getMutableHeaders();
    }
 
    @Override
