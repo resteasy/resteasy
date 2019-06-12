98fdc6b0daaeb289dc9638097d7a1cca87c0e8a1
Samuel Cox <sacox@paypal.com>
2019-06-12 13:22:20 -0500

    spring-reactor RxInvoker (almost exact copy of rxjava2 one)

---
+++ resteasy-reactor/src/main/java/org/jboss/resteasy/reactor/MonoRxInvokerImpl.java
@@ -0,0 +1,171 @@
+package org.jboss.resteasy.reactor;
+
+import javax.ws.rs.client.CompletionStageRxInvoker;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.Response;
+
+import reactor.core.publisher.Mono;
+
+@SuppressWarnings("unchecked")
+public class MonoRxInvokerImpl implements MonoRxInvoker
+{
+   private final CompletionStageRxInvoker completionStageRxInvoker;
+   private final MonoProvider monoProvider;
+
+   public MonoRxInvokerImpl(final CompletionStageRxInvoker completionStageRxInvoker)
+   {
+      this.completionStageRxInvoker = completionStageRxInvoker;
+      this.monoProvider = new MonoProvider();
+   }
+
+   @Override
+   public Mono<Response> get()
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.get());
+   }
+
+   @Override
+   public <T> Mono<T> get(Class<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.get(responseType));
+   }
+
+   @Override
+   public <T> Mono<T> get(GenericType<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.get(responseType));
+   }
+
+   @Override
+   public Mono<Response> put(Entity<?> entity)
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.put(entity));
+   }
+
+   @Override
+   public <T> Mono<T> put(Entity<?> entity, Class<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.put(entity, responseType));
+   }
+
+   @Override
+   public <T> Mono<T> put(Entity<?> entity, GenericType<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.put(entity, responseType));
+   }
+
+   @Override
+   public Mono<Response> post(Entity<?> entity)
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.post(entity));
+   }
+
+   @Override
+   public <T> Mono<T> post(Entity<?> entity, Class<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.post(entity, responseType));
+   }
+
+   @Override
+   public <T> Mono<T> post(Entity<?> entity, GenericType<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.post(entity, responseType));
+   }
+
+   @Override
+   public Mono<Response> delete()
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.delete());
+   }
+
+   @Override
+   public <T> Mono<T> delete(Class<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.delete(responseType));
+   }
+
+   @Override
+   public <T> Mono<T> delete(GenericType<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.delete(responseType));
+   }
+
+   @Override
+   public Mono<Response> head()
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.head());
+   }
+
+   @Override
+   public Mono<Response> options()
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.options());
+   }
+
+   @Override
+   public <T> Mono<T> options(Class<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.options(responseType));
+   }
+
+   @Override
+   public <T> Mono<T> options(GenericType<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.options(responseType));
+   }
+
+   @Override
+   public Mono<Response> trace()
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.trace());
+   }
+
+   @Override
+   public <T> Mono<T> trace(Class<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.trace(responseType));
+   }
+
+   @Override
+   public <T> Mono<T> trace(GenericType<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.trace(responseType));
+   }
+
+   @Override
+   public Mono<Response> method(String name)
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.method(name));
+   }
+
+   @Override
+   public <T> Mono<T> method(String name, Class<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.method(name, responseType));
+   }
+
+   @Override
+   public <T> Mono<T> method(String name, GenericType<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.method(name, responseType));
+   }
+
+   @Override
+   public Mono<Response> method(String name, Entity<?> entity)
+   {
+      return (Mono<Response>) monoProvider.fromCompletionStage(completionStageRxInvoker.method(name, entity));
+   }
+
+   @Override
+   public <T> Mono<T> method(String name, Entity<?> entity, Class<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.method(name, entity, responseType));
+   }
+
+   @Override
+   public <T> Mono<T> method(String name, Entity<?> entity, GenericType<T> responseType)
+   {
+      return (Mono<T>) monoProvider.fromCompletionStage(completionStageRxInvoker.method(name, entity, responseType));
+   }
+}
---
+++ resteasy-reactor/src/test/java/org/jboss/resteasy/reactor/ReactorTest.java
@@ -0,0 +1,108 @@
+package org.jboss.resteasy.reactor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import org.jboss.logging.Logger;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.plugins.server.netty.NettyJaxrsServer;
+import org.jboss.resteasy.test.TestPortProvider;
+import static org.jboss.resteasy.test.TestPortProvider.generateURL;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class ReactorTest
+{
+   private static NettyJaxrsServer server;
+
+   private static CountDownLatch latch;
+   private static AtomicReference<Object> value = new AtomicReference<Object>();
+   private static final Logger LOG = Logger.getLogger(NettyJaxrsServer.class);
+
+   @BeforeClass
+   public static void beforeClass() throws Exception
+   {
+      server = new NettyJaxrsServer();
+      server.setPort(TestPortProvider.getPort());
+      server.setRootResourcePath("/");
+      server.start();
+      List<Class> classes = server.getDeployment().getActualResourceClasses();
+      classes.add(ReactorResource.class);
+      List<Class> providers = server.getDeployment().getActualProviderClasses();
+      providers.add(ReactorInjector.class);
+      server.getDeployment().registration();
+   }
+
+   @AfterClass
+   public static void afterClass() throws Exception
+   {
+      server.stop();
+      server = null;
+   }
+
+   private ResteasyClient client;
+
+   @Before
+   public void before()
+   {
+      client = ((ResteasyClientBuilder)ClientBuilder.newBuilder())
+            .readTimeout(5, TimeUnit.SECONDS)
+            .connectionCheckoutTimeout(5, TimeUnit.SECONDS)
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .build();
+      value.set(null);
+      latch = new CountDownLatch(1);
+   }
+
+   @After
+   public void after()
+   {
+      client.close();
+   }
+
+   @Test
+   public void testMono() throws Exception {
+      Mono<Response> mono = client.target(generateURL("/mono")).request().rx(MonoRxInvoker.class).get();
+      mono.subscribe((Response r) -> {value.set(r.readEntity(String.class)); latch.countDown();});
+      latch.await();
+      assertEquals("got it", value.get());
+   }
+
+   @Test
+   public void testFlux() throws Exception {
+      FluxRxInvoker invoker = client.target(generateURL("/flux")).request().rx(FluxRxInvoker.class);
+      @SuppressWarnings("unchecked")
+      Flux<String> flux = (Flux<String>) invoker.get();
+      List<String> data = new ArrayList<>();
+      flux.subscribe(
+         (String s) -> data.add(s),
+         (Throwable t) -> LOG.error(t.getMessage(), t),
+         () -> latch.countDown());
+      latch.await();
+      assertArrayEquals(new String[] {"one", "two"}, data.toArray());
+   }
+
+   @Test
+   public void testInjection()
+   {
+      Integer data = client.target(generateURL("/injection")).request().get(Integer.class);
+      assertEquals((Integer)42, data);
+
+      data = client.target(generateURL("/injection-async")).request().get(Integer.class);
+      assertEquals((Integer)42, data);
+   }
+
+}
