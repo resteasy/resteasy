5695a056a28925b6f43331b6f380864bb0fc7359
Matt Drees <matt.drees@cru.org>
2019-07-31 17:56:53 -0600

    [RESTEASY-2315] check for more unproxyable providers
    This checks for inherited final methods,
    in addition to final methods declared directly by the provider.
    It also checks for non-usable constructors.

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/cdi/injection/resource/ProviderOneArgConstructorStringHandler.java
@@ -0,0 +1,13 @@
+package org.jboss.resteasy.test.cdi.injection.resource;
+
+public class ProviderOneArgConstructorStringHandler {
+   private String c;
+
+   public String getC() {
+      return c;
+   }
+
+   public void setC(String c) {
+      this.c = c;
+   }
+}
