d807f46565df8754a1282b5ef8f23f8ba0660e1e
Samuel Cox <sacox@paypal.com>
2019-08-30 06:20:53 -0500

    resteasy-reactor: Fix a `null` case that causes hang
    The replaced code in `MonoProvider` was blindly copied from rxjava2.  We
    use this code in our application and the code in `testToCompletionStageNullCase`
    represents a scenario our app deals with.  With the `MonoAdapter`, that
    code hangs.  Leveraging the existing `Mono#toFuture`, which I should
    have done in the first place, addresses that hang.

---
+++ resteasy-reactor/src/test/java/org/jboss/resteasy/reactor/MonoProviderTest.java
@@ -0,0 +1,42 @@
+package org.jboss.resteasy.reactor;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import org.junit.Test;
+import reactor.core.publisher.Mono;
+
+public class MonoProviderTest
+{
+    private final MonoProvider provider = new MonoProvider();
+
+    @Test
+    public void testFromCompletionStage()
+    {
+        final CompletableFuture<Integer> cs = new CompletableFuture<>();
+        cs.complete(1);
+        final Mono<?> mono = provider.fromCompletionStage(cs);
+        assertEquals(1, mono.block());
+    }
+
+    @Test
+    public void testToCompletionStageCase() throws Exception
+    {
+        final Object actual = provider.toCompletionStage(Mono.just(1)).toCompletableFuture().get();
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void testToCompletionStageNullCase() throws Exception
+    {
+        // Kind of a weird test, but added with code that fixed a hang.
+        final CompletableFuture<Integer> cs = new CompletableFuture<>();
+        cs.complete(null);
+        final Mono<?> mono = Mono.fromCompletionStage(cs);
+        final Object actual = provider.toCompletionStage(mono).toCompletableFuture().get();
+        assertNull(actual);
+    }
+}
