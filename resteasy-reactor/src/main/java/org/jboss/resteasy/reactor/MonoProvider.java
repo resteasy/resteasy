f5f0785eda39efe39f181c7ef6d7dcaaa2333710
Samuel Cox <crankydillo@gmail.com>
2019-09-12 11:11:55 -0500

    [RESTEASY-2343] resteasy-reactor: Fix a `null` case that causes hang (#2150)
    * resteasy-reactor: Fix a `null` case that causes hang
    
    The replaced code in `MonoProvider` was blindly copied from rxjava2.  We
    use this code in our application and the code in `testToCompletionStageNullCase`
    represents a scenario our app deals with.  With the `MonoAdapter`, that
    code hangs.  Leveraging the existing `Mono#toFuture`, which I should
    have done in the first place, addresses that hang.
    
    * Remove unused imports.

--- resteasy-reactor/src/main/java/org/jboss/resteasy/reactor/MonoProvider.java
+++ resteasy-reactor/src/main/java/org/jboss/resteasy/reactor/MonoProvider.java
@@ -1,38 +1,20 @@
 package org.jboss.resteasy.reactor;
 
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import javax.ws.rs.ext.Provider;
 
 import org.jboss.resteasy.spi.AsyncClientResponseProvider;
 import org.jboss.resteasy.spi.AsyncResponseProvider;
-import reactor.core.Disposable;
 import reactor.core.publisher.Mono;
 
 @Provider
 public class MonoProvider implements AsyncResponseProvider<Mono<?>>, AsyncClientResponseProvider<Mono<?>>
 {
-   private static class MonoAdaptor<T> extends CompletableFuture<T>
-   {
-      private Disposable subscription;
-
-      MonoAdaptor(final Mono<T> single)
-      {
-         this.subscription = single.subscribe(this::complete, this::completeExceptionally);
-      }
-
-      @Override
-      public boolean cancel(boolean mayInterruptIfRunning)
-      {
-         subscription.dispose();
-         return super.cancel(mayInterruptIfRunning);
-      }
-   }
 
    @Override
    public CompletionStage<?> toCompletionStage(Mono<?> asyncResponse)
    {
-      return new MonoAdaptor<>(asyncResponse);
+      return asyncResponse.toFuture();
    }
 
    @Override
