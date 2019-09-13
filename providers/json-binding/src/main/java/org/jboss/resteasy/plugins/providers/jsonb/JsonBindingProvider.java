ad89f72c43cc36dde3488fc355735e080c8938e4
R Searls <rsearls@redhat.com>
2019-09-13 18:44:08 -0400

    [RESTEASY-2347] added property check and related unit test

--- providers/json-binding/src/main/java/org/jboss/resteasy/plugins/providers/jsonb/JsonBindingProvider.java
+++ providers/json-binding/src/main/java/org/jboss/resteasy/plugins/providers/jsonb/JsonBindingProvider.java
@@ -49,8 +49,12 @@ public class JsonBindingProvider extends AbstractJsonBindingProvider
    public JsonBindingProvider() {
       super();
       ResteasyConfiguration context = ResteasyProviderFactory.getContextData(ResteasyConfiguration.class);
-      disabled = (context != null && (Boolean.parseBoolean(context.getParameter(ResteasyContextParameters.RESTEASY_PREFER_JACKSON_OVER_JSONB))
-            || Boolean.parseBoolean(context.getParameter("resteasy.jsonp.enable"))));
+      if (context == null) {
+         disabled = Boolean.getBoolean(ResteasyContextParameters.RESTEASY_PREFER_JACKSON_OVER_JSONB);
+      } else {
+         disabled = (Boolean.parseBoolean(context.getParameter(ResteasyContextParameters.RESTEASY_PREFER_JACKSON_OVER_JSONB))
+                 || Boolean.parseBoolean(context.getParameter("resteasy.jsonp.enable")));
+      }
    }
 
    @Override
