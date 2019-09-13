7e18dd463215153925c2c86fbacbc0846cf2ef0e
R Searls <rsearls@redhat.com>
2019-09-13 18:44:08 -0400

    [RESTEASY-2347] added property check and related unit test

---
+++ testsuite/unit-tests/src/test/java/org/jboss/resteasy/test/providers/JsonBindingTest.java
@@ -0,0 +1,43 @@
+package org.jboss.resteasy.test.providers;
+
+import org.jboss.resteasy.plugins.providers.jsonb.JsonBindingProvider;
+import org.jboss.resteasy.plugins.server.servlet.ResteasyContextParameters;
+import javax.ws.rs.core.MediaType;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Check when ResteasyConfiguration.class is null in JsonBindingProvider
+ * system property is checked and the property is set accordingly.
+ * There are integration tests that check the properly setting when
+ * a ResteasyConfiguration.class is provided.
+ */
+public class JsonBindingTest {
+    private MediaType mediaType = new MediaType("application", "json");
+
+    @Test
+    public void testUseJSONB() throws Exception
+    {
+        String origValue = System.getProperty(ResteasyContextParameters.RESTEASY_PREFER_JACKSON_OVER_JSONB);
+
+        System.setProperty(ResteasyContextParameters.RESTEASY_PREFER_JACKSON_OVER_JSONB, "true");
+        JsonBindingProvider jbp = new JsonBindingProvider();
+        boolean result = jbp.isReadable(null, null, null, mediaType);
+
+        if (origValue == null) {
+            System.setProperty(ResteasyContextParameters.RESTEASY_PREFER_JACKSON_OVER_JSONB, "");
+        } else {
+            System.setProperty(ResteasyContextParameters.RESTEASY_PREFER_JACKSON_OVER_JSONB, origValue);
+        }
+        assertFalse(result);
+    }
+
+    @Test
+    public void testUseJACKSON() throws Exception
+    {
+        JsonBindingProvider jbp = new JsonBindingProvider();
+        boolean result = jbp.isReadable(null, null, null, mediaType);
+        assertTrue(result);
+    }
+}
