f02d82b846ee697e43d615a4c8d58178d8dc9b53
Gursel, Anil(agursel) <agursel@paypal.com>
2019-11-06 13:34:51 -0600

    RESTEASY-2459 MonoRxInvoker should be lazy

--- resteasy-core-spi/src/main/java/org/jboss/resteasy/spi/AsyncClientResponseProvider.java
+++ resteasy-core-spi/src/main/java/org/jboss/resteasy/spi/AsyncClientResponseProvider.java
@@ -1,6 +1,7 @@
 package org.jboss.resteasy.spi;
 
 import java.util.concurrent.CompletionStage;
+import java.util.function.Supplier;
 
 /**
  * Used to turn a CompletionStage into another reactive class.
@@ -8,5 +9,23 @@ import java.util.concurrent.CompletionStage;
  */
 public interface AsyncClientResponseProvider<T> {
 
+    /**
+     * Turns {@link CompletionStage} to a reactive type.
+     *
+     * @param completionStage The {@link CompletionStage} that will produce a value.
+     *
+     * @return T reactive type
+     */
    T fromCompletionStage(CompletionStage<?> completionStage);
+
+    /**
+     * Turns {@link CompletionStage} to a reactive type in a deferred fashion.  For instance, in the case of
+     * Rx or Reactor, the {@link Supplier#get()} will only be called when subscription happens.
+     *
+     * @param completionStageSupplier The {@link Supplier} of a {@link CompletionStage} that will produce a value.
+     *                                This allows lazy triggering of future-based APIs.
+     *
+     * @return T reactive type
+     */
+   T fromCompletionStage(Supplier<CompletionStage<?>> completionStageSupplier);
 }
