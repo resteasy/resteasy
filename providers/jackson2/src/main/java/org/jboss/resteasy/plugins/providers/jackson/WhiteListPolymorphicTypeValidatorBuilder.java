5cf0f508f084ba166fa3a40d086d230ed90af5a6
Alessio Soldano <asoldano@redhat.com>
2019-11-19 00:33:13 +0100

    [RESTEASY-2411] WhiteListPolymorphicTypeValidator

---
+++ providers/jackson2/src/main/java/org/jboss/resteasy/plugins/providers/jackson/WhiteListPolymorphicTypeValidatorBuilder.java
@@ -0,0 +1,45 @@
+package org.jboss.resteasy.plugins.providers.jackson;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.StringTokenizer;
+
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+
+public class WhiteListPolymorphicTypeValidatorBuilder extends BasicPolymorphicTypeValidator.Builder
+{
+   private static final long serialVersionUID = 464558058341488449L;
+   private static final String BASE_TYPE_PROP = "resteasy.jackson.deserialization.whitelist.allowIfBaseType.prefix";
+   private static final String SUB_TYPE_PROP = "resteasy.jackson.deserialization.whitelist.allowIfSubType.prefix";
+
+   public WhiteListPolymorphicTypeValidatorBuilder() {
+      super();
+      String allowIfBaseType = getProperty(BASE_TYPE_PROP);
+      if (allowIfBaseType != null) {
+         StringTokenizer st = new StringTokenizer(allowIfBaseType, ",", false);
+         while (st.hasMoreTokens()) {
+            allowIfBaseType(st.nextToken());
+         }
+      }
+      String allowIfSubType = getProperty(SUB_TYPE_PROP);
+      if (allowIfSubType != null) {
+         StringTokenizer st = new StringTokenizer(allowIfSubType, ",", false);
+         while (st.hasMoreTokens()) {
+            allowIfSubType(st.nextToken());
+         }
+      }
+   }
+
+   private String getProperty(String propertyName) {
+      final SecurityManager sm = System.getSecurityManager();
+      if (sm != null) {
+         return AccessController.doPrivileged(new PrivilegedAction<String>() {
+            @Override
+            public String run() {
+               return System.getProperty(propertyName);
+            }
+         });
+      }
+      return System.getProperty(propertyName);
+   }
+}
