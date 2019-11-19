1d57e4f00e70ee53f95d742fa5157fd6ded21bdc
Ivo Studensky <istudens@redhat.com>
2019-11-19 21:00:33 +0100

    [RESTEASY-2411] test-case for WhiteListPolymorphicTypeValidatorBuilder

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/jackson2/whitelist/JaxRsActivator.java
@@ -0,0 +1,12 @@
+package org.jboss.resteasy.test.providers.jackson2.whitelist;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author bmaxwell
+ *
+ */
+@ApplicationPath("/")
+public class JaxRsActivator extends Application {
+}
