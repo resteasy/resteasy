ffe22eaefcc432b04beec5c8a9dfae68e7bf1281
Stephane Epardaud <stef@epardaud.fr>
2019-12-03 12:23:54 +0100

    REST client: handle async exception properly RESTEASY-2432

--- resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/internal/ClientInvocationBuilder.java
+++ resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/internal/ClientInvocationBuilder.java
@@ -58,6 +58,14 @@ public class ClientInvocationBuilder implements Invocation.Builder
       return invocation.headers;
    }
 
+   /**
+    * exposes the client invocation for easier integration with other libraries
+    * @return the underlying client invocation
+    */
+   public ClientInvocation getClientInvocation() {
+       return invocation;
+   }
+
    @Override
    public Invocation.Builder accept(String... mediaTypes)
    {
