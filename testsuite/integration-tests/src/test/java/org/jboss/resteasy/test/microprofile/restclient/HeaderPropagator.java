cfad85fccfb087c0b6e2ac09dfefc6b2ae5bdadd
Stephane Epardaud <stef@epardaud.fr>
2019-12-03 10:48:12 +0100

    Test header propagation and context cleanup RESTEASY-2401 RESTEASY-2443

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/microprofile/restclient/HeaderPropagator.java
@@ -0,0 +1,21 @@
+package org.jboss.resteasy.test.microprofile.restclient;
+
+import java.util.List;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MultivaluedMap;
+
+import org.eclipse.microprofile.rest.client.ext.ClientHeadersFactory;
+import org.jboss.resteasy.core.ResteasyContext;
+import org.junit.Assert;
+
+public class HeaderPropagator implements ClientHeadersFactory {
+   @Override
+   public MultivaluedMap<String, String> update(MultivaluedMap<String, String> containerRequestHeaders, MultivaluedMap<String, String> clientRequestHeaders) {
+       Assert.assertNull(ResteasyContext.getContextData(HttpHeaders.class));
+       List<String> prop = containerRequestHeaders.get("X-Propagated");
+       if(prop != null)
+          clientRequestHeaders.addAll("X-Propagated", prop);
+       return clientRequestHeaders;
+   }
+}
--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/microprofile/restclient/HelloClient.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/microprofile/restclient/HelloClient.java
@@ -7,10 +7,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
+import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
 import io.reactivex.Single;
 
 @Path("/")
 @Produces("text/plain")
+@RegisterRestClient(baseUri = "http://localhost:8080/RestClientProxyTest")
+@RegisterClientHeaders(HeaderPropagator.class)
 public interface HelloClient {
 
     @GET
@@ -22,6 +28,26 @@ public interface HelloClient {
     Single<String> single(@PathParam("id") String id);
 
     @GET
-    @Path("some/{id}")
+    @Path("cs/{id}")
     CompletionStage<String> cs(@PathParam("id") String id);
+
+    @GET
+    @Path("async-client-target")
+    CompletionStage<String> asyncClientTarget();
+
+    @GET
+    @Path("async-client")
+    @ClientHeaderParam(name = "X-Propagated", value = "got-a-value")
+    @ClientHeaderParam(name = "X-Not-Propagated", value = "got-a-value")
+    CompletionStage<String> asyncClient();
+
+    @GET
+    @Path("client-target")
+    String clientTarget();
+
+    @GET
+    @Path("client")
+    @ClientHeaderParam(name = "X-Propagated", value = "got-a-value")
+    @ClientHeaderParam(name = "X-Not-Propagated", value = "got-a-value")
+    String client();
 }
