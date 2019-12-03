ddc025469b2175416b0d52f9e543001c4c4a08cb
Stephane Epardaud <stef@epardaud.fr>
2019-12-03 10:48:12 +0100

    Test header propagation and context cleanup RESTEASY-2401 RESTEASY-2443

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/microprofile/restclient/HelloResource.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/microprofile/restclient/HelloResource.java
@@ -3,15 +3,26 @@ package org.jboss.resteasy.test.microprofile.restclient;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.microprofile.client.ExceptionMapping;
+import org.jboss.resteasy.microprofile.client.ExceptionMapping.HandlerException;
+import org.junit.Assert;
 
 import io.reactivex.Single;
 
 @Path("/")
 public class HelloResource {
+
+   @Inject
+   @RestClient
+   HelloClient rest;
 
     @GET
     @Produces("text/plain")
@@ -27,8 +38,44 @@ public class HelloResource {
     }
 
     @GET
-    @Path("/some/{id}")
+    @Path("/cs/{id}")
     public CompletionStage<String> cs(@PathParam("id") String id) {
        return CompletableFuture.completedFuture(id);
+    }
+
+    @GET
+    @Path("async-client-target")
+    public CompletionStage<String> asyncClientTarget(@HeaderParam("X-Propagated") String propagatedHeader,
+                                                @HeaderParam("X-Not-Propagated") String nonPropagatedHeader) {
+       Assert.assertNull(nonPropagatedHeader);
+       Assert.assertEquals("got-a-value", propagatedHeader);
+       return CompletableFuture.completedFuture("OK");
+    }
+
+    @GET
+    @Path("async-client")
+    public CompletionStage<String> asyncClient(@HeaderParam("X-Propagated") String propagatedHeader,
+                                               @HeaderParam("X-Not-Propagated") String nonPropagatedHeader){
+       Assert.assertEquals("got-a-value", propagatedHeader);
+       Assert.assertEquals("got-a-value", nonPropagatedHeader);
+       return rest.asyncClientTarget();
+    }
+
+    @GET
+    @Path("client-target")
+    public String clientTarget(@HeaderParam("X-Propagated") String propagatedHeader,
+                               @HeaderParam("X-Not-Propagated") String nonPropagatedHeader) {
+       Assert.assertNull(nonPropagatedHeader);
+       Assert.assertEquals("got-a-value", propagatedHeader);
+       return "OK";
+    }
+
+    @GET
+    @Path("client")
+    public String client(@HeaderParam("X-Propagated") String propagatedHeader,
+                         @HeaderParam("X-Not-Propagated") String nonPropagatedHeader){
+       Assert.assertEquals("got-a-value", propagatedHeader);
+       Assert.assertEquals("got-a-value", nonPropagatedHeader);
+       return rest.clientTarget();
     }
 }
