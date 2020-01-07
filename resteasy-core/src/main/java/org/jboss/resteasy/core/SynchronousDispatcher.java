ed82bb3bf5829e5ccb1513f38352c27c4a3c35b2
阿男 <l.weinan@gmail.com>
2020-01-07 13:52:44 +0800

    correct logic

--- resteasy-core/src/main/java/org/jboss/resteasy/core/SynchronousDispatcher.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/core/SynchronousDispatcher.java
@@ -200,7 +200,7 @@ public class SynchronousDispatcher implements Dispatcher
             }
          }
       }
-      if (response.isCommitted() && !response.suppressExceptionDuringChunkedTransfer())
+      if (response.isCommitted() && response.suppressExceptionDuringChunkedTransfer())
       {
          LogMessages.LOGGER.debug(Messages.MESSAGES.responseIsCommitted());
          onComplete.accept(null);
