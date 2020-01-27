6f4c249e759949b6b75cf1e17342496e95d1c224
Ron Sigal <rsigal@redhat.com>
2020-01-27 20:15:10 +0100

    [RESTEASY-2481] Support MicroProfile REST Client 1.4-RC1.

--- resteasy-client-microprofile/src/main/java/org/jboss/resteasy/microprofile/client/RestClientExtension.java
+++ resteasy-client-microprofile/src/main/java/org/jboss/resteasy/microprofile/client/RestClientExtension.java
@@ -2,14 +2,19 @@ package org.jboss.resteasy.microprofile.client;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.AfterBeanDiscovery;
 import javax.enterprise.inject.spi.AfterDeploymentValidation;
+import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
 import javax.enterprise.inject.spi.WithAnnotations;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Optional;
@@ -20,6 +25,15 @@ public class RestClientExtension implements Extension {
     private Set<RestClientData> proxyTypes = new LinkedHashSet<>();
 
     private Set<Throwable> errors = new LinkedHashSet<>();
+
+    private static BeanManager manager;
+
+    /**
+     * Verify that CDI is active.
+     */
+    public void observeBeforeBeanDiscovery(@Observes BeforeBeanDiscovery event, BeanManager beanManager) {
+       manager = beanManager;
+     }
 
     public void registerRestClient(@Observes
                                    @WithAnnotations(RegisterRestClient.class) ProcessAnnotatedType<?> type) {
@@ -58,6 +72,10 @@ public class RestClientExtension implements Extension {
         }
     }
 
+    public static boolean isCDIActive() {
+       return manager != null;
+    }
+
     private static class RestClientData {
         private final Class<?> javaClass;
         private final Optional<String> baseUri;
@@ -81,5 +99,43 @@ public class RestClientExtension implements Extension {
         public int hashCode() {
             return Objects.hash(javaClass);
         }
+    }
+
+    /**
+     * Lifted from CdiConstructorInjector in resteasy-cdi
+     */
+    public static Object construct(Class<?> clazz)
+    {
+       Set<Bean<?>> beans = manager.getBeans(clazz);
+       if (beans.size() == 0) {
+          return null;
+       }
+
+       if (beans.size() > 1)
+       {
+          Set<Bean<?>> modifiableBeans = new HashSet<Bean<?>>();
+          modifiableBeans.addAll(beans);
+          // Ambiguous dependency may occur if a resource has subclasses
+          // Therefore we remove those beans
+          for (Iterator<Bean<?>> iterator = modifiableBeans.iterator(); iterator.hasNext();)
+          {
+             Bean<?> bean = iterator.next();
+             if (!bean.getBeanClass().equals(clazz) && !bean.isAlternative())
+             {
+                // remove Beans that have clazz in their type closure but not as a base class
+                iterator.remove();
+             }
+          }
+          beans = modifiableBeans;
+       }
+       Bean<?> bean = manager.resolve(beans);
+       if (bean == null) {
+          return null;
+       }
+       CreationalContext<?> context = manager.createCreationalContext(bean);
+       if (context == null) {
+          return null;
+       }
+       return manager.getReference(bean, clazz, context);
     }
 }
