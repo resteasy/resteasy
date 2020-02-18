a49a0a9414263b6aedb9161226b27bc15e990980
Stephane Epardaud <stef@epardaud.fr>
2020-02-18 16:20:05 +0100

    Async the SSE writer

--- resteasy-core/src/main/java/org/jboss/resteasy/plugins/providers/sse/SseEventProvider.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/plugins/providers/sse/SseEventProvider.java
@@ -1,5 +1,6 @@
 package org.jboss.resteasy.plugins.providers.sse;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -8,6 +9,8 @@ import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
@@ -22,13 +25,15 @@ import javax.ws.rs.ext.Provider;
 import javax.ws.rs.sse.OutboundSseEvent;
 
 import org.jboss.resteasy.resteasy_jaxrs.i18n.Messages;
+import org.jboss.resteasy.spi.AsyncMessageBodyWriter;
+import org.jboss.resteasy.spi.AsyncOutputStream;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
 import org.jboss.resteasy.util.MediaTypeHelper;
 
 @Provider
 @Produces({"text/event-stream", "application/x-stream-general"})
 @Consumes({"text/event-stream", "application/x-stream-general"})
-public class SseEventProvider implements MessageBodyWriter<OutboundSseEvent>, MessageBodyReader<SseEventInputImpl>
+public class SseEventProvider implements AsyncMessageBodyWriter<OutboundSseEvent>, MessageBodyReader<SseEventInputImpl>
 {
    public static final MediaType GENERAL_STREAM_TYPE = new MediaType("application", "x-stream-general");
 
@@ -201,6 +206,161 @@ public class SseEventProvider implements MessageBodyWriter<OutboundSseEvent>, Me
          }
       }
       return new SseEventInputImpl(annotations, streamType, mediaType, httpHeaders, entityStream);
+   }
+
+   @Override
+   public CompletionStage<Void> asyncWriteTo(OutboundSseEvent event, Class<?> type, Type genericType,
+                                             Annotation[] annotations, MediaType mediaType,
+                                             MultivaluedMap<String, Object> httpHeaders, AsyncOutputStream entityStream)
+   {
+      Charset charset = StandardCharsets.UTF_8;
+      boolean textLike = MediaTypeHelper.isTextLike(mediaType);
+      boolean escape = event instanceof OutboundSseEventImpl ? ((OutboundSseEventImpl)event).isEscape() : false;
+      CompletionStage<Void> ret = CompletableFuture.completedFuture(null);
+      if (event.getComment() != null)
+      {
+         for (final String comment : event.getComment().split("\n"))
+         {
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.COMMENT_LEAD));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(comment.getBytes(charset)));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.EOL));
+         }
+      }
+
+      if (event.getType() != null)
+      {
+         if (event.getName() != null)
+         {
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.NAME_LEAD));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(event.getName().getBytes(charset)));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.EOL));
+         }
+         if (event.getId() != null)
+         {
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.ID_LEAD));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(event.getId().getBytes(charset)));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.EOL));
+         }
+         if (event.getReconnectDelay() > -1)
+         {
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.RETRY_LEAD));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(Long.toString(event.getReconnectDelay()).getBytes(StandardCharsets.UTF_8)));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.EOL));
+         }
+
+         if (event.getData() != null)
+         {
+            Class<?> payloadClass = event.getType();
+            Type payloadType = event.getGenericType();
+            if (payloadType == null)
+            {
+               payloadType = payloadClass;
+            }
+
+            if (payloadType == null && payloadClass == null)
+            {
+               payloadType = Object.class;
+               payloadClass = Object.class;
+            }
+            Class<?> finalPayloadClass = payloadClass;
+            Type finalPayloadType = payloadType;
+
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.DATA_LEAD));
+            AsyncMessageBodyWriter writer = (AsyncMessageBodyWriter)ResteasyProviderFactory.getInstance().getMessageBodyWriter(payloadClass,
+                  payloadType, annotations, event.getMediaType());
+
+            if (writer == null)
+            {
+               throw new ServerErrorException(Messages.MESSAGES.notFoundMBW(payloadClass.getName()),
+                     Response.Status.INTERNAL_SERVER_ERROR);
+            }
+
+            ret = ret.thenCompose(v -> writer.asyncWriteTo(event.getData(), finalPayloadClass, finalPayloadType, annotations, event.getMediaType(), httpHeaders,
+               new AsyncOutputStream()
+               {
+                  boolean isNewLine = false;
+
+                  @Override
+                  public CompletionStage<Void> rxFlush()
+                  {
+                     return entityStream.rxFlush();
+                  }
+
+                  @Override
+                  public CompletionStage<Void> rxWrite(byte[] bytes, int offset, int length)
+                  {
+                     return entityStream.rxWrite(escape(textLike, escape, bytes, offset, length));
+                  }
+
+                  private byte[] escape(boolean textLike, boolean escape, byte[] data, int offset, int length) {
+                     ByteArrayOutputStream os = new ByteArrayOutputStream(data.length);
+
+                     for (int i = 0; i < length; i++)
+                     {
+                        byte b = data[i + offset];
+                        if (textLike)
+                        {
+                           if (b == '\n' || b == '\r')
+                           {
+                              if (!isNewLine)
+                              {
+                                 try
+                                 {
+                                    os.write(SseConstants.EOL);
+                                 } catch (IOException e)
+                                 {
+                                    // cannot happen
+                                    throw new RuntimeException(e);
+                                 }
+                              }
+                              isNewLine = true;
+                           }
+                           else
+                           {
+                              if (isNewLine)
+                              {
+                                 try
+                                 {
+                                    os.write(SseConstants.DATA_LEAD);
+                                 } catch (IOException e)
+                                 {
+                                    // cannot happen
+                                    throw new RuntimeException(e);
+                                 }
+                              }
+                              os.write(b);
+                              isNewLine = false;
+                           }
+                        }
+                        else
+                        {
+                           if (escape && (b == '\n' || b == '\r' || b == '\\'))
+                           {
+                              os.write('\\');
+                              os.write(b);
+                           }
+                           else
+                           {
+                              os.write(b);
+                           }
+                        }
+                     }
+
+                     return os.toByteArray();
+                  }
+
+                  @Override
+                  public void write(int b) throws IOException
+                  {
+                     throw new IllegalStateException("Not supported");
+                  }
+               }));
+            ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.EOL));
+
+         }
+
+      }
+      return ret = ret.thenCompose(v -> entityStream.rxWrite(SseConstants.EOL));
    }
 
 }
