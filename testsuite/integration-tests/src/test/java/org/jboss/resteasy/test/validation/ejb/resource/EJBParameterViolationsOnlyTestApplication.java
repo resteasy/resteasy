577b9a14495266e337741e58813fd10b46446c65
Ron Sigal <rsigal@redhat.com>
2020-03-14 16:19:30 -0400

    [RESTEASY-2503] Throw ViolationException for EJBs with just parameter violations.

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/validation/ejb/resource/EJBParameterViolationsOnlyTestApplication.java
@@ -0,0 +1,9 @@
+package org.jboss.resteasy.test.validation.ejb.resource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("app")
+public class EJBParameterViolationsOnlyTestApplication extends Application
+{
+}
