d122bf0df937e1d94a7c0c59502aa67ff52fd68b
Ron Sigal <rsigal@redhat.com>
2020-03-16 16:35:24 -0400

    [RESTEASY-2503] Throw ViolationException for EJBs with just parameter
    violations.

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/validation/ejb/resource/EJBParameterViolationsOnlyStatefulResource.java
@@ -0,0 +1,27 @@
+package org.jboss.resteasy.test.validation.ejb.resource;
+
+import javax.ejb.Stateful;
+import javax.ws.rs.Path;
+
+@Stateful
+@Path("stateful")
+public class EJBParameterViolationsOnlyStatefulResource implements EJBParameterViolationsOnlyResourceIntf
+{
+   private static boolean executionFlag;
+
+   @Override
+   public String testValidation(EJBParameterViolationsOnlyDataObject payload) {
+      executionFlag = true;
+      return payload.getDirection();
+   }
+
+   @Override
+   public boolean used() {
+      return executionFlag;
+   }
+
+   @Override
+   public void reset() {
+      executionFlag = false;
+   }
+}
