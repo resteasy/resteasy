9c277e1afe220ffbd8d81a61fb110fb314812666
R Searls <rsearls@redhat.com>
2020-04-07 15:18:29 -0400

    added dummy comment to force travis to run

--- server-adapters/resteasy-vertx/src/test/java/org/jboss/resteasy/test/asyncio/SSEResource.java
+++ server-adapters/resteasy-vertx/src/test/java/org/jboss/resteasy/test/asyncio/SSEResource.java
@@ -8,6 +8,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.sse.Sse;
 import javax.ws.rs.sse.SseEventSink;
 
+/** require dummy comment here **/
+
 @Path("close")
 public class SSEResource {
 
