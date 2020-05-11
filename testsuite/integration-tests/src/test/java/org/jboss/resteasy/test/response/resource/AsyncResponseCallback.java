a42fb1971d2003df9a37d1359d10a9c85f8533a1
Alessio Soldano <asoldano@redhat.com>
2020-05-11 11:24:20 +0200

    [RESTEASY-2582] CompletionStageResponseTest intermittent failures (#2390)
    * Prevent memory visibility issues
    
    * Refactor AsyncResponseCallback to isolate data from different tests
    
    * Same sleep time for similar methods

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/resource/AsyncResponseCallback.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/response/resource/AsyncResponseCallback.java
@@ -1,5 +1,7 @@
 package org.jboss.resteasy.test.response.resource;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -7,32 +9,36 @@ import javax.ws.rs.container.CompletionCallback;
 
 public class AsyncResponseCallback implements CompletionCallback {
 
-   private static CountDownLatch latch;
-   private static Throwable error;
+   private static Map<String, CountDownLatch> latches = new ConcurrentHashMap<String, CountDownLatch>();
+   private static Map<String, Throwable> errors = new ConcurrentHashMap<String, Throwable>();
+   private final String s;
 
-   public AsyncResponseCallback()
+   public AsyncResponseCallback(final String s)
    {
-      latch = new CountDownLatch(1);
-      error = null;
+      this.s = s;
+      latches.put(s, new CountDownLatch(1));
+      errors.remove(s);
    }
 
    @Override
    public void onComplete(Throwable throwable)
    {
-      latch.countDown();
-      error = throwable;
+      latches.get(s).countDown();
+      if (throwable != null)
+         errors.put(s, throwable);
    }
 
-   public static void assertCalled(boolean withError)
+   public static void assertCalled(String s, boolean withError)
    {
       boolean called = false;
       try {
-         called = latch.await(10, TimeUnit.SECONDS);
+         called = latches.get(s).await(10, TimeUnit.SECONDS);
       } catch (Exception e) {
          //ignore
       }
       if(!called)
          throw new AssertionError("Not called");
+      Throwable error = errors.get(s);
       if(withError && error == null
             || !withError && error != null)
          throw new AssertionError("Error mismatch");
