dbea4c6233f616b394caa1c0cc0a1819230b702c
NicoNes <nicolas.nesmon@gmail.com>
2020-06-20 18:32:22 -0400

    Properties or provider registered in a dynamic feature for a specific method should be retrievable in this method request context.
    Signed-off-by: NicoNes <nicolas.nesmon@gmail.com>

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/interceptor/resource/AddFeature.java
@@ -0,0 +1,32 @@
+package org.jboss.resteasy.test.interceptor.resource;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Feature;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class AddFeature implements Feature {
+
+   public static final String PROPERTY = "FeatureProperty";
+   public static final String PROPERTY_VALUE = "FeaturePropertyValue";
+
+   public static final class DoNothingGlobalRequestFilter implements ContainerRequestFilter {
+
+      @Override
+      public void filter(ContainerRequestContext requestContext) throws IOException {
+      }
+
+   }
+
+   @Override
+   public boolean configure(FeatureContext context) {
+         context.property(PROPERTY, PROPERTY_VALUE);
+         context.register(DoNothingGlobalRequestFilter.class);
+         return true;
+   }
+
+}
