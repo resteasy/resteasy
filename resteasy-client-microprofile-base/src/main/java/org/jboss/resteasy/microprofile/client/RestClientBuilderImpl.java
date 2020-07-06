8bc56ed4967bdfcf39eae0a51eafe6f00f2d314b
R Searls <rsearls@redhat.com>
2020-07-06 13:17:47 -0400

    [Resteasy 2618] mp rest client query param style and tests
    fixed tck versioning issue

--- resteasy-client-microprofile-base/src/main/java/org/jboss/resteasy/microprofile/client/RestClientBuilderImpl.java
+++ resteasy-client-microprofile-base/src/main/java/org/jboss/resteasy/microprofile/client/RestClientBuilderImpl.java
@@ -16,6 +16,7 @@ import org.jboss.resteasy.client.jaxrs.internal.LocalResteasyProviderFactory;
 import org.jboss.resteasy.microprofile.client.async.AsyncInterceptorRxInvokerProvider;
 import org.jboss.resteasy.microprofile.client.header.ClientHeaderProviders;
 import org.jboss.resteasy.microprofile.client.header.ClientHeadersRequestFilter;
+import org.jboss.resteasy.microprofile.client.impl.MpClient;
 import org.jboss.resteasy.microprofile.client.impl.MpClientBuilderImpl;
 import org.jboss.resteasy.specimpl.ResteasyUriBuilderImpl;
 import org.jboss.resteasy.spi.ResteasyProviderFactory;
@@ -113,8 +114,8 @@ public class RestClientBuilderImpl implements RestClientBuilder {
     }
 
     @Override
-    public RestClientBuilder queryParamStyle(QueryParamStyle var1) {
-        // TODO implement under a different jira and branch
+    public RestClientBuilder queryParamStyle(QueryParamStyle queryParamStyle) {
+        this.queryParamStyle = queryParamStyle;
         return this;
     }
 
@@ -273,6 +274,7 @@ public class RestClientBuilderImpl implements RestClientBuilder {
 
         resteasyClientBuilder.hostnameVerifier(hostnameVerifier);
         resteasyClientBuilder.setIsTrustSelfSignedCertificates(false);
+        checkQueryParamStyleProperty(aClass);
         checkFollowRedirectProperty (aClass);
         resteasyClientBuilder.setFollowRedirects(followRedirect);
 
@@ -291,6 +293,7 @@ public class RestClientBuilderImpl implements RestClientBuilder {
         }
         client = resteasyClientBuilder
                 .build();
+        ((MpClient)client).setQueryParamStyle(queryParamStyle);
         client.register(AsyncInterceptorRxInvokerProvider.class);
 
         actualClient = client.target(baseURI)
@@ -331,6 +334,40 @@ public class RestClientBuilderImpl implements RestClientBuilder {
                 .findFirst();
     }
 
+    private void checkQueryParamStyleProperty(Class aClass) {
+        // User's programmatic setting takes precedence over
+        // microprofile-config.properties.
+        if (queryParamStyle == null) {
+            if (config != null) {
+                // property using fully-qualified class name takes precedence
+                Optional<String> prop = config.getOptionalValue(
+                        aClass.getCanonicalName()+"/mp-rest/queryParamStyle", String.class);
+                if (prop.isPresent()) {
+                    queryParamStyle(QueryParamStyle.valueOf(
+                            prop.get().trim().toUpperCase()));
+
+                } else {
+                    RegisterRestClient registerRestClient =
+                            (RegisterRestClient)aClass.getAnnotation(RegisterRestClient.class);
+                    if (registerRestClient !=null &&
+                            registerRestClient.configKey() != null &&
+                            !registerRestClient.configKey().isEmpty()) {
+
+                        //property using configKey
+                        prop = config.getOptionalValue(registerRestClient.configKey()
+                                + "/mp-rest/queryParamStyle", String.class);
+                        if (prop.isPresent()) {
+                            queryParamStyle(QueryParamStyle.valueOf(
+                                    prop.get().trim().toUpperCase()));
+                        }
+                    }
+                }
+            }
+        }
+        if (queryParamStyle == null) {
+            queryParamStyle = QueryParamStyle.MULTI_PAIRS;
+        }
+    }
     private void checkFollowRedirectProperty (Class aClass) {
         // User's programmatic setting takes precedence over
         // microprofile-config.properties.
@@ -700,6 +737,7 @@ public class RestClientBuilderImpl implements RestClientBuilder {
     private HostnameVerifier hostnameVerifier;
     private Boolean useURLConnection;
     private boolean followRedirect;
+    private QueryParamStyle queryParamStyle = null;
 
     private Set<Object> localProviderInstances = new HashSet<>();
 }
--- resteasy-client-microprofile-base/src/main/java/org/jboss/resteasy/microprofile/client/impl/MpClientWebTarget.java
+++ resteasy-client-microprofile-base/src/main/java/org/jboss/resteasy/microprofile/client/impl/MpClientWebTarget.java
@@ -1,55 +1,65 @@
 package org.jboss.resteasy.microprofile.client.impl;
 
-import java.net.URI;
-import java.util.List;
-
-import javax.ws.rs.core.UriBuilder;
-
 import org.eclipse.microprofile.rest.client.ext.AsyncInvocationInterceptorFactory;
+import org.eclipse.microprofile.rest.client.ext.QueryParamStyle;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.internal.ClientConfiguration;
 import org.jboss.resteasy.client.jaxrs.internal.ClientInvocationBuilder;
 import org.jboss.resteasy.client.jaxrs.internal.ClientWebTarget;
 
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.List;
+
 public class MpClientWebTarget extends ClientWebTarget {
 
    private List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories;
+   private QueryParamStyle queryParamStyle;
 
    protected MpClientWebTarget(final ResteasyClient client, final ClientConfiguration configuration,
-                                final List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories)
+                                final List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories,
+                                final QueryParamStyle queryParamStyle)
     {
         super(client, configuration);
         this.asyncInterceptorFactories = asyncInterceptorFactories;
+        this.queryParamStyle = queryParamStyle;
     }
 
     public MpClientWebTarget(final ResteasyClient client, final String uri, final ClientConfiguration configuration,
-                             final List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories) throws IllegalArgumentException, NullPointerException
+                             final List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories,
+                             final QueryParamStyle queryParamStyle) throws IllegalArgumentException, NullPointerException
     {
-       super(client, uri, configuration);
+       super(client, new MpUriBuilder().uri(uri,queryParamStyle), configuration);
        this.asyncInterceptorFactories = asyncInterceptorFactories;
+       this.queryParamStyle = queryParamStyle;
     }
 
     public MpClientWebTarget(final ResteasyClient client, final URI uri, final ClientConfiguration configuration,
-                             final List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories) throws NullPointerException
+                             final List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories,
+                             final QueryParamStyle queryParamStyle) throws NullPointerException
     {
-       super(client, uri, configuration);
+       super(client, new MpUriBuilder().uri(uri,queryParamStyle), configuration);
        this.asyncInterceptorFactories = asyncInterceptorFactories;
+       this.queryParamStyle = queryParamStyle;
     }
 
     public MpClientWebTarget(final ResteasyClient client, final UriBuilder uriBuilder, final ClientConfiguration configuration,
-                             final List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories) throws NullPointerException
+                             final List<AsyncInvocationInterceptorFactory> asyncInterceptorFactories,
+                             final QueryParamStyle queryParamStyle) throws NullPointerException
     {
        super(client, uriBuilder, configuration);
        this.asyncInterceptorFactories = asyncInterceptorFactories;
+       this.queryParamStyle = queryParamStyle;
     }
 
     @Override
     protected ClientWebTarget newInstance(ResteasyClient client, UriBuilder uriBuilder, ClientConfiguration configuration) {
-        return new MpClientWebTarget(client, uriBuilder, configuration, asyncInterceptorFactories);
+        return new MpClientWebTarget(client, uriBuilder, configuration, asyncInterceptorFactories, queryParamStyle);
     }
 
     @Override
     protected ClientInvocationBuilder createClientInvocationBuilder(ResteasyClient client, URI uri, ClientConfiguration configuration) {
         return new MpClientInvocationBuilder(client, uri, configuration, asyncInterceptorFactories);
     }
+
 }
