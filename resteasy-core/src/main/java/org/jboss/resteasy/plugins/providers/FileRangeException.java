d32df9a455df3b989300b7114b8bc381a69d3ccd
Alessio Soldano <asoldano@redhat.com>
2020-08-05 12:32:11 +0200

    [RESTEASY-2669] Dealt with a few warnings

--- resteasy-core/src/main/java/org/jboss/resteasy/plugins/providers/FileRangeException.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/plugins/providers/FileRangeException.java
@@ -11,6 +11,8 @@ import java.io.File;
  */
 public class FileRangeException extends WebApplicationException
 {
+   private static final long serialVersionUID = -5615796352743435769L;
+
    public FileRangeException(final MediaType mediaType, final File file, final long begin, final long end)
    {
       super(Response.status(206).entity(new FileRange(file, begin, end)).type(mediaType).build());
