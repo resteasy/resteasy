d5497a2bfd66306d937a4c59da15fc69260b40ef
Ron Sigal <rsigal@redhat.com>
2020-09-08 20:45:35 -0400

    [RESTEASY-2684] Added unit test.

---
+++ testsuite/unit-tests/src/test/java/org/jboss/resteasy/test/client/DefaultResteasyProviderFactoryTest.java
@@ -0,0 +1,34 @@
+package org.jboss.resteasy.test.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.client.Client;
+
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
+import org.jboss.resteasy.core.providerfactory.ResteasyProviderFactoryImpl;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @tpSubChapter Client
+ * @tpChapter Unit tests
+ * @tpTestCaseDetails Test for RESTEASY-2684
+ * @tpSince RESTEasy 4.6
+ */
+public class DefaultResteasyProviderFactoryTest {
+
+   @Test
+   public void testDefaultResteasyProviderFactory() {
+      ResteasyProviderFactory rpf = new ResteasyProviderFactoryImpl();
+      Map<String, Object> map = new HashMap<String, Object>();
+      map.put("foo", "bar");
+      rpf.setProperties(map);
+      ResteasyClientBuilderImpl.setProviderFactory(rpf);
+      Client client = ResteasyClientBuilderImpl.newClient();
+      Assert.assertEquals("foo", client.getConfiguration().getProperty("bar"));
+      client.close();
+   }
+
+}
