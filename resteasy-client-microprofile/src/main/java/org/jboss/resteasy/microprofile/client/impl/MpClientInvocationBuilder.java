6498013275b5edb56408d29d41d76256c859ea6c
R Searls <rsearls@redhat.com>
2020-09-30 16:43:08 -0400

    [RESTEASY-2558] using AsyncInterceptorRxInvoker in MpClientInvocationBuilder rx

--- resteasy-client-microprofile/src/main/java/org/jboss/resteasy/microprofile/client/impl/MpClientInvocationBuilder.java
+++ resteasy-client-microprofile/src/main/java/org/jboss/resteasy/microprofile/client/impl/MpClientInvocationBuilder.java
@@ -7,12 +7,20 @@ import org.jboss.resteasy.client.jaxrs.internal.ClientConfiguration;
 import org.jboss.resteasy.client.jaxrs.internal.ClientInvocation;
 import org.jboss.resteasy.client.jaxrs.internal.ClientInvocationBuilder;
 import org.jboss.resteasy.client.jaxrs.internal.ClientRequestHeaders;
+import org.jboss.resteasy.microprofile.client.async.AsyncInterceptorRxInvoker;
+
+import javax.ws.rs.client.CompletionStageRxInvoker;
 
 
 public class MpClientInvocationBuilder extends ClientInvocationBuilder {
 
     public MpClientInvocationBuilder(final ResteasyClient client, final URI uri, final ClientConfiguration configuration) {
         super(client, uri, configuration);
+    }
+
+    @Override
+    public CompletionStageRxInvoker rx() {
+        return new AsyncInterceptorRxInvoker(this, invocation.getClient().asyncInvocationExecutor());
     }
 
     @Override
