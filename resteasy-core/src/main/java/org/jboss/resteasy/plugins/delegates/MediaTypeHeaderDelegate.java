e2f4fcbe8aece875ed7be6efb5c4ee76e12082b1
R Searls <rsearls@redhat.com>
2020-11-24 15:54:05 -0500

    [RESTEASY-2772] changed exception type and adjusted unit tests

--- resteasy-core/src/main/java/org/jboss/resteasy/plugins/delegates/MediaTypeHeaderDelegate.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/plugins/delegates/MediaTypeHeaderDelegate.java
@@ -11,6 +11,7 @@ import java.security.PrivilegedAction;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import javax.ws.rs.BadRequestException;
 
 /**
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
@@ -93,7 +94,7 @@ public class MediaTypeHeaderDelegate implements RuntimeDelegate.HeaderDelegate<M
          }
          if (!MediaType.MEDIA_TYPE_WILDCARD.equals(major))
          {
-            throw new IllegalArgumentException(Messages.MESSAGES.failureParsingMediaType(type));
+            throw new BadRequestException(Messages.MESSAGES.failureParsingMediaType(type));
          }
          subtype = MediaType.MEDIA_TYPE_WILDCARD;
       }
@@ -111,11 +112,11 @@ public class MediaTypeHeaderDelegate implements RuntimeDelegate.HeaderDelegate<M
       }
       if (major.length() < 1 || subtype.length() < 1)
       {
-         throw new IllegalArgumentException(Messages.MESSAGES.failureParsingMediaType(type));
+         throw new BadRequestException(Messages.MESSAGES.failureParsingMediaType(type));
       }
       if (!isValid(major) || !isValid(subtype))
       {
-         throw new IllegalArgumentException(Messages.MESSAGES.failureParsingMediaType(type));
+         throw new BadRequestException(Messages.MESSAGES.failureParsingMediaType(type));
       }
       String params = null;
       if (paramIndex > -1) params = type.substring(paramIndex + 1);
