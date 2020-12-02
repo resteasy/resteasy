612144ce16300ff2a30316fa66ebaaaf1c4b5407
Jim Ma <ema@redhat.com>
2020-12-02 16:15:27 +0800

    Change the package name to jakarta.*

--- resteasy-core-spi/src/main/java/org/jboss/resteasy/resteasy_jaxrs/i18n/LogMessages.java
+++ resteasy-core-spi/src/main/java/org/jboss/resteasy/resteasy_jaxrs/i18n/LogMessages.java
@@ -3,7 +3,7 @@ package org.jboss.resteasy.resteasy_jaxrs.i18n;
 import java.lang.reflect.AccessibleObject;
 import java.net.URL;
 
-import javax.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MediaType;
 
 import org.jboss.logging.BasicLogger;
 import org.jboss.logging.Logger;
