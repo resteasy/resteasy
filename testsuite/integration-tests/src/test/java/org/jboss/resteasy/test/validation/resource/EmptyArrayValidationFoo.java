8a394e4cbf80696c4d927753d649c3ffc16e7d1a
Ron Sigal <rsigal@redhat.com>
2020-12-02 18:42:24 -0500

    [RESTEASY-2765] Fix validation of empty array

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/validation/resource/EmptyArrayValidationFoo.java
@@ -0,0 +1,24 @@
+package org.jboss.resteasy.test.validation.resource;
+
+import javax.validation.constraints.NotEmpty;
+
+public class EmptyArrayValidationFoo {
+
+   @NotEmpty
+   Object[] array;
+
+   public EmptyArrayValidationFoo(final Object[] array) {
+      this.array = array;
+   }
+
+   public EmptyArrayValidationFoo() {
+   }
+
+   public Object[] getArray() {
+      return array;
+   }
+
+   public void setArray(final Object[] array) {
+      this.array = array;
+   }
+}
