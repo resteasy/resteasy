94037aa6cde41a6946671272183f02d54845c4af
Jim Ma <ema@redhat.com>
2020-12-04 10:59:00 +0800

    Add travis build

---
+++ travis-script/prebuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+git clone --depth=1 --branch=ee9-experimental https://github.com/jimma/jboss-jakarta-jaxrs-api_spec.git $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec
+pushd $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec
+mvn --quiet clean install -DskipTest=true
+popd
+
+git clone --depth=1 --branch=master https://github.com/stuartwdouglas/optimus.git $TRAVIS_HOME/optimus
+pushd $TRAVIS_HOME/optimus
+mvn --quiet clean install -DskipTest=true
+popd
