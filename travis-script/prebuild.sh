8c045b2f7c0a9b67b8d51a052ebce13713f9827f
Jim Ma <ema@redhat.com>
2020-12-04 15:19:21 +0800

    Fix path

--- travis-script/prebuild.sh
+++ travis-script/prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 git clone --depth=1 --branch=ee9-experimental https://github.com/jimma/jboss-jakarta-jaxrs-api_spec.git $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec
-pushd $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec
+pushd $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec/jaxrs-api
 mvn --quiet clean install -DskipTest=true
 popd
 
