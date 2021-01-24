3c9dbac416d275321f843d2703bc0c7f73843aa6
Ron Sigal <rsigal@redhat.com>
2021-01-23 19:03:30 -0500

    [RESTEASY-2812] Fix MediaType cache poisoning with trailing semi-colon (#2653)
    [Fix by Steve Newson]

---
+++ testsuite/unit-tests/src/test/java/org/jboss/resteasy/test/mediatype/MediaTypeHeaderDelegateTest.java
@@ -0,0 +1,74 @@
+package org.jboss.resteasy.test.mediatype;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.plugins.delegates.MediaTypeHeaderDelegate;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MediaTypeHeaderDelegateTest {
+
+   private MediaTypeHeaderDelegate delegate;
+
+   @Before
+   public void setUp() throws Exception {
+      delegate = new MediaTypeHeaderDelegate();
+
+      // We need to clear the cache here since the cache is static and will be reused between tests.
+      // This will not work if these tests are run in parallel.
+      MediaTypeHeaderDelegate.clearCache();
+   }
+
+   @Test
+   public void testParseStripsTrailingSemicolonWhenParsingDodgyContentType() {
+      MediaTypeHeaderDelegate.parse("application/json;");
+
+      assertEquals("application/json", delegate.toString(new MediaType("application", "json")));
+   }
+
+   @Test
+   public void testParseHandlesMediaTypeWithCharset() {
+      MediaTypeHeaderDelegate.parse("application/json; charset=utf-8");
+
+      assertEquals("application/json;charset=utf-8", delegate.toString(new MediaType("application", "json", "utf-8")));
+   }
+
+   @Test
+   public void testParseHandlesMediaTypeWithCharsetAndAnotherParameter() {
+      MediaTypeHeaderDelegate.parse("application/json; charset=utf-8; a=bobbytables");
+
+      Map<String, String> parameters = new HashMap<>();
+      parameters.put("charset", "utf-8");
+      parameters.put("a", "bobbytables");
+      assertEquals("application/json;a=bobbytables;charset=utf-8", delegate.toString(new MediaType("application", "json", parameters)));
+   }
+
+   @Test
+   public void testParseHandlesMediaTypeWithCharsetAndAnotherParameterAndTrailingSemiColon() {
+      MediaTypeHeaderDelegate.parse("application/json; charset=utf-8; a=bobbytables;");
+
+      Map<String, String> parameters = new HashMap<>();
+      parameters.put("charset", "utf-8");
+      parameters.put("a", "bobbytables");
+      assertEquals("application/json;a=bobbytables;charset=utf-8", delegate.toString(new MediaType("application", "json", parameters)));
+   }
+
+   @Test
+   public void initialisedDelegateCacheShouldNotBePoisonedByMediaTypeWithTrailingSemiColon() {
+      // Seed the cache by asking to parse a valid
+      MediaTypeHeaderDelegate.parse("application/json");
+
+      // Verify that getting retrieving the media type returns the expected media type
+      assertEquals("application/json", delegate.toString(new MediaType("application", "json")));
+
+      // Poison the cache with a trailing semi-colon
+      MediaTypeHeaderDelegate.parse("application/json;");
+
+      // Verify that getting retrieving the media type returns the expected media type
+      assertEquals("application/json", delegate.toString(new MediaType("application", "json")));
+   }
+}
