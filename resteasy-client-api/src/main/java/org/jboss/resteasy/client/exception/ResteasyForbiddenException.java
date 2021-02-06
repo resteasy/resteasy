ac789b2094f8f45a001ba4628f339a9dd92b2965
Ron Sigal <rsigal@redhat.com>
2021-02-06 12:16:27 -0500

    [RESTEASY-2825 Clients running in a resource method throw safer WebApplicationException (#2671)

---
+++ resteasy-client-api/src/main/java/org/jboss/resteasy/client/exception/ResteasyForbiddenException.java
@@ -0,0 +1,27 @@
+package org.jboss.resteasy.client.exception;
+
+import static org.jboss.resteasy.client.exception.WebApplicationExceptionWrapper.sanitize;
+
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.core.Response;
+
+/**
+ * Wraps a {@link ForbiddenException} with a {@linkplain #sanitize(Response) sanitized} response.
+ *
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+public class ResteasyForbiddenException extends ForbiddenException implements WebApplicationExceptionWrapper<ForbiddenException> {
+
+   private static final long serialVersionUID = -581285336820307590L;
+   private final ForbiddenException wrapped;
+
+    ResteasyForbiddenException(final ForbiddenException wrapped) {
+        super(wrapped.getMessage(), sanitize(wrapped.getResponse()), wrapped.getCause());
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public ForbiddenException unwrap() {
+        return wrapped;
+    }
+}
---
+++ resteasy-client-api/src/main/java/org/jboss/resteasy/client/exception/ResteasyServiceUnavailableException.java
@@ -0,0 +1,38 @@
+package org.jboss.resteasy.client.exception;
+
+import static org.jboss.resteasy.client.exception.WebApplicationExceptionWrapper.sanitize;
+
+import java.util.Date;
+import javax.ws.rs.ServiceUnavailableException;
+import javax.ws.rs.core.Response;
+
+/**
+ * Wraps a {@link ServiceUnavailableException} with a {@linkplain #sanitize(Response) sanitized} response.
+ *
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+public class ResteasyServiceUnavailableException extends ServiceUnavailableException implements WebApplicationExceptionWrapper<ServiceUnavailableException> {
+
+   private static final long serialVersionUID = -4477873328299557209L;
+   private final ServiceUnavailableException wrapped;
+
+    ResteasyServiceUnavailableException(final ServiceUnavailableException wrapped) {
+        super(wrapped.getMessage(), sanitize(wrapped.getResponse()), wrapped.getCause());
+        this.wrapped = wrapped;
+    }
+
+    @Override
+    public boolean hasRetryAfter() {
+        return wrapped.hasRetryAfter();
+    }
+
+    @Override
+    public Date getRetryTime(final Date requestTime) {
+        return wrapped.getRetryTime(requestTime);
+    }
+
+    @Override
+    public ServiceUnavailableException unwrap() {
+        return wrapped;
+    }
+}
