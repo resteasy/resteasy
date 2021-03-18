94915bd951e7018d346812bc3b384b3f5d19202f
阿男 <l.weinan@gmail.com>
2021-03-18 23:19:49 +0800

    replace bash script with jbang

--- docbook/scripts/set_spring_dep_ver.sh
+++
@@ -1,7 +0,0 @@
-set -x
-
-pushd ../resteasy-spring
-SPRING_VER=$(mvn dependency:tree | grep spring | grep 'org.springframework:spring-core' | awk -F':' '{print $4}' | head -n 1)
-popd
-
-sed -ie "s:_SPRING_VER_:${SPRING_VER}:" "${BASEDIR}"/reference/en/en-US/modules/RESTEasy_Spring_Integration.xml
