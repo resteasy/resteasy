94abafb153cc1a69e9adf72c71a832592b84fb7a
Alessio Soldano <asoldano@redhat.com>
2021-04-23 12:22:52 +0200

    Further javax.ws.rs -> jakarta.ws.rs changes

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/context/resource/HttpServletResponseContextResource.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/context/resource/HttpServletResponseContextResource.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 
 import org.infinispan.commons.dataconversion.MediaType;
 
