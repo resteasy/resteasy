1536ce0c34b80a33c7c6019133f3ae76e1bf5401
Andy McCright <j.andrew.mccright@gmail.com>
2021-05-04 09:42:37 -0500

    [RESTEASY-2880] Ensure sys props reset after test
    Signed-off-by: Andy McCright <j.andrew.mccright@gmail.com>

--- providers/multipart/src/test/java/org/jboss/resteasy/plugins/providers/multipart/Mime4JWorkaroundTest.java
+++ providers/multipart/src/test/java/org/jboss/resteasy/plugins/providers/multipart/Mime4JWorkaroundTest.java
@@ -2,15 +2,47 @@ package org.jboss.resteasy.plugins.providers.multipart;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Mime4JWorkaroundTest {
 
+   private static final Map<String, String> preTestProperties = new HashMap<>();
+
+   @BeforeClass
+   public static void saveCurrentStateOfMemThresholdProperty()
+   {
+      if (System.getProperties().containsKey(Mime4JWorkaround.MEM_THRESHOLD_PROPERTY))
+      {
+         String value = System.getProperty(Mime4JWorkaround.MEM_THRESHOLD_PROPERTY);
+         preTestProperties.put(Mime4JWorkaround.MEM_THRESHOLD_PROPERTY, value);
+      }
+   }
+
+   @AfterClass
+   public static void resetPropertiesToPreTestValues()
+   {
+      if (!preTestProperties.containsKey(Mime4JWorkaround.MEM_THRESHOLD_PROPERTY))
+      {
+         System.clearProperty(Mime4JWorkaround.MEM_THRESHOLD_PROPERTY);
+      }
+      else
+      {
+         preTestProperties.forEach(System::setProperty);
+      }
+   }
+
    @Before
-   public void unsetMemThresholdProperty() {
+   public void unsetMemThresholdProperty()
+   {
       System.clearProperty(Mime4JWorkaround.MEM_THRESHOLD_PROPERTY);
    }
+
    @Test
    public void testMemThresholdDefault()
    {
