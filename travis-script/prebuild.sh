a2b365264d3293f9541a9cebfd46ce6b7b5f929c
R Searls <rsearls@redhat.com>
2021-06-02 08:52:27 -0400

    add debug info to prebuild.sh. use optimus branch with dup tag fix.

--- travis-script/prebuild.sh
+++ travis-script/prebuild.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -ex
+
 git clone --depth=1 --branch=ee9-experimental https://github.com/jimma/jboss-jakarta-jaxrs-api_spec.git $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec
 pushd $TRAVIS_HOME/jboss-jakarta-jaxrs-api_spec/jaxrs-api
 mvn --quiet clean install -DskipTest=true
 popd
 
-git clone --depth=1 --branch=main https://github.com/stuartwdouglas/optimus.git $TRAVIS_HOME/optimus
+git clone --depth=1 --branch=fix-smallrye-duplication-tag https://github.com/rsearls/optimus.git $TRAVIS_HOME/optimus
 pushd $TRAVIS_HOME/optimus
 mvn --quiet clean install -DskipTest=true
 popd
 
 git clone --depth=1 --branch=master https://github.com/wildfly/wildfly.git $TRAVIS_HOME/wildfly
 pushd $TRAVIS_HOME/wildfly
-mvn --quiet clean install -DskipTest=true
+mvn --quiet clean install -DskipTest=true -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2
 popd
