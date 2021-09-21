30c652d229581c84237e269f28b9bd85fe134d91
James R. Perkins <jperkins@redhat.com>
2021-09-21 13:15:27 -0700

    Merge pull request #2882 from dnagarajan89/reactor-netty-adapter-bytes-issue
    [RESTEASY-2996] Calling send for each chunk with flush and completing the future

---
+++ server-adapters/resteasy-reactor-netty/src/main/java/org/jboss/resteasy/plugins/server/reactor/netty/ReactorNettySendException.java
@@ -0,0 +1,8 @@
+package org.jboss.resteasy.plugins.server.reactor.netty;
+
+public class ReactorNettySendException extends RuntimeException {
+
+    public ReactorNettySendException(final Throwable cause) {
+        super(cause);
+    }
+}
