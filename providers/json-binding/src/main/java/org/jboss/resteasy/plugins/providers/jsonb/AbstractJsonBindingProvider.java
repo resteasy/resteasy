e8a96c11ccb107ae97abfe205e93dd53db62f1fc
James R. Perkins <jperkins@redhat.com>
2021-12-01 14:48:56 -0800

    [RESTEASY-2891] Do not depend on Yasson for the Jakarta EE JSON Binding implementation. Also ensure the Jsonb type is closed after invocation.
    https://issues.redhat.com/browse/RESTEASY-2891
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- providers/json-binding/src/main/java/org/jboss/resteasy/plugins/providers/jsonb/AbstractJsonBindingProvider.java
+++ providers/json-binding/src/main/java/org/jboss/resteasy/plugins/providers/jsonb/AbstractJsonBindingProvider.java
@@ -1,56 +1,37 @@
 package org.jboss.resteasy.plugins.providers.jsonb;
 
-import org.eclipse.yasson.JsonBindingProvider;
-import org.eclipse.yasson.internal.JsonBindingBuilder;
-import org.glassfish.json.JsonProviderImpl;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 import javax.json.bind.Jsonb;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.ContextResolver;
-import java.nio.charset.Charset;
+import javax.ws.rs.ext.Providers;
 
 
 /**
  * Created by rsearls
  */
-public class AbstractJsonBindingProvider extends JsonBindingProvider {
+public class AbstractJsonBindingProvider {
 
    private static final String JSON = "json";
    private static final String PLUS_JSON = "+json";
 
    @Context
-   javax.ws.rs.ext.Providers providers;
-   private volatile Jsonb jsonbObj;
+   private Providers providers;
 
    protected Jsonb getJsonb(Class<?> type) {
       ContextResolver<Jsonb> contextResolver = providers.getContextResolver(Jsonb.class, MediaType.APPLICATION_JSON_TYPE);
-      if (contextResolver != null)
-      {
-         return contextResolver.getContext(type);
+      Jsonb delegate = null;
+      if (contextResolver != null) {
+         delegate = contextResolver.getContext(type);
       }
-      else
-      {
-         Jsonb currentJsonbObj = jsonbObj;
-         if (currentJsonbObj == null)
-         {
-            synchronized (this)
-            {
-               currentJsonbObj = jsonbObj;
-               if (currentJsonbObj == null) {
-                  JsonProviderImpl jProviderImpl = new JsonProviderImpl();
-                  JsonBindingBuilder jbBuilder = new JsonBindingBuilder();
-                  currentJsonbObj = jbBuilder.withProvider(jProviderImpl).build();
-                  this.jsonbObj = currentJsonbObj;
-               }
-            }
-         }
-         return currentJsonbObj;
-      }
+      return new ManagedJsonb(delegate);
    }
 
    public static Charset getCharset(final MediaType mediaType) {
-      return Charset.forName("utf-8");
+      return StandardCharsets.UTF_8;
    }
 
    public static boolean isSupportedMediaType(final MediaType mediaType) {
