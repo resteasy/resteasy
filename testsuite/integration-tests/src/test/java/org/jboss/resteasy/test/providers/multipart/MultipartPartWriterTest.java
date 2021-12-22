577415b72eeaba093363b8ce43d935c3e126c967
James R. Perkins <jperkins@redhat.com>
2021-12-22 10:08:05 -0800

    [RESTEASY-3069] Do not assume the MessageBodyWriter for a multipart part is asynchronous. While it's not ideal, in some cases they are not and will need to be written synchronously.
    https://issues.redhat.com/browse/RESTEASY-3069
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/multipart/MultipartPartWriterTest.java
@@ -0,0 +1,104 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ *
+ * Copyright 2021 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.resteasy.test.providers.multipart;
+
+import java.net.URL;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartRelatedInput;
+import org.jboss.resteasy.test.providers.multipart.resource.GreetAsync;
+import org.jboss.resteasy.test.providers.multipart.resource.Greeter;
+import org.jboss.resteasy.test.providers.multipart.resource.GreeterAsyncMessageBodyWriter;
+import org.jboss.resteasy.test.providers.multipart.resource.GreeterMessageBodyWriter;
+import org.jboss.resteasy.test.providers.multipart.resource.GreeterResource;
+import org.jboss.resteasy.utils.TestUtil;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests which use a non-{@linkplain org.jboss.resteasy.spi.AsyncMessageBodyWriter} and a
+ * {@linkplain org.jboss.resteasy.spi.AsyncMessageBodyWriter} for parts to ensure the
+ * {@link org.jboss.resteasy.plugins.providers.multipart.MultipartWriter} can use writers of both sync and async.
+ *
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class MultipartPartWriterTest {
+
+    private static Client client;
+    @ArquillianResource
+    private URL url;
+
+    @AfterClass
+    public static void closeClient() {
+        if (client != null) {
+            client.close();
+        }
+    }
+
+    @Deployment
+    public static Archive<?> createDeployment() {
+        WebArchive war = TestUtil.prepareArchive(MultipartPartWriterTest.class.getSimpleName())
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addClasses(Greeter.class, GreetAsync.class);
+        return TestUtil.finishContainerPrepare(war, null,
+                GreeterMessageBodyWriter.class, GreeterAsyncMessageBodyWriter.class, GreeterResource.class);
+    }
+
+    @BeforeClass
+    public static void initClient() {
+        client = ClientBuilder.newClient();
+    }
+
+    @Test
+    public void sync() throws Exception {
+        final Response response = client.target(TestUtil.generateUri(url, "greet"))
+                .request()
+                .get();
+        Assert.assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+        final MultipartRelatedInput input = response.readEntity(MultipartRelatedInput.class);
+        Assert.assertNotNull(input);
+        Assert.assertEquals("Hello Sync", input.getRootPart().getBodyAsString());
+    }
+
+    @Test
+    public void async() throws Exception {
+        final Response response = client.target(TestUtil.generateUri(url, "greet/async"))
+                .request()
+                .get();
+        Assert.assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
+        final MultipartRelatedInput input = response.readEntity(MultipartRelatedInput.class);
+        Assert.assertNotNull(input);
+        Assert.assertEquals("Hello Async", input.getRootPart().getBodyAsString());
+    }
+}
---
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/providers/multipart/resource/GreeterResource.java
@@ -0,0 +1,48 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ *
+ * Copyright 2021 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.resteasy.test.providers.multipart.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartRelatedOutput;
+
+/**
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+@Path("greet")
+public class GreeterResource {
+
+    @GET
+    public MultipartRelatedOutput sync() {
+        final MultipartRelatedOutput output = new MultipartRelatedOutput();
+        output.addPart(new Greeter(), MediaType.TEXT_PLAIN_TYPE);
+        return output;
+    }
+
+    @GET
+    @Path("/async")
+    @GreetAsync
+    public MultipartRelatedOutput async() {
+        final MultipartRelatedOutput output = new MultipartRelatedOutput();
+        output.addPart(new Greeter(), MediaType.TEXT_PLAIN_TYPE);
+        return output;
+    }
+}
