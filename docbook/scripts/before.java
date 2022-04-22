a2d5f3ac9104ca9e5dbab3619709d84d8e2a932b
James R. Perkins <jperkins@redhat.com>
2022-04-22 11:41:47 -0700

    Add a profile to include the docbook and distribution modules for releasing. Also, some minor updates ot the compiler configuration.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- docbook/scripts/before.java
+++ docbook/scripts/before.java
@@ -9,9 +9,10 @@ import java.util.regex.Pattern;
 class before {
 
     public static void main(String... args) throws Exception {
-        String ver = getVer();
+        final String baseDir = args == null ? "." : args[0];
+        String ver = getVer(baseDir);
 
-        Path path = Paths.get("reference/en/en-US/modules/RESTEasy_Spring_Integration.xml");
+        Path path = Paths.get(baseDir, "reference", "en", "en-US", "modules", "RESTEasy_Spring_Integration.xml");
         Charset charset = StandardCharsets.UTF_8;
 
         String content = new String(Files.readAllBytes(path), charset);
@@ -22,8 +23,8 @@ class before {
         Files.write(path, content.getBytes(charset));
     }
 
-    public static String getVer() throws Exception {
-        Path path = Paths.get("../resteasy-dependencies-bom/pom.xml");
+    public static String getVer(final String dir) throws Exception {
+        Path path = Paths.get(dir, "../resteasy-dependencies-bom/pom.xml");
         Charset charset = StandardCharsets.UTF_8;
 
         String content = new String(Files.readAllBytes(path), charset);
