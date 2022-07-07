cc2c96cd689e87efef773e5e3ab6aa4f8ef86b57
James R. Perkins <jperkins@redhat.com>
2022-07-06 17:53:14 -0700

    [RESTEASY-3161] Use the ASLv2 license.
    https://issues.redhat.com/browse/RESTEASY-3161
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- resteasy-core-spi/src/main/java/org/jboss/resteasy/spi/AsyncWriterInterceptor.java
+++ resteasy-core-spi/src/main/java/org/jboss/resteasy/spi/AsyncWriterInterceptor.java
@@ -1,17 +1,20 @@
 /*
- * Copyright (c) 2011, 2017 Oracle and/or its affiliates. All rights reserved.
+ * JBoss, Home of Professional Open Source.
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0, which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ * Copyright 2022 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
  *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the
- * Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
- * version 2 with the GNU Classpath Exception, which is available at
- * https://www.gnu.org/software/classpath/license.html.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.jboss.resteasy.spi;
