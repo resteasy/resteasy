2ed6f2163e19f78a23e6211a036c4a857e616ff8
James R. Perkins <jperkins@redhat.com>
2022-08-10 18:19:14 -0700

    [RESTEASY-3177] Ensure that all async ClientInvocation's happen asynchronously.
    https://issues.redhat.com/browse/RESTEASY-3177
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/internal/ClientInvocation.java
+++ resteasy-client/src/main/java/org/jboss/resteasy/client/jaxrs/internal/ClientInvocation.java
@@ -10,7 +10,7 @@ import java.net.URI;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.function.Function;
@@ -50,6 +50,7 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.engines.AsyncClientHttpEngine;
 import org.jboss.resteasy.client.jaxrs.engines.AsyncClientHttpEngine.ResultExtractor;
 import org.jboss.resteasy.client.jaxrs.engines.ReactiveClientHttpEngine;
+import org.jboss.resteasy.client.jaxrs.i18n.LogMessages;
 import org.jboss.resteasy.client.jaxrs.internal.proxy.ClientInvoker;
 import org.jboss.resteasy.core.ResteasyContext;
 import org.jboss.resteasy.core.ResteasyContext.CloseableContext;
@@ -561,8 +562,21 @@ public class ClientInvocation implements Invocation
    private <T> Future<T> doSubmit(boolean buffered, InvocationCallback<T> callback, ResultExtractor<T> extractor) {
       if (client.httpEngine() instanceof AsyncClientHttpEngine)
       {
-         return asyncSubmit(getFutureExtractorFunction(buffered, callback), extractor,
-               getAsyncAbortedFunction(callback), getAsyncExceptionFunction(callback));
+         return submitRequest(extractor, buffered)
+                 .whenComplete((response, error) -> {
+                    if (callback != null) {
+                       if (error != null) {
+                          callback.failed(error);
+                       } else {
+                          try {
+                             callback.completed(response);
+                          } catch (Throwable t) {
+                             LogMessages.LOGGER.exceptionIgnored(t);
+                          }
+                       }
+                    }
+                 })
+                 .toCompletableFuture();
       }
       else
       {
@@ -574,38 +588,10 @@ public class ClientInvocation implements Invocation
        return client.asyncInvocationExecutor();
    }
 
-   private <T> Function<ResultExtractor<T>, CompletableFuture<T>> getCompletableFutureExtractorFunction(boolean buffered) {
-      final ClientHttpEngine httpEngine = client.httpEngine();
-      return (httpEngine instanceof AsyncClientHttpEngine)
-            ? ext -> ((AsyncClientHttpEngine) httpEngine).submit(this, buffered, ext, asyncInvocationExecutor()) : null;
-   }
-
-   private <T> Function<ResultExtractor<T>, Future<T>> getFutureExtractorFunction(boolean buffered, InvocationCallback<T> callback) {
-      final ClientHttpEngine httpEngine = client.httpEngine();
-      return (httpEngine instanceof AsyncClientHttpEngine)
-            ? ext -> ((AsyncClientHttpEngine) httpEngine).submit(this, buffered, callback, ext) : null;
-   }
-
    private <T> Function<ResultExtractor<T>, Publisher<T>> getPublisherExtractorFunction(boolean buffered) {
       final ClientHttpEngine httpEngine = client.httpEngine();
       return (httpEngine instanceof ReactiveClientHttpEngine)
           ? ext -> ((ReactiveClientHttpEngine) httpEngine).submitRx(this, buffered, ext) : null;
-   }
-
-   private static <T> Function<T, Future<T>> getAsyncAbortedFunction(InvocationCallback<T> callback) {
-      return result -> {
-         callCompletedNoThrow(callback, result);
-         return CompletableFuture.completedFuture(result);
-      };
-   }
-
-   private static <T> Function<Exception, Future<T>> getAsyncExceptionFunction(InvocationCallback<T> callback) {
-      return ex -> {
-         callFailedNoThrow(callback, ex);
-         CompletableFuture<T> completableFuture = new CompletableFuture<>();
-         completableFuture.completeExceptionally(new ExecutionException(ex));
-         return completableFuture;
-      };
    }
 
    @SuppressWarnings("unchecked")
@@ -644,14 +630,8 @@ public class ClientInvocation implements Invocation
    private <T> CompletableFuture<T> doSubmit(ResultExtractor<T> extractor, boolean buffered) {
       if (client.httpEngine() instanceof AsyncClientHttpEngine)
       {
-         return asyncSubmit(getCompletableFutureExtractorFunction(buffered),
-               extractor,
-               CompletableFuture::completedFuture,
-               ex -> {
-                  CompletableFuture<T> completableFuture = new CompletableFuture<>();
-                  completableFuture.completeExceptionally(new ExecutionException(ex));
-                  return completableFuture;
-               });
+         return submitRequest(extractor, buffered)
+                 .toCompletableFuture();
       }
       else
       {
@@ -811,38 +791,28 @@ public class ClientInvocation implements Invocation
       return response;
    }
 
-   private <Q extends Future<T>, T> Q asyncSubmit(
-           final Function<ResultExtractor<T>, Q> asyncHttpEngineSubmitFn,
-           final ResultExtractor<T> extractor,
-           final Function<T, Q> abortedFn,
-           final Function<Exception, Q> exceptionFn)
-   {
+   private <T> CompletionStage<T> submitRequest(final ResultExtractor<T> extractor, final boolean buffered) {
       final ClientRequestContextImpl requestContext = new ClientRequestContextImpl(this);
-      try(CloseableContext ctx = pushProvidersContext())
-      {
-         ClientResponse aborted = filterRequest(requestContext);
-         if (aborted != null)
-         {
-            // spec requires that aborted response go through filter/interceptor chains.
-            aborted = filterResponse(requestContext, aborted);
-            T result = extractor.extractResult(aborted);
-            return abortedFn.apply(result);
-         }
-      }
-      catch (Exception ex)
-      {
-         return exceptionFn.apply(ex);
-      }
-
-      return asyncHttpEngineSubmitFn.apply(response -> {
-         try(CloseableContext ctx = pushProvidersContext())
-         {
-            return extractor.extractResult(filterResponse(requestContext, response));
-         }
-      });
+      return CompletableFuture.supplyAsync(() -> {
+                 try (CloseableContext ctx = pushProvidersContext()) {
+                    ClientResponse aborted = filterRequest(requestContext);
+                    if (aborted != null) {
+                       // spec requires that aborted response go through filter/interceptor chains.
+                       aborted = filterResponse(requestContext, aborted);
+                       return extractor.extractResult(aborted);
+                    }
+                 }
+                 return null;
+              }
+      , asyncInvocationExecutor())
+              .thenCompose(aborted -> {
+                 if (aborted != null) {
+                    return CompletableFuture.completedFuture(aborted);
+                 }
+                 final ResultExtractor<T> wrapped = (response) -> extractor.extractResult(filterResponse(requestContext, response));
+                 return ((AsyncClientHttpEngine) client.httpEngine()).submit(ClientInvocation.this, buffered, wrapped);
+              });
    }
-
-
 
    private <T> CompletableFuture<T> executorSubmit(ExecutorService executor, final InvocationCallback<T> callback,
          final ResultExtractor<T> extractor)
