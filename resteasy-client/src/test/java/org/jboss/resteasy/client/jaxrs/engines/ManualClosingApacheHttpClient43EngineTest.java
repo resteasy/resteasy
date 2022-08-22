4f31b25882a7470e18e6ef67ea263daddbf7864b
James R. Perkins <jperkins@redhat.com>
2022-08-22 15:44:02 -0700

    Revert "merged to upstream/main"
    This reverts commit 7e0ba4f5453545c8aba507be29dd7dfb559cd4b3.

--- resteasy-client/src/test/java/org/jboss/resteasy/client/jaxrs/engines/ManualClosingApacheHttpClient43EngineTest.java
+++ resteasy-client/src/test/java/org/jboss/resteasy/client/jaxrs/engines/ManualClosingApacheHttpClient43EngineTest.java
@@ -1,14 +1,46 @@
 package org.jboss.resteasy.client.jaxrs.engines;
 
 import static org.junit.Assert.assertEquals;
+import static org.jboss.resteasy.client.jaxrs.engines.ManualClosingApacheHttpClient43Engine.FILE_UPLOAD_IN_MEMORY_THRESHOLD_PROPERTY;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ManualClosingApacheHttpClient43EngineTest {
 
+   private static final Map<String, String> preTestProperties = new HashMap<>();
+
+   @BeforeClass
+   public static void saveCurrentStateOfMemThresholdProperty()
+   {
+      if (System.getProperties().containsKey(FILE_UPLOAD_IN_MEMORY_THRESHOLD_PROPERTY))
+      {
+         String value = System.getProperty(FILE_UPLOAD_IN_MEMORY_THRESHOLD_PROPERTY);
+         preTestProperties.put(FILE_UPLOAD_IN_MEMORY_THRESHOLD_PROPERTY, value);
+      }
+   }
+
+   @AfterClass
+   public static void resetPropertiesToPreTestValues()
+   {
+      if (!preTestProperties.containsKey(FILE_UPLOAD_IN_MEMORY_THRESHOLD_PROPERTY))
+      {
+         System.clearProperty(FILE_UPLOAD_IN_MEMORY_THRESHOLD_PROPERTY);
+      }
+      else
+      {
+         preTestProperties.forEach(System::setProperty);
+      }
+   }
+
    @Before
-   public void unsetMemThresholdProperty() {
+   public void unsetMemThresholdProperty()
+   {
       System.clearProperty(ManualClosingApacheHttpClient43Engine.FILE_UPLOAD_IN_MEMORY_THRESHOLD_PROPERTY);
    }
    @Test
