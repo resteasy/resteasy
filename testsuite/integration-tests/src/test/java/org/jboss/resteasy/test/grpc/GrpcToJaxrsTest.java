b0e16361423b9e3d2627176ed89b19ea4970e4ea
Ron Sigal <rsigal@localhost.localdomain>
2022-10-17 23:02:51 -0400

    [RESTEASY-2656] Changed io.grpc:grpc-netty-shaded version in     GrpcToJaxrsTest, again

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/grpc/GrpcToJaxrsTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/grpc/GrpcToJaxrsTest.java
@@ -20,7 +20,7 @@ import org.jboss.resteasy.utils.TestUtil;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
-import org.jboss.shrinkwrap.api.exporter.ZipExporter;
+//import org.jboss.shrinkwrap.api.exporter.ZipExporter;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -78,7 +78,7 @@ public class GrpcToJaxrsTest
 //         + "Dependencies: com.google.guava services\n"));
          war.merge(ShrinkWrap.createFromZipFile( WebArchive.class, TestUtil.resolveDependency("jaxrs.example:jaxrs.example.grpc:war:0.0.25")));
          TestUtil.addOtherLibrary(war, "org.jboss.resteasy:grpc-bridge-runtime:jar:6.2.2.Final-SNAPSHOT");
-         TestUtil.addOtherLibrary(war, "io.grpc:grpc-netty-shaded:1.39.0");
+         TestUtil.addOtherLibrary(war, "io.grpc:grpc-netty-shaded:1.41.2");
          war.addClass(jaxrs.example.CC1MessageBodyReaderWriter.class);
          war.setManifest(new StringAsset("Manifest-Version: 1.0\n"
                + "Dependencies: io.grpc, com.google.guava services, org.jboss.resteasy.grpc-bridge-runtime export, org.jboss.as.weld export services \n"));
@@ -93,7 +93,7 @@ public class GrpcToJaxrsTest
                new RuntimePermission("shutdownHooks"),
                new SocketPermission("*", "accept, listen,resolve")
          ), "permissions.xml");
-         archive.as(ZipExporter.class).exportTo(new File("/tmp/GrpcToJaxrs.jar"), true);
+//         archive.as(ZipExporter.class).exportTo(new File("/tmp/GrpcToJaxrs.jar"), true);
          return archive;
    }
 
