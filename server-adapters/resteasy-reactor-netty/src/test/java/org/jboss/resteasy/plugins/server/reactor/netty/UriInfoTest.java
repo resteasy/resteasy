147846e9ca569fbe403adb1600a8c122c69de6ac
Samuel Cox <crankydillo@gmail.com>
2022-12-01 21:20:43 -0600

    Use the IP address if the Host header is invalid.

--- server-adapters/resteasy-reactor-netty/src/test/java/org/jboss/resteasy/plugins/server/reactor/netty/UriInfoTest.java
+++ server-adapters/resteasy-reactor-netty/src/test/java/org/jboss/resteasy/plugins/server/reactor/netty/UriInfoTest.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -41,34 +43,87 @@ public class UriInfoTest {
     public void testUriInfoUsingFullUriWithHostname() throws Exception
     {
         final String uri = TestPortProvider.generateURL("/uriinfo");
-        Assert.assertEquals(uri, test(uri));
+        assertThat(uri, equalTo(uriInfoRequestUri(uri)));
     }
 
     @Test
     public void testUriInfoUsingFullUriWithIp() throws Exception
     {
         final String uri = TestPortProvider.generateURL("/uriinfo").replace("localhost", "127.0.0.1");
-        Assert.assertEquals(uri, test(uri));
+        assertThat(uri, equalTo(uriInfoRequestUri(uri)));
     }
 
     @Test
     public void testUriInfoUsingPartialUri() throws Exception
     {
         final String uri = "/uriinfo";
-
-        final String response = test(uri);
-
+        final String response = uriInfoRequestUri(uri);
         final String absoluteUri = TestPortProvider.generateURL(uri);
         assertThat(response, equalTo(absoluteUri));
     }
 
-    private String test(final String uri) throws UnknownHostException, IOException {
-        final String hostHeaderValue = TestPortProvider.getHost() + ":" + TestPortProvider.getPort();
+    @Test
+    public void testNoHostHeader() throws Exception
+    {
+        testProblematicHostHeader(Collections.emptyMap());
+    }
+
+    @Test
+    public void testEmptyStringHostHeader() throws Exception
+    {
+        final Map<String, String> additionalHeaders = new HashMap<>();
+        additionalHeaders.put("Host", "");
+        testProblematicHostHeader(additionalHeaders);
+    }
+
+    @Test
+    public void testOnlySpacesHostHeader() throws Exception
+    {
+        final Map<String, String> additionalHeaders = new HashMap<>();
+        additionalHeaders.put("Host", "                  ");
+        testProblematicHostHeader(additionalHeaders);
+    }
+
+    /**
+     * Meant to test cases related to a problematic request Host header (no Host header, empty Host header, etc).
+     * @param hostHeader
+     */
+    private void testProblematicHostHeader(final Map<String, String> hostHeader) throws IOException {
+        // Optional<Tuple2<String, String>> is the best 'design' data structure, but not going there for this test..
+        final String uri = "/uriinfo";
+        final String response = uriInfoRequestUri(uri, hostHeader);
+        final String expectedUri = "http://127.0.0.1:" + TestPortProvider.getPort() + uri;
+        assertThat(response, equalTo(expectedUri));
+    }
+
+    /**
+     * @see #uriInfoRequestUri(String, Map<String, String>) always sends the Host header in the request.
+     */
+    private String uriInfoRequestUri(final String uri) throws IOException {
+        final Map<String, String> additionalHeaders = new HashMap<>();
+        additionalHeaders.put("Host", TestPortProvider.getHost() + ":" + TestPortProvider.getPort());
+        return uriInfoRequestUri(uri, additionalHeaders);
+
+    }
+
+    /**
+     *
+     */
+    /**
+     *
+     * Returns what the server saw in {@link UriInfo#getRequestUri()}.
+     *
+     * @param uri
+     * @param additionalHeaders added to the request
+     * @return
+     * @throws IOException
+     */
+    private String uriInfoRequestUri(final String uri, Map<String, String> additionalHeaders) throws IOException {
         try (Socket client = new Socket(TestPortProvider.getHost(), TestPortProvider.getPort())) {
             try (PrintWriter out = new PrintWriter(client.getOutputStream(), true)) {
                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                out.printf("GET %s HTTP/1.1\r\n", uri);
-               out.printf("Host: %s\r\n", hostHeaderValue);
+               additionalHeaders.forEach((n, v) -> out.printf("%s: %s\r\n", n, v));
                out.print("Connection: close\r\n");
                out.print("\r\n");
                out.flush();
@@ -105,6 +160,4 @@ public class UriInfoTest {
        }
 
     }
-
-
 }
