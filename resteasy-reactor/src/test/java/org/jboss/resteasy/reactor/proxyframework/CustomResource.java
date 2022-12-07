721094f8ec1d6ace600737070015a299ec799223
duuude <architect@soft4rchitecture.org>
2022-12-07 21:10:08 +0100

    RESTEASY-3259 adds Proxy Framework test with Mono<T> in remote service interface

---
+++ resteasy-reactor/src/test/java/org/jboss/resteasy/reactor/proxyframework/CustomResource.java
@@ -0,0 +1,18 @@
+package org.jboss.resteasy.reactor.proxyframework;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import reactor.core.publisher.Mono;
+
+
+@Path("api/v1")
+public  class CustomResource {
+
+    public static final String THE_CUSTOM_RESOURCE = "the custom resource";
+
+    @GET
+    @Path("resource/custom")
+    public Mono<String> getCustomResource() {
+        return Mono.just(THE_CUSTOM_RESOURCE);
+    }
+}
