fa08ed92524b0b194cb852ca9899c2d954426dd4
Dimitris Kafetzis <dkafetzi@redhat.com>
2023-04-03 16:03:59 +0300

    [RESTEASY-3300] Do not append single '&' with empty query parameters

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/RequestNamedQueryParameterTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/RequestNamedQueryParameterTest.java
@@ -1,29 +1,43 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ *
+ * Copyright 2023 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.resteasy.test.client;
 
+import java.io.IOException;
 import java.util.List;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.ProcessingException;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.ClientRequestContext;
+import jakarta.ws.rs.client.ClientRequestFilter;
 import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.resteasy.annotations.jaxrs.QueryParam;
-import org.jboss.resteasy.client.jaxrs.ClientHttpEngine;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
-import org.jboss.resteasy.client.jaxrs.internal.AbortedResponse;
-import org.jboss.resteasy.client.jaxrs.internal.ClientInvocation;
-import org.jboss.resteasy.core.ServerResponse;
 import org.jboss.resteasy.utils.TestUtil;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -59,6 +73,32 @@ public class RequestNamedQueryParameterTest extends ClientTestBase {
         return war;
     }
 
+    enum testType {
+        NOPARAMETERS,
+        MIXEDPARAMETERS,
+        FULLPARAMETERS
+    }
+
+    public class AssertFilter implements ClientRequestFilter {
+        final testType setting;
+
+        @Override
+        public void filter(ClientRequestContext requestContext) throws IOException {
+            if (setting == testType.NOPARAMETERS) {
+                Assert.assertEquals("", requestContext.getUri().getQuery());
+            } else if (setting == testType.MIXEDPARAMETERS) {
+                Assert.assertEquals("listA=stuff1&listA=stuff2&listC=stuff1&listC=stuff2", requestContext.getUri().getQuery());
+            } else {
+                Assert.assertEquals("listA=stuff1&listA=stuff2&listB=stuff1&listB=stuff2&listC=stuff1&listC=stuff2",
+                        requestContext.getUri().getQuery());
+            }
+        }
+
+        AssertFilter(final testType setting) {
+            this.setting = setting;
+        }
+    }
+
     /**
      * @tpTestDetails Check if empty named query parameters of a request are properly handled.
      * @tpPassCrit The query for this request invocation should be empty.
@@ -69,30 +109,13 @@ public class RequestNamedQueryParameterTest extends ClientTestBase {
     @Test
     public void testWithEmptyNamedQueryParameters() {
         ResteasyClientBuilder builder = (ResteasyClientBuilder) ClientBuilder.newBuilder();
-        ResteasyClient client = builder.httpEngine(new ClientHttpEngine() {
-            @Override
-            public Response invoke(Invocation request) {
-                Assert.assertEquals("", ((ClientInvocation) request).getUri().getQuery());
-                return new AbortedResponse(null, new ServerResponse());
-            }
-
-            @Override
-            public SSLContext getSslContext() {
-                return null;
-            }
-
-            @Override
-            public HostnameVerifier getHostnameVerifier() {
-                return null;
-            }
-
-            @Override
-            public void close() {
-            }
-        }).build();
+        ResteasyClient client = builder.build();
+        client.register(new AssertFilter(testType.NOPARAMETERS));
 
         try {
-            client.target("http://a.place.org").proxy(SomeResource.class).methodWithLists(List.of(), List.of(), List.of());
+            client.target("http://localhost").proxy(SomeResource.class).methodWithLists(List.of(), List.of(), List.of());
+        } catch (ProcessingException e) {
+            //The address for the request does not exist so this is expected to throw a Processing Exception.
         } finally {
             client.close();
         }
@@ -110,32 +133,14 @@ public class RequestNamedQueryParameterTest extends ClientTestBase {
     @Test
     public void testWithMixedNamedQueryParameters() {
         ResteasyClientBuilder builder = (ResteasyClientBuilder) ClientBuilder.newBuilder();
-        ResteasyClient client = builder.httpEngine(new ClientHttpEngine() {
-            @Override
-            public Response invoke(Invocation request) {
-                Assert.assertEquals("listA=stuff1&listA=stuff2&listC=stuff1&listC=stuff2",
-                        ((ClientInvocation) request).getUri().getQuery());
-                return new AbortedResponse(null, new ServerResponse());
-            }
-
-            @Override
-            public SSLContext getSslContext() {
-                return null;
-            }
-
-            @Override
-            public HostnameVerifier getHostnameVerifier() {
-                return null;
-            }
-
-            @Override
-            public void close() {
-            }
-        }).build();
+        ResteasyClient client = builder.build();
+        client.register(new AssertFilter(testType.MIXEDPARAMETERS));
 
         try {
-            client.target("http://a.place.org").proxy(SomeResource.class).methodWithLists(List.of("stuff1", "stuff2"),
+            client.target("http://localhost").proxy(SomeResource.class).methodWithLists(List.of("stuff1", "stuff2"),
                     List.of(), List.of("stuff1", "stuff2"));
+        } catch (ProcessingException e) {
+            //The address for the request does not exist so this is expected to throw a Processing Exception.
         } finally {
             client.close();
         }
@@ -151,32 +156,14 @@ public class RequestNamedQueryParameterTest extends ClientTestBase {
     @Test
     public void testWithFullNamedQueryParameters() {
         ResteasyClientBuilder builder = (ResteasyClientBuilder) ClientBuilder.newBuilder();
-        ResteasyClient client = builder.httpEngine(new ClientHttpEngine() {
-            @Override
-            public Response invoke(Invocation request) {
-                Assert.assertEquals("listA=stuff1&listA=stuff2&listB=stuff1&listB=stuff2&listC=stuff1&listC=stuff2",
-                        ((ClientInvocation) request).getUri().getQuery());
-                return new AbortedResponse(null, new ServerResponse());
-            }
-
-            @Override
-            public SSLContext getSslContext() {
-                return null;
-            }
-
-            @Override
-            public HostnameVerifier getHostnameVerifier() {
-                return null;
-            }
-
-            @Override
-            public void close() {
-            }
-        }).build();
+        ResteasyClient client = builder.build();
+        client.register(new AssertFilter(testType.FULLPARAMETERS));
 
         try {
-            client.target("http://a.place.org").proxy(SomeResource.class).methodWithLists(List.of("stuff1", "stuff2"),
+            client.target("http://localhost").proxy(SomeResource.class).methodWithLists(List.of("stuff1", "stuff2"),
                     List.of("stuff1", "stuff2"), List.of("stuff1", "stuff2"));
+        } catch (ProcessingException e) {
+            //The address for the request does not exist so this is expected to throw a Processing Exception.
         } finally {
             client.close();
         }
