9ecc1cfb0514ed0ab604b9ed65738b5a41263e6d
James R. Perkins <jperkins@redhat.com>
2023-04-20 07:13:14 -0700

    [RESTEASY-3300] Add back the null check. Minor updates to the test.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/RequestNamedQueryParameterTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/RequestNamedQueryParameterTest.java
@@ -19,7 +19,7 @@
 
 package org.jboss.resteasy.test.client;
 
-import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 import jakarta.ws.rs.Consumes;
@@ -35,12 +35,11 @@ import jakarta.ws.rs.core.Response;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.resteasy.annotations.jaxrs.QueryParam;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.utils.TestUtil;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -53,51 +52,25 @@ import org.junit.runner.RunWith;
  */
 @RunWith(Arquillian.class)
 @RunAsClient
-public class RequestNamedQueryParameterTest extends ClientTestBase {
+public class RequestNamedQueryParameterTest {
 
     @Path("someResource")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public interface SomeResource {
+    public interface TestResource {
 
         @GET
-        void methodWithLists(@QueryParam("listA") List<String> listA, @QueryParam("listB") List<String> listB,
+        Response query(@QueryParam("listA") List<String> listA, @QueryParam("listB") List<String> listB,
                 @QueryParam("listC") List<String> listC);
     }
 
+    @ArquillianResource
+    private URI uri;
+
     @Deployment
     public static Archive<?> deploy() {
-        WebArchive war = TestUtil.prepareArchive(ClientInvocationBuilderTest.class.getSimpleName());
-        war.addClass(SomeResource.class);
-        war.addClass(ClientTestBase.class);
-        return war;
-    }
-
-    enum testType {
-        NOPARAMETERS,
-        MIXEDPARAMETERS,
-        FULLPARAMETERS
-    }
-
-    public class AssertFilter implements ClientRequestFilter {
-        final testType setting;
-
-        @Override
-        public void filter(ClientRequestContext requestContext) throws IOException {
-            if (setting == testType.NOPARAMETERS) {
-                Assert.assertEquals("", requestContext.getUri().getQuery());
-            } else if (setting == testType.MIXEDPARAMETERS) {
-                Assert.assertEquals("listA=stuff1&listA=stuff2&listC=stuff1&listC=stuff2", requestContext.getUri().getQuery());
-            } else {
-                Assert.assertEquals("listA=stuff1&listA=stuff2&listB=stuff1&listB=stuff2&listC=stuff1&listC=stuff2",
-                        requestContext.getUri().getQuery());
-            }
-            requestContext.abortWith(Response.accepted().build());
-        }
-
-        AssertFilter(final testType setting) {
-            this.setting = setting;
-        }
+        return TestUtil.prepareArchive(ClientInvocationBuilderTest.class.getSimpleName())
+                .addClasses(TestResource.class);
     }
 
     /**
@@ -109,16 +82,13 @@ public class RequestNamedQueryParameterTest extends ClientTestBase {
      */
     @Test
     public void testWithEmptyNamedQueryParameters() {
-        ResteasyClientBuilder builder = (ResteasyClientBuilder) ClientBuilder.newBuilder();
-        ResteasyClient client = builder.build();
-
-        try {
-            client.register(new AssertFilter(testType.NOPARAMETERS));
-            client.target("http://localhost").proxy(SomeResource.class).methodWithLists(List.of(), List.of(), List.of());
-        } finally {
-            client.close();
+        try (ResteasyClient client = createClient();
+                Response response = client.target(uri)
+                        .proxy(TestResource.class)
+                        .query(List.of(), List.of(), List.of())) {
+            Assert.assertEquals(Response.Status.OK, response.getStatusInfo());
+            Assert.assertEquals("", response.readEntity(String.class));
         }
-
     }
 
     /**
@@ -131,16 +101,14 @@ public class RequestNamedQueryParameterTest extends ClientTestBase {
      */
     @Test
     public void testWithMixedNamedQueryParameters() {
-        ResteasyClientBuilder builder = (ResteasyClientBuilder) ClientBuilder.newBuilder();
-        ResteasyClient client = builder.build();
-        try {
-            client.register(new AssertFilter(testType.MIXEDPARAMETERS));
-            client.target("http://localhost").proxy(SomeResource.class).methodWithLists(List.of("stuff1", "stuff2"), List.of(),
-                    List.of("stuff1", "stuff2"));
-        } finally {
-            client.close();
+        try (ResteasyClient client = createClient();
+                Response response = client.target(uri)
+                        .proxy(TestResource.class)
+                        .query(List.of("stuff1", "stuff2"), List.of(),
+                                List.of("stuff1", "stuff2"))) {
+            Assert.assertEquals(Response.Status.OK, response.getStatusInfo());
+            Assert.assertEquals("listA=stuff1&listA=stuff2&listC=stuff1&listC=stuff2", response.readEntity(String.class));
         }
-
     }
 
     /**
@@ -151,15 +119,28 @@ public class RequestNamedQueryParameterTest extends ClientTestBase {
      */
     @Test
     public void testWithFullNamedQueryParameters() {
-        ResteasyClientBuilder builder = (ResteasyClientBuilder) ClientBuilder.newBuilder();
-        ResteasyClient client = builder.build();
+        try (ResteasyClient client = createClient();
+                Response response = client.target(uri)
+                        .proxy(TestResource.class)
+                        .query(List.of("stuff1", "stuff2"),
+                                List.of("stuff1", "stuff2"), List.of("stuff1", "stuff2"))) {
+            Assert.assertEquals(Response.Status.OK, response.getStatusInfo());
+            Assert.assertEquals("listA=stuff1&listA=stuff2&listB=stuff1&listB=stuff2&listC=stuff1&listC=stuff2",
+                    response.readEntity(String.class));
+        }
+    }
 
-        try {
-            client.register(new AssertFilter(testType.FULLPARAMETERS));
-            client.target("http://localhost").proxy(SomeResource.class).methodWithLists(List.of("stuff1", "stuff2"),
-                    List.of("stuff1", "stuff2"), List.of("stuff1", "stuff2"));
-        } finally {
-            client.close();
+    @SuppressWarnings("resource")
+    private static ResteasyClient createClient() {
+        return (ResteasyClient) ClientBuilder.newClient()
+                .register(new AssertFilter());
+    }
+
+    private static class AssertFilter implements ClientRequestFilter {
+
+        @Override
+        public void filter(ClientRequestContext requestContext) {
+            requestContext.abortWith(Response.ok(requestContext.getUri().getQuery()).build());
         }
     }
 
