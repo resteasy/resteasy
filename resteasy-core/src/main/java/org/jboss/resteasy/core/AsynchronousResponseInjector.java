a2b4bbd867dcbfcf5c17e42363ecba16981b13b7
Alexey Tonkovid <alexey.tonkovid@gmail.com>
2023-12-08 20:17:07 +0200

    [RESTEASY-3385] Async request filters using `suspend`/`resume` on `SuspendableContainerRequestContext` cause 400 BAD REQUEST

--- resteasy-core/src/main/java/org/jboss/resteasy/core/AsynchronousResponseInjector.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/core/AsynchronousResponseInjector.java
@@ -27,7 +27,9 @@ public class AsynchronousResponseInjector implements ValueInjector {
     @Override
     public Object inject(HttpRequest request, HttpResponse response, boolean unwrapAsync) {
         ResteasyAsynchronousResponse asynchronousResponse = null;
-        if (timeout == -1) {
+        if (request.getAsyncContext().isSuspended()) {
+            asynchronousResponse = request.getAsyncContext().getAsyncResponse();
+        } else if (timeout == -1) {
             asynchronousResponse = request.getAsyncContext().suspend();
         } else {
             asynchronousResponse = request.getAsyncContext().suspend(timeout, unit);
