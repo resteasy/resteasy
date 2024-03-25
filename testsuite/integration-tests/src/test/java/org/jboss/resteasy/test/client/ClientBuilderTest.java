1775392286db4328041f5bfcaf0c76d4db17c95f
Tommaso Borgato <tborgato@redhat.com>
2024-03-25 18:18:07 +0100

    Test BootableJar using Channel Manifest

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/ClientBuilderTest.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/client/ClientBuilderTest.java
@@ -20,6 +20,7 @@ import org.jboss.resteasy.utils.TestUtil;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -29,6 +30,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * @tpSince RESTEasy 3.0.17
  */
 @ExtendWith(ArquillianExtension.class)
+@Tag("NotForBootableJar")
 public class ClientBuilderTest {
 
     @SuppressWarnings(value = "unchecked")
