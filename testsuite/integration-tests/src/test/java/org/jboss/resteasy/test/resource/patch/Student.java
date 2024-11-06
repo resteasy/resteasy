0aafb44ceb3a14ef831dbfa35af398690241c328
Jan Kašík <jkasik@redhat.com>
2024-11-06 10:49:49 +0100

    Extended JSON merge patch coverage

--- testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/resource/patch/Student.java
+++ testsuite/integration-tests/src/test/java/org/jboss/resteasy/test/resource/patch/Student.java
@@ -1,4 +1,25 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ *
+ * Copyright 2024 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.resteasy.test.resource.patch;
+
+import java.util.Objects;
 
 public class Student {
     private Long id;
@@ -54,5 +75,21 @@ public class Student {
     public Student setGender(String gender) {
         this.gender = gender;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Student student = (Student) o;
+        return Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName)
+                && Objects.equals(school, student.school) && Objects.equals(gender, student.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, school, gender);
     }
 }
