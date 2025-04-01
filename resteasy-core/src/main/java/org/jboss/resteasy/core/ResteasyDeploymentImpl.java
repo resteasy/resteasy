66b36e49edf7d0f3432994a3595a3bddc7dbd097
James R. Perkins <jperkins@redhat.com>
2025-04-01 15:03:27 -0700

    [RESTEASY-3593] Pass the ResteasyConfiguration to the Option.getValue() when the security manager is enabled.
    https://issues.redhat.com/browse/RESTEASY-3593
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- resteasy-core/src/main/java/org/jboss/resteasy/core/ResteasyDeploymentImpl.java
+++ resteasy-core/src/main/java/org/jboss/resteasy/core/ResteasyDeploymentImpl.java
@@ -1057,6 +1057,6 @@ public class ResteasyDeploymentImpl implements ResteasyDeployment {
         if (System.getSecurityManager() == null) {
             return option.getValue(config);
         }
-        return AccessController.doPrivileged((PrivilegedAction<T>) option::getValue);
+        return AccessController.doPrivileged((PrivilegedAction<T>) () -> option.getValue(config));
     }
 }
