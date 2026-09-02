41aec7dddbe4d98f841acd443353e54efc403770
James R. Perkins <jperkins@ibm.com>
2026-09-02 16:06:56 -0700

    [RESTEASY-3794] Lookup the CDI extension via the BeanManager.getExtension() instead of using a reference lookup.
    Also, don't register a ResteasyBeanContainer as a bean. Just expose it via the ResteasyCdiExtension.
    
    https://redhat.atlassian.net/browse/RESTEASY-3794
    Signed-off-by: James R. Perkins <jperkins@ibm.com>

--- resteasy-cdi/src/main/java/org/jboss/resteasy/cdi/ResteasyCdiExtension.java
+++ resteasy-cdi/src/main/java/org/jboss/resteasy/cdi/ResteasyCdiExtension.java
@@ -89,6 +89,7 @@ public class ResteasyCdiExtension implements Extension {
     private boolean generateClientBean = true;
     private boolean addContextProducers = true;
     private boolean noApplicationFound = true;
+    private volatile ResteasyBeanContainer resteasyBeanContainer;
 
     public ResteasyCdiExtension() {
         enhancedCdiSupportEnabled = CdiOptions.ENHANCED_CDI_SUPPORT.getValue();
@@ -134,10 +135,7 @@ public class ResteasyCdiExtension implements Extension {
         }
         final Set<Class<?>> resources = Set.copyOf(beanContainer);
         beanContainer.clear();
-        final ResteasyBeanContainer instance = resources::contains;
-        event.addBean().addType(ResteasyBeanContainer.class)
-                .scope(ApplicationScoped.class)
-                .createWith(ctx -> instance);
+        resteasyBeanContainer = resources::contains;
     }
 
     /**
@@ -335,6 +333,10 @@ public class ResteasyCdiExtension implements Extension {
 
     public Map<Class<?>, Type> getSessionBeanInterface() {
         return sessionBeanInterface;
+    }
+
+    ResteasyBeanContainer beanContainer() {
+        return resteasyBeanContainer;
     }
 
     private boolean isSessionBean(AnnotatedType<?> annotatedType) {
